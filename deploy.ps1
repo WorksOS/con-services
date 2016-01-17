644f21c13f6af3ead07f6105fd10dcf789dd98e5
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2016-01-18 12:25:58 +1300

    Unsafe PS

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Unrestricted
 Import-Module WebAdministration
 New-Item "IIS:\Sites\Default Web Site\Landfill" -physicalPath "C:\Landfill\Landfill" -type Application 2>>c:\error.txt
 Start-Process "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" install 2>>c:\error.txt
