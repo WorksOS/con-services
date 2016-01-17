b1b8541b0758697f5820d652ef0be6b64bfada7c
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2016-01-18 08:52:57 +1300

    Updated deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
 Import-Module WebAdministration
-New-Item "IIS:\Sites\Default Web Site\Landfill" -physicalPath "C:\Landfill\Landfill" -type Application
-& "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" install
-& "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" start
+New-Item "IIS:\Sites\Default Web Site\Landfill" -physicalPath "C:\Landfill\Landfill" -type Application 2>>c:\error.txt
+Start-Process "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" install 2>>c:\error.txt
+Start-Process "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" start  2>>c:\error.txt
