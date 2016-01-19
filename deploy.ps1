ab767552bfba2737ff80c95f066db9fa146a7ff6
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2016-01-20 09:08:54 +1300

    Updated for the deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,5 @@
 Set-ExecutionPolicy Unrestricted 2>>c:\error.txt
 Import-Module WebAdministration
 New-Item "IIS:\Sites\Default Web Site\Landfill" -physicalPath "C:\Landfill\Landfill" -type Application 2>>c:\error.txt
-Start-Process "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" install 2>>c:\error.txt
-Start-Process "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" start  2>>c:\error.txt
-Invoke-Expression (\"C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe install\")
+& "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" install 2>>c:\error.txt
+& "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" start 2>>c:\error.txt
