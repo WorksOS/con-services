6b055ee07ec39a88088ab676a426e803a8e0e158
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2016-01-27 13:26:42 +1300

    #11375 Updated deploy script

--- deploy_services.ps1
+++ deploy_services.ps1
@@ -1,3 +1,5 @@
 Set-ExecutionPolicy Unrestricted 2>>c:\error.txt
 & "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" install 2>>c:\error.txt
 & "C:\Landfill\LandFillDataSync\LandFillServiceDataSynchronizer.exe" start 2>>c:\error.txt
+& "C:\Landfill\LandFillMDM\VSS.VisionLink.Landfill.MDMService.exe" install 2>>c:\error.txt
+& "C:\Landfill\LandFillMDM\VSS.VisionLink.Landfill.MDMService.exe" start 2>>c:\error.txt
