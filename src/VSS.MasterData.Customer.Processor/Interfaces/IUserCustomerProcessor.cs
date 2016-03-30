025af1f7268c3449d1d0275f78bb8f812418fd25
Anatoli Kozoub <anatoli_kozoub@trimble.com>
2016-03-30 16:48:48 +1300

    US #16565 Java Kafka support for UserCustomer

---
+++ src/VSS.MasterData.Customer.Processor/Interfaces/IUserCustomerProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSS.Customer.Processor.Interfaces
+{
+  interface IUserCustomerProcessor
+  {
+    void Process();
+    void Stop();
+  }
+}
