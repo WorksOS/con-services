b7de3e2645e0616f305e9ada3ae803707694fda1
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2016-07-04 10:29:18 +1200

    #23480: Fix bug in updating machine name

--- src/Common/Context/LandfillContext.cs
+++ src/Common/Context/LandfillContext.cs
@@ -827,7 +827,7 @@ namespace LandfillService.Common.Context
       {
         //Match on AssetID and IsJohnDoe only as MachineName can change.
         var query = @"SELECT ID, MachineName FROM Machine
-                      WHERE AssetID = @assetId AND IsjohnDoe = @isJohnDoe";
+                      WHERE AssetID = @assetId AND IsJohnDoe = @isJohnDoe";
 
         long existingId = 0;
         bool updateName = false;
@@ -841,8 +841,8 @@ namespace LandfillService.Common.Context
         }
         if (updateName)
         {
-          var command = @"UPDATE Machine SET MachineName = @machineName";
-          MySqlHelper.ExecuteNonQuery(sqlConn, command, sqlParams);
+          var command = @"UPDATE Machine SET MachineName = @machineName WHERE ID = @machineId";
+          MySqlHelper.ExecuteNonQuery(sqlConn, command, new MySqlParameter("@machineId", existingId));
         }
         return existingId;
       }
