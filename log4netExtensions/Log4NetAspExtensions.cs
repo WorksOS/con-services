ee0193bcffcb4891bfc7b9eb9abc6171a45b7dae
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2016-09-12 12:41:51 +1200

    Added log4net adapter

---
+++ log4netExtensions/Log4NetAspExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using log4net;
+using log4net.Config;
+using Microsoft.Extensions.Logging;
+using log4net.Repository;
+using System;
+
+namespace log4netExtensions
+{
+  public static class Log4NetAspExtensions
+  {
+    public static void ConfigureLog4Net(this IHostingEnvironment appEnv, string configFileRelativePath, string repoName)
+    {
+      ConfigureLog4Net(appEnv.ContentRootPath, configFileRelativePath, repoName);
+    }
+
+    public static void ConfigureLog4Net(string currentDir, string configFileRelativePath, string repoName)
+    {
+      var fullName = Path.Combine(currentDir, configFileRelativePath);
+      //Console.WriteLine("ConfigureLog4Net: currentDir={0}, configFileRelativePath={1}, fullName={2}", currentDir, configFileRelativePath, fullName);
+      FileInfo fi = new FileInfo(fullName);
+      ILoggerRepository loggerRepo = null;
+      foreach (ILoggerRepository repo in LogManager.GetAllRepositories())
+      {
+        if (repo.Name == repoName)
+        {
+          loggerRepo = repo;
+        }
+      }
+      if (loggerRepo == null)
+      {
+        loggerRepo = LogManager.CreateRepository(repoName);
+      }
+      GlobalContext.Properties["appRoot"] = currentDir;
+      XmlConfigurator.Configure(loggerRepo, fi);
+    }
+
+    public static void AddLog4Net(this ILoggerFactory loggerFactory, string repoName)
+    {
+      loggerFactory.AddProvider(new Log4NetProvider(repoName));
+    }
+  }
+}
---
+++ log4netExtensions/Log4NetProvider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using log4net.Repository;
+
+namespace log4netExtensions
+{
+  public class Log4NetProvider : ILoggerProvider
+  {
+    private IDictionary<string, ILogger> _loggers = new Dictionary<string, ILogger>();
+    private string repoName;
+
+    public Log4NetProvider(string repoName)
+    {
+      this.repoName = repoName;
+    }
+
+    public ILogger CreateLogger(string name)
+    {
+      if (!_loggers.ContainsKey(name))
+      {
+        lock (_loggers)
+        {
+          // Have to check again since another thread may have gotten the lock first
+          if (!_loggers.ContainsKey(name))
+          {
+            _loggers[name] = new Log4NetAdapter(repoName, name);
+          }
+        }
+      }
+      return _loggers[name];
+    }
+
+    public void Dispose()
+    {
+      _loggers.Clear();
+      _loggers = null;
+    }
+  }
+}
