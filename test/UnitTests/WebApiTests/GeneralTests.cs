6662067f15034fa69bf666bf2ce2abf06e0bc541
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2016-12-20 16:25:46 +1300

    #38073: Add unit test project and ATs VS2015 solution

---
+++ test/UnitTests/WebApiTests/GeneralTests.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSS.Raptor.Service.WebApiModels.Interfaces;
+using VSS.Raptor.Service.WebApiModels.ResultHandling;
+
+namespace WebApiTests
+{
+  [TestClass]
+  public class GeneralTests
+  {
+    class TestContainer : RequestExecutorContainer
+    {
+      protected override ContractExecutionResult ProcessEx<T>(T item)
+      {
+        ContractExecutionStates.ClearDynamic();
+        return new ContractExecutionResult(1, "test result");
+      }
+
+      protected override void ProcessErrorCodes()
+      {
+        ContractExecutionStates.DynamicAddwithOffset("OnSubmissionResult. ConnectionFailure.",
+                -10);
+        ContractExecutionStates.DynamicAddwithOffset(
+                "The TAG file was found to be corrupted on its pre-processing scan.",
+                15);
+      }
+    }
+
+    [TestMethod]
+    public void GenerateErrorlistTest()
+    {
+      TestContainer container = new TestContainer();
+      Assert.AreEqual(10, container.GenerateErrorlist().Count);
+      container.Process(new ContractExecutionResult());//any object will do here
+      Assert.AreEqual(8, container.GenerateErrorlist().Count);
+    }
+
+  }
+}
