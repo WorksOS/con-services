34e33dad647b30a2416ba4650d577337f39a35b9
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-01-09 11:31:04 +1300

    More MDM stuff

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 RMDIR /S /Q artifacts
+dotnet restore
 dotnet publish ./src/MasterDataConsumer -o artifacts/MasterDataConsumer -f netcoreapp1.1 
+dotnet publish ./src/ProjectWebApi -o artifacts/ProjectWebApi -f netcoreapp1.1 
 
---
+++ src/MasterDataConsumer/runme.sh
@@ -0,0 +1,9 @@
+echo ">>> Making sure MySQL is up"
+#/bin/bash wait-for-it.sh db:3306 -t 0
+echo "<<< Done checking on MySQL"
+
+echo ">>> Making sure Kafka is up"
+#/bin/bash wait-for-it.sh kafka:9092 -t 0
+echo "<<< Done checking on Kafka"
+
+dotnet DatafeedService.dll
