72ba4eedac7476a773a9ffba0e1696bc20fa0470
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-01-09 15:58:39 +1300

    #38073: Fixes for Docker for Windows containers

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ if exist Artifacts rd /s /q Artifacts
 dotnet publish ./src/WebApi -o ./Artifacts/WebApi -f net451 -c Docker
 dotnet build ./test/UnitTests/WebApiTests
 copy src\WebApi\appsettings.json Artifacts\WebApi\
+copy src\WebApi\Dockerfile Artifacts\WebApi\
 
 mkdir Artifacts\Logs
 rem cd .\test\ComponentTests\scripts
