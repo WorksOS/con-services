de662d949b76311d816261dff99470b1e6580e64
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-01-13 11:49:35 +1300

    #38073: Update for new AWS machine

---
+++ RunDockerLocal.ps1
@@ -0,0 +1,4 @@
+$env:DOCKER_TLS_VERIFY=1
+$env:DOCKER_HOST="tcp://10.97.96.197:2376"
+$env:DOCKER_CERT_PATH="$env:USERPROFILE/.docker/machine/machines/Merino-VL_Productivity-WinServer2016-Docker"
+$env:DOCKER_MACHINE_NAME="Merino-VL_Productivity-WinServer2016-Docker"
