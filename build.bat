76930aaca5760ea48d38eaa6468fadbb21df9ac2
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-01-17 16:39:43 +1300

    #38073: Try dotnet restore in build batch file

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 RMDIR /S /Q Artifacts
 if exist Artifacts rd /s /q Artifacts
 
+dotnet restore
 dotnet publish ./src/WebApi -o ./Artifacts/WebApi -f net451 -c Docker
 dotnet build ./test/UnitTests/WebApiTests
 copy src\WebApi\appsettings.json Artifacts\WebApi\
