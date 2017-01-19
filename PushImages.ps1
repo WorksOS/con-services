fea8fbba38e853f81aa8feaa98f1eb6de65d8264
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-01-20 12:32:32 +1300

    #38073: Revert to vss-grant profile

--- PushImages.ps1
+++ PushImages.ps1
@@ -1,7 +1,7 @@
 param (
   [Parameter(Mandatory=$true)][string]$fullVersion
 )
-$command = '$(aws ecr get-login --region us-west-2 --profile vss-merino)'
+$command = '$(aws ecr get-login --region us-west-2 --profile vss-grant)'
 Invoke-Expression $command
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi
