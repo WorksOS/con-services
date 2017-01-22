eca706b6f78517db5bf2d139c2196974e89fb34b
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-01-23 09:32:04 +1300

    Home dump

--- PushImages.ps1
+++ PushImages.ps1
@@ -3,6 +3,10 @@ param (
 )
 & echo Dumping Var
 & echo $env:AWS_CONFIG_FILE
+& echo Dumping home
+& echo $env:HOME
+& echo Dumping homepath
+& echo $env:HOMEPATH
 $command = '$(aws ecr get-login --region us-west-2 --profile vss-grant)'
 Invoke-Expression $command
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
