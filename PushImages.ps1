cd9243dfb243cab0401051cd9a44a2fb4cf0f073
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-01-23 09:52:43 +1300

    Fixed bug related to wincred

--- PushImages.ps1
+++ PushImages.ps1
@@ -1,14 +1,11 @@
 param (
   [Parameter(Mandatory=$true)][string]$fullVersion
 )
-& echo Dumping Var
-& echo $env:AWS_CONFIG_FILE
-& echo Dumping home
-& echo $env:HOME
-& echo Dumping homepath
-& echo $env:HOMEPATH
 $command = '$(aws ecr get-login --region us-west-2 --profile vss-grant)'
-Invoke-Expression $command
+#bug in docker login
+$fixedCommand = $command.Replace("https://","")
+Invoke-Expression $fixedCommand
+& docker login
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi
 & docker rmi -f 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
