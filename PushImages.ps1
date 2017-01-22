511994d927dc619ee7008be584b0a65f77557b9d
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-01-23 09:59:04 +1300

    Proper invocation

--- PushImages.ps1
+++ PushImages.ps1
@@ -1,11 +1,9 @@
 param (
   [Parameter(Mandatory=$true)][string]$fullVersion
 )
-$command = '$(aws ecr get-login --region us-west-2 --profile vss-grant)'
-#bug in docker login
+$command = & aws ecr get-login --region us-west-2 --profile vss-grant
 $fixedCommand = $command.Replace("https://","")
 Invoke-Expression $fixedCommand
-& docker login
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi
 & docker rmi -f 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
