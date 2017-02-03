6d4d9fc77f5a25a08973412750604b6a4244506e
David Glassenbury <david_glassenbury@trimble.com>
2017-02-03 15:05:01 +1300

    Set the delay to 2 minutes for master data consumer

--- src/MasterDataConsumer/runme.sh
+++ src/MasterDataConsumer/runme.sh
@@ -1,6 +1,6 @@
 
-echo "Master data kafka consumer starting in 60 seconds....."
-sleep 60s
+echo "Master data kafka consumer starting in 160 seconds....."
+sleep 160s
 
 echo "Master data kafka consumer starting Now "
 dotnet MasterDataConsumer.dll
