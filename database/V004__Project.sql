e27c71c13929ce759af31683c1689529d913ee7e
Jeannie May <Jeannie_May@trimble.com>
2017-02-16 16:21:40 +1300

    US38245 43619 Enrich Project Service with ProjectBoundary from the CreateProjectEvent

--- database/V004__Project.sql
+++ database/V004__Project.sql
@@ -13,9 +13,24 @@ CREATE TABLE IF NOT EXISTS Project (
   StartDate date DEFAULT NULL,
   EndDate date DEFAULT NULL,
   
+  GeometryWKT varchar(4000) DEFAULT NULL,
+  
   LastActionedUTC datetime(6) DEFAULT NULL,
   InsertUTC datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   UpdateUTC datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   PRIMARY KEY (ID),
   UNIQUE KEY UIX_Project_ProjectUID (ProjectUID ASC)
 ) ENGINE=InnoDB CHARSET = DEFAULT COLLATE = DEFAULT;
+
+
+/* currently defaults to null. This could be changed later when existing tables are backfilled */
+SET @s = (SELECT IF(
+    (SELECT COUNT(*)
+       FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE table_name = 'Project'
+        AND table_schema = DATABASE()
+        AND column_name = 'GeofenceUID'
+    ) > 0,
+    "SELECT 1",
+    "ALTER TABLE `Project` ADD COLUMN `GeometryWKT` varchar(4000) DEFAULT NULL, AFTER `EndDate`"
+));
