862b1837fe254e48d01721b4f1343e0a2639b6e7
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-02-20 15:54:14 +1300

    Fixed build script

--- AcceptanceTests/scripts/deploy_linux.sh
+++ AcceptanceTests/scripts/deploy_linux.sh
@@ -12,12 +12,6 @@ dotnet restore
 cd tests
 dotnet publish IntegrationTests -o ../deploy/IntegrationTests -f netcoreapp1.1
 dotnet publish WebApiTests -o ../deploy/WebApiTests -f netcoreapp1.1
-dotnet publish EventTests -o ../deploy/EventTests -f netcoreapp1.1
-dotnet publish KafkaTests -o ../deploy/KafkaTests -f netcoreapp1.1
-dotnet publish RepositoryTests -o ../deploy/RepositoryTests -f netcoreapp1.1
-
-cp KafkaTests/appsettings.json ../deploy/KafkaTests/
-cp RepositoryTests/appsettings.json ../deploy/RepositoryTests/
 
 cd ..
 cd utilities
