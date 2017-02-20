178d7f389807994ae5396d01437546d600a4228a
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-02-21 10:52:12 +1300

    Added more logging - reflection doesn't work for some reason

--- MasterDataRepositories/src/RepositoryFactory.cs
+++ MasterDataRepositories/src/RepositoryFactory.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 using VSS.Masterdata;
 using VSS.VisionLink.Interfaces.Events.MasterData.Interfaces;
 
@@ -8,25 +9,36 @@ namespace MasterDataConsumer
 {
   public class RepositoryFactory : IRepositoryFactory
   {
-    private static readonly Dictionary<Type, object> container = new Dictionary<Type, object>();
+        private readonly ILogger log;
+        private static readonly Dictionary<Type, object> container = new Dictionary<Type, object>();
 
       public RepositoryFactory(IRepository<ICustomerEvent> custRepository, IRepository<IProjectEvent> projRepository,
-          IRepository<ISubscriptionEvent> subsRepository, IRepository<IGeofenceEvent> geoRepository)
+          IRepository<ISubscriptionEvent> subsRepository, IRepository<IGeofenceEvent> geoRepository, ILoggerFactory logger)
       {
+          log = logger.CreateLogger<RepositoryFactory>();
           if (container.Any()) return;
+
+          log.LogTrace("Registering repositories");
+
           container.Add(typeof(IRepository<ICustomerEvent>), custRepository);
           container.Add(typeof(IRepository<IProjectEvent>), projRepository);
           container.Add(typeof(IRepository<ISubscriptionEvent>), subsRepository);
           container.Add(typeof(IRepository<IGeofenceEvent>), geoRepository);
-      }
+
+          log.LogTrace("Registered {0} repos", container.Count);
+        }
 
       public IRepository<T> GetRepository<T>()
       {
           object result;
+          log.LogTrace("Resolving repo of type {0}", typeof(T).ToString());
           if (container.TryGetValue(typeof(T), out result))
-              return result as IRepository<T>;
-          else
-              return null;
+          {
+                log.LogDebug("Resolved to {0}", result.ToString());
+                return result as IRepository<T>;
+          }
+            log.LogWarning("Can not resolve repo of type {0}", typeof(T).ToString());
+            return null;
       }
   }
 }
