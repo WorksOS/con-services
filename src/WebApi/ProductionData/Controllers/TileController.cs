ab94009e1f681cf49f6f34ced453fcb25deb02c8
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-07 11:19:14 +1300

    Fixes in raw tile controller

--- src/WebApi/ProductionData/Controllers/TileController.cs
+++ src/WebApi/ProductionData/Controllers/TileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VSS.Raptor.Service.Common.Filters.Authentication;
 using VSS.Raptor.Service.Common.Interfaces;
@@ -58,29 +59,30 @@ namespace VSS.Raptor.Service.WebApi.ProductionData.Controllers
       return tileResult;
     }
 
-    /// <summary>
-    /// This requests returns raw array of bytes with PNG without any diagnostic information. If it fails refer to the request with disgnostic info.
-    /// Supplies tiles of rendered overlays for a number of different thematic sets of data held in a project such as elevation, compaction, temperature, cut/fill, volumes etc
-    /// </summary>
-    /// <param name="request">A representation of the tile rendering request.</param>
-    /// <returns>An HTTP response containing an error code is there is a failure, or a PNG image if the request succeeds. If the size of a pixel in the rendered tile coveres more than 10.88 meters in width or height, then the pixel will be rendered in a 'representational style' where black (currently, but there is a work item to allow this to be configurable) is used to indicate the presense of data. Representational style rendering performs no filtering what so ever on the data.10.88 meters is 32 (number of cells across a subgrid) * 0.34 (default width in meters of a single cell).</returns>
-    /// <executor>TilesExecutor</executor> 
-    [ProjectIdVerifier]
-    [NotLandFillProjectVerifier]
-    [ProjectUidVerifier]
-    [NotLandFillProjectWithUIDVerifier]
-    [Route("api/v1/tiles/png")]
-    [HttpPost]
-    public byte[] PostRaw([FromBody] TileRequest request)
-    {
-      request.Validate();
-      var tileResult = RequestExecutorContainer.Build<TilesExecutor>(logger, raptorClient, null).Process(request) as TileResult;
-      if (tileResult != null)
+      /// <summary>
+      /// This requests returns raw array of bytes with PNG without any diagnostic information. If it fails refer to the request with disgnostic info.
+      /// Supplies tiles of rendered overlays for a number of different thematic sets of data held in a project such as elevation, compaction, temperature, cut/fill, volumes etc
+      /// </summary>
+      /// <param name="request">A representation of the tile rendering request.</param>
+      /// <returns>An HTTP response containing an error code is there is a failure, or a PNG image if the request succeeds. If the size of a pixel in the rendered tile coveres more than 10.88 meters in width or height, then the pixel will be rendered in a 'representational style' where black (currently, but there is a work item to allow this to be configurable) is used to indicate the presense of data. Representational style rendering performs no filtering what so ever on the data.10.88 meters is 32 (number of cells across a subgrid) * 0.34 (default width in meters of a single cell).</returns>
+      /// <executor>TilesExecutor</executor> 
+      [ProjectIdVerifier]
+      [NotLandFillProjectVerifier]
+      [ProjectUidVerifier]
+      [NotLandFillProjectWithUIDVerifier]
+      [Route("api/v1/tiles/png")]
+      [HttpPost]
+      public FileResult PostRaw([FromBody] TileRequest request)
       {
-        Response.Headers.Add("X-Warning", tileResult.TileOutsideProjectExtents.ToString());
-        return tileResult.TileData;
+          request.Validate();
+          var tileResult =
+              RequestExecutorContainer.Build<TilesExecutor>(logger, raptorClient, null).Process(request) as TileResult;
+          if (tileResult != null)
+          {
+              Response.Headers.Add("X-Warning", tileResult.TileOutsideProjectExtents.ToString());
+              return new FileStreamResult(new MemoryStream(tileResult.TileData), "image/png");
+          }
+          return null;
       }
-      return null;
-    }
   }
 }
