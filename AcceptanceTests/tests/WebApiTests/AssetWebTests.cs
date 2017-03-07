a0f542cf733e7e8b71ea8f5358140396ee9b2f57
ghiggins <grant_higgins@trimble.com>
2017-03-07 15:35:59 +1300

    begin adding AssetWebTests

--- AcceptanceTests/tests/WebApiTests/AssetWebTests.cs
+++ AcceptanceTests/tests/WebApiTests/AssetWebTests.cs
@@ -13,10 +13,10 @@ namespace WebApiTests
   public class AssetWebTests
   {
     [TestMethod]
-    public void InjectAnAssetIntoDatabaseAndRetrieveAssetIdFromWebapi()
+    public void Createadevicetype3andassetwithradioserial()
     {
       var msg = new Msg();
-      msg.Title("Asset Test 1", "Inject an asset into database and retrieve AssetId from web api");
+      msg.Title("Asset WebTest 1", "Inject device type3 with assetId (radio serial), call webAPI to get asset Id");
       var ts = new TestSupport {IsPublishToKafka = false};
       var legacyAssetId = ts.SetLegacyAssetId();
       var eventArray = new[] {
@@ -27,14 +27,14 @@ namespace WebApiTests
       var deviceUid = Guid.NewGuid();
       var deviceEventArray = new[] {
          "| TableName | EventDate   | DeviceSerialNumber | DeviceState | DeviceType | DeregisteredUTC | DeviceUID   | DataLinkType | GatewayFirmwarePartNumber | MainboardSoftwareVersion | ModuleType | RadioFirmwarePartNumber |",
-        $"| Device    | 0d+09:00:00 | {deviceUid}        | Subscribed  | SNM940     | 0d+09:00:00     | {deviceUid} | CDMA         | 1.23                      | 3.54                     | modtyp     | 88                      |"};
+        $"| Device    | 0d+09:00:00 | {deviceUid}        | Subscribed  | Series522  | 0d+09:00:00     | {deviceUid} | CDMA         | 1.23                      | 3.54                     | modtyp     | 88                      |"};
       ts.PublishEventCollection(deviceEventArray);
       var assetdeviceEventArray = new[] {
          "| TableName   | EventDate   | fk_AssetUID   | fk_DeviceUID |",
         $"| AssetDevice | 0d+09:10:00 | {ts.AssetUid} | {deviceUid}  |"};
       ts.PublishEventCollection(assetdeviceEventArray);
 
-      var request = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 6, deviceUid.ToString());
+      var request = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 3, deviceUid.ToString());
       var requestJson = JsonConvert.SerializeObject(request, ts.jsonSettings );
       var restClient = new RestClient();
       var uri = ts.GetBaseUri() + "api/v1/asset/getId";
@@ -43,7 +43,63 @@ namespace WebApiTests
       msg.DisplayWebApi(method, uri, response, requestJson);
       var actualResult = JsonConvert.DeserializeObject<GetAssetIdResult>(response,ts.jsonSettings);
       Assert.AreEqual(legacyAssetId, actualResult.assetId, " Legacy asset id's do not match");
+    }
+
+    [TestMethod]
+    public void CallwebAPItogetassetIdfornonexistentradioserial()
+    {
+      var msg = new Msg();
+      msg.Title("Asset WebTest 2", "Call webAPI to get asset Id for non existent rado serial");
+      var ts = new TestSupport { IsPublishToKafka = false };
+      var deviceUid = Guid.NewGuid();
+      var request = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 3, deviceUid.ToString());
+      var requestJson = JsonConvert.SerializeObject(request, ts.jsonSettings );
+      var restClient = new RestClient();
+      var uri = ts.GetBaseUri() + "api/v1/asset/getId";
+      var method = HttpMethod.Post.ToString();
+      var response = restClient.DoHttpRequest(uri,method, requestJson);
+      msg.DisplayWebApi(method, uri, response, requestJson);
+     // var actualResult = JsonConvert.DeserializeObject<GetAssetIdResult>(response,ts.jsonSettings);
+     // Assert.AreEqual(legacyAssetId, actualResult.assetId, " Legacy asset id's do not match");
 
     }
-  }
+        [TestMethod]
+        public void Createadevicetype3andassetwithradioserialwithownercustomerandman3dsubscription()
+        {
+            var msg = new Msg();
+            msg.Title("Asset WebTest 3", "Inject Man3D PM sub, device type3 with assetId(radio serial) WITH OWNERCUSTOMER, call webAPI to get asset Id");
+            var ts = new TestSupport { IsPublishToKafka = false };
+            var legacyAssetId = ts.SetLegacyAssetId();
+            var eventArray = new[] {
+       "| TableName | EventDate   | AssetUID      | LegacyAssetID   | Name      | MakeCode | SerialNumber | Model | IconKey | AssetType  | ",
+      $"| Asset     | 0d+09:00:00 | {ts.AssetUid} | {legacyAssetId} | AssetT1   | CAT      | XAT1         | 345D  | 10      | Excavators | "};
+            ts.PublishEventCollection(eventArray);
+
+            var deviceUid = Guid.NewGuid();
+            var deviceEventArray = new[] {
+         "| TableName | EventDate   | DeviceSerialNumber | DeviceState | DeviceType | DeregisteredUTC | DeviceUID   | DataLinkType | GatewayFirmwarePartNumber | MainboardSoftwareVersion | ModuleType | RadioFirmwarePartNumber |",
+        $"| Device    | 0d+09:00:00 | {deviceUid}        | Subscribed  | Series522  | 0d+09:00:00     | {deviceUid} | CDMA         | 1.23                      | 3.54                     | modtyp     | 88                      |"};
+            ts.PublishEventCollection(deviceEventArray);
+            var assetdeviceEventArray = new[] {
+         "| TableName   | EventDate   | fk_AssetUID   | fk_DeviceUID |",
+        $"| AssetDevice | 0d+09:10:00 | {ts.AssetUid} | {deviceUid}  |"};
+            ts.PublishEventCollection(assetdeviceEventArray);
+//create owner-customer
+//create asset
+//create manual device
+//add Manual 3D sub
+
+
+
+            var request = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 3, deviceUid.ToString());
+            var requestJson = JsonConvert.SerializeObject(request, ts.jsonSettings);
+            var restClient = new RestClient();
+            var uri = ts.GetBaseUri() + "api/v1/asset/getId";
+            var method = HttpMethod.Post.ToString();
+            var response = restClient.DoHttpRequest(uri, method, requestJson);
+            msg.DisplayWebApi(method, uri, response, requestJson);
+            var actualResult = JsonConvert.DeserializeObject<GetAssetIdResult>(response, ts.jsonSettings);
+            Assert.AreEqual(legacyAssetId, actualResult.assetId, " Legacy asset id's do not match");
+        }
+    }
 }
