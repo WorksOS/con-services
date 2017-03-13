bd9589a051c1068d9ec055d8ca1f318a29f4b8a3
Alan Rose <alan_rose@trimble.com>
2017-03-14 09:25:27 +1300

    Changes to fix Jenkins build

--- build.bat
+++ build.bat
@@ -3,12 +3,13 @@ RMDIR /S /Q Artifacts
 if exist Artifacts rd /s /q Artifacts
 
 rem echo %PATH%
-dotnet publish ./src/WebApi -o ./Artifacts/WebApi -f net46 -c Docker
+dotnet publish ./src/WebApi -o ./Artifacts/WebApi -f net462 -c Docker
 dotnet build ./test/UnitTests/WebApiTests
 copy src\WebApi\appsettings.json Artifacts\WebApi\
-copy src\WebApi\Dockerfile Artifacts\WebApi\
 
 mkdir Artifacts\Logs
 rem cd .\test\ComponentTests\scripts
 rem deploy_win.bat
+
+pause
 
