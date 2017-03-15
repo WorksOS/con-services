ebdc206fab5b1cf4db6db4ce685454b653497a82
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-15 15:55:42 +1300

    Copy help file

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ rm -rf artifacts
 dotnet publish ./src/WebApi -o ../../artifacts/WebApi -f netcoreapp1.1 -c Docker
 
 cp src/WebApi/appsettings.json artifacts/WebApi/
+cp src/WebApi/bin/Docker/netcoreapp1.1/WebApi.xml artifacts/WebApi/
 
 mkdir artifacts/logs
