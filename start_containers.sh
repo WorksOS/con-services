606fff749418d8a744fcb56cc0205f3c51aaa660
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-15 14:28:10 +1300

    Typo in grepping

--- start_containers.sh
+++ start_containers.sh
@@ -4,4 +4,4 @@ eval '$(aws ecr get-login --region us-west-2 --profile vss-grant)'
 docker pull 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-masterdataconsumer:latest
 docker pull 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-masterdataconsumer-db:latest
 
-docker-compose up --build -d 2>&1 >/dev/null | grep -v -o '\b\w*schema_\w*\b' > testcontainers
+docker-compose up --build -d 2>&1 >/dev/null | grep -v '\b\w*schema_\w*\b' > testcontainers
