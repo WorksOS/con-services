99746ce4b7f0fadaed3911badf858fb60b5d0f1b
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-16 11:04:53 +1300

    Wrong path in the build script

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ RMDIR /S /Q Artifacts
 if exist Artifacts rd /s /q Artifacts
 
 rem echo %PATH%
-dotnet publish ./src/WebApi -o ../../Artifacts/WebApi -f net462 -c Docker
+dotnet publish ./src/WebApi -o ./Artifacts/WebApi -f net462 -c Docker
 dotnet build ./test/UnitTests/WebApiTests
 copy src\WebApi\appsettings.json Artifacts\WebApi\
 
