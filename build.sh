ab0fc3f490b1729f3cd072e8bdca42270f0e230a
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-15 15:09:58 +1300

    Respect new path pattern

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf artifacts
 
-dotnet publish ./src/WebApi -o artifacts/WebApi -f netcoreapp1.1 -c Docker
+dotnet publish ./src/WebApi -o ../../artifacts/WebApi -f netcoreapp1.1 -c Docker
 
 cp src/WebApi/appsettings.json artifacts/WebApi/
 
