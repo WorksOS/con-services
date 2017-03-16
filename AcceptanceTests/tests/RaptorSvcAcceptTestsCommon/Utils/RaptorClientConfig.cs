782a9cc738ac7c90853e2ad9280b6e3863c66786
Alan Rose <alan_rose@trimble.com>
2017-03-16 16:36:18 +1300

    fix config

--- AcceptanceTests/tests/RaptorSvcAcceptTestsCommon/Utils/RaptorClientConfig.cs
+++ AcceptanceTests/tests/RaptorSvcAcceptTestsCommon/Utils/RaptorClientConfig.cs
@@ -115,7 +115,7 @@ namespace RaptorSvcAcceptTestsCommon.Utils
                 server = "http://mer-vm-tc-01.ap.trimblecorp.net";
             else if (TestEnvironment == "Dev")
             {
-              server = File.ReadAllText(TestDataPath+"webapiaddress.txt");
+              server = "http://"+File.ReadAllText(TestDataPath+"webapiaddress.txt");
             }
           else if (TestEnvironment == "T01")
             server = "http://t01-aslv01.vssengg.com/RaptorWebAPI";
--- runacceptancetests.bat
+++ runacceptancetests.bat
@@ -1,6 +1,5 @@
 set content=
 for /F "delims=" %%i in (container.txt) do set content=%%i
-docker inspect --format '{{ .NetworkSettings.Networks.nat.IPAddress }}' dev_webapi_1
 for /f %%i in ('docker inspect --format "{{ .NetworkSettings.Networks.nat.IPAddress }}" %content%') do set ipaddress=%%i
 cd AcceptanceTests\tests\ProductionDataSvc.AcceptanceTests\bin\Debug
 del *.trx
