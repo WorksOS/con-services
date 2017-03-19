fcdd46416ca0e59f7f931d1f1da8b99b589b7e7d
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-20 10:09:39 +1300

    Script to handle failed containers

--- wait_container.sh
+++ wait_container.sh
@@ -6,4 +6,13 @@ result+=" $line"
 done < "$1"
 
 echo "Waiting for containers $result"
-docker wait $result
+{ docker wait $result; } &
+{ docker-compose ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | grep -v 0
+  while [ $? <>  0 ]; do.
+   echo Containers ok
+   sleep 1s
+   docker-compose ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | grep -v 0
+  done; } &
+
+wait -n
+pkill -P $$
