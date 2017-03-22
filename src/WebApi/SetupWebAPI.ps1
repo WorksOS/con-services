330eeb5fa7736bcbd77b6436258c1ba8966052e0
Alan Rose <alan_rose@trimble.com>
2017-03-22 15:05:30 +1300

    Fix setupwebapi script

--- src/WebApi/SetupWebAPI.ps1
+++ src/WebApi/SetupWebAPI.ps1
@@ -1,5 +1,5 @@
 #This script sets up the envirionment variables for RaptorServices WebAPI
-Write-host "The user `"$env:username`" logged in to laptop  `"$env:computername`" on $(Get-Date)"  -ForegroundColor Yellow
+Write-host "The user `"$env:username`" run SetupWebAPI.ps1 on machine `"$env:computername`" on $(Get-Date)"  -ForegroundColor Yellow
 
 $OKTORUN = "OK"
 
@@ -14,6 +14,10 @@ Write-host "ASNODEIP=$ASNIP"
 $IONIP = (Get-ChildItem Env:\IONODEIP).Value
 Write-host "IONODEIP=$IONIP"
 
+$SHAREUNC = (Get-ChildItem Env:\SHAREUNC).Value
+Write-host "SHAREUNC=$SHAREUNC"
+
+
 if ($ASNIP -eq $null)
   { Write-host "Error! Environment variable ASNODEIP is not set"  -ForegroundColor Red; $OKTORUN = "Bad"}
 else 
@@ -24,7 +28,23 @@ if ($IONIP -eq $null)
 else 
   { (Get-Content velociraptor.config.xml).replace('[IONodeIP]', $IONIP) | Set-Content velociraptor.config.xml}
 
+# now we need to mount a share for the design files and reports
+if ($SHAREUNC -eq $null)
+  { Write-host "Error! Environment variable SHAREUNC is not set"  -ForegroundColor Red; $OKTORUN = "Bad"}
+else 
+  { 
+   & sc.exe qc lanmanworkstation
+   & sc.exe config lanmanworkstation depend= "MrxSmb20/NSI"
+   & sc.exe qc lanmanworkstation
+   & sc.exe start lanmanworkstation
+   $SHAREUNC = "dev-iolv01.vssengg.com"
+   $cmd="net use Z: $SHAREUNC /user:svcRaptor v3L0c1R^pt0R!"
+   Invoke-Expression $cmd
+   #& net use Z: $SHAREUNC /user:svcRaptor "v3L0c1R^pt0R!"
+  }
+
 if ($OKTORUN -eq "OK")
   {& .\\WebAPI.exe}
 else
-  { Write-host "Error! Not running WebAPI due to setup error. Check Environment variables ASNODEIP and IONODEIP are defined"  -ForegroundColor Red}
+  { Write-host "Error! Not running WebAPI due to setup error. Check Environment variables ASNODEIP, IONODEIP and SHAREUNC are defined"  -ForegroundColor Red}
+
