dbf166821f4f61e834dcc073e09dc197afc660fd
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-23 16:02:35 +1300

    V4 controller implementation with proxies

--- src/ProjectWebApi/Controllers/ProjectV4Controller.cs
+++ src/ProjectWebApi/Controllers/ProjectV4Controller.cs
@@ -13,188 +13,200 @@ using Repositories;
 using Repositories.DBModels;
 using VSS.GenericConfiguration;
 using VSS.VisionLink.Interfaces.Events.MasterData.Interfaces;
-using ProjectWebApi.ResultsHandling;
 using VSS.VisionLink.Interfaces.Events.MasterData.Models;
+using ProjectWebApi.ResultsHandling;
+using VSS.Raptor.Service.Common.Interfaces;
+using VSS.Raptor.Service.Common.Utilities;
 using VSP.MasterData.Project.WebAPI.Controllers.V3;
 
 namespace VSP.MasterData.Project.WebAPI.Controllers.V4
 {
-  public class ProjectV4Controller : ProjectV3Controller
-  {
-    //TODO COnvert
-
-    public ProjectV4Controller(IKafka producer, IRepository<IProjectEvent> projectRepo,
-        IRepository<ISubscriptionEvent> subscriptionsRepo, IConfigurationStore store, ILoggerFactory logger) : base (producer,projectRepo,subscriptionsRepo,store,logger)
+    public class ProjectV4Controller : ProjectV3Controller
     {
-    }
+        //TODO COnvert
+
+        public ProjectV4Controller(IKafka producer, IRepository<IProjectEvent> projectRepo,
+            IRepository<ISubscriptionEvent> subscriptionsRepo, IConfigurationStore store, ISubscriptionProxy subsProxy,
+            IGeofenceProxy geofenceProxy, ILoggerFactory logger)
+            : base(producer, projectRepo, subscriptionsRepo, store, subsProxy, geofenceProxy, logger)
+        {
+        }
 
 
-    /// <summary>
-    /// Gets a list of projects for a customer. The list includes projects of all project types
-    /// and both active and archived projects.
-    /// </summary>
-    /// <returns>A list of projects</returns>
-    [Route("api/v4/project")]
-    [HttpGet]
-    public async Task<List<ProjectDescriptor>> GetProjectsV4()
-    {
-      log.LogInformation("GetProjectsV4"); // todo logging
-      return await GetProjectsV3().ConfigureAwait(false);
-    }
+        /// <summary>
+        /// Gets a list of projects for a customer. The list includes projects of all project types
+        /// and both active and archived projects.
+        /// </summary>
+        /// <returns>A list of projects</returns>
+        [Route("api/v4/project")]
+        [HttpGet]
+        public async Task<List<ProjectDescriptor>> GetProjectsV4()
+        {
+            log.LogInformation("GetProjectsV4"); // todo logging
+            return await GetProjectsV3().ConfigureAwait(false);
+        }
 
 
-    // POST: api/project
-    /// <summary>
-    /// Create Project
-    ///    as of v4 this creates a project AND the association to Customer
-    /// </summary>
-    /// <param name="project">CreateProjectEvent model</param>
-    /// <remarks>Create new project</remarks>
-    /// <response code="200">Ok</response>
-    /// <response code="400">Bad request</response>
-    [Route("api/v4/project")]
-    [HttpPost]
-    public async Task CreateProjectV4([FromBody] CreateProjectEvent project)
-    {
-      Console.WriteLine("CreateProjectV4. Project: {0}", JsonConvert.SerializeObject(project));
+        // POST: api/project
+        /// <summary>
+        /// Create Project
+        ///    as of v4 this creates a project AND the association to Customer
+        /// </summary>
+        /// <param name="project">CreateProjectEvent model</param>
+        /// <remarks>Create new project</remarks>
+        /// <response code="200">Ok</response>
+        /// <response code="400">Bad request</response>
+        [Route("api/v4/project")]
+        [HttpPost]
+        public async Task CreateProjectV4([FromBody] CreateProjectEvent project)
+        {
+            Console.WriteLine("CreateProjectV4. Project: {0}", JsonConvert.SerializeObject(project));
 
-      // todo ensure it has a legacyProjectId - another US
+            // todo ensure it has a legacyProjectId - another US
 
-      // validate project
-      ProjectDataValidator.Validate(project, _projectService);
-      project.ReceivedUTC = DateTime.UtcNow;
-      ProjectBoundaryValidator.ValidateWKT(project.ProjectBoundary);
+            // validate project
+            ProjectDataValidator.Validate(project, projectService);
+            project.ReceivedUTC = DateTime.UtcNow;
+            ProjectBoundaryValidator.ValidateWKT(project.ProjectBoundary);
 
-      string wktBoundary = project.ProjectBoundary;
-      await DoesProjectOverlap(project, wktBoundary);
-      // todo ensure a legacyProjectID and legacyCustomerId exist & put in project - another US 
+            string wktBoundary = project.ProjectBoundary;
+            await DoesProjectOverlap(project, wktBoundary);
+            // todo ensure a legacyProjectID and legacyCustomerId exist & put in project - another US 
 
-      //Convert to old format for Kafka for consistency on kakfa queue
-      string kafkaBoundary = project.ProjectBoundary
-              .Replace(ProjectBoundaryValidator.POLYGON_WKT, string.Empty)
-              .Replace("))", string.Empty)
-              .Replace(',', ';')
-              .Replace(' ', ',');
-
-
-      // validate projectCustomer
-      AssociateProjectCustomer customerProject = new AssociateProjectCustomer()
-      {
-        CustomerUID = project.CustomerUID,
-        LegacyCustomerID = project.CustomerID,
-        ProjectUID = project.ProjectUID,
-        RelationType = RelationType.Owner, // todo? not in CreateProjectEvent
-        ActionUTC = project.ActionUTC,
-        ReceivedUTC = project.ReceivedUTC
-      };
-
-      ProjectDataValidator.Validate(customerProject, _projectService);
-      customerProject.ReceivedUTC = DateTime.UtcNow;
-    
-
-      await CreateProject(project, kafkaBoundary, wktBoundary).ConfigureAwait(false);
-      
-      await CreateAssociateProjectCustomer(customerProject);
-      Console.WriteLine("CreateProjectV4. completed succesfully");
-    }
-    
-    /// <summary>
-    /// Determines if the project boundary overlaps any exising project for the customer in time and space.
-    ///    not needed for v1 or 3 and they come via CGen which already does this overlap checking.
-    /// </summary>    
-    /// <param name="project">The create project event</param>
-    /// <param name="databaseProjectBoundary">The project boundary in the new format (WKT)</param>
-    /// <returns></returns>
-    private async Task<bool> DoesProjectOverlap(CreateProjectEvent project, string databaseProjectBoundary)
-    {
-      var overlaps = await _projectService.DoesPolygonOverlap(project.CustomerUID.ToString(), databaseProjectBoundary, project.ProjectStartDate, project.ProjectEndDate).ConfigureAwait(false);
-      if (overlaps)
-      {
-        throw new ServiceException(HttpStatusCode.Forbidden,
-                  new ContractExecutionResult(ContractExecutionStatesEnum.NoValidSubscription,
-                                          "Project boundary overlaps another project, for this customer and time span"));
-      }
-      return overlaps;
-    }
-
-   
-    /// <summary>
-    /// Creates an association.
-    /// </summary>
-    /// <param name="customerProject">The create projectCustomer event</param>
-    /// <returns></returns>
-    private async Task CreateAssociateProjectCustomer(AssociateProjectCustomer customerProject)
-    {
-      var messagePayload = JsonConvert.SerializeObject(new { AssociateProjectCustomer = customerProject });
-      producer.Send(kafkaTopicName,
-          new List<KeyValuePair<string, string>>()
-          {
-                new KeyValuePair<string, string>(customerProject.ProjectUID.ToString(), messagePayload)
-          });
-      await _projectService.StoreEvent(customerProject).ConfigureAwait(false);
-
-    }
-
-    // PUT: api/Project
-    /// <summary>
-    /// Update Project
-    /// </summary>
-    /// <param name="project">UpdateProjectEvent model</param>
-    /// <remarks>Updates existing project</remarks>
-    /// <response code="200">Ok</response>
-    /// <response code="400">Bad request</response>
-    [Route("api/v4/project")]
-    [HttpPut]
-    public async Task UpdateProjectV4([FromBody] UpdateProjectEvent project)
-    {
-      Console.WriteLine("UpdateProjectV4. Project: {0}", JsonConvert.SerializeObject(project));
-
-      ProjectDataValidator.Validate(project, _projectService);
-      project.ReceivedUTC = DateTime.UtcNow;
-
-      // todo ensure it has a legacyProjectId - another US
-      //   i.e. don't create if doesn't exist, with a null legacyID, it MUST exist in DB
-
-      var messagePayload = JsonConvert.SerializeObject(new { UpdateProjectEvent = project });
-      producer.Send(kafkaTopicName,
-          new List<KeyValuePair<string, string>>()
-          {
-                new KeyValuePair<string, string>(project.ProjectUID.ToString(), messagePayload)
-          });
-      await _projectService.StoreEvent(project).ConfigureAwait(false);
-
-      Console.WriteLine("UpdateProjectV4. Completed successfully");
-    }
+            //Convert to old format for Kafka for consistency on kakfa queue
+            string kafkaBoundary = project.ProjectBoundary
+                .Replace(ProjectBoundaryValidator.POLYGON_WKT, string.Empty)
+                .Replace("))", string.Empty)
+                .Replace(',', ';')
+                .Replace(' ', ',');
 
 
-    // DELETE: api/Project/
-    /// <summary>
-    /// Delete Project
-    /// </summary>
-    /// <param name="project">DeleteProjectEvent model</param>
-    /// <remarks>Deletes existing project</remarks>
-    /// <response code="200">Ok</response>
-    /// <response code="400">Bad request</response>
-    [Route("api/v4/project")]
-    [HttpDelete]
-    public async Task DeleteProjectV4([FromBody] DeleteProjectEvent project)
-    {
-      Console.WriteLine("DeleteProjectV4. Project: {0}", JsonConvert.SerializeObject(project));
+            // validate projectCustomer
+            AssociateProjectCustomer customerProject = new AssociateProjectCustomer()
+            {
+                CustomerUID = project.CustomerUID,
+                LegacyCustomerID = project.CustomerID,
+                ProjectUID = project.ProjectUID,
+                RelationType = RelationType.Owner, // todo? not in CreateProjectEvent
+                ActionUTC = project.ActionUTC,
+                ReceivedUTC = project.ReceivedUTC
+            };
 
-      ProjectDataValidator.Validate(project, _projectService);
-      project.ReceivedUTC = DateTime.UtcNow;
+            ProjectDataValidator.Validate(customerProject, projectService);
+            customerProject.ReceivedUTC = DateTime.UtcNow;
 
-      var messagePayload = JsonConvert.SerializeObject(new { DeleteProjectEvent = project });
-      producer.Send(kafkaTopicName,
-          new List<KeyValuePair<string, string>>()
-          {
+            await ValidateAssociateSubscriptions(project).ConfigureAwait(false);
+
+            await CreateProject(project, kafkaBoundary, wktBoundary).ConfigureAwait(false);
+
+            await CreateAssociateProjectCustomer(customerProject);
+
+            var userUid = ((this.User as GenericPrincipal).Identity as GenericIdentity).Name;
+            geofenceProxy.CreateGeofence(project.CustomerUID, project.ProjectName, "", "", project.ProjectBoundary, 0,
+                true, Guid.Parse(userUid), Request.Headers.GetCustomHeaders());
+
+            log.LogDebug("CreateProjectV4. completed succesfully");
+        }
+
+        /// <summary>
+        /// Determines if the project boundary overlaps any exising project for the customer in time and space.
+        ///    not needed for v1 or 3 and they come via CGen which already does this overlap checking.
+        /// </summary>    
+        /// <param name="project">The create project event</param>
+        /// <param name="databaseProjectBoundary">The project boundary in the new format (WKT)</param>
+        /// <returns></returns>
+        private async Task<bool> DoesProjectOverlap(CreateProjectEvent project, string databaseProjectBoundary)
+        {
+            var overlaps =
+                await projectService.DoesPolygonOverlap(project.CustomerUID.ToString(), databaseProjectBoundary,
+                    project.ProjectStartDate, project.ProjectEndDate).ConfigureAwait(false);
+            if (overlaps)
+            {
+                throw new ServiceException(HttpStatusCode.Forbidden,
+                    new ContractExecutionResult(ContractExecutionStatesEnum.NoValidSubscription,
+                        "Project boundary overlaps another project, for this customer and time span"));
+            }
+            return overlaps;
+        }
+
+
+        /// <summary>
+        /// Creates an association.
+        /// </summary>
+        /// <param name="customerProject">The create projectCustomer event</param>
+        /// <returns></returns>
+        private async Task CreateAssociateProjectCustomer(AssociateProjectCustomer customerProject)
+        {
+            var messagePayload = JsonConvert.SerializeObject(new {AssociateProjectCustomer = customerProject});
+            producer.Send(kafkaTopicName,
+                new List<KeyValuePair<string, string>>()
+                {
+                    new KeyValuePair<string, string>(customerProject.ProjectUID.ToString(), messagePayload)
+                });
+            await projectService.StoreEvent(customerProject).ConfigureAwait(false);
+
+        }
+
+        // PUT: api/Project
+        /// <summary>
+        /// Update Project
+        /// </summary>
+        /// <param name="project">UpdateProjectEvent model</param>
+        /// <remarks>Updates existing project</remarks>
+        /// <response code="200">Ok</response>
+        /// <response code="400">Bad request</response>
+        [Route("api/v4/project")]
+        [HttpPut]
+        public async Task UpdateProjectV4([FromBody] UpdateProjectEvent project)
+        {
+            Console.WriteLine("UpdateProjectV4. Project: {0}", JsonConvert.SerializeObject(project));
+
+            ProjectDataValidator.Validate(project, projectService);
+            project.ReceivedUTC = DateTime.UtcNow;
+
+            // todo ensure it has a legacyProjectId - another US
+            //   i.e. don't create if doesn't exist, with a null legacyID, it MUST exist in DB
+
+            var messagePayload = JsonConvert.SerializeObject(new {UpdateProjectEvent = project});
+            producer.Send(kafkaTopicName,
+                new List<KeyValuePair<string, string>>()
+                {
                     new KeyValuePair<string, string>(project.ProjectUID.ToString(), messagePayload)
-          });
-      await _projectService.StoreEvent(project).ConfigureAwait(false);
+                });
+            await projectService.StoreEvent(project).ConfigureAwait(false);
 
-      Console.WriteLine("DeleteProjectV4. Completed succesfully");
+            Console.WriteLine("UpdateProjectV4. Completed successfully");
+        }
+
+
+        // DELETE: api/Project/
+        /// <summary>
+        /// Delete Project
+        /// </summary>
+        /// <param name="project">DeleteProjectEvent model</param>
+        /// <remarks>Deletes existing project</remarks>
+        /// <response code="200">Ok</response>
+        /// <response code="400">Bad request</response>
+        [Route("api/v4/project")]
+        [HttpDelete]
+        public async Task DeleteProjectV4([FromBody] DeleteProjectEvent project)
+        {
+            Console.WriteLine("DeleteProjectV4. Project: {0}", JsonConvert.SerializeObject(project));
+
+            ProjectDataValidator.Validate(project, projectService);
+            project.ReceivedUTC = DateTime.UtcNow;
+
+            var messagePayload = JsonConvert.SerializeObject(new {DeleteProjectEvent = project});
+            producer.Send(kafkaTopicName,
+                new List<KeyValuePair<string, string>>()
+                {
+                    new KeyValuePair<string, string>(project.ProjectUID.ToString(), messagePayload)
+                });
+            await projectService.StoreEvent(project).ConfigureAwait(false);
+
+            Console.WriteLine("DeleteProjectV4. Completed succesfully");
+        }
+
     }
-    
-  }
 }
 
--- src/ProjectWebApi/Startup.cs
+++ src/ProjectWebApi/Startup.cs
@@ -11,100 +11,105 @@ using KafkaConsumer.Kafka;
 using Repositories;
 using VSS.GenericConfiguration;
 using ProjectWebApi.ResultsHandling;
+using VSS.Raptor.Service.Common.Interfaces;
+using VSS.Raptor.Service.Common.Proxies;
 
 namespace ProjectWebApi
 {
-  public class Startup
-  {
-    private readonly string loggerRepoName = "WebApi";
-    private bool isDevEnv = false;
-    IServiceCollection serviceCollection;
-
-    public Startup(IHostingEnvironment env)
+    public class Startup
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+        private readonly string loggerRepoName = "WebApi";
+        private bool isDevEnv = false;
+        IServiceCollection serviceCollection;
 
-      env.ConfigureLog4Net("log4net.xml", loggerRepoName);
-      isDevEnv = env.IsEnvironment("Development");
-      if (isDevEnv)
-      {
-        // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-        builder.AddApplicationInsightsSettings(developerMode: true);
-      }
-
-      builder.AddEnvironmentVariables();
-      Configuration = builder.Build();
-    }
-
-    public IConfigurationRoot Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddLogging();
-
-      //Configure CORS
-      services.AddCors(options =>
-      {
-        options.AddPolicy("VSS", builder => builder.AllowAnyOrigin()
-            .WithHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization",
-                "X-VisionLink-CustomerUid", "X-VisionLink-UserUid")
-            .WithMethods("OPTIONS", "TRACE", "GET", "HEAD", "POST", "PUT", "DELETE"));
-      });
-
-      // Add framework services.
-      services.AddApplicationInsightsTelemetry(Configuration);
-      services.AddTransient<IRepository<IProjectEvent>, ProjectRepository>();
-      services.AddTransient<IRepository<ISubscriptionEvent>, SubscriptionRepository>();
-      services.AddTransient<IKafka, RdKafkaDriver>();
-      services.AddSingleton<IConfigurationStore, GenericConfiguration>();
-      services.AddMvc();
-      //Configure swagger
-      services.AddSwaggerGen();
-
-      services.ConfigureSwaggerGen(options =>
-      {
-        options.SingleApiVersion(new Info
+        public Startup(IHostingEnvironment env)
         {
-          Version = "v3",
-          Title = "Project Master Data API",
-          Description = "API for project data",
-          TermsOfService = "None"
-        });
-        string path = isDevEnv ? "bin/Debug/netcoreapp1.1/" : string.Empty;
-        options.IncludeXmlComments(path + "ProjectWebApi.xml");
-        options.IgnoreObsoleteProperties();
-        options.DescribeAllEnumsAsStrings();
-      });
-      serviceCollection = services;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            env.ConfigureLog4Net("log4net.xml", loggerRepoName);
+            isDevEnv = env.IsEnvironment("Development");
+            if (isDevEnv)
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+
+            //Configure CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("VSS", builder => builder.AllowAnyOrigin()
+                    .WithHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization",
+                        "X-VisionLink-CustomerUid", "X-VisionLink-UserUid")
+                    .WithMethods("OPTIONS", "TRACE", "GET", "HEAD", "POST", "PUT", "DELETE"));
+            });
+
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddTransient<IRepository<IProjectEvent>, ProjectRepository>();
+            services.AddTransient<IRepository<ISubscriptionEvent>, SubscriptionRepository>();
+            services.AddTransient<IKafka, RdKafkaDriver>();
+            services.AddSingleton<IConfigurationStore, GenericConfiguration>();
+            services.AddSingleton<ISubscriptionProxy, SubscriptionProxy>();
+            services.AddSingleton<IGeofenceProxy, GeofenceProxy>();
+
+            services.AddMvc();
+            //Configure swagger
+            services.AddSwaggerGen();
+
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v3",
+                    Title = "Project Master Data API",
+                    Description = "API for project data",
+                    TermsOfService = "None"
+                });
+                string path = isDevEnv ? "bin/Debug/netcoreapp1.1/" : string.Empty;
+                options.IncludeXmlComments(path + "ProjectWebApi.xml");
+                options.IgnoreObsoleteProperties();
+                options.DescribeAllEnumsAsStrings();
+            });
+            serviceCollection = services;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            serviceCollection.AddSingleton<ILoggerFactory>(loggerFactory);
+            //new DependencyInjectionProvider(serviceCollection.BuildServiceProvider());
+            serviceCollection.BuildServiceProvider();
+
+            loggerFactory.AddDebug();
+            loggerFactory.AddLog4Net(loggerRepoName);
+
+            app.UseExceptionTrap();
+            //Enable CORS before TID so OPTIONS works without authentication
+            app.UseCors("VSS");
+            //Enable TID here
+            app.UseTIDAuthentication();
+
+            app.UseApplicationInsightsRequestTelemetry();
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUi();
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-    {
-      serviceCollection.AddSingleton<ILoggerFactory>(loggerFactory);
-      //new DependencyInjectionProvider(serviceCollection.BuildServiceProvider());
-      serviceCollection.BuildServiceProvider();
-
-      loggerFactory.AddDebug();
-      loggerFactory.AddLog4Net(loggerRepoName);
-
-      app.UseExceptionTrap();
-      //Enable CORS before TID so OPTIONS works without authentication
-      app.UseCors("VSS");
-      //Enable TID here
-      app.UseTIDAuthentication();
-
-      app.UseApplicationInsightsRequestTelemetry();
-      app.UseApplicationInsightsExceptionTelemetry();
-
-      app.UseMvc();
-
-      app.UseSwagger();
-      app.UseSwaggerUi();
-    }
-  }
 }
