3d72460d0ef4b50bbb40e4d4813917813ff1ad03
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-23 14:21:24 +1300

    BaseProxy is a nuget package now

--- src/Common/Interfaces/IGeofenceProxy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace VSS.Raptor.Service.Common.Interfaces
-{
-  public interface IGeofenceProxy
-  {
-    string GetGeofenceBoundary(string geofenceUid, IDictionary<string, string> customHeaders = null);
-  }
-}
