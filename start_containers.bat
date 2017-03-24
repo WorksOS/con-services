f33bac298bbe5eca8487742380f58c1a3ee6bf4e
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-24 17:36:20 +1300

    Use docker-compose

--- start_containers.bat
+++ start_containers.bat
@@ -2,6 +2,5 @@ aws ecr get-login --region us-west-2 --profile vss-grant > temp.cmd
 call temp.cmd
 del temp.cmd
 
-docker pull 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-mockproject-webapi:latest
-
+docker-compose pull
 docker-compose up --build -d 2>&1 | grep -o \w*_webapi_\w* > container.txt
