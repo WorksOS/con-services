8440feaa9f8b68e4aabcbb595741c3d2024e5ec8
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-24 17:32:45 +1300

    Force pull images

--- start_containers.sh
+++ start_containers.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+eval '$(aws ecr get-login --region us-west-2 --profile vss-grant)' 
+docker pull 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-masterdataconsumer:latest
+docker pull 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-masterdataconsumer-db:latest
+
 docker-compose up --build -d 2>&1 >/dev/null | grep -o '\b\w*test_\w*\b' > testcontainers
