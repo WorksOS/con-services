1b1ba452596a20d440ececd5e0a1f7b1662d4869
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-30 15:45:07 +1300

    Fix bat file

--- runacceptancetests.bat
+++ runacceptancetests.bat
@@ -1,10 +1,10 @@
 set content=
 for /F "delims=" %%i in (container.txt) do set content=%%i
 for /f %%i in ('docker inspect --format "{{ .NetworkSettings.Networks.nat.IPAddress }}" %content%') do set ipaddress=%%i
+PowerShell.exe -ExecutionPolicy Bypass -Command .\waitForContainer.ps1 -IP %ipaddress%
 cd AcceptanceTests\tests\ProductionDataSvc.AcceptanceTests\bin\Debug
 del *.trx
 echo %ipaddress% > TestData\webapiaddress.txt
-PowerShell.exe -ExecutionPolicy Bypass -Command .\\waitForContainer.ps1 -IP %ipaddress%
 mstest /testcontainer:ProductionDataSvc.AcceptanceTests.dll /resultsfile:testresults.trx
 docker logs %content% > logs.txt
 exit 0
