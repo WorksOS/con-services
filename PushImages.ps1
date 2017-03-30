522cbcdc9b5c8f45a729cc6aeb556a69ec545cd0
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-03-30 14:50:26 +1300

    Image push fixes

--- PushImages.ps1
+++ PushImages.ps1
@@ -5,6 +5,6 @@ $command = & aws ecr get-login --region us-west-2 --profile vss-grant
 $fixedCommand = $command.Replace("https://","")
 Invoke-Expression $fixedCommand
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
-& docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi
+& docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:latest
 & docker rmi -f 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
 & docker rmi -f 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:latest
