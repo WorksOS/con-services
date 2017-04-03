43692885e047d28791c0ab6f3cf009076664ec4d
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-04-03 13:01:33 +1200

    Added linux image

--- MockProjectWebApi/src/mockapis.bat
+++ MockProjectWebApi/src/mockapis.bat
@@ -1,5 +1,8 @@
 dotnet restore --no-cache
 dotnet publish  -o ./Artifacts/MockProjectWebApi -f net462 -c Docker
+dotnet publish  -o ./Artifacts/MockProjectWebApi_Linux -f netcoreapp1.1 -c Docker
 copy appsettings.json Artifacts\MockProjectWebApi\
+copy appsettings.json Artifacts\MockProjectWebApi_Linux\
 copy Dockerfile Artifacts\MockProjectWebApi\
+copy Dockerfile_linux Artifacts\MockProjectWebApi_Linux\Dockerfile
 
