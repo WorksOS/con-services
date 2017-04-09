cbff080fcd4bd4140192886205780e72daa29eb0
Josh Nimmo <josh_nimmo@trimble.com>
2017-04-10 10:37:07 +1200

    only add padding if required

--- src/LandfillService.WebApi/Auth/JWTHelper.cs
+++ src/LandfillService.WebApi/Auth/JWTHelper.cs
@@ -213,7 +213,11 @@ namespace VSS.VisionLink.Utilization.WebApi.Helpers
 
         //Tempory fix to handle changes in #TPAAS-4770, token length is now variable however 
         // to decode successfully claimData % 4 == 0 must be true therefore padding is added
-        claimData = claimData.PadRight(claimData.Length + (4 - claimData.Length % 4), '=');
+        // in Base64 1 char - > 2char so there will never be padding > 2 see https://tools.ietf.org/html/rfc4648#section-8
+        if (claimData.Length % 4 != 0)
+        {
+          claimData = claimData.PadRight(claimData.Length + (4 - claimData.Length % 4), '=');
+        }
 
 
         // Convert the claimsPart to Base64 format
