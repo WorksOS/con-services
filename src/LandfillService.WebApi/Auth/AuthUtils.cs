6fbc322ac5e00b2ba459306f4c89e3edbdfaa2eb
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-05-02 15:07:28 +1200

    #48191: Use nuget package for JWT token

--- src/LandfillService.WebApi/Auth/AuthUtils.cs
+++ src/LandfillService.WebApi/Auth/AuthUtils.cs
@@ -6,7 +6,7 @@ using VSP.MasterData.Customer.WebAPI.Models;
 using VSS.Customer.Data.Interfaces;
 using VSS.Project.Data.Interfaces;
 using VSS.Subscription.Data.Interfaces;
-using VSS.VisionLink.Utilization.WebApi.Helpers;
+using VSS.Authentication.JWT;
 
 public class AuthUtilities
 {
@@ -23,74 +23,39 @@ public class AuthUtilities
 
   public AssociatedCustomer GetContext(HttpRequestHeaders headers, out string errorMessage, out string userId)
   {
-    userId = String.Empty;
-    try
-    {
-      string token = String.Empty;
-      var jwt = JwtHelper.TryGetJwtToken(headers, out token);
-      if (jwt)
-      {
-        if (JwtHelper.IsValidJwtToken(token))
-        {
-          userId = JwtHelper.DecodeJwtToken(token).Uuid;
-          var customer = this._customerService.GetAssociatedCustomerbyUserUid(Guid.Parse(userId));
-          AssociatedCustomer associatedCustomer = new AssociatedCustomer()
-          {
-              CustomerUID = Guid.Parse(customer.CustomerUID),
-              CustomerName = customer.CustomerName,
-              CustomerType = customer.CustomerType
-          };
-           
-          errorMessage = "";
-          return associatedCustomer;
-        }
-        errorMessage = "Invalid token";
-        return null;
-      }
-      errorMessage = "No token";
-      return null;
-    }
-    catch (Exception ex)
-    {
-      errorMessage = "Can not retrieve cusomer context";
-      return null;
-    }
+    string token = String.Empty;
+    return GetContext(headers, out errorMessage, out userId, out token);
   }
 
-  public AssociatedCustomer GetContext(HttpRequestHeaders headers, out string errorMessage, out string userId, out string jwtToken)
+  public AssociatedCustomer GetContext(HttpRequestHeaders headers, out string errorMessage, out string userId, out string token)
   {
     userId = String.Empty;
-    jwtToken = String.Empty;
+    token = String.Empty;
     try
     {
-      string token = String.Empty;
-      var jwt = JwtHelper.TryGetJwtToken(headers, out token);
+      var jwt = TryGetJwtToken(headers, out token);
       if (jwt)
       {
-        jwtToken = token;
-        if (JwtHelper.IsValidJwtToken(token))
+        var jwtToken = new TPaaSJWT(token);
+        userId = jwtToken.UserUid.ToString();
+        var customer = this._customerService.GetAssociatedCustomerbyUserUid(Guid.Parse(userId));
+        AssociatedCustomer associatedCustomer = new AssociatedCustomer()
         {
-          userId = JwtHelper.DecodeJwtToken(token).Uuid;
-          var customer = this._customerService.GetAssociatedCustomerbyUserUid(Guid.Parse(userId));
-          AssociatedCustomer associatedCustomer = new AssociatedCustomer()
-          {
-            CustomerUID = Guid.Parse(customer.CustomerUID),
-            CustomerName = customer.CustomerName,
-            CustomerType = customer.CustomerType
-          };
+          CustomerUID = Guid.Parse(customer.CustomerUID),
+          CustomerName = customer.CustomerName,
+          CustomerType = customer.CustomerType
+        };
 
-          errorMessage = "";
-          return associatedCustomer;
-        }
-        errorMessage = "Invalid token";
-        return null;
+        errorMessage = "";
+        return associatedCustomer;
+  
       }
       errorMessage = "No token";
       return null;
     }
     catch (Exception ex)
     {
-      errorMessage = "Can not retrieve cusomer context";
+      errorMessage = "Can not retrieve cusomer context: Invalid token";
       return null;
     }
   }
@@ -119,6 +84,42 @@ public class AuthUtilities
     }
   }
 
-  
+  /// <summary>
+  ///   This method is used to get the Jwt Assertion Token string from the HTTP Request Header
+  /// </summary>
+  /// <param name="httpRequestHeaders">Incoming Request Headers</param>
+  /// <param name="jwtToken">Output parameter - Jwt Assetion Token string</param>
+  /// <returns>true, if Http Headers contain Jwt; false, otherwise</returns>
+  private bool TryGetJwtToken(HttpRequestHeaders httpRequestHeaders, out string jwtToken)
+  {
+    return TryGetHeader(httpRequestHeaders, "X-Jwt-Assertion", out jwtToken);
+  }
+
+  /// <summary>
+  ///   This method is used to get the Jwt Assertion Token string from the HTTP Request Header
+  /// </summary>
+  /// <param name="httpRequestHeaders">Incoming Request Headers</param>
+  /// <param name="jwtToken">Output parameter - Jwt Assetion Token string</param>
+  /// <returns>true, if Http Headers contain Jwt; false, otherwise</returns>
+  private bool TryGetHeader(HttpRequestHeaders httpRequestHeaders, string headerName, out string headerValue)
+  {
+    headerValue = null;
+    try
+    {
+      if (httpRequestHeaders.Contains(headerName))
+      {
+        //if present read the first element from HTTP Request Header headerName
+        headerValue = httpRequestHeaders.GetValues(headerName).FirstOrDefault();
+        return true;
+      }
+      //If no headerName header in the request, then return false with null headerValue output param
+      return false;
+    }
+    catch
+    {
+      //If any exceptions in getting headerName header, then return false with null headerValue output param
+      return false;
+    }
+  }
 
 }
