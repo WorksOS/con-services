cea624bb2279cabc3f1668ac8d7f6098664fd701
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-05-11 10:45:17 +1200

    First migration step

--- AcceptanceTests/scripts/deploy_win.bat
+++ AcceptanceTests/scripts/deploy_win.bat
@@ -10,10 +10,10 @@ mkdir deploy\testresults
 dotnet restore --no-cache
 
 cd tests
-dotnet publish IntegrationTests -o ..\deploy\IntegrationTests -f netcoreapp1.1
-dotnet publish WebApiTests -o ..\deploy\WebApiTests -f netcoreapp1.1
+dotnet publish IntegrationTests -o ..\..\deploy\IntegrationTests -f netcoreapp1.1
+dotnet publish WebApiTests -o ..\..\deploy\WebApiTests -f netcoreapp1.1
 
 cd ..
 cd utilities
-dotnet publish TestRun -o ..\deploy\TestRun -f netcoreapp1.1
+dotnet publish TestRun -o ..\..\deploy\TestRun -f netcoreapp1.1
 cd ..
