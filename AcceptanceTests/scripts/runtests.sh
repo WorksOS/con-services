1b21bf27e93510e126160ad1413a7fb7eb027ec6
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-05-10 14:27:07 +1200

    Docker params update

--- AcceptanceTests/scripts/runtests.sh
+++ AcceptanceTests/scripts/runtests.sh
@@ -2,12 +2,6 @@
 echo "Accept tests are starting .... "
 echo "Wait for 60 seconds"
 sleep 60s
-echo "Check the database and kafka to see if port is available"
-# Polling the database and kafka status before test
-/bin/bash wait-for-it.sh db:3306 -t 0
-/bin/bash wait-for-it.sh kafka:9092 -t 0
-echo "Wait for 120 seconds"
-sleep 120s
 # Run the component tests
 echo "Run the component tests"
 echo "Run Integration tests starting...."
