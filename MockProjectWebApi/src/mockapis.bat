407f6aac5022782d7565a172b0581594935c4fdf
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-05-11 10:00:39 +1200

    Migration of all projects to vs2017

--- MockProjectWebApi/src/mockapis.bat
+++ MockProjectWebApi/src/mockapis.bat
@@ -1,6 +1,6 @@
 dotnet restore --no-cache
-dotnet publish  -o ./Artifacts/MockProjectWebApi -f net462 -c Docker
-dotnet publish  -o ./Artifacts/MockProjectWebApi_Linux -f netcoreapp1.1 -c Docker
+dotnet publish  -o ../../Artifacts/MockProjectWebApi -f net462 -c Docker
+dotnet publish  -o ../../Artifacts/MockProjectWebApi_Linux -f netcoreapp1.1 -c Docker
 copy appsettings.json Artifacts\MockProjectWebApi\
 copy appsettings.json Artifacts\MockProjectWebApi_Linux\
 copy Dockerfile Artifacts\MockProjectWebApi\
