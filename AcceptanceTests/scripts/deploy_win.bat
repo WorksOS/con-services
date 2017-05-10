19bc62f49898289ad1c48267b685f9119196ea91
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-05-11 10:19:29 +1200

    Migrate to vs2017

--- AcceptanceTests/scripts/deploy_win.bat
+++ AcceptanceTests/scripts/deploy_win.bat
@@ -10,16 +10,16 @@ mkdir deploy\testresults
 dotnet restore
 
 cd tests
-dotnet publish EventTests -o ..\deploy\EventTests -f netcoreapp1.1
-dotnet publish ExecutorTests -o ..\deploy\ExecutorTests -f netcoreapp1.1
-dotnet publish IntegrationTests -o ..\deploy\IntegrationTests -f netcoreapp1.1
-dotnet publish RepositoryTests -o ..\deploy\RepositoryTests -f netcoreapp1.1
-dotnet publish WebApiTests -o ..\deploy\WebApiTests -f netcoreapp1.1
+dotnet publish EventTests -o ..\..\deploy\EventTests -f netcoreapp1.1
+dotnet publish ExecutorTests -o ..\..\deploy\ExecutorTests -f netcoreapp1.1
+dotnet publish IntegrationTests -o ..\..\deploy\IntegrationTests -f netcoreapp1.1
+dotnet publish RepositoryTests -o ..\..\deploy\RepositoryTests -f netcoreapp1.1
+dotnet publish WebApiTests -o ..\..\deploy\WebApiTests -f netcoreapp1.1
 
 copy ExecutorTests\appsettings.json ..\deploy\ExecutorTests\
 copy RepositoryTests\appsettings.json ..\deploy\RepositoryTests\
 
 cd ..
 cd utilities
-dotnet publish TestRun -o ..\deploy\TestRun -f netcoreapp1.1
+dotnet publish TestRun -o ..\..\deploy\TestRun -f netcoreapp1.1
 cd ..
