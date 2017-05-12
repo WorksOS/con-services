966d05a21f869bd69bd1fd9c90042f1d4e785428
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-05-12 16:41:41 +1200

    Some formatting

--- test/UnitTests/WebApiTests/Models/AssetIdRequestTests.cs
+++ test/UnitTests/WebApiTests/Models/AssetIdRequestTests.cs
@@ -3,16 +3,16 @@ using WebApiModels.Models;
 
 namespace WebApiTests.Models
 {
-  [TestClass]
-  public class AssetIdRequestTests
+    [TestClass]
+    public class AssetIdRequestTests
     {
-    [TestMethod]
-    public void ValidateGetAssetIdRequest_ValidatorCase1()
-    {
-      GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 0, "");
-      var ex = Assert.ThrowsException<ServiceException>(() => assetIdRequest.Validate());
-      Assert.AreNotEqual(-1, ex.GetContent.IndexOf("Must have assetId and/or projectID"));
-    }
+        [TestMethod]
+        public void ValidateGetAssetIdRequest_ValidatorCase1()
+        {
+            GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 0, "");
+            var ex = Assert.ThrowsException<ServiceException>(() => assetIdRequest.Validate());
+            Assert.AreNotEqual(-1, ex.GetContent.IndexOf("Must have assetId and/or projectID"));
+        }
 
         [TestMethod]
         public void ValidateGetAssetIdRequest_ValidatorCase15()
@@ -23,41 +23,41 @@ namespace WebApiTests.Models
         }
 
         [TestMethod]
-    public void ValidateGetAssetIdRequest_ValidatorCase2()
-    {
-      GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 0, "ASerial5");
-      var ex = Assert.ThrowsException<ServiceException>(() => assetIdRequest.Validate());
-      Assert.AreNotEqual(-1, ex.GetContent.IndexOf("AssetId must have valid deviceType"));
-    }
+        public void ValidateGetAssetIdRequest_ValidatorCase2()
+        {
+            GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 0, "ASerial5");
+            var ex = Assert.ThrowsException<ServiceException>(() => assetIdRequest.Validate());
+            Assert.AreNotEqual(-1, ex.GetContent.IndexOf("AssetId must have valid deviceType"));
+        }
 
-    [TestMethod]
-    public void ValidateGetAssetIdRequest_ValidatorCase3()
-    {
-      GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 100, "ASerial5");
-      var ex = Assert.ThrowsException<ServiceException>(() => assetIdRequest.Validate());
-      Assert.AreNotEqual(-1, ex.GetContent.IndexOf("AssetId must have valid deviceType"));
-    }
+        [TestMethod]
+        public void ValidateGetAssetIdRequest_ValidatorCase3()
+        {
+            GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 100, "ASerial5");
+            var ex = Assert.ThrowsException<ServiceException>(() => assetIdRequest.Validate());
+            Assert.AreNotEqual(-1, ex.GetContent.IndexOf("AssetId must have valid deviceType"));
+        }
 
-    [TestMethod]
-    public void ValidateGetAssetIdRequest_ValidatorCase4()
-    {
-      GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(456661, 0, "ASerial5");
-      assetIdRequest.Validate();
-    }
+        [TestMethod]
+        public void ValidateGetAssetIdRequest_ValidatorCase4()
+        {
+            GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(456661, 0, "ASerial5");
+            assetIdRequest.Validate();
+        }
 
-    [TestMethod]
-    public void ValidateGetAssetIdRequest_ValidatorCase5()
-    {
-      GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 6, "AGo ial6");
-      assetIdRequest.Validate();
-    }
+        [TestMethod]
+        public void ValidateGetAssetIdRequest_ValidatorCase5()
+        {
+            GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(-1, 6, "AGo ial6");
+            assetIdRequest.Validate();
+        }
 
-    [TestMethod]
-    public void ValidateGetAssetIdRequest_ValidatorCase6()
-    {
-      GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(745651, 6, "AGo ial6");
-      assetIdRequest.Validate();
-    }
+        [TestMethod]
+        public void ValidateGetAssetIdRequest_ValidatorCase6()
+        {
+            GetAssetIdRequest assetIdRequest = GetAssetIdRequest.CreateGetAssetIdRequest(745651, 6, "AGo ial6");
+            assetIdRequest.Validate();
+        }
 
-  }
+    }
 }
