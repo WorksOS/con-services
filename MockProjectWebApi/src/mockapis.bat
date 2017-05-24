2161d5506820d8c7585d677b23baa97f62510740
Jeannie May <Jeannie_May@trimble.com>
2017-05-24 13:17:13 +1200

    fixup image build

--- MockProjectWebApi/src/mockapis.bat
+++ MockProjectWebApi/src/mockapis.bat
@@ -1,8 +1,5 @@
 dotnet restore --no-cache
 dotnet publish  -o ./Artifacts/MockProjectWebApi -f net462 -c Docker
-dotnet publish  -o ./Artifacts/MockProjectWebApi_Linux -f netcoreapp1.1 -c Docker
 copy appsettings.json Artifacts\MockProjectWebApi\
-copy appsettings.json Artifacts\MockProjectWebApi_Linux\
 copy Dockerfile Artifacts\MockProjectWebApi\
-copy Dockerfile_linux Artifacts\MockProjectWebApi_Linux\Dockerfile
 
--- MockProjectWebApi/src/mockapis.sh
+++ MockProjectWebApi/src/mockapis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-mv project_linux.json project.json
 dotnet restore --no-cache
-dotnet publish -o ./Artifacts/MockProjectWebApi_linux -f netcoreapp1.1 -c Docker
-cp appsettings.json Artifacts/MockProjectWebApi_linux/
-cp Dockerfile_linux Artifacts/MockProjectWebApi_linux/Dockerfile
+dotnet publish -o ./Artifacts/MockProjectWebApi -f netcoreapp1.1 -c Docker
+cp appsettings.json Artifacts/MockProjectWebApi
+cp Dockerfile Artifacts/MockProjectWebApi
 
