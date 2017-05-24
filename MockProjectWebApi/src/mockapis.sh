a573ea8b9dcee2794ed4adcca924ce4f16c8a97e
Jeannie May <Jeannie_May@trimble.com>
2017-05-24 13:26:31 +1200

    Fixup Dockerfile_linux

--- MockProjectWebApi/src/mockapis.sh
+++ MockProjectWebApi/src/mockapis.sh
@@ -2,5 +2,5 @@
 dotnet restore --no-cache
 dotnet publish -o ./Artifacts/MockProjectWebApi -f netcoreapp1.1 -c Docker
 cp appsettings.json Artifacts/MockProjectWebApi
-cp Dockerfile Artifacts/MockProjectWebApi
+cp Dockerfile_linux Artifacts/MockProjectWebApi/Dockerfile
 
