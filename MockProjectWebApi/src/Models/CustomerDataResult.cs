8ebbd18ffd2d0d6a5f98a6b092c4e3ec3eb7b6e2
Jeannie May <Jeannie_May@trimble.com>
2017-05-30 13:27:37 +1200

    #49265 (49975) ProjectMDM: WebAPI to validate selected Customer against requesting user

--- MockProjectWebApi/src/Models/CustomerDataResult.cs
+++ MockProjectWebApi/src/Models/CustomerDataResult.cs
@@ -5,6 +5,10 @@ namespace MockProjectWebApi.Models
 {
   public class CustomerDataResult : ContractExecutionResult
   {
+    public CustomerDataResult(int code, string message = "success") : base(code, message)
+    {
+    }
+
     /// <summary>
     /// Gets or sets the customer descriptors.
     /// </summary>
