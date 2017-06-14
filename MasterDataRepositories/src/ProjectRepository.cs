13342e82d9fdce9896062de7ebff76c2be09b088
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-06-14 14:43:43 +1200

    Support for MySql closed polygon in projectrepository

--- MasterDataRepositories/src/ProjectRepository.cs
+++ MasterDataRepositories/src/ProjectRepository.cs
@@ -66,8 +66,9 @@ namespace Repositories
             projectEvent.ProjectBoundary =
               string.Concat(polygonStr + "((", projectEvent.ProjectBoundary, "))");
           }
+          //Polygon must start and end with the same point
 
-          project.GeometryWKT = projectEvent.ProjectBoundary;
+          project.GeometryWKT = projectEvent.ProjectBoundary.ParseGeometryData().ClosePolygonIfRequired().ToPolygonWKT(); 
           upsertedCount = await UpsertProjectDetail(project, "CreateProjectEvent");
         }
       }
@@ -1173,5 +1174,68 @@ namespace Repositories
     }
 
     #endregion gettersSpatial
+  }
+
+  internal class Point
+  {
+    public double X;
+    public double Y;
+    public string WKTSubstring => $"{X} {Y}";
+
+    public override bool Equals(object obj)
+    {
+      var source = (Point)obj;
+      return (source.X == X) && (source.Y == Y);
+    }
+  }
+
+  internal static class ExtensionString
+  {
+    private static Dictionary<string, string> _replacements = new Dictionary<string, string>();
+
+    static ExtensionString()
+    {
+      _replacements["LINESTRING"] = "";
+      _replacements["CIRCLE"] = "";
+      _replacements["POLYGON"] = "";
+      _replacements["POINT"] = "";
+      _replacements["("] = "";
+      _replacements[")"] = "";
+    }
+
+    public static List<Point> ClosePolygonIfRequired(this List<Point> s)
+    {
+      if (Equals(s.First(), s.Last()))
+        return s;
+      s.Add(s.First());
+      return s;
+    }
+
+    public static string ToPolygonWKT(this List<Point> s)
+    {
+      var internalString = s.Select(p => p.WKTSubstring).Aggregate((i, j) => $"{i},{j}");
+      return $"POLYGON(({internalString}))";
+    }
+
+    public static List<Point> ParseGeometryData(this string s)
+    {
+      var points = new List<Point>();
+
+      foreach (string to_replace in _replacements.Keys)
+      {
+        s = s.Replace(to_replace, _replacements[to_replace]);
+      }
+
+      string[] pointsArray = s.Split(',').Select(str => str.Trim()).ToArray();
+
+      IEnumerable<string[]> coordinates;
+
+      //gets x and y coordinates split by space, trims whitespace at pos 0, converts to double array
+      coordinates = pointsArray.Select(point => point.Trim().Split(null)
+        .Where(v => !string.IsNullOrWhiteSpace(v)).ToArray());
+      points = coordinates.Select(p => new Point() { X = double.Parse(p[0]), Y = double.Parse(p[1]) }).ToList();
+
+      return points;
+    }
   }
 }
