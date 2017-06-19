249e5b835909da87e2b591f9e4d5ae58a945e2cd
Aaron Morgan <aaron_morgan@trimble.com>
2017-06-20 11:32:08 +1200

    #50140: Updated to v1.0.74 MasterDataRepos.netcore and added ProjectRepositoryExtensionsTests.

---
+++ MasterDataConsumers/AcceptanceTests/tests/RepositoryTests/ProjectRepositoryExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repo.Extensions;
+
+namespace RepositoryTests
+{
+  [TestClass]
+  public class ProjectRepositoryExtensionsTests
+    {
+      [TestMethod]
+      [DataRow(0, 0)]
+      [DataRow(1, 1)]
+      [DataRow(2, 1)]
+      [DataRow(4, 4)]
+      public void Integer_CalculateUpsertCount(int upsertCount, int expected)
+      {
+        Assert.AreEqual(expected, upsertCount.CalculateUpsertCount());
+      }
+  }
+}
