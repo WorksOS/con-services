12422683cab0e69f5803c1b6305111381c314523
David Glassenbury <david_glassenbury@trimble.com>
2017-07-03 11:35:56 +1200

    aws login script change

--- awslogin.sh
+++ awslogin.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 result="$(aws ecr get-login --region us-west-2 --profile vss-grant)"
 replold="-e none"
-replstr=" "
-#echo "${result}"
-newresult="${result/$replold/$replstr}"
-#echo "${newresult}"
+replnew=" "
+newresult="${result/$replold/$replnew}"
 eval "${newresult}"
