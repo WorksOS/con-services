e2a1015ddcf7667ef00988972cf579fa82510bc5
David Glassenbury <david_glassenbury@trimble.com>
2017-07-03 12:17:23 +1200

    Fix aws  docker login

--- PushImages.ps1
+++ PushImages.ps1
@@ -3,6 +3,7 @@ param (
 )
 $command = & aws ecr get-login --region us-west-2 --profile vss-grant
 $fixedCommand = $command.Replace("https://","")
+$fixedCommand = $fixedCommand -replace "-e none", " "
 Invoke-Expression $fixedCommand
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:${fullVersion}
 & docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-raptor-webapi:latest
