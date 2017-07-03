29872ff44f449859fe44f2057f78f8e021e8d0ba
Raymond Wilson <raymond_wilson@trimble.com>
2017-07-04 10:41:39 +1200

    Remove redundant comment

--- RaptorClassLibrary/GridFabric/Requests/TileRenderRequest.cs
+++ RaptorClassLibrary/GridFabric/Requests/TileRenderRequest.cs
@@ -21,57 +21,6 @@ namespace VSS.VisionLink.Raptor.GridFabric.Requests
 {
     public static class TileRenderRequest
     {
-        /*
-                /// <summary>
-                /// The ID of the SiteModel to execute the request against
-                /// </summary>
-                public long SiteModelID { get; set; } = -1;
-
-                public DisplayMode Mode { get; set; } = DisplayMode.Height;
-
-                public BoundingWorldExtent3D Extents = BoundingWorldExtent3D.Inverted();
-
-                public bool CoordsAreGrid { get; set;  } = false;
-
-                public ushort PixelsX { get; set; } = 256;
-                public ushort PixelsY { get; set; } = 256;
-
-                public CombinedFilter Filter1 { get; set; } = null;
-                public CombinedFilter Filter2 { get; set; } = null;
-
-                public TileRenderRequest(long siteModelID,
-                                         DisplayMode mode,
-                                         BoundingWorldExtent3D extents,
-                                         bool coordsAreGrid,
-                                         ushort pixelsX,
-                                         ushort pixelsY,
-                                         CombinedFilter filter1,
-                                         CombinedFilter filter2)
-                {
-                    SiteModelID = siteModelID;
-                    Mode = mode;
-                    Extents = extents;
-                    CoordsAreGrid = coordsAreGrid;
-                    PixelsX = pixelsX;
-                    PixelsY = pixelsY;
-                    Filter1 = filter1;
-                    Filter2 = filter2;
-                }
-
-                public Bitmap Execute()
-                {
-                    RenderOverlayTile render = new RenderOverlayTile
-                        (SiteModelID,
-                         Mode,
-                         new XYZ(Extents.MinX, Extents.MinY),
-                         new XYZ(Extents.MaxX, Extents.MaxY),
-                         CoordsAreGrid,
-                         PixelsX, PixelsY,
-                         Filter1, Filter2);
-
-                    return render.Execute();
-                }
-        */
         public static Bitmap Execute(TileRenderRequestArgument arg)
         {
             // Construct the function to be used
@@ -93,6 +42,5 @@ namespace VSS.VisionLink.Raptor.GridFabric.Requests
             // Send the appropriate response to the caller
             return result;
         }
-
     }
 }
