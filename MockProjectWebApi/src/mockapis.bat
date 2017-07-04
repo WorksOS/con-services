2ab202503adbd8a41edc73899400e3a1825c799f
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-07-04 16:32:22 +1200

    Mocks now target net47

--- MockProjectWebApi/src/mockapis.bat
+++ MockProjectWebApi/src/mockapis.bat
@@ -1,6 +1,6 @@
 dotnet restore --no-cache
-dotnet clean
-dotnet publish src.csproj  -o ./Artifacts/MockProjectWebApi -f net462 -c Docker
+dotnet clean src.csproj
+dotnet publish src.csproj  -o ./Artifacts/MockProjectWebApi -f net47 -c Docker
 copy appsettings.json Artifacts\MockProjectWebApi\
 copy Dockerfile Artifacts\MockProjectWebApi\
 
