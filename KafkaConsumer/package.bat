7e13c843560567fd9bc9c7b79f1b2524e61bf233
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-07-06 13:23:40 +1200

    More fixes

--- KafkaConsumer/package.bat
+++ KafkaConsumer/package.bat
@@ -1,3 +1,3 @@
-dotnet restore --no-cache
+dotnet restore --no-cache KafkaConsumer.csproj
 dotnet pack KafkaConsumer.csproj -c Release
 nuget push .\bin\release\*.nupkg -so https://packages.vspengg.com/ qATxVIHO5rIPF3K7 -verbosity detailed
