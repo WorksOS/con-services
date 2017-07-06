995f63c6c6d4bf416598599729dc3432a1d4f76f
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-07-06 12:26:30 +1200

    More fixes for Jenkins build

--- MockProjectWebApi/src/mockapis.bat
+++ MockProjectWebApi/src/mockapis.bat
@@ -1,4 +1,4 @@
-dotnet restore --no-cache
+dotnet restore --no-cache src.csproj
 dotnet clean src.csproj
 dotnet publish src.csproj  -o ./Artifacts/MockProjectWebApi -f net47 -c Docker
 copy appsettings.json Artifacts\MockProjectWebApi\
