f5c1bf9be2bf671782245576d18dbb4d28cedee3
Josh Nimmo <josh_nimmo@trimble.com>
2017-07-07 13:23:36 +1200

    set env vars here

--- runacceptancetests_NEW.bat
+++ runacceptancetests_NEW.bat
@@ -1,9 +1,26 @@
-echo "ENVIRONMENT VARIABLES ARE"
-echo %RAPTOR_WEBSERVICES_HOST%
-echo %TEST_DATA_PATH%
+
+
+
+set content=
+for /F "delims=" %%i in (container.txt) do set content=%%i
+for /f %%i in ('docker inspect --format "{{ .NetworkSettings.Networks.nat.IPAddress }}" %content%') do set ipaddress=%%i
+PowerShell.exe -ExecutionPolicy Bypass -Command .\waitForContainer.ps1 -IP %ipaddress%
+
+rem SET ENVIRONMENT VARIABLES
+set TEST_ENVIRONMENT=Local
+set TEST_DATA_PATH=../../TestData/
+set COMPACTION_SVC_BASE_URI=:80
+set NOTIFICATION_SVC_BASE_URI=:80
+set REPORT_SVC_BASE_URI=:80
+set TAG_SVC_BASE_URI=:80
+set COORD_SVC_BASE_URI=:80
+set PROD_SVC_BASE_URI=:80
+set COORD_SVC_BASE_URI=:80
+set RAPTOR_WEBSERVICES_HOST=%ipaddress%
+
 cd AcceptanceTests\tests\ProductionDataSvc.AcceptanceTests\bin\Debug
 del *.trx
-rem echo %ipaddress% > TestData\webapiaddress.txt
+echo %ipaddress% > TestData\webapiaddress.txt
 mstest /testcontainer:ProductionDataSvc.AcceptanceTests.dll /resultsfile:testresults.trx
-docker logs %container_id% > logs.txt
-rem exit 0
+docker logs %content% > logs.txt
+exit 0
