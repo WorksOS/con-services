fde18af5515fea533a77089e62a1b2d9908e8313
Josh Nimmo <josh_nimmo@trimble.com>
2017-07-10 16:27:46 +1200

    not needed

--- setup_test_environment.bat
+++
@@ -1,7 +0,0 @@
-set content=
-rem Get Raptor webservices container ID
-for /F "delims=" %%i in (container.txt) do set container_id=%%i
-rem Get Raptor webservices container IP from the ID
-for /f %%i in ('docker inspect --format "{{ .NetworkSettings.Networks.nat.IPAddress }}" %container_id%') do set RAPTOR_WEBSERVICES_IP=%%i
-PowerShell.exe -ExecutionPolicy Bypass -Command .\waitForContainer.ps1 -IP %RAPTOR_WEBSERVICES_IP%
-PowerShell.exe -ExecutionPolicy Bypass -Command .\AcceptanceTests\scripts\setEnvironmentVariablesLocal.ps1 -RAPTOR_WEBSERVICES_HOST %RAPTOR_WEBSERVICES_IP%
