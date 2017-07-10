21e87348f880a84c864fa3477c15ab8168959a21
Josh Nimmo <josh_nimmo@trimble.com>
2017-07-10 16:43:58 +1200

    Method to try and get ip address again after delay, occasionally docker doesn't provide one quick enough after bringing up containers

--- runacceptancetests.bat
+++ runacceptancetests.bat
@@ -1,7 +1,14 @@
 set content=
 for /F "delims=" %%i in (container.txt) do set content=%%i
 for /f %%i in ('docker inspect --format "{{ .NetworkSettings.Networks.nat.IPAddress }}" %content%') do set ipaddress=%%i
+
 PowerShell.exe -ExecutionPolicy Bypass -Command .\waitForContainer.ps1 -IP %ipaddress%
+
+IF /I "%ERRORLEVEL%" NEQ "0" (
+	ping 127.0.0.1
+	for /f %%i in ('docker inspect --format "{{ .NetworkSettings.Networks.nat.IPAddress }}" %content%') do set ipaddress=%%i
+	PowerShell.exe -ExecutionPolicy Bypass -Command .\waitForContainer.ps1 -IP %ipaddress%
+	)
 
 rem SET ENVIRONMENT VARIABLES
 set TEST_DATA_PATH=../../TestData/
