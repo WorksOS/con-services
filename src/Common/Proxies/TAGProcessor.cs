8c2ddfe4fcef4277f603a0b421995d935d798214
Josh Nimmo <josh_nimmo@trimble.com>
2017-07-10 15:08:24 +1200

    Alter to handle when DNS fails to return a proper result.
    Add extra logging.

--- src/Common/Proxies/TAGProcessor.cs
+++ src/Common/Proxies/TAGProcessor.cs
@@ -24,27 +24,42 @@ namespace VSS.Productivity3D.Common.Proxies
     /// <returns></returns>
     public TAGProcessorClient ProjectDataServerTAGProcessorClient()
     {
+      IPAddress PDSAddress;
       try
       {
-        log.LogInformation("PDS server location  {0}", VLPDServiceLocations.__Global.VLPDSvcLocations().VLPDTAGProcServiceIPAddress);
+        log.LogInformation("PDS server location  {0}:{1}", VLPDServiceLocations.__Global.VLPDSvcLocations().VLPDTAGProcServiceIPAddress,
+            (short)VLPDServiceLocations.__Global.VLPDSvcLocations().VLPDTAGProcServiceIPPort);
 
-        IPHostEntry host = Dns.GetHostEntry(VLPDServiceLocations.__Global.VLPDSvcLocations().VLPDTAGProcServiceIPAddress);
-        if (host == null)
+        string hostIdentifier = VLPDServiceLocations.__Global.VLPDSvcLocations().VLPDTAGProcServiceIPAddress;
+
+        if (hostIdentifier.Length == 0) {
           throw new ArgumentNullException("Missing PDS TAG Processor IPAddress in configuration file");
-
-        foreach (IPAddress address in host.AddressList)
-        {
-          if (address.AddressFamily == AddressFamily.InterNetwork)
-          {
-            TAGProcessorClient client = new TAGProcessorClient(address, (short)VLPDServiceLocations.__Global.VLPDSvcLocations().VLPDTAGProcServiceIPPort);
-            return client;
-          }
         }
 
-        return null;
+
+        if (!IPAddress.TryParse(hostIdentifier, out PDSAddress)) {
+          IPHostEntry host = Dns.GetHostEntry(VLPDServiceLocations.__Global.VLPDSvcLocations().VLPDTAGProcServiceIPAddress);
+          log.LogInformation("Have got {0} addresses for PDS server", host.AddressList.Length);
+
+          foreach (IPAddress address in host.AddressList) {
+            log.LogInformation("Checking PDS Server address {0}", address);
+            if (address.AddressFamily == AddressFamily.InterNetwork)
+            {
+              PDSAddress = address;
+              break;
+            }
+          }
+
+        }
+
+        log.LogInformation("PDS Server address ok, creating TAG Processor Client with address {0}", PDSAddress);
+        return new TAGProcessorClient(PDSAddress, (short)VLPDServiceLocations.__Global.VLPDSvcLocations().VLPDTAGProcServiceIPPort);
+
+        //return null;
       }
       catch (System.Exception ex)
       {
+        log.LogWarning("EXCEPTION {0}", ex.ToString());
         log.LogWarning("Exception creating PDS TAG processor client. {0}", ex.Message);
         return null;
       }
