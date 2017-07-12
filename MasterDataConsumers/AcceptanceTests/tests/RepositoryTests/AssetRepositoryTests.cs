eae12b71193839121d2629335862fbef635bf6bd
Aaron Morgan <aaron_morgan@trimble.com>
2017-07-13 11:29:13 +1200

    Upgraded to new MD Repos package that has namespace fixes included.

--- MasterDataConsumers/AcceptanceTests/tests/RepositoryTests/AssetRepositoryTests.cs
+++ MasterDataConsumers/AcceptanceTests/tests/RepositoryTests/AssetRepositoryTests.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
