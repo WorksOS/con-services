002a400e227ab982deb81b7ed705442504e77025
Aaron Morgan <aaron_morgan@trimble.com>
2017-07-24 09:20:41 +1200

    Fixed AcceptanceTests deploy_linux.sh.

--- AcceptanceTests/scripts/deploy_linux.sh
+++ AcceptanceTests/scripts/deploy_linux.sh
@@ -10,9 +10,9 @@ mkdir deploy/testresults
 dotnet restore --no-cache
 
 cd tests
-dotnet publish IntegrationTests -o ../../deploy/IntegrationTests -f netcoreapp1.1
-dotnet publish WebApiTests -o ../../deploy/WebApiTests -f netcoreapp1.1
-dotnet publish ExecutorTests -o ../../deploy/ExecutorTests -f netcoreapp1.1
+dotnet publish IntegrationTests/IntegrationTests.csproj -o ../../deploy/IntegrationTests -f netcoreapp1.1
+dotnet publish WebApiTests/WebApiTests.csproj -o ../../deploy/WebApiTests -f netcoreapp1.1
+dotnet publish ExecutorTests/ExecutorTests.csproj -o ../../deploy/ExecutorTests -f netcoreapp1.1
 
 cd ..
 cd utilities
