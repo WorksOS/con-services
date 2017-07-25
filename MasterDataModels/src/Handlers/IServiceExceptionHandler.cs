4e28ecb34c7d191b13e990e2280bad723f6c8a63
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-07-25 16:46:51 +1200

    Added more common stuff to the Models package

---
+++ MasterDataModels/src/Handlers/IServiceExceptionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace VSS.Common.Exceptions
+{
+  /// <summary>
+  /// Common controller ServiceException handler.
+  /// </summary>
+  public interface IServiceExceptionHandler
+  {
+    /// <summary>
+    /// Correctly throw ServiceException for controller types.
+    /// </summary>
+    ServiceException ThrowServiceException(HttpStatusCode statusCode, int errorNumber, string resultCode = null, string errorMessage1 = null, string errorMessage2 = null);
+  }
+}
---
+++ MasterDataModels/src/Handlers/ServiceExceptionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using VSS.Common.ResultsHandling;
+
+namespace VSS.Common.Exceptions
+{
+  /// <summary>
+  /// Common controller ServiceException handler.
+  /// </summary>
+  public class ServiceExceptionHandler : IServiceExceptionHandler
+  {
+    /// <summary>
+    /// The contract execution states enum
+    /// </summary>
+    private readonly ContractExecutionStatesEnum _contractExecutionStatesEnum = new ContractExecutionStatesEnum();
+
+    /// <summary>
+    /// Correctly throw ServiceException for controller types.
+    /// </summary>
+    public ServiceException ThrowServiceException(HttpStatusCode statusCode, int errorNumber, string resultCode = null, string errorMessage1 = null, string errorMessage2 = null)
+    {
+      throw new ServiceException(statusCode,
+        new ContractExecutionResult(_contractExecutionStatesEnum.GetErrorNumberwithOffset(errorNumber),
+          string.Format(_contractExecutionStatesEnum.FirstNameWithOffset(errorNumber), resultCode, errorMessage1 ?? "null", errorMessage2 ?? "null")));
+    }
+  }
+}
