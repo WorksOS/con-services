98047e339f239bf576cb3081640918ad849826a0
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-07-26 14:33:32 +1200

    Added net47

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
 
+#if NET_4_7
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using System.Diagnostics;
+#endif
+
+
 namespace VSS.Productivity3D.TagFileAuth.WebAPI
 {
   public class Program
     {
         public static void Main(string[] args)
         {
+#if NET_4_7
+      //To run the service use https://docs.microsoft.com/en-us/aspnet/core/hosting/windows-service
+      var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+      var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+
+
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(pathToContentRoot)
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .UseApplicationInsights()
+        .Build();
+
+      host.RunAsService();
+#else
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -15,6 +38,7 @@ namespace VSS.Productivity3D.TagFileAuth.WebAPI
                 .Build();
 
             host.Run();
+#endif
         }
     }
 }
