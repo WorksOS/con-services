b4a12444e24f6bc531e5a5280dfe4b2b43e7380e
Raymond Wilson <raymond_wilson@trimble.com>
2017-07-31 16:29:46 +1200

    Upgrade to Ignite 2.1

---
+++ RunFullRaptorIgniteStackAndClient_Debug.bat
@@ -0,0 +1,4 @@
+start C:\Dev\VSS.Raptor.IgnitePOC\RaptorPSNodeServer\bin\debug\RaptorPSNodeServer.exe SpatialDivision=0
+start C:\Dev\VSS.Raptor.IgnitePOC\RaptorServerApplication\bin\debug\RaptorServerApplication.exe
+start C:\Dev\VSS.Raptor.IgnitePOC\WindowsFormsApplication1\bin\debug\WindowsFormsApplication1.exe
+
