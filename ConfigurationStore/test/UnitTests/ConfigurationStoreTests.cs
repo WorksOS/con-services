ecf359cae8f41da61c3a99bbf7d6e5ad6e9aae6c
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-08-01 12:13:50 +1200

    Added unit tests for ConfigStore

---
+++ ConfigurationStore/test/UnitTests/ConfigurationStoreTests.cs
@@ -0,0 +1,102 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using VSS.Log4Net.Extensions;
+
+namespace VSS.ConfigurationStore.UnitTests
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  [TestClass]
+  public class ConfigurationStoreTests
+  {
+
+    public IServiceProvider ServiceProvider;
+
+
+    /// <summary>
+    /// Initializes the test.
+    /// </summary>
+    [TestInitialize]
+    public virtual void InitTest()
+    {
+      var serviceCollection = new ServiceCollection();
+
+      string loggerRepoName = "UnitTestLogTest";
+      var logPath = Directory.GetCurrentDirectory();
+      Log4NetAspExtensions.ConfigureLog4Net(logPath, "log4nettest.xml", loggerRepoName);
+
+      ILoggerFactory loggerFactory = new LoggerFactory();
+      loggerFactory.AddDebug();
+      loggerFactory.AddLog4Net(loggerRepoName);
+
+      serviceCollection.AddLogging();
+      serviceCollection.AddSingleton<ILoggerFactory>(loggerFactory);
+      serviceCollection.AddSingleton<IConfigurationStore, GenericConfiguration>();
+      ServiceProvider = serviceCollection.BuildServiceProvider();
+    }
+
+    [TestMethod]
+    public void CanCreateConfigStore()
+    {
+      Assert.IsNotNull(ServiceProvider.GetRequiredService<IConfigurationStore>());
+      Assert.IsInstanceOfType(ServiceProvider.GetRequiredService<IConfigurationStore>(),typeof(GenericConfiguration));
+    }
+
+
+    [TestMethod]
+    public void CanGetConnectionString()
+    {
+      var configuration = (ServiceProvider.GetRequiredService<IConfigurationStore>());
+      Assert.IsFalse(String.IsNullOrEmpty(configuration.GetConnectionString("VSPDB")));
+    }
+
+
+    [TestMethod]
+    public void ThrowsWhenInvalidConnectionName()
+    {
+      var configuration = (ServiceProvider.GetRequiredService<IConfigurationStore>());
+      Assert.ThrowsException<InvalidOperationException>(()=>configuration.GetConnectionString("VSPDB2"));
+    }
+
+    [TestMethod]
+    public void CanGetString()
+    {
+      var configuration = (ServiceProvider.GetRequiredService<IConfigurationStore>());
+      Assert.IsFalse(String.IsNullOrEmpty(configuration.GetValueString("KAFKA_TOPIC_NAME_SUFFIX")));
+    }
+
+    [TestMethod]
+    public void CanGetInt()
+    {
+      var configuration = (ServiceProvider.GetRequiredService<IConfigurationStore>());
+      Assert.AreEqual(1,configuration.GetValueInt("KAFKA_STACKSIZE"));
+    }
+
+    [TestMethod]
+    public void ReturnsNegativeWhenInvalidInteger()
+    {
+      var configuration = (ServiceProvider.GetRequiredService<IConfigurationStore>());
+      Assert.AreEqual(-1,configuration.GetValueInt("KAFKA_PORT"));
+    }
+
+    [TestMethod]
+    public void ReturnsNullWhenInvalidBool()
+    {
+      var configuration = (ServiceProvider.GetRequiredService<IConfigurationStore>());
+      Assert.IsNull(configuration.GetValueBool("KAFKA_PORT"));
+    }
+
+    [TestMethod]
+    public void CenGetBool()
+    {
+      var configuration = (ServiceProvider.GetRequiredService<IConfigurationStore>());
+      Assert.IsTrue(configuration.GetValueBool("KAFKA_AUTO_COMMIT").HasValue);
+      Assert.IsFalse(configuration.GetValueBool("KAFKA_AUTO_COMMIT").Value);
+    }
+  }
+}
