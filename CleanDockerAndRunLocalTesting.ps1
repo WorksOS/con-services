cff4ac0851937d8ea0c02d345dab5a98d9af415e
David Glassenbury <david_glassenbury@trimble.com>
2017-08-02 13:24:24 +1200

    Additional files for scripts

--- CleanDockerAndRunLocalTesting.ps1
+++ CleanDockerAndRunLocalTesting.ps1
@@ -5,7 +5,7 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q --filter "dangling=true")
 docker rmi 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-masterdataconsumer-db
 docker rmi 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-masterdataconsumer
-docker rmi 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-project-webapi
+docker rmi 276986344560.dkr.ecr.us-west-2.amazonaws.com/vssproductivity3dfilter_webapi
 docker rmi 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-mockproject-webapi:latest-linux
 
 $Cmd = 'aws'
