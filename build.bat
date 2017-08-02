264efaba1b74da38fdcb023121fa84fce2415f1e
David Glassenbury <david_glassenbury@trimble.com>
2017-08-03 10:53:31 +1200

    Update scripts for mysql database deploy

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ dotnet publish ./src/VSS.Productivity3D.Filter.WebApi/VSS.Productivity3D.Filter.
 dotnet build ./test/UnitTests/VSS.Productivity3D.Filter.Tests/VSS.Productivity3D.Filter.Tests.csproj
 copy src\VSS.Productivity3D.Filter.WebApi\appsettings.json artifacts\VSS.Productivity3D.Filter.WebApi\
 copy src\VSS.Productivity3D.Filter.WebApi\Dockerfile artifacts\VSS.Productivity3D.Filter.WebApi\
+copy src\VSS.Productivity3D.Filter.WebApi\bin\Docker\netcoreapp1.1\VSS.Productivity3D.Filter.WebAPI.xml artifacts\VSS.Productivity3D.Filter.WebApi\
 
-
-mkdir Artifacts\Logs
+mkdir artifacts\logs
