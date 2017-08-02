fd79ada0749c0501977659f189489c4adeb061d5
David Glassenbury <david_glassenbury@trimble.com>
2017-08-02 13:19:40 +1200

    Update docker/batch file scripts

---
+++ UpdateEnvFileIpAddress.ps1
@@ -0,0 +1,6 @@
+# Get the ip address from the network adaptor 
+$ipV4 = Get-NetAdapter | ? name -eq ‘Ethernet’ | Get-NetIPAddress -ErrorAction 0 | ? PrefixOrigin -eq ‘Dhcp’ | Select -ExpandProperty IPV4Address
+$ipV4
+
+(Get-Content docker-compose-local.env) | Foreach-Object {$_ -replace "LOCALIPADDRESS", $ipV4} | Set-Content docker-compose-local.env
+(Get-Content DockerEnvironmentVariables.ps1) | Foreach-Object {$_ -replace "LOCALIPADDRESS", $ipV4} | Set-Content DockerEnvironmentVariables.ps1
