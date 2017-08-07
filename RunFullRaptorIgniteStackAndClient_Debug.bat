fbfe92dda4363c41ac3bc6d046923a043692f637
Raymond Wilson <raymond_wilson@trimble.com>
2017-08-07 14:12:58 +1200

    Added Activator utility and Binarizable test class to source

--- RunFullRaptorIgniteStackAndClient_Debug.bat
+++ RunFullRaptorIgniteStackAndClient_Debug.bat
@@ -1,4 +1,5 @@
 start C:\Dev\VSS.Raptor.IgnitePOC\RaptorPSNodeServer\bin\debug\RaptorPSNodeServer.exe SpatialDivision=0
 start C:\Dev\VSS.Raptor.IgnitePOC\RaptorServerApplication\bin\debug\RaptorServerApplication.exe
 start C:\Dev\VSS.Raptor.IgnitePOC\WindowsFormsApplication1\bin\debug\WindowsFormsApplication1.exe
+start C:\Dev\VSS.Raptor.IgnitePOC\RaptorGridActivator\bin\debug\RaptorGridActivator.exe
 
