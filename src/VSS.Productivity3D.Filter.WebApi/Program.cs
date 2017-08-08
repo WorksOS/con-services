3cf52dc0197f53969dfe927f94077cd8b2fc8cfd
Aaron Morgan <aaron_morgan@trimble.com>
2017-08-09 11:05:11 +1200

    Reverted changes to Program.cs ensuring we run as service under NET47.

--- src/VSS.Productivity3D.Filter.WebApi/Program.cs
+++ src/VSS.Productivity3D.Filter.WebApi/Program.cs
@@ -18,6 +18,19 @@ namespace VSS.Productivity3D.Filter.WebApi
     /// </summary>
     public static void Main(string[] args)
     {
+#if NET_4_7 //To run the service use https://docs.microsoft.com/en-us/aspnet/core/hosting/windows-service
+      var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+      var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(pathToContentRoot)
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.RunAsService();
+#else
       var host = new WebHostBuilder()
         .UseKestrel()
         .UseContentRoot(Directory.GetCurrentDirectory())
@@ -26,6 +39,7 @@ namespace VSS.Productivity3D.Filter.WebApi
         .Build();
 
       host.Run();
+#endif
     }
   }
 }
