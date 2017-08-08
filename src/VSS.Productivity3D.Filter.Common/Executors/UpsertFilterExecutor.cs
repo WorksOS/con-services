903e3ecd211cc5ba8f02beedbd644349353a2b36
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2017-08-08 16:52:31 +1200

    #53388. Updated wiith the latest versions of the VSS.MasterData.Models and VSS.MasterData.Proxies packages...

--- src/VSS.Productivity3D.Filter.Common/Executors/UpsertFilterExecutor.cs
+++ src/VSS.Productivity3D.Filter.Common/Executors/UpsertFilterExecutor.cs
@@ -9,6 +9,7 @@ using VSS.Common.Exceptions;
 using VSS.Common.ResultsHandling;
 using VSS.ConfigurationStore;
 using VSS.KafkaConsumer.Kafka;
+using VSS.MasterData.Models.Handlers;
 using VSS.MasterData.Models.Models;
 using VSS.MasterData.Repositories;
 using VSS.MasterData.Proxies.Interfaces;
