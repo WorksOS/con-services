13df3b11ec0338250d24e4c8c23f8df40b78687a
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-08-11 10:47:23 +1200

    Explicitly specify project to build

--- build47.bat
+++ build47.bat
@@ -1,4 +1,4 @@
 RMDIR /S /Q artifacts
 dotnet restore --no-cache
-dotnet publish ./src/VSS.Productivity3D.Filter.WebApi -o ../../artifacts/FilterWebApiNet47 -f net47
+dotnet publish ./src/VSS.Productivity3D.Filter.WebApi/VSS.Productivity3D.Filter.WebApi.csproj -o ../../artifacts/FilterWebApiNet47 -f net47
 7z a FilterWebApiNet47.zip -r ./artifacts/FilterWebApiNet47/
