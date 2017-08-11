563ed52d10aa521214d262d5450d192ffeccc02c
Aaron Morgan <aaron_morgan@trimble.com>
2017-08-11 14:48:58 +1200

    Fixed failing unit test.

--- test/UnitTests/WebApiTests/Common/Filters/ValidProjectUIdAttributeTest.cs
+++ test/UnitTests/WebApiTests/Common/Filters/ValidProjectUIdAttributeTest.cs
@@ -8,10 +8,10 @@ namespace VSS.Productivity3D.WebApiTests.Common.Filters
   public class ValidProjectUIdAttributeTest
   {
     [TestMethod]
-    [DataRow(null)]
     [DataRow(-1)]
     [DataRow(0)]
     [DataRow(true)]
+    [DataRow("00000000-0000-0000-0000-000000000000")]
     [DataRow("c35a613c-05a8-4151-af2a-08007b26799b")]
     public void Should_fail_validation_When_input_is_an_incompatible_type(object projectUId)
     {
@@ -19,9 +19,15 @@ namespace VSS.Productivity3D.WebApiTests.Common.Filters
     }
 
     [TestMethod]
-    public void Should_fail_validation_When_input_is_a_null_Guid()
+    public void Should_fail_validation_When_input_is_an_empty_Guid()
     {
       Assert.IsFalse(new ValidProjectUIDAttribute().IsValid(Guid.Empty));
+    }
+
+    [TestMethod]
+    public void Should_pass_validation_When_input_is_null()
+    {
+      Assert.IsTrue(new ValidProjectUIDAttribute().IsValid(null));
     }
 
     [TestMethod]
