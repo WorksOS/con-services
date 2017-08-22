3fa48a9de9d899fdcfb980864103d7c1bf2eb6d7
Aaron Morgan <aaron_morgan@trimble.com>
2017-08-22 14:50:05 +1200

    Added design profile acceptance test to exercise the service contract API.
    Removed unused files from first attempt at test setup.

--- AcceptanceTests/tests/ProductionDataSvc.AcceptanceTests/ProfileV2.feature.cs
+++
@@ -1,3 +0,0 @@
-﻿#error Parsing error near 'itory expecting http error code <httpCode>'
-#error Parsing error: this line is not in the buffer
-#error Parsing error near 'itory expecting http error code <httpCode>'
---
+++ AcceptanceTests/tests/ProductionDataSvc.AcceptanceTests/StepDefinitions/CompactionDesignProfileSteps.cs
@@ -0,0 +1,53 @@
+﻿using ProductionDataSvc.AcceptanceTests.Models;
+using RaptorSvcAcceptTestsCommon.Utils;
+using System;
+using TechTalk.SpecFlow;
+
+namespace ProductionDataSvc.AcceptanceTests.StepDefinitions
+{
+  [Binding, Scope(Feature = "CompactionDesignProfile")]
+  public class CompactionDesignProfileSteps
+  {
+    private Getter<CompactionProfileResult> profileRequester;
+
+    private string url;
+    private string projectUid;
+    private string queryParameters = string.Empty;
+
+    [Given(@"the Compaction Profile service URI ""(.*)""")]
+    public void GivenTheCompactionProfileServiceURI(string url)
+    {
+      this.url = RaptorClientConfig.CompactionSvcBaseUri + url;
+    }
+    [Given(@"a projectUid ""(.*)""")]
+    public void GivenAProjectUid(string projectUid)
+    {
+      this.projectUid = projectUid;
+    }
+
+    [Given(@"a startLatDegrees ""(.*)"" and a startLonDegrees ""(.*)"" and an endLatDegrees ""(.*)"" And an endLonDegrees ""(.*)""")]
+    public void GivenAStartLatDegreesAndAStartLonDegreesAndAnEndLatDegreesAndAnEndLonDegrees(Decimal startLatDegrees, Decimal startLonDegrees, Decimal endLatDegrees, Decimal endLonDegrees)
+    {
+      queryParameters =
+        $"&startLatDegrees={startLatDegrees}&startLonDegrees={startLonDegrees}&endLatDegrees={endLatDegrees}&endLonDegrees={endLonDegrees}";
+    }
+
+    [Given(@"a importedFileUid ""(.*)""")]
+    public void GivenAImportedFileUid(string importedFileUid)
+    {
+      queryParameters += $"&importedFileUid={importedFileUid}";
+    }
+
+    [When(@"I request a Compaction Design Profile")]
+    public void WhenIRequestACompactionProfile()
+    {
+      profileRequester = Getter<CompactionProfileResult>.GetIt<CompactionProfileResult>(url, projectUid, queryParameters);
+    }
+
+    [Then(@"the Compaction Design Profile should be")]
+    public void ThenTheCompactionProfileShouldBe(string multilineText)
+    {
+      profileRequester.CompareIt<CompactionProfileResult>(multilineText);
+    }
+  }
+}
