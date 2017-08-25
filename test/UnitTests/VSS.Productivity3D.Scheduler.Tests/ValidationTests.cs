c43fb0023e19a673b7e3e129719bb19768bdebdb
David Glassenbury <david_glassenbury@trimble.com>
2017-08-25 14:43:58 +1200

    Add dummy unit tests

---
+++ test/UnitTests/VSS.Productivity3D.Scheduler.Tests/ValidationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSS.Productivity3D.Scheduler.Tests
+{
+  [TestClass]
+  public class ValidationTests
+  {
+    [TestMethod]
+    public void SingleTest()
+    {
+      Console.WriteLine("Dummy test");
+    }
+  }
+}
