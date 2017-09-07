4c1a1576718622380d588d5e4aacf6e8e550de0b
Raymond Wilson <raymond_wilson@trimble.com>
2017-09-07 23:42:50 +1200

    Configure full stack and client debug batch file to run 4 PS Nodes

--- RunFullRaptorIgniteStackAndClient_Debug.bat
+++ RunFullRaptorIgniteStackAndClient_Debug.bat
@@ -1,5 +1,11 @@
 start C:\Dev\VSS.Raptor.IgnitePOC\RaptorPSNodeServer\bin\debug\RaptorPSNodeServer.exe SpatialDivision=0
 timeout 10
+start C:\Dev\VSS.Raptor.IgnitePOC\RaptorPSNodeServer\bin\debug\RaptorPSNodeServer.exe SpatialDivision=1
+timeout 10
+start C:\Dev\VSS.Raptor.IgnitePOC\RaptorPSNodeServer\bin\debug\RaptorPSNodeServer.exe SpatialDivision=2
+timeout 10
+start C:\Dev\VSS.Raptor.IgnitePOC\RaptorPSNodeServer\bin\debug\RaptorPSNodeServer.exe SpatialDivision=3
+timeout 10
 start C:\Dev\VSS.Raptor.IgnitePOC\RaptorServerApplication\bin\debug\RaptorServerApplication.exe
 timeout 10
 start C:\Dev\VSS.Raptor.IgnitePOC\WindowsFormsApplication1\bin\debug\WindowsFormsApplication1.exe
