b9ecb08633d2778c6bc1aaaf2e9eb708c1a9af63
Jeannie May <Jeannie_May@trimble.com>
2017-09-12 12:09:00 +1200

    #55228 fix jenkins for multiple projects

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf artifacts
 rm -rf ./backup*
 
-dotnet publish ./src/WebApi -o ../../artifacts/WebApi -f netcoreapp1.1 -c Docker
+dotnet publish ./src/WebApi/VSS.Productivity3D.TagFileAuth.WebAPI.csproj -o ../../artifacts/WebApi -f netcoreapp1.1 -c Docker
 
 cp src/WebApi/appsettings.json artifacts/WebApi/
 cp src/WebApi/bin/Docker/netcoreapp1.1/VSS.Productivity3D.TagFileAuth.WebAPI.xml artifacts/WebApi/
