07fd3f3b740086315774d4ee92c287af4811be94
Jeannie May <Jeannie_May@trimble.com>
2017-09-12 12:34:55 +1200

    #55228 fix jenkins for multiple projects

--- AcceptanceTests/scripts/deploy_linux.sh
+++ AcceptanceTests/scripts/deploy_linux.sh
@@ -10,11 +10,11 @@ mkdir deploy/testresults
 dotnet restore --no-cache VSS.TagFileAuth.Service.AcceptanceTests.sln
 
 cd tests
-dotnet publish EventTests\EventTests.csproj -o ../../deploy/EventTests -f netcoreapp1.1
-dotnet publish ExecutorTests\ExecutorTests.csproj -o ../../deploy/ExecutorTests -f netcoreapp1.1
-dotnet publish IntegrationTests\IntegrationTests.csproj -o ../../deploy/IntegrationTests -f netcoreapp1.1
-dotnet publish RepositoryTests\RepositoryTests.csproj -o ../../deploy/RepositoryTests -f netcoreapp1.1
-dotnet publish  WebApiTests\WebApiTests.csproj -o ../../deploy/WebApiTests -f netcoreapp1.1
+dotnet publish EventTests/EventTests.csproj -o ../../deploy/EventTests -f netcoreapp1.1
+dotnet publish ExecutorTests/ExecutorTests.csproj -o ../../deploy/ExecutorTests -f netcoreapp1.1
+dotnet publish IntegrationTests/IntegrationTests.csproj -o ../../deploy/IntegrationTests -f netcoreapp1.1
+dotnet publish RepositoryTests/RepositoryTests.csproj -o ../../deploy/RepositoryTests -f netcoreapp1.1
+dotnet publish  WebApiTests/WebApiTests.csproj -o ../../deploy/WebApiTests -f netcoreapp1.1
 
 cp ExecutorTests/appsettings.json ../deploy/ExecutorTests/
 cp RepositoryTests/appsettings.json ../deploy/RepositoryTests/
