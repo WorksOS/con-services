76d6883566632cafb3758ce9d0c6e5c11b9288e5
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-09-14 13:04:41 +1200

    #55759 Swap error codes

--- src/ProjectWebApiCommon/Executors/UpsertProjectSettingsExecutor.cs
+++ src/ProjectWebApiCommon/Executors/UpsertProjectSettingsExecutor.cs
@@ -97,7 +97,7 @@ namespace VSS.MasterData.Project.WebAPI.Common.Executors
       {
         log.LogError(
           $"RaptorValidateProjectSettings: RaptorServices failed with exception. projectUid:{request.projectUid} settings:{request.settings}. Exception Thrown: {e.Message}. ");
-        serviceExceptionHandler.ThrowServiceException(HttpStatusCode.InternalServerError, 70,
+        serviceExceptionHandler.ThrowServiceException(HttpStatusCode.BadRequest, 70,
           "raptorProxy.ValidateProjectSettings", e.Message);
       }
 
@@ -109,7 +109,7 @@ namespace VSS.MasterData.Project.WebAPI.Common.Executors
         log.LogError(
           $"RaptorValidateProjectSettings: RaptorServices failed. projectUid:{request.projectUid} settings:{request.settings}. Reason: {result?.Code ?? -1} {result?.Message ?? "null"}. ");
 
-        serviceExceptionHandler.ThrowServiceException(HttpStatusCode.BadRequest, 67, result.Code.ToString(),
+        serviceExceptionHandler.ThrowServiceException(HttpStatusCode.InternalServerError, 67, result.Code.ToString(),
           result.Message);
       }
       return;
