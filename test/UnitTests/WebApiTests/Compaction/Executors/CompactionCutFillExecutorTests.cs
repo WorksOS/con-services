8a7ef0e86df336697457cdc8a3abc36bed1432cc
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2017-09-15 14:13:40 +1200

    #56641 Start executor unit tests

---
+++ test/UnitTests/WebApiTests/Compaction/Executors/CompactionCutFillExecutorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSS.Common.Exceptions;
+using VSS.Common.ResultsHandling;
+using VSS.ConfigurationStore;
+using VSS.MasterData.Models.Handlers;
+using VSS.MasterData.Models.ResultHandling;
+
+namespace VSS.Productivity3D.WebApiTests.Compaction.Executors
+{
+  [TestClass]
+  public class CompactionCutFillExecutorTests
+  {
+    private static IServiceProvider serviceProvider;
+    private static ILoggerFactory logger;
+
+    [ClassInitialize]
+    public static void ClassInit(TestContext context)
+    {
+      ILoggerFactory loggerFactory = new LoggerFactory();
+      loggerFactory.AddDebug();
+
+      var serviceCollection = new ServiceCollection();
+      serviceCollection.AddLogging();
+      serviceCollection.AddSingleton(loggerFactory);
+      serviceCollection
+        //.AddSingleton<IConfigurationStore, GenericConfiguration>()
+        .AddTransient<IServiceExceptionHandler, ServiceExceptionHandler>()
+        .AddTransient<IErrorCodesProvider, ErrorCodesProvider>();
+
+      serviceProvider = serviceCollection.BuildServiceProvider();
+
+      logger = serviceProvider.GetRequiredService<ILoggerFactory>();
+    }
+
+    [TestMethod]
+    public void CutFillExecutorNoResult()
+    {
+    }
+
+    [TestMethod]
+    public void CutFillExecutorSuccess()
+    {
+    }
+  }
+}
