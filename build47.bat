25b39c2ddadfc7f38f00ac8581c78cac42d309ce
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-09-26 12:45:32 +1300

    Added lost 47 framework

--- build47.bat
+++ build47.bat
@@ -1,4 +1,4 @@
 RMDIR /S /Q artifacts
 dotnet restore --no-cache VSS.TagFileAuth.Service.sln
-dotnet publish ./src/WebApi -o ../../artifacts/TagFileAuthWebApiNet47 -f net47
+dotnet publish ./src/WebApi/VSS.Productivity3D.TagFileAuth.WebAPI.csproj -o ../../artifacts/TagFileAuthWebApiNet47 -f net47
 7z a TagFileAuthWebApiNet47.zip -r ./artifacts/TagFileAuthWebApiNet47/
