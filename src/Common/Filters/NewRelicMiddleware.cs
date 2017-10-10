07aa0ccb5c3c12cf4cc67405f19cbcfa9d8215ef
Aaron Morgan <aaron_morgan@trimble.com>
2017-10-10 13:06:30 +1300

    Patched NewRelicMiddleware to make it thread safe.

--- src/Common/Filters/NewRelicMiddleware.cs
+++ src/Common/Filters/NewRelicMiddleware.cs
@@ -1,46 +1,53 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using VSS.Productivity3D.Common.Filters.Authentication.Models;
 
 namespace VSS.Productivity3D.Common.Filters
 {
-  //This middleware logs events into NewRelic. This must be added after TIDAuth middleware
+  /// <summary>
+  /// This middleware logs events into NewRelic. This must be added after TIDAuth middleware
+  /// </summary>
   public class NewRelicMiddleware
   {
-    private readonly RequestDelegate _next;
-    private Dictionary<string, object> _eventAttributes;
+    private readonly RequestDelegate NextRequestDelegate;
 
-    public NewRelicMiddleware(RequestDelegate next)
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    /// <param name="nextRequestDelegate"></param>
+    public NewRelicMiddleware(RequestDelegate nextRequestDelegate)
     {
-      _next = next;
+      this.NextRequestDelegate = nextRequestDelegate;
     }
 
+    /// <summary>
+    /// Callback executed on each request made to the server.
+    /// </summary>
+    /// <param name="context">The current <see cref="HttpContext"/> object.</param>
     public async Task Invoke(HttpContext context)
     {
-
       var watch = System.Diagnostics.Stopwatch.StartNew();
-
-      await _next.Invoke(context);
-
+      await this.NextRequestDelegate.Invoke(context);
       watch.Stop();
 
       if (context.User is RaptorPrincipal principal)
       {
-        string projectUid=String.Empty;
+        string projectUid = String.Empty;
         string origin = String.Empty;
 
         if (context.Request.Query.ContainsKey("projectuid"))
+        {
           projectUid = context.Request.Query["projectuid"];
+        }
 
         if (context.Request.Headers.ContainsKey("Origin"))
+        {
           origin = context.Request.Headers["Origin"];
+        }
 
-
-        _eventAttributes = new Dictionary<string, object>()
+        var eventAttributes = new Dictionary<string, object>
         {
           {"endpoint", context.Request.Path},
           {"userUid", principal.Identity.Name},
@@ -53,10 +60,8 @@ namespace VSS.Productivity3D.Common.Filters
           {"result", context.Response.StatusCode.ToString() }
         };
 
-        NewRelic.Api.Agent.NewRelic.RecordCustomEvent("3DPM_Request", _eventAttributes);
+        NewRelic.Api.Agent.NewRelic.RecordCustomEvent("3DPM_Request", eventAttributes);
       }
-
-
     }
   }
 }
