58ca82d7805d1d24249ce5774e9ca77c970b238b
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-10-12 11:56:10 +1300

    Added s3 push

--- MasterDataConsumers/build47.bat
+++ MasterDataConsumers/build47.bat
@@ -2,3 +2,4 @@ RMDIR /S /Q artifacts
 dotnet restore --no-cache VSS.Productivity3D.MasterDataConsumer.sln
 dotnet publish ./src/MasterdataConsumer/VSS.Productivity3D.MasterDataConsumer.csproj -o ../../artifacts/MasterdataConsumerNet47 -f net47
 7z a MasterdataConsumerNet47.zip -r ./artifacts/MasterdataConsumerNet47/
+7z a MasterdataConsumerDb.zip -r ./database/
