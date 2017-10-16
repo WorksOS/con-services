508ffa9f5bdd88a005c7a6f76e863889bed0c049
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-10-16 14:36:08 +1300

    Fixup build47

--- build47.bat
+++ build47.bat
@@ -1,4 +1,4 @@
 RMDIR /S /Q artifacts
-dotnet restore --no-cache
+dotnet restore --no-cache VSS.Productivity3D.Scheduler.sln
 dotnet publish ./src/VSS.Productivity3D.Scheduler.WebApi/VSS.Productivity3D.Scheduler.WebApi.csproj -o ../../artifacts/VSS.Productivity3D.Scheduler.WebApiNet47 -f net47
 7z a VSS.Productivity3D.Scheduler.WebApiNet47.zip -r ./artifacts/VSS.Productivity3D.Scheduler.WebApiNet47/
