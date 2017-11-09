af0830aa0f6ed7dd3339eb851df22eaa5fd520e8
Raymond Wilson <raymond_wilson@trimble.com>
2017-11-09 16:34:13 +1300

    Initial code conversion TTM file support from Delphi to C#

---
+++ VSS.Velociraptor.Designs.TTMTests/TrimbleTinModelTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSS.Velociraptor.Designs.TTM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSS.Velociraptor.Designs.TTM.Tests
+{
+    [TestClass()]
+    public class TrimbleTinModelTests
+    {
+        [TestMethod()]
+        public void BuildStartPointListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TrimbleTinModelTest()
+        {
+            TrimbleTinModel TTM = new TrimbleTinModel();
+
+            Assert.IsTrue(TTM != null, "Failed to create TrimbleTinModel instance");
+        }
+
+        [TestMethod()]
+        public void SetUpSizesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WriteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WriteDefaultTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadFromFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveToFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveToFileTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuildEdgeListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuildStartPointListTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsTTMFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadHeaderFromFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetElevationRangeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
