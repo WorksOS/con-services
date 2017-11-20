897b16b792c5fa6f6a44919214c6c3223a0343eb
David Glassenbury <david_glassenbury@trimble.com>
2017-11-20 16:03:07 +1300

    #59262 add acceptance tests.

---
+++ AcceptanceTests/tests/ProductionDataSvc.AcceptanceTests/Report.feature
@@ -0,0 +1,34 @@
+﻿Feature: Report
+The report feature produce xls files for the grid and station offset reports. 
+
+@ignore
+Scenario Outline: Grid Report
+Given the report service uri "/api/v2/report/grid"
+And I set request parameters projectUid 'ff91dd40-1569-4765-a2bc-014321f76ace' and filterUid '9c27697f-ea6d-478a-a168-ed20d6cd9a20'
+And I select columns '<Elevation>' '<CMV>' '<MDP>' '<PassCount>' '<Temperature>' '<CutFill>'
+And I select grid report parameters '<cutfillDesignUid>' '<gridInterval>' '<gridReportOption>' '<startNorthing>' '<startEasting>' '<endNorthing>' '<endEasting>' '<azimuth>'
+And the result file 'ReportGridResponse.json'
+When I request a report
+Then the result should match the '<ResultName>' from the repository
+Examples:
+#  Y or N                                                                   Design UID                            metres          
+| ResultName  | Elevation | CMV | MDP | PassCount | Temperature | CutFill | cutfillDesignUid                     | gridInterval | gridReportOption | startNorthing | startEasting | endNorthing | endEasting | azimuth |
+| gridreport1 | Y         | Y   | Y   | Y         | Y           | Y       | dd64fe2e-6f27-4a78-82a3-0c0e8a5e84ff | 1.0          | Automatic        | 1146.36       | 2889.8       | 1300.0      | 2700.0     | 10.0    |
+| gridreport2 | Y         | N   | N   | N         | N           | N       |                                      | 1.5          | Automatic        | 1146.36       | 2889.8       | 1300.0      | 2700.0     | 5.0     |
+| gridreport3 | Y         | N   | N   | Y         | Y           | N       |                                      | 1.0          | Automatic        | 1146.36       | 2889.8       | 1300.0      | 2700.0     | 10.0    |
+| gridreport4 | N         | N   | N   | N         | N           | Y       | dd64fe2e-6f27-4a78-82a3-0c0e8a5e84ff | 1.0          | Automatic        | 1146.36       | 2889.8       | 1300.0      | 2700.0     | 2.0     |
+| gridreport5 | N         | N   | N   | N         | Y           | Y       | dd64fe2e-6f27-4a78-82a3-0c0e8a5e84ff | 1.0          | Automatic        | 1146.36       | 2889.8       | 1300.0      | 2700.0     | 1.0     |
+
+
+@ignore
+Scenario Outline: Station offset Report
+Given the report service uri "/api/v2/report/stationoffset"
+And I set request parameters projectUid 'ff91dd40-1569-4765-a2bc-014321f76ace' and filterUid '9c27697f-ea6d-478a-a168-ed20d6cd9a20'
+And I select columns '<Elevation>' '<CMV>' '<MDP>' '<PassCount>' '<Temperature>' '<CutFill>'
+And I select Station offset report parameters '<cutfillDesignUid>' '<alignmentDesignUid>' '<crossSectionInterval>' '<startStation>' '<endStation>' '<offsets>' 
+And the result file 'ReportstationoffsetResponse.json'
+When I request a report
+Then the result should match the '<ResultName>' from the repository
+Examples:                           
+| ResultName     | Elevation | CMV | MDP | PassCount | Temperature | CutFill | cutfillDesignUid                     | alignmentDesignUid                   | crossSectionInterval | startStation | endStation | offsets  |
+| stationreport1 | Y         | Y   | Y   | Y         | Y           | Y       | dd64fe2e-6f27-4a78-82a3-0c0e8a5e84ff | dd64fe2e-6f27-4a78-82a3-0c0e8a5e84ff | 1.4                  | 1146.36      | 2889.8     | 1.0, 3.5 | 
---
+++ AcceptanceTests/tests/ProductionDataSvc.AcceptanceTests/StepDefinitions/ReportSteps.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductionDataSvc.AcceptanceTests.Models;
+using RaptorSvcAcceptTestsCommon.Utils;
+using TechTalk.SpecFlow;
+
+namespace ProductionDataSvc.AcceptanceTests.StepDefinitions
+{
+  [Binding, Scope(Feature = "Report")]
+  public class ReportSteps
+  {
+    private Getter<CompactionReportGridResult> gridReportRequester;
+    private string url;
+
+    [Given(@"the report service uri ""(.*)""")]
+    public void GivenTheReportServiceUri(string uri)
+    {
+      url = RaptorClientConfig.CompactionSvcBaseUri + uri;
+      
+    }
+
+    [Given(@"the result file '(.*)'")]
+    public void GivenTheResultFile(string resultFile)
+    {
+      gridReportRequester = new Getter<CompactionReportGridResult>(url, resultFile);
+    }
+
+    [Given(@"I set request parameters projectUid '(.*)' and filterUid '(.*)'")]
+    public void GivenISetRequestParametersProjectUidAndFilterUid(string projectUid, string filterUid)
+    {
+      gridReportRequester.QueryString.Add("projectUid", projectUid);
+      gridReportRequester.QueryString.Add("filterUid", filterUid);
+    }
+
+    [Given(@"I select columns '(.*)' '(.*)' '(.*)' '(.*)' '(.*)' '(.*)'")]
+    public void GivenISelectColumns(string colElevation, string colCmv, string colMdp, string colPassCount, string colTemperature, string colCutFill)
+    {
+      if (colElevation == "Y")
+      {
+        gridReportRequester.QueryString.Add("reportElevation", "true");
+      }
+      if (colCmv == "Y")
+      {
+        gridReportRequester.QueryString.Add("reportCMV", "true");
+      }
+      if (colMdp == "Y")
+      {
+        gridReportRequester.QueryString.Add("reportMDP", "true");
+      }
+      if (colPassCount == "Y")
+      {
+        gridReportRequester.QueryString.Add("reportPassCount", "true");
+      }
+      if (colTemperature == "Y")
+      {
+        gridReportRequester.QueryString.Add("reportTemperature", "true");
+      }
+      if (colCutFill == "Y")
+      {
+        gridReportRequester.QueryString.Add("reportCutFill", "true");
+      }
+    }
+
+    [Given(@"I select Station offset report parameters '(.*)' '(.*)' '(.*)' '(.*)' '(.*)' '(.*)'")]
+    public void GivenISelectStationOffsetReportParameters(string cutfillDesignUid, string alignmentDesignUid,double crossSectionInterval, double startStation, double endStation, string offsets)
+    {
+      if (!string.IsNullOrEmpty(cutfillDesignUid))
+      {
+        gridReportRequester.QueryString.Add("cutfillDesignUid", cutfillDesignUid);
+      }
+      if (!string.IsNullOrEmpty(alignmentDesignUid))
+      {
+        gridReportRequester.QueryString.Add("alignmentDesignUid", alignmentDesignUid);
+      }
+      if (crossSectionInterval > 0)
+      {
+        gridReportRequester.QueryString.Add("crossSectionInterval", crossSectionInterval.ToString(CultureInfo.InvariantCulture));
+      }
+      if (startStation > 0)
+      {
+        gridReportRequester.QueryString.Add("startStation", startStation.ToString(CultureInfo.InvariantCulture));
+      }
+      if (endStation > 0)
+      {
+        gridReportRequester.QueryString.Add("endStation", endStation.ToString(CultureInfo.InvariantCulture));
+      }
+      if (!string.IsNullOrEmpty(offsets))
+      {
+        var laoArray = Array.ConvertAll(offsets.Split(','), double.Parse);
+        foreach (var oneoffset in laoArray)
+        {
+          gridReportRequester.QueryString.Add("offsets", oneoffset.ToString(CultureInfo.InvariantCulture));
+        }        
+      }
+    }
+
+    [Given(@"I select grid report parameters '(.*)' '(.*)' '(.*)' '(.*)' '(.*)' '(.*)' '(.*)' '(.*)'")]
+    public void GivenISelectReportParameters(string cutfillDesignUid, double gridInterval, string gridReportOption, double startNorthing, double startEasting, double endNorthing, double endEasting, double azimuth)
+    {
+      if (!string.IsNullOrEmpty(cutfillDesignUid))
+      {
+        gridReportRequester.QueryString.Add("cutfillDesignUid", cutfillDesignUid);
+      }
+
+      if (gridInterval > 0)
+      {
+        gridReportRequester.QueryString.Add("gridInterval", gridInterval.ToString(CultureInfo.InvariantCulture));
+      }
+      if (!string.IsNullOrEmpty(gridReportOption))
+      {
+        gridReportRequester.QueryString.Add("gridReportOption", gridReportOption);
+      }
+      if (startNorthing > 0)
+      {
+        gridReportRequester.QueryString.Add("startNorthing", startNorthing.ToString(CultureInfo.InvariantCulture));
+      }
+      if (startEasting > 0)
+      {
+        gridReportRequester.QueryString.Add("startEasting", startEasting.ToString(CultureInfo.InvariantCulture));
+      }
+      if (endNorthing > 0)
+      {
+        gridReportRequester.QueryString.Add("endNorthing", endNorthing.ToString(CultureInfo.InvariantCulture));
+      }
+      if (endEasting > 0)
+      {
+        gridReportRequester.QueryString.Add("endEasting", endEasting.ToString(CultureInfo.InvariantCulture));
+      }
+      if (azimuth > 0)
+      {
+        gridReportRequester.QueryString.Add("azimuth", azimuth.ToString(CultureInfo.InvariantCulture));
+      }
+    }
+
+    [When(@"I request a report")]
+    public void WhenIRequestAReport()
+    {
+      gridReportRequester.DoValidRequest(url);
+    }
+
+
+    [Then(@"the result should match the '(.*)' from the repository")]
+    public void ThenTheResultShouldMatchTheFromTheRepository(string resultName)
+    {
+      Assert.AreEqual(gridReportRequester.ResponseRepo[resultName], gridReportRequester.CurrentResponse);
+    }
+  }
+}
