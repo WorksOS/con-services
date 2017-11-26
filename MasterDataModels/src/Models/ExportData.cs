335d8f346cd905dfebfc9daad0b667045b24e250
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-11-27 12:09:23 +1300

    Models to support 3DP-Scheduler interaction

---
+++ MasterDataModels/src/Models/ExportData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VSS.MasterData.Models.Models
+{
+  /// <summary>
+  /// Container for export data
+  /// </summary>
+  /// <seealso cref="VSS.MasterData.Models.Models.BaseDataResult" />
+  public class ExportData : BaseDataResult
+  {
+    /// <summary>
+    /// Gets or sets the data.
+    /// </summary>
+    /// <value>
+    /// The data.
+    /// </value>
+    public byte[] Data { get; set; }
+  }
+}
---
+++ MasterDataModels/src/ResultHandling/ExportResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VSS.Common.ResultsHandling;
+
+namespace VSS.MasterData.Models.ResultHandling
+{
+  /// <summary>
+  /// Generic container for Export binary data.
+  /// </summary>
+  /// <seealso cref="VSS.Common.ResultsHandling.ContractExecutionResult" />
+  public class ExportResult : ContractExecutionResult
+  {
+    /// <summary>
+    /// Gets or sets the export data.
+    /// </summary>
+    /// <value>
+    /// The export data.
+    /// </value>
+    public byte[] ExportData { get; set; }
+    /// <summary>
+    /// Gets or sets the result code.
+    /// </summary>
+    /// <value>
+    /// The result code.
+    /// </value>
+    public short ResultCode { get; set; }
+  }
+}
