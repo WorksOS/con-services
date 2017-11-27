1d85c99ea4b381e4fc903752b5b11c0a329d0a91
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-11-27 14:32:46 +1300

    #59775 Added mocks for Scheduler APIs

---
+++ MockProjectWebApi/src/Controllers/MockSchedulerContoller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MockProjectWebApi.Controllers
+{
+    public class MockSchedulerContoller : Controller
+    {
+
+      [Route("/api/v1/export/veta")]
+      [HttpGet]
+      public string StartVetaJob(
+        [FromQuery] Guid projectUid,
+        [FromQuery] string fileName,
+        [FromQuery] string machineNames,
+        [FromQuery] Guid? filterUid)
+      {
+        return "Test_Job_1";
+      }
+
+      [Route("/api/v1/export/veta/{projectUId}/{jobId}")]
+      [HttpGet]
+      public Tuple<string, string> GetVetaExportStatus(Guid projectUid, string jobId)
+      {
+        return new Tuple<string, string>($"{projectUid.ToString()}/{jobId}", "Succeeded ");
+      }
+
+  }
+}
