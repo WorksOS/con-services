f2c66cc613e65a4d8b85f036ab25a926cb972b0d
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2017-11-27 13:16:34 +1300

    #59775 Updated scheduler contracts

--- MasterDataProxies/src/Interfaces/ISchedulerProxy.cs
+++ MasterDataProxies/src/Interfaces/ISchedulerProxy.cs
@@ -6,7 +6,7 @@ namespace VSS.MasterData.Proxies
 {
   public interface ISchedulerProxy
   {
-    Task<Dictionary<string, string>> ScheduleVetaExportJob(string jobId);
+    Task<Tuple<string, string>> GetVetaExportJobStatus(Guid projectUId, string jobId);
 
     Task<string> ScheduleVetaExportJob(Guid projectUid,
       string fileName, string machineNames, Guid? filterUid, IDictionary<string, string> customHeaders = null);
