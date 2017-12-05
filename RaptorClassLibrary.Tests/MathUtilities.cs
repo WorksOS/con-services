bedc54801f2ef2d3067d0bbe37c97f8274a11636
Raymond Wilson <raymond_wilson@trimble.com>
2017-12-06 11:48:28 +1300

    Added MathUtilities tests

---
+++ RaptorClassLibrary.Tests/MathUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSS.VisionLink.Raptor.Utilities;
+
+namespace VSS.VisionLink.Raptor.RaptorClassLibrary.Tests
+{
+    [TestClass]
+    public class MathUtilitiesTests
+    {
+        [TestMethod]
+        public void Test_MathUtilities_Hypot()
+        {
+            Assert.IsTrue(MathUtilities.Hypot(3, 4) == 5, "3, 4, 5 right angled triangle did not compute correct hypotenuse");
+            Assert.IsTrue(MathUtilities.Hypot(1, 1) == Math.Sqrt(2), "1, 1, root(2) right angled triangle did not compute correct hypotenuse");
+        }
+    }
+}
