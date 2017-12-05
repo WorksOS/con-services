aa15abddbcb63b94d70049ba86adf0b0ff6a8888
Raymond Wilson <raymond_wilson@trimble.com>
2017-12-06 11:25:58 +1300

    Added PassCountTests

---
+++ RaptorClassLibrary.Tests/PassCountTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSS.VisionLink.Raptor.Utilities;
+
+namespace VSS.VisionLink.Raptor.RaptorClassLibrary.Tests
+{
+    [TestClass]
+    public class PassCountTests
+    {
+        [TestMethod]
+        public void Test_PassCountSizes()
+        {
+            Assert.IsTrue(PassCountSize.Calculate(0) == 1, "Incorrect pass count size for count == 0");
+            Assert.IsTrue(PassCountSize.Calculate(1) == 1, "Incorrect pass count size for count == 1");
+            Assert.IsTrue(PassCountSize.Calculate(255) == 1, "Incorrect pass count size for count == 255");
+            Assert.IsTrue(PassCountSize.Calculate(256) == 2, "Incorrect pass count size for count == 256");
+            Assert.IsTrue(PassCountSize.Calculate(65535) == 2, "Incorrect pass count size for count == 65535");
+            Assert.IsTrue(PassCountSize.Calculate(65536) == 3, "Incorrect pass count size for count == 65536");
+            Assert.IsTrue(PassCountSize.Calculate(1000000) == 3, "Incorrect pass count size for count == 1000000");
+        }
+    }
+}
