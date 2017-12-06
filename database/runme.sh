ce50624a63cfbb689bfaa510fca49f1c40aaf535
Jeannie May <Jeannie_May@trimble.com>
2017-12-07 11:05:02 +1300

    jenkins build

--- database/runme.sh
+++ database/runme.sh
@@ -1,6 +1,6 @@
 echo ">>> Making sure MySQL is up"
-echo ">>> Sleep for 10 seconds"
-sleep 10s
+echo ">>> Sleep for 25 seconds"
+sleep 25s
 eval /scripts/wait-for-it.sh "$MYSQL_SERVER_NAME:$MYSQL_PORT" -t 0
 echo "<<< Done checking on MySQL"
 echo "<<< Migrate the database with flyway"
