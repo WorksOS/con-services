e2db8e4602d0fe9755d43898e17674e87b67d08b
Jeannie May <Jeannie_May@trimble.com>
2017-12-07 10:59:02 +1300

    try to fix jenkins build

--- AcceptanceTests/scripts/runtestsjenkins.sh
+++ AcceptanceTests/scripts/runtestsjenkins.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Accept tests are starting .... "
-echo "Wait for 30 seconds"
-sleep 30s
+echo "Wait for 120 seconds"
+sleep 120s
 echo "Check the database and kafka to see if port is available"
 # Polling the database and kafka status before test
 /bin/bash wait-for-it.sh db:3306 -t 0
 #/bin/bash wait-for-it.sh dbmssql:1433 -t 0
 #/bin/bash wait-for-it.sh kafka:9092 -t 0
-echo "Wait for 30 seconds"
-sleep 30s
+echo "Wait for 120 seconds"
+sleep 120s
 # Run the component tests
 echo "Run the component tests"
 
