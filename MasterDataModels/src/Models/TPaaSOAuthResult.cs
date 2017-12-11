32e685e9388b986c8eb66e8d309ba186b1824297
Jeannie May <Jeannie_May@trimble.com>
2017-12-11 14:04:08 +1300

    #60250 (6122) SS import - periodically re-generate application token

---
+++ MasterDataModels/src/Models/TPaaSOAuthResult.cs
@@ -0,0 +1,32 @@
+﻿namespace VSS.MasterData.Models.Models
+{
+  /// <summary>
+  /// This is the Proxy result for Get3dPmSchedulerBearerToken().
+  /// </summary>
+  public class TPaasOauthResult : BaseDataResult
+  {
+
+    public TPaasOauthRawResult tPaasOauthRawResult;
+  }
+
+  public class TPaasOauthRawResult 
+  {
+
+    /// <summary>
+    ///   Bearer
+    /// </summary>
+    public string token_type;
+
+    /// <summary>
+    ///   Time, in seconds that this access_token will be current
+    ///      50400 seconds = 14 hours
+    /// </summary>
+    public int expires_in;
+
+    /// <summary>
+    ///   Bearer token to use when calling 3dpmService
+    /// </summary>
+    /// 
+    public string access_token;
+  }
+}
