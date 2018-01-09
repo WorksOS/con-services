c7f4325dbc48007c8ff4e825161ed222c0d9efff
Josh Nimmo <josh_nimmo@trimble.com>
2018-01-10 09:12:39 +1300

    #62668 update powershell script to capture container name properly

--- start_containers.ps1
+++ start_containers.ps1
@@ -2,4 +2,7 @@ WriteMsg "Logging in to image host"
 Invoke-Expression -Command (aws ecr get-login --no-include-email --profile vss-grant --region us-west-2)
 
 docker-compose pull
-docker-compose up --build -d 2>&1 | grep -o \w*_webapi_\w* > container.txt
+#Two matches are required to to filter out the string _webapi_ which started appearing when grep alone is used in powershell
+$matchPattern = "\w*_webapi_\w*"
+(docker-compose up --build -d 2>&1) -match $matchPattern | grep -o $matchPattern > container.txt
+$result = docker-compose up --build -d 2>&1
