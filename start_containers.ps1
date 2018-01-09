3dbb84a80f630ed1ebc60ab9aa0854707fa64789
Josh Nimmo <josh_nimmo@trimble.com>
2018-01-10 09:54:46 +1300

    #62668 Tidy and add extra debug information

--- start_containers.ps1
+++ start_containers.ps1
@@ -1,8 +1,9 @@
-WriteMsg "Logging in to image host"
+"Logging in to image host"
 Invoke-Expression -Command (aws ecr get-login --no-include-email --profile vss-grant --region us-west-2)
 
 docker-compose pull
 #Two matches are required to to filter out the string _webapi_ which started appearing when grep alone is used in powershell
 $matchPattern = "\w*_webapi_\w*"
 (docker-compose up --build -d 2>&1) -match $matchPattern | grep -o $matchPattern > container.txt
-$result = docker-compose up --build -d 2>&1
+"Built web api container"
+cat container.txt
