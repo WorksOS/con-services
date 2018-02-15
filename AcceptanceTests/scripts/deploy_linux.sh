26dfc6262d577056b67478c96b223c183e0615e3
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-02-15 16:46:46 +1300

    Fail on failed tests build

--- AcceptanceTests/scripts/deploy_linux.sh
+++ AcceptanceTests/scripts/deploy_linux.sh
@@ -9,12 +9,17 @@ cp scripts/rm_cr.sh deploy/
 mkdir deploy/testresults
 
 dotnet restore --no-cache VSS.Productivity3D.Scheduler.AcceptanceTests.sln
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 cd tests
 dotnet publish RepositoryTests/RepositoryTests.csproj -o ../../deploy/RepositoryTests -f netcoreapp1.1
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 dotnet publish SchedulerTestsFilterCleanup/SchedulerTestsFilterCleanup.csproj -o ../../deploy/SchedulerTestsFilterCleanup -f netcoreapp1.1
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 dotnet publish SchedulerTestsImportedFileSync/SchedulerTestsImportedFileSync.csproj -o ../../deploy/SchedulerTestsImportedFileSync -f netcoreapp1.1
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 cd ..
 cd utilities
 dotnet publish TestRun/TestRun.csproj -o ../../deploy/TestRun -f netcoreapp1.1
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
