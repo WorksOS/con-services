a35dd37a54db42253839d55d38dc1d1a5d695b63
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2018-02-19 16:46:35 +1300

    #64778 Fix mocks for custom boundaries. Add proxy for getting boundaries.

---
+++ MasterDataProxies/src/Interfaces/IBoundaryProxy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VSS.MasterData.Models.Models;
+
+namespace VSS.MasterData.Proxies.Interfaces
+{
+  public interface IBoundaryProxy : ICacheProxy
+  {
+    Task<List<GeofenceData>> GetBoundaries(string projectUid, IDictionary<string, string> customHeaders = null);
+
+  }
+}
