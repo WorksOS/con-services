0c4c1933bdfdb7b002a9d2b9ba82848730e10ed1
Aaron Morgan <aaron_morgan@trimble.com>
2018-02-28 14:55:58 +1300

    Updated start_containers.ps1 script, removed dependency on Grep.

--- start_containers.ps1
+++ start_containers.ps1
@@ -1,9 +1,11 @@
 "Logging in to image host"
-Invoke-Expression -Command (aws ecr get-login --no-include-email --profile vss-grant --region us-west-2)
+Invoke-Expression -Command (aws ecr get-login --no-include-email --region us-west-2)
 
 docker-compose pull
-#Two matches are required to to filter out the string _webapi_ which started appearing when grep alone is used in powershell
-$matchPattern = "\w*_webapi_\w*"
-(docker-compose up --build -d 2>&1) -match $matchPattern | grep -o $matchPattern > container.txt
-"Built web api container"
-cat container.txt
+docker-compose up --build -d 2>&1 > container.txt
+
+# Parse the docker-compose output and retrieve the container name.
+$containerName = Get-Content container.txt | select-string -Pattern "\w*_webapi_\w*"  | Select-Object -ExpandProperty Matches |  Select-Object -First 1 -ExpandProperty Value
+
+$containerName > container.txt
+Write-Host $containerName
