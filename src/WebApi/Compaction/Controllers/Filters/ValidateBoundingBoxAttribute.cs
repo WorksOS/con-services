e8fbfa56de31fb004467c353dff4127956e832e9
Aaron Morgan <aaron_morgan@trimble.com>
2018-03-06 01:17:50 +0000

    Merged PR 8256: Add validation to Tile controller bbox (bounding box) query parameters
    **Problem being solved:**
    
    _For example, create a Postman query to hit the tiles service endpoint and supply this URL:_
    
    _https://api-stg.trimble.com/t/trimble.com/vss-alpha-3dproductivity/2.0/compaction/productiondatatiles?projectUid=c8ae5c33-e1d1-4a78-a8bb-268344a3ad73&mode=height&width=256&height=256&_
    
    _The result is the following error: Object reference not set to an instance of an object._
    
    **What I did:**
    Tile controller action methods were not validating bbox query parameters before performing `String.Split()` operations on what could be a null object. I've added this validation and refactored the Tile controller to move commonly used parameter validation into filter classes. Added missing unit tests.
    
    Other minor code tidying.
    
    Related work items: #65189

---
+++ src/WebApi/Compaction/Controllers/Filters/ValidateBoundingBoxAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using VSS.Common.Exceptions;
+using VSS.Common.ResultsHandling;
+
+namespace VSS.Productivity3D.WebApi.Compaction.Controllers.Filters
+{
+  /// <summary>
+  /// Validates the bounding box parameter has been provided.
+  /// </summary>
+  public class ValidateBoundingBoxAttribute : ActionFilterAttribute
+  {
+    /// <summary>
+    /// Executes before the action method is executed.
+    /// </summary>
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+      if (string.IsNullOrEmpty(context.HttpContext.Request.Query["bbox"]))
+      {
+        throw new ServiceException(HttpStatusCode.BadRequest,
+          new ContractExecutionResult(ContractExecutionStatesEnum.ValidationError,
+            "Service requires bounding box dimensioms to be provided."));
+      }
+
+      base.OnActionExecuting(context);
+    }
+  }
+}
