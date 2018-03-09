644144beb40c04dd0468460258670aea354d7400
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2018-03-09 14:13:46 +1300

    #63630. The includeSurveyedSurfaces flag should be set to true for the SurfaceExport report type only.

--- src/WebApiModels/Compaction/Helpers/ExportRequestHelper.cs
+++ src/WebApiModels/Compaction/Helpers/ExportRequestHelper.cs
@@ -127,7 +127,7 @@ namespace VSS.Productivity3D.WebApi.Models.Compaction.Helpers
         false,
         outputType,
         machineList,
-        true,
+        exportType == ExportTypes.kSurfaceExport,
         fileName,
         exportType,
         ConvertUserPreferences(this.userPreferences, this.projectDescriptor.projectTimeZone));
