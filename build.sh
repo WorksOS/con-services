d27fa214c807ef2bcb61e0cdda236e85b4848895
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2018-03-12 12:32:04 +1300

    #66349 Update jenkins files

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf artifacts
 
-dotnet publish ./src/VSS.Productivity3D.Scheduler.WebApi/VSS.Productivity3D.Scheduler.WebApi.csproj -o ../../artifacts/VSS.Productivity3D.Scheduler.WebApi -f netcoreapp1.1
+dotnet publish ./src/VSS.Productivity3D.Scheduler.WebApi/VSS.Productivity3D.Scheduler.WebApi.csproj -o ../../artifacts/VSS.Productivity3D.Scheduler.WebApi -f netcoreapp2.0
 if [[ $rc != 0 ]]; then exit $rc; fi
 
 cp src/VSS.Productivity3D.Scheduler.WebApi/appsettings.json artifacts/VSS.Productivity3D.Scheduler.WebApi/
--- unittests.sh
+++ unittests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(cd ./test/UnitTests/VSS.Productivity3D.Scheduler.Tests && dotnet test VSS.Productivity3D.Scheduler.Tests.csproj -f netcoreapp1.1 )
+(cd ./test/UnitTests/VSS.Productivity3D.Scheduler.Tests && dotnet test VSS.Productivity3D.Scheduler.Tests.csproj -f netcoreapp2.0 )
 if [[ $rc != 0 ]]; then exit $rc; fi
