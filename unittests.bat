5a435e8675dd088d0e9f1534b7f4cf444a6313ea
David Glassenbury <david_glassenbury@trimble.com>
2018-03-14 16:41:59 +1300

    Run unit tests as x86

--- unittests.bat
+++ unittests.bat
@@ -1,3 +1,5 @@
-dotnet restore ./test/UnitTests/WebApiTests/VSS.Productivity3D.WebApi.Tests.csproj  
+"%ProgramFiles%\dotnet\dotnet.exe" restore ./test/UnitTests/WebApiTests/VSS.Productivity3D.WebApi.Tests.csproj
 cd test/UnitTests/WebApiTests
-dotnet test VSS.Productivity3D.WebApi.Tests.csproj -f net471
+
+"%ProgramFiles%\dotnet\dotnet.exe" build VSS.Productivity3D.WebApi.Tests.csproj -c Debug
+"%ProgramFiles%\dotnet\dotnet.exe" vstest bin\Debug\net471\VSS.Productivity3D.WebApiTests.dll /Platform:x64
