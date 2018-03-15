4da475470de0c97515da0ad5344ec75c83bffd3f
Jeannie May <Jeannie_May@trimble.com>
2018-03-16 09:10:45 +1300

    fix merge issue

--- unittests.sh
+++ unittests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-(cd ./test/UnitTests/MasterDataProjectTests && dotnet test -f netcoreapp2.0 )
+(cd ./test/UnitTests/MasterDataProjectTests && dotnet test VSS.Project.WebApi.Tests.csproj -f netcoreapp2.0 )
 if [ $? -ne 0 ]; then exit 1
 fi
 
