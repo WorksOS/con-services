0d4b1ddb9d4fce40db0e69c51697d6dd33c51072
Aaron Morgan <aaron_morgan@trimble.com>
2018-04-03 12:02:56 +1200

    Attempt at response cache 'cache busting' fix. #67231 - Ignored broken DateTimeExtension tests while DST fix is being worked on.

--- src/Common/Extensions/CacheKeyExtensions.cs
+++ src/Common/Extensions/CacheKeyExtensions.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.ResponseCaching.Internal;
+﻿using Microsoft.AspNetCore.ResponseCaching.Internal;
+using System;
 
 namespace VSS.Productivity3D.Common.Extensions
 {
-    public static class CacheKeyExtensions
+  public static class CacheKeyExtensions
+  {
+    public static string GetProjectCacheKey(this string projectUid)
     {
-      public static string GetProjectCacheKey(this string projectUid)
-      {
-        return $"PRJUID={projectUid.ToUpperInvariant()}";
-      }
+      return $"PRJUID={projectUid.ToUpperInvariant()}";
+    }
 
-      public static void InvalidateReponseCacheForProject(this IResponseCache cache, string projectUid)
-      {
-        cache.Set(projectUid.GetProjectCacheKey(),null,TimeSpan.FromTicks(1));
-      }
+    public static void InvalidateReponseCacheForProject(this IResponseCache cache, string projectUid)
+    {
+      InvalidateResponseCacheItem(cache, projectUid);
+    }
 
-      public static void InvalidateReponseCacheForProject(this IResponseCache cache, Guid projectUid)
-      {
-        cache.Set(projectUid.ToString().GetProjectCacheKey(), null, TimeSpan.FromTicks(1));
-      }
+    public static void InvalidateReponseCacheForProject(this IResponseCache cache, Guid projectUid)
+    {
+      InvalidateResponseCacheItem(cache, projectUid.ToString());
+    }
 
+    private static void InvalidateResponseCacheItem(IResponseCache cache, string projectUid)
+    {
+      var key = projectUid.GetProjectCacheKey();
+
+      cache.Set(key, null, TimeSpan.FromTicks(1));
+
+      // Response caching behaves as an HTTP cache.
+      // To invalidate the removed item now rather than wait for the natural cleanup we need to try to Get the item, forcing it to be invalidated.
+      cache.Get(key);
+    }
   }
 }
