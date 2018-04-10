85de8dd66ed2619e3a0352b84def233af67de0f3
Raymond Wilson <raymond_wilson@trimble.com>
2018-04-10 20:44:25 +1200

    Enable acceptance-test style unit test for SimpleVolumes against a running TRex grid

--- tests/RaptorClassLibrary.Tests.netcore/Volumes/SimpleVolumesRequestTests.cs
+++ tests/RaptorClassLibrary.Tests.netcore/Volumes/SimpleVolumesRequestTests.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using VSS.VisionLink.Raptor.Filters;
 using VSS.VisionLink.Raptor.Volumes.GridFabric.Requests;
 using VSS.VisionLink.Raptor.Volumes.GridFabric.Responses;
 using VSS.VisionLink.Raptor.Volumes.GridFabric.Arguments;
 using VSS.VisionLink.Raptor.Geometry;
+using VSS.VisionLink.Raptor.Rendering.Servers.Client;
+using VSS.VisionLink.Raptor.Types;
+using VSS.VisionLink.Raptor.Volumes;
 using Xunit;
 
 namespace VSS.VisionLink.Raptor.RaptorClassLibrary.Tests.Volumes
 {
-        public class SimpleVolumesRequestTests
+    public class SimpleVolumesRequestTests
     {
+        RaptorSimpleVolumesServer Server = RaptorSimpleVolumesServer.NewInstance();
+
         [Fact]
-        public void TesT_SimpleVolumesRequest_Creation()
+        public void Test_SimpleVolumesRequest_Creation1()
+        {
+            SimpleVolumesRequest_ApplicationService request = new SimpleVolumesRequest_ApplicationService();
+
+            Assert.NotNull(request);
+        }
+
+        [Fact]
+        public void Test_SimpleVolumesRequest_Creation2()
         {
             SimpleVolumesRequest_ClusterCompute request = new SimpleVolumesRequest_ClusterCompute();
 
@@ -18,25 +32,41 @@ namespace VSS.VisionLink.Raptor.RaptorClassLibrary.Tests.Volumes
         }
 
         [Fact]
-        public void Test_SimpleVolumesRequest_Execute()
+        public void Test_SimpleVolumesRequest_DefaultFilterToFilter_Execute()
         {
-            SimpleVolumesRequest_ClusterCompute request = new SimpleVolumesRequest_ClusterCompute();
-            SimpleVolumesRequestArgument arg = new SimpleVolumesRequestArgument();
+            SimpleVolumesRequest_ApplicationService request = new SimpleVolumesRequest_ApplicationService();
+            SimpleVolumesRequestArgument arg = new SimpleVolumesRequestArgument()
+            {
+                SiteModelID = 6,
+                VolumeType = VolumeComputationType.Between2Filters,
+                BaseFilter = new CombinedFilter()
+                {
+                    AttributeFilter = 
+                    {
+                        ReturnEarliestFilteredCellPass = true,
+                    }
+                },
+                TopFilter = new CombinedFilter(),
+                BaseDesignID = long.MinValue,
+                TopDesignID = long.MinValue,
+                CutTolerance = 0.001,
+                FillTolerance = 0.001
+            };
 
             SimpleVolumesResponse response = request.Execute(arg);
 
-            // This request will fail (Ignite not accessibel from unit tests) and will return a 
+            // This request will fail (Ignite not accessible from unit tests) and will return a 
             // response with null values
 
             Assert.NotNull(response);
-            Assert.True(!response.Cut.HasValue &&
-                        !response.Fill.HasValue &&
-                        !response.TotalCoverageArea.HasValue &&
-                        !response.CutArea.HasValue &&
-                        !response.FillArea.HasValue &&
-                        response.BoundingExtentGrid.Equals(BoundingWorldExtent3D.Null()) &&
-                        response.BoundingExtentLLH.Equals(BoundingWorldExtent3D.Null()),
-                        "Reponse is not null as expected");
+            Assert.True(response.Cut.HasValue &&
+                        response.Fill.HasValue &&
+                        response.TotalCoverageArea.HasValue &&
+                        response.CutArea.HasValue &&
+                        response.FillArea.HasValue &&
+                        !response.BoundingExtentGrid.Equals(BoundingWorldExtent3D.Null()),
+                        // No LL converesion available yet && !response.BoundingExtentLLH.Equals(BoundingWorldExtent3D.Null()),
+                        "Reponse is null, unexpected");
         }
     }
 }
