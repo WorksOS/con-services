5dd6cf3a8a2ce94338f63a4cb71a824f5f98684f
Aaron Morgan <aaron_morgan@trimble.com>
2018-04-11 15:34:36 +1200

    Reverted AWS loging to use the correct AWS configuration profile.

--- start_containers.ps1
+++ start_containers.ps1
@@ -3,7 +3,7 @@ param(
 )
 
 Write-Host "Logging in to image host" -ForegroundColor DarkGray
-Invoke-Expression -Command (aws ecr get-login --no-include-email --profile default --region us-west-2)
+Invoke-Expression -Command (aws ecr get-login --no-include-email --profile vss-grant --region us-west-2)
 
 docker-compose pull
 if ($branch -eq "Release" -or $branch -eq "master") {
