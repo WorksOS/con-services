5662f0d42a0281c8a6712b55dd7be23c38ed931e
Aaron Morgan <aaron_morgan@trimble.com>
2018-04-20 10:45:12 +1200

    Added .editorconfig, tided solution files, removed unused folders and refactored SetDockerEnvironmentVariables.ps1 script.

--- SetDockerEnvironmentVariables.ps1
+++ SetDockerEnvironmentVariables.ps1
@@ -1,42 +1,52 @@
-<#
-   To setup environment variables to allow you to debug using your local mysql container:
-   
-   1) File\Open Windows PowerShell\Open Windows PowerShell as administrator
-   2) change directory to the folder containing this file
-   3) type (or copy) this command and run in PS: Set-ExecutionPolicy RemoteSigned
-   4) type (or copy) this command and press enter in PS: .\DockerEnvironmentVariables.ps1
-   Note: if you change environment settings whilst you have the Visual Studio open.
-   	You need to resart Visual Studio
+param (
+    [string] $environment
+)
 
-#>
-<#
-[Environment]::SetEnvironmentVariable("MYSQL_DATABASE_NAME", "VSS-TagFileAuth", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_PORT", "3306", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_USERNAME", "root", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_ROOT_PASSWORD", "abc123", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_SERVER_NAME_VSPDB", "localhost", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_SERVER_NAME_ReadVSPDB", "localhost", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_URI", "localhost", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_PORT", "2181", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_GROUP_NAME", "TagFileAuth-Datafeed", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_TOPIC_NAME_NOTIFICATIONS", "VSS.Interfaces.Events.MasterData.INotificationEvent", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_TOPIC_NAME_SUFFIX", "-TFA", "Machine")
-[Environment]::SetEnvironmentVariable("WEBAPI_URI", "http://webapi:80/", "Machine")
-[Environment]::SetEnvironmentVariable("WEBAPI_DEBUG_URI", "http://localhost:3001/", "Machine")
-#>
-<#  Dev environment#>
-[Environment]::SetEnvironmentVariable("MYSQL_DATABASE_NAME", "VSS-TagFileAuth", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_PORT", "3306", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_USERNAME", "root", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_ROOT_PASSWORD", "d3vRDS1234_", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_SERVER_NAME_VSPDB", "rdsmysql-8469.c31ahitxrkg7.us-west-2.rds.amazonaws.com", "Machine")
-[Environment]::SetEnvironmentVariable("MYSQL_SERVER_NAME_ReadVSPDB", "rdsmysql-8469.c31ahitxrkg7.us-west-2.rds.amazonaws.com", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_URI", "10.97.99.172", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_PORT", "9092", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_GROUP_NAME", "TagFileAuth-Datafeed", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_TOPIC_NAME_NOTIFICATIONS", "VSS.Interfaces.Events.MasterData.INotificationEvent", "Machine")
-[Environment]::SetEnvironmentVariable("KAFKA_TOPIC_NAME_SUFFIX", "-TFA", "Machine")
-[Environment]::SetEnvironmentVariable("WEBAPI_URI", "http://10.97.96.103:3001/", "Machine")
-[Environment]::SetEnvironmentVariable("WEBAPI_DEBUG_URI", "http://10.97.96.103:3001/", "Machine")
+# Used when running the collaborating services locally.
+$localhostEnvironmentVariables = @(
+    @{key = "MYSQL_DATABASE_NAME"; value = "VSS-TagFileAuth"},
+    @{key = "MYSQL_PORT"; value = "3306"},
+    @{key = "MYSQL_USERNAME"; value = "root"},
+    @{key = "MYSQL_ROOT_PASSWORD"; value = "abc123"},
+    @{key = "MYSQL_SERVER_NAME_VSPDB"; value = "localhost"},
+    @{key = "MYSQL_SERVER_NAME_ReadVSPDB"; value = "localhost"},
+    @{key = "KAFKA_URI"; value = "localhost"},
+    @{key = "KAFKA_PORT"; value = "2181"},
+    @{key = "KAFKA_GROUP_NAME"; value = "TagFileAuth-Datafeed"},
+    @{key = "KAFKA_TOPIC_NAME_NOTIFICATIONS"; value = "VSS.Interfaces.Events.MasterData.INotificationEvent"},
+    @{key = "KAFKA_TOPIC_NAME_SUFFIX"; value = "-TFA"},
+    @{key = "WEBAPI_URI"; value = "http://webapi:80/"},
+    @{key = "WEBAPI_DEBUG_URI"; value = "http://localhost:3001/"})
 
+# Used when connecting to the remote development server services.
+$devEnvironmentVariables = @(
+    @{key = "MYSQL_DATABASE_NAME"; value = "VSS-TagFileAuth"},
+    @{key = "MYSQL_PORT"; value = "3306"},
+    @{key = "MYSQL_USERNAME"; value = "root"},
+    @{key = "MYSQL_ROOT_PASSWORD"; value = "d3vRDS1234_"},
+    @{key = "MYSQL_SERVER_NAME_VSPDB"; value = "rdsmysql-8469.c31ahitxrkg7.us-west-2.rds.amazonaws.com"},
+    @{key = "MYSQL_SERVER_NAME_ReadVSPDB"; value = "rdsmysql-8469.c31ahitxrkg7.us-west-2.rds.amazonaws.com"},
+    @{key = "KAFKA_URI"; value = "10.97.99.172"},
+    @{key = "KAFKA_PORT"; value = "9092"},
+    @{key = "KAFKA_GROUP_NAME"; value = "TagFileAuth-Datafeed"},
+    @{key = "KAFKA_TOPIC_NAME_NOTIFICATIONS"; value = "VSS.Interfaces.Events.MasterData.INotificationEvent"},
+    @{key = "KAFKA_TOPIC_NAME_SUFFIX"; value = "-TFA"},
+    @{key = "WEBAPI_URI"; value = "http://10.97.96.103:3001/"},
+    @{key = "WEBAPI_DEBUG_URI"; value = "http://10.97.96.103:3001/"})
 
+if ($environment -ieq "--localhost") {
+    $environmentVariables = $localhostEnvironmentVariables
+    Write-Host "Setting environment variables for LOCALHOST..."
+}
+else {
+    $environmentVariables = $devEnvironmentVariables
+    Write-Host "Setting environment variables for REMOTE DEV Server..."
+}
+
+foreach ($_ in $environmentVariables) {
+    $line = "  " + $_.key + ": " + $_.value
+    Write-Host $line -ForegroundColor DarkGray
+    [Environment]::SetEnvironmentVariable($_.key, $_.value, "Machine")
+}
+
+Write-Host "`nFinished" -ForegroundColor Green
