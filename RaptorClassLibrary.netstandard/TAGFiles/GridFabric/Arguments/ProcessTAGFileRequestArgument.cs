344329365f39e38d03b9c446bdef8e44af9eb6da
Raymond Wilson <raymond_wilson@trimble.com>
2018-04-24 16:26:38 +1200

    #68518: Ditto

--- RaptorClassLibrary.netstandard/TAGFiles/GridFabric/Arguments/ProcessTAGFileRequestArgument.cs
+++ RaptorClassLibrary.netstandard/TAGFiles/GridFabric/Arguments/ProcessTAGFileRequestArgument.cs
@@ -10,13 +10,14 @@ namespace VSS.VisionLink.Raptor.TAGFiles.GridFabric.Arguments
         /// ID of the project to process the TAG files into
         /// </summary>
         public long ProjectID { get; set; } = -1;
+        public Guid ProjectUID { get; set; }
 
         /// <summary>
         /// ID of the asset to process the TAG files into
         /// </summary>
         public long AssetID { get; set; } = -1;
-
-
+        public Guid AssetUID { get; set; }
+    
         /// <summary>
         /// A dictionary mapping TAG file nams to the content of each file
         /// </summary>
