da7cd9262949f5f1088aced13b906cce1f0ba43f
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2018-05-01 09:12:31 +1200

    #68812 Create a new nuget package for common Web API configuration

---
+++ WebApi/src/ServiceCollectionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace VSS.WebApi.Common
+{
+  /// <summary>
+  /// Extensions methods for setting up common Web API service configuration
+  /// </summary>
+  public static class ServiceCollectionExtensions
+  {
+    /// <summary>
+    /// Adds CORS, Swagger ... for Web API service
+    /// </summary>
+    public static IServiceCollection AddCommon<T>(this IServiceCollection services, string serviceTitle, string version="v1")
+    {
+      if (services == null)
+        throw new ArgumentNullException("services");
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("VSS", builder => builder.AllowAnyOrigin()
+          .WithHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization",
+            "X-VisionLink-CustomerUID", "X-VisionLink-UserUid", "X-Jwt-Assertion", "X-VisionLink-ClearCache", "Cache-Control")
+          .WithMethods("OPTIONS", "TRACE", "GET", "HEAD", "POST", "PUT", "DELETE")
+          .SetPreflightMaxAge(TimeSpan.FromSeconds(2520)));
+      });
+
+      //Configure swagger
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc(version, new Info { Title = serviceTitle, Version = version });
+      });
+
+      services.ConfigureSwaggerGen(options =>
+      {
+        string pathToXml;
+
+        var moduleName = typeof(T).GetTypeInfo().Assembly.ManifestModule.Name;
+        var assemblyName = moduleName.Substring(0, moduleName.LastIndexOf('.'));
+
+        if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), assemblyName + ".xml")))
+          pathToXml = Directory.GetCurrentDirectory();
+        else if (File.Exists(Path.Combine(System.AppContext.BaseDirectory, assemblyName + ".xml")))
+          pathToXml = System.AppContext.BaseDirectory;
+        else
+        {
+          var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+          pathToXml = Path.GetDirectoryName(pathToExe);
+        }
+        options.IncludeXmlComments(Path.Combine(pathToXml, assemblyName + ".xml"));
+        options.IgnoreObsoleteProperties();
+        options.DescribeAllEnumsAsStrings();
+      });
+
+      return services;
+    }
+  }
+}
---
+++ WebApi/src/WebApiBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using VSS.MasterData.Models.FIlters;
+
+namespace VSS.WebApi.Common
+{
+  /// <summary>
+  /// Extensions methods for adding common Web API middleware to the request execution pipeline
+  /// </summary>
+  public static class WebApiBuilderExtensions
+  {
+    /// <summary>
+    /// Adds exceptions trap, CORS, Swagger, MVC, ... to the application builder
+    /// </summary>
+    public static IApplicationBuilder UseCommon(this IApplicationBuilder app, string serviceTitle)
+    {
+      if (app == null)
+        throw new ArgumentNullException("app");
+
+      app.UseExceptionTrap();
+
+
+      app.UseCors("VSS");
+
+      app.UseSwagger();
+
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", serviceTitle);
+      });
+
+      //app.UseTIDAuthentication();
+      app.UseMvc();
+      return app;
+    }
+  }
+}
