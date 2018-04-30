094f3f7df315888b5067a0a15c7284d68a5d3db7
Raymond Wilson <raymond_wilson@trimble.com>
2018-04-30 15:43:06 +1200

    #68518: Use a static queue singleton rather than creating a new queue for each submitted TAG file

--- RaptorClassLibrary.netstandard/TAGFiles/Executors/SubmitTAGFileExecutor.cs
+++ RaptorClassLibrary.netstandard/TAGFiles/Executors/SubmitTAGFileExecutor.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
 using log4net;
 using VSS.TRex.TAGFiles.Classes.Queues;
-using VSS.TRex.TAGFiles.GridFabric.Requests;
 using VSS.TRex.TAGFiles.GridFabric.Responses;
-using VSS.VisionLink.Raptor.TAGFiles.GridFabric.Arguments;
-using VSS.VisionLink.Raptor.TAGFiles.GridFabric.Responses;
 
 namespace VSS.TRex.TAGFiles.Executors
 {
@@ -20,85 +15,90 @@ namespace VSS.TRex.TAGFiles.Executors
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-      private static bool ValidSubmission()
-      {
-      // Do the TFA thing here
+        /// <summary>
+        /// Local static/singleton TAG file buffer queue reference to use when adding TAG files to the queue
+        /// </summary>
+        private static TAGFileBufferQueue queue = new TAGFileBufferQueue();
 
-        // Check file already processing??? 
-        // check count of submitted tags is not to high else pause 1 sec
-        // xml file ???
-
-
-      return true;
-      }
-
-
-
-
-    /// <summary>
-    /// Receive a TAG file to be processed, validate TAG File Authorisation for the file, and add it to the 
-    /// queue to be processed.
-    /// </summary>
-    /// <param name="ProjectID">Project ID to be used as an override to any project ID that may be determined via TAG file authorization</param>
-    /// <param name="AssetID">Asset ID to be used as an override to any Asset ID that may be determined via TAG file authorization</param>
-    /// <param name="TAGFileName">Name of the physical tagfile for archiving and logging</param>
-    /// <param name="TAGFileContent">The content of the TAG file to be processed, expressed as a byte array</param>
-    /// <param name="TCCOrgID">Used by TFA service to match VL customer to TCC org when looking for project if multiple projects and/or machine ID not in tag file</param>
-    /// 
-    /// <returns></returns>
-    public static SubmitTAGFileResponse Execute(long ProjectID, long AssetID, string TAGFileName, byte [] TAGFileContent, string TCCOrgID)
+        private static bool ValidSubmission()
         {
-      // Execute TFA based business logic along with override IDs to determine final project and asset
-      // identities to be used for processing the TAG file
-      // ...
+            // Do the TFA thing here
+
+            // Check file already processing??? 
+            // check count of submitted tags is not to high else pause 1 sec
+            // xml file ???
+
+
+            return true;
+        }
+
+        /// <summary>
+        /// Receive a TAG file to be processed, validate TAG File Authorisation for the file, and add it to the 
+        /// queue to be processed.
+        /// </summary>
+        /// <param name="ProjectID">Project ID to be used as an override to any project ID that may be determined via TAG file authorization</param>
+        /// <param name="AssetID">Asset ID to be used as an override to any Asset ID that may be determined via TAG file authorization</param>
+        /// <param name="TAGFileName">Name of the physical tagfile for archiving and logging</param>
+        /// <param name="TAGFileContent">The content of the TAG file to be processed, expressed as a byte array</param>
+        /// <param name="TCCOrgID">Used by TFA service to match VL customer to TCC org when looking for project if multiple projects and/or machine ID not in tag file</param>
+        /// 
+        /// <returns></returns>
+        public static SubmitTAGFileResponse Execute(long ProjectID, long AssetID, string TAGFileName,
+            byte[] TAGFileContent, string TCCOrgID)
+        {
+            // Execute TFA based business logic along with override IDs to determine final project and asset
+            // identities to be used for processing the TAG file
+            // ...
 
             Log.Info($"#In# SubmitTAGFileResponse. Processing {TAGFileName} TAG file into project {ProjectID}, asset {AssetID}");
 
-            SubmitTAGFileResponse response = new SubmitTAGFileResponse();
-            response.FileName = TAGFileName;
-            response.Success = false;
-            response.Exception = "Unknown";
+            SubmitTAGFileResponse response = new SubmitTAGFileResponse
+            {
+                FileName = TAGFileName,
+                Success = false,
+                Exception = "Unknown"
+            };
 
             // Place the validated TAG file content and processing meta data (project ID, asset ID, etc) into
             // the TAG file processing queue cache.
             // ...
 
-          if (ValidSubmission())
-          {
- 
-              //Guid projectUID = Guid.NewGuid(); // todo convert to use GUID
-              //Guid assetUID = Guid.NewGuid(); // todo convert to use GUID
-              TAGFileBufferQueue queue = new TAGFileBufferQueue();
-              TAGFileBufferQueueKey tagKey = new TAGFileBufferQueueKey(TAGFileName, ProjectID, AssetID /*projectUID, assetUID*/);
+            if (ValidSubmission())
+            {
 
-              // todo AssetID is now GUID
+                //Guid projectUID = Guid.NewGuid(); // todo convert to use GUID
+                //Guid assetUID = Guid.NewGuid(); // todo convert to use GUID
+                TAGFileBufferQueueKey tagKey =
+                    new TAGFileBufferQueueKey(TAGFileName, ProjectID, AssetID /*projectUID, assetUID*/);
 
-              TAGFileBufferQueueItem tagItem = new TAGFileBufferQueueItem
-                                             {
-                                                 InsertUTC = DateTime.Now,
-                                                 //ProjectUID = projectUID,
-                                                 //AssetUID = Guid.NewGuid(),
-                                                 ProjectID = ProjectID,
-                                                 AssetID = AssetID,
-                                                 FileName = TAGFileName,
-                                                 Content = TAGFileContent
-                                             };
+                // todo AssetID is now GUID
 
-              if (queue.Add(tagKey, tagItem))
-              {
-                response.Success = true;
-                response.Exception = "";
-              }
-              else
-              {
-                response.Success = false;
-                response.Exception = "Failed to submit to tagfile processing queue";
-              }
-          }
+                TAGFileBufferQueueItem tagItem = new TAGFileBufferQueueItem
+                {
+                    InsertUTC = DateTime.Now,
+                    //ProjectUID = projectUID,
+                    //AssetUID = Guid.NewGuid(),
+                    ProjectID = ProjectID,
+                    AssetID = AssetID,
+                    FileName = TAGFileName,
+                    Content = TAGFileContent
+                };
 
-          Log.Info($"#Out# SubmitTAGFileResponse. Processed {TAGFileName} Result. Success:{response.Success}, Exception:{response.Exception}");
+                if (queue.Add(tagKey, tagItem))
+                {
+                    response.Success = true;
+                    response.Exception = "";
+                }
+                else
+                {
+                    response.Success = false;
+                    response.Exception = "Failed to submit to tagfile processing queue";
+                }
+            }
 
-          return response;
+            Log.Info($"#Out# SubmitTAGFileResponse. Processed {TAGFileName} Result. Success:{response.Success}, Exception:{response.Exception}");
+
+            return response;
         }
     }
 }
