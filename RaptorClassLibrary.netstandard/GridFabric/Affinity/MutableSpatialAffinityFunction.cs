bd7db967686f98abd4d260685730358aabd20b0b
Raymond Wilson <raymond_wilson@trimble.com>
2018-05-11 22:42:03 +1200

    #69032: Prevent negative partition numbers in spatial affinity key

--- RaptorClassLibrary.netstandard/GridFabric/Affinity/MutableSpatialAffinityFunction.cs
+++ RaptorClassLibrary.netstandard/GridFabric/Affinity/MutableSpatialAffinityFunction.cs
@@ -27,7 +27,7 @@ namespace VSS.VisionLink.Raptor.GridFabric.Affinity
             SubGridSpatialAffinityKey value = (SubGridSpatialAffinityKey)key;
 
             // Compute partition number as the modulo NumPartitions result against the project iD in the spatial affinity key
-            return value.ProjectID.GetHashCode() % NumPartitions;
+            return Math.Abs(value.ProjectID.GetHashCode()) % NumPartitions;
         }
     }
 }
