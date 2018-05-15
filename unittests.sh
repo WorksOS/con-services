4abfcb98b57dcec01e5f5b0cfbaf7c34c78982bd
David Glassenbury <david_glassenbury@trimble.com>
2018-05-16 10:48:01 +1200

    alter unit tests script

--- unittests.sh
+++ unittests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-(cd ./test/UnitTests/VSS.Productivity3D.Scheduler.Tests && dotnet test VSS.Productivity3D.Scheduler.Tests.csproj -f netcoreapp2.0 )
-if [[ $rc != 0 ]]; then exit $rc; fi
+#(cd ./test/UnitTests/VSS.Productivity3D.Scheduler.Tests && dotnet test VSS.Productivity3D.Scheduler.Tests.csproj -f netcoreapp2.0 )
+#if [[ $rc != 0 ]]; then exit $rc; fi
+
+set -euo pipefail
+cd ./test/UnitTests/VSS.Productivity3D.Scheduler.Test
+dotnet test VSS.Productivity3D.Scheduler.Tests.csproj -f netcoreapp2.0
