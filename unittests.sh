037681970c2fe64ca1a71daa3e2a9bb9d28b277b
David Glassenbury <david_glassenbury@trimble.com>
2018-05-16 10:52:23 +1200

    Missed a character off directory in shell script

--- unittests.sh
+++ unittests.sh
@@ -3,5 +3,5 @@
 #if [[ $rc != 0 ]]; then exit $rc; fi
 
 set -euo pipefail
-cd ./test/UnitTests/VSS.Productivity3D.Scheduler.Test
+cd ./test/UnitTests/VSS.Productivity3D.Scheduler.Tests
 dotnet test VSS.Productivity3D.Scheduler.Tests.csproj -f netcoreapp2.0
