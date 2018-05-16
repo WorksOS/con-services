728737e92564b041c41067fe63a4a55729d8080f
Raymond Wilson <raymond_wilson@trimble.com>
2018-05-16 12:40:21 +1200

    #70125: Restructure full framework solution space

---
+++ src/netstandard/RaptorClassLibrary.netstandard/TAGFiles/Classes/ValueMatcher/Compaction/CCA/TAGCCARightFrontValueMatcher.cs
@@ -0,0 +1,35 @@
+﻿using VSS.TRex.Cells;
+using VSS.TRex.TAGFiles.Types;
+
+namespace VSS.TRex.TAGFiles.Classes.ValueMatcher.Compaction.CCA
+{
+    public class TAGCCARightFrontValueMatcher : TAGValueMatcher
+  {
+    public TAGCCARightFrontValueMatcher(TAGProcessorStateBase valueSink, TAGValueMatcherState state) : base(valueSink, state)
+    {
+    }
+
+      private static readonly string[] valueTypes = { TAGValueNames.kTagFileICCCARightFrontTag };
+
+      public override string[] MatchedValueTypes() => valueTypes;
+
+    public override bool ProcessEmptyValue(TAGDictionaryItem valueType)
+    {
+      valueSink.SetICCCARightFrontValue(CellPass.NullCCA);
+
+      return true;
+    }
+
+    public override bool ProcessUnsignedIntegerValue(TAGDictionaryItem valueType, uint value)
+    {
+      if (valueType.Type != TAGDataType.t8bitUInt)
+      {
+        return false;
+      }
+
+      valueSink.SetICCCARightFrontValue((byte)value);
+
+      return true;
+    }
+  }
+}
---
+++ src/netstandard/RaptorClassLibrary.netstandard/TAGFiles/Types/MachineSide.cs
@@ -0,0 +1,16 @@
+﻿namespace VSS.TRex.TAGFiles.Types
+{
+    public static class MachineSideConst
+  {
+      public const int None = 0;
+      public const int Left = 1;
+      public const int Right = 2;
+  }
+
+  public enum MachineSide
+  {
+    None = 0,
+    Left = 1,
+    Right = 2
+  }
+}
