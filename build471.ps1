f22ae2c9ad30557c5825341b9cb4bcbb5d9d5c5c
David Glassenbury <david_glassenbury@trimble.com>
2018-05-22 11:31:20 +1200

    Update build471.ps1 to target 471 framework

--- build471.ps1
+++ build471.ps1
@@ -19,12 +19,12 @@ If (Test-path $artfifactZip) {
 
 # Restore, build/publish for configuration net47.
 Write-Host "Restoring .NET packages..." -ForegroundColor "darkgray"
-Invoke-Expression "dotnet restore --no-cache VSS.Productivity3D.Scheduler.sln"
+Invoke-Expression "dotnet restore --no-cache -r net471 VSS.Productivity3D.Scheduler.sln"
 
 Write-Host "Publishing WebApi project..." -ForegroundColor "darkgray"
 $artifactsWorkingDir = "$artifactsDir\VSS.Productivity3D.Scheduler.WebApiNet471"
 
-Invoke-Expression "dotnet publish ./src/VSS.Productivity3D.Scheduler.WebApi/VSS.Productivity3D.Scheduler.WebApi.csproj -o ../../artifacts/VSS.Productivity3D.Scheduler.WebApi -f net471 -c Release /p:Version={$versionNumber}"
+Invoke-Expression "dotnet publish ./src/VSS.Productivity3D.Scheduler.WebApi/VSS.Productivity3D.Scheduler.WebApi.csproj -o ../../artifacts/VSS.Productivity3D.Scheduler.WebApi -f net471 -c Release /p:Version=$versionNumber"
 Invoke-Expression "dotnet build ./test/UnitTests/VSS.Productivity3D.Scheduler.Tests/VSS.Productivity3D.Scheduler.Tests.csproj"
 
 # Compress build artifacts.
