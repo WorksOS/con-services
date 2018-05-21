d99058a3b1c7b6569891bff8da59c9f9bbc71916
Josh Nimmo <josh_nimmo@trimble.com>
2018-05-21 15:41:38 +1200

    update config location

--- BuildTools/InstallTools.ps1
+++ BuildTools/InstallTools.ps1
@@ -3,5 +3,5 @@ choco install kubernetes-helm
 choco install kubernetes-cli
 helm init --client-only
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
-mkdir $HOME\Documents\.kube\
-cp config $HOME\Documents\.kube\config
+mkdir $HOME\.kube\
+cp config $HOME\.kube\config
