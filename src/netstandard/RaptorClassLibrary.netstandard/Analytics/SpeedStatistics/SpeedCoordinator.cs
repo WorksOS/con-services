2cf6919cf579e3bb94efe0b2cde8a4e4f522269f
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2018-05-21 13:21:41 +1200

    #70041. Implementation. Phase II.

--- src/netstandard/RaptorClassLibrary.netstandard/Analytics/SpeedStatistics/SpeedCoordinator.cs
+++ src/netstandard/RaptorClassLibrary.netstandard/Analytics/SpeedStatistics/SpeedCoordinator.cs
@@ -1,6 +1,9 @@
-﻿using VSS.TRex.Analytics.Aggregators;
+﻿using Microsoft.Extensions.Logging;
+using System.Reflection;
+using VSS.TRex.Analytics.Aggregators;
 using VSS.TRex.Analytics.Coordinators;
 using VSS.TRex.Analytics.SpeedStatistics.GridFabric;
+using VSS.TRex.Types;
 
 namespace VSS.TRex.Analytics.SpeedStatistics
 {
@@ -10,19 +13,51 @@ namespace VSS.TRex.Analytics.SpeedStatistics
 	/// </summary>
   public class SpeedCoordinator : BaseAnalyticsCoordinator<SpeedStatisticsArgument, SpeedStatisticsResponse>
 	{
-		public override AggregatorBase ConstructAggregator(SpeedStatisticsArgument argument)
-		{
-			throw new System.NotImplementedException();
-		}
+		private static readonly ILogger Log = Logging.Logger.CreateLogger(MethodBase.GetCurrentMethod().DeclaringType.Name);
 
-		public override AnalyticsComputor ConstructComputor(SpeedStatisticsArgument argument, AggregatorBase aggregator)
+		/// <summary>
+		/// Constructs the aggregator from the supplied argument to be used for the Speed statistics analytics request
+		/// Create the aggregator to collect and reduce the results.
+		/// </summary>
+		/// <param name="argument"></param>
+		/// <returns></returns>
+		public override AggregatorBase ConstructAggregator(SpeedStatisticsArgument argument) => new SpeedAggregator
 		{
-			throw new System.NotImplementedException();
-		}
+			RequiresSerialisation = true,
+			SiteModelID = argument.DataModelID,
+			//LiftBuildSettings := LiftBuildSettings;
+			CellSize = SiteModel.Grid.CellSize,
+			TargetMinMachineSpeed = argument.TargetMinMachineSpeed,
+			TargetMaxMachineSpeed = argument.TargetMaxMachineSpeed
+		};
 
+		/// <summary>
+		/// Constructs the computer from the supplied argument and aggregator for the Speed statistics analytics request
+		/// </summary>
+		/// <param name="argument"></param>
+		/// <param name="aggregator"></param>
+		/// <returns></returns>
+		public override AnalyticsComputor ConstructComputor(SpeedStatisticsArgument argument, AggregatorBase aggregator) => new AnalyticsComputor()
+		{
+			RequestDescriptor = RequestDescriptor,
+			SiteModel = SiteModel,
+			Aggregator = aggregator,
+			Filter = argument.Filter,
+			IncludeSurveyedSurfaces = true,
+			RequestedGridDataType = GridDataType.MachineSpeed
+		};
+
+		/// <summary>
+		/// Pull the required counts information from the internal Speed aggregator state
+		/// </summary>
+		/// <param name="aggregator"></param>
+		/// <param name="response"></param>
 		public override void ReadOutResults(AggregatorBase aggregator, SpeedStatisticsResponse response)
 		{
-			throw new System.NotImplementedException();
+			response.AboveTargetCellsCount = ((SpeedAggregator)aggregator).AboveTargetCellsCount;
+			response.BelowTargetCellsCount = ((SpeedAggregator)aggregator).BelowTargetCellsCount;
+			response.MatchTargetCellsCount = ((SpeedAggregator)aggregator).MatchTargetCellsCount;
+			response.CoverageArea = ((SpeedAggregator)aggregator).CoverageArea;
 		}
 	}
 }
---
+++ src/netstandard/RaptorClassLibrary.netstandard/Analytics/SpeedStatistics/SpeedResult.cs
@@ -0,0 +1,10 @@
+﻿using VSS.TRex.Analytics.Models;
+using VSS.TRex.Analytics.SpeedStatistics.GridFabric;
+
+namespace VSS.TRex.Analytics.SpeedStatistics
+{
+  public class SpeedResult : AnalyticsResult<SpeedStatisticsResponse>
+	{
+		// ...
+  }
+}
