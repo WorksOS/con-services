bb9b28ff7ea6ad1c7304ed67ac83bdb4bcd14e51
Aaron Morgan <aaron_morgan@trimble.com>
2018-05-22 12:51:58 +1200

    Added artifact filename as a parameter to the build script.

--- build471.ps1
+++ build471.ps1
@@ -1,7 +1,7 @@
-param ([switch] $uploadArtifact = $false)
+param ([switch] $uploadArtifact = $false,
+       [string] $artifactFilename = "Filter_WebApi.zip")
 
 $artifactsDir = "$PSScriptRoot/artifacts"
-$artfifactZip = "FilterWebApiNet47.zip"
 
 # Tidy up old artifacts.
 Write-Host "Removing existing build artifacts..." -ForegroundColor "darkgray"
@@ -10,8 +10,8 @@ if (Test-Path -path $artifactsDir) {
     Remove-Item -Force -Recurse -Path $artifactsDir 
 }
 
-If (Test-path $artfifactZip) {
-    Remove-item $artfifactZip
+If (Test-path $artifactFilename) {
+    Remove-item $artifactFilename
 }
 
 # Restore, build/publish for configuration net471.
@@ -36,10 +36,10 @@ Rename-Item -path "$artifactsWorkingDir\Dockerfile_win" -newName "Dockerfile"
 Write-Host "Compressing build artifacts..." -ForegroundColor "darkgray"
 
 Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::CreateFromDirectory($artifactsDir, "$PSScriptRoot/$artfifactZip") 
+[io.compression.zipfile]::CreateFromDirectory($artifactsDir, "$PSScriptRoot/$artifactFilename") 
 
 # Upload build artifacts to S3.
 if ($uploadArtifact) {
     Write-Host "Uploading build artifacts to S3..." -ForegroundColor "darkgray"
-    Invoke-Expression "aws s3 cp $artfifactZip s3://vss-merino/Productivity3D/Releases/$artfifactZip --acl public-read --profile vss-grant"
+    Invoke-Expression "aws s3 cp $artifactFilename s3://vss-merino/Productivity3D/Releases/$artifactFilename --acl public-read --profile vss-grant"
 }
