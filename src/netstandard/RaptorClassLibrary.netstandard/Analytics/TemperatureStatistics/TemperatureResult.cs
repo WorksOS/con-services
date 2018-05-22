4fde85c7270cd5f0fd828da5974198cf5b069526
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2018-05-22 15:57:39 +1200

    #7044. Implementation. Phase III.

---
+++ src/netstandard/RaptorClassLibrary.netstandard/Analytics/TemperatureStatistics/TemperatureResult.cs
@@ -0,0 +1,10 @@
+﻿using VSS.TRex.Analytics.Models;
+using VSS.TRex.Analytics.TemperatureStatistics.GridFabric;
+
+namespace VSS.TRex.Analytics.TemperatureStatistics
+{
+  public class TemperatureResult : AnalyticsResult<TemperatureStatisticsResponse>
+	{
+		// ...
+  }
+}
