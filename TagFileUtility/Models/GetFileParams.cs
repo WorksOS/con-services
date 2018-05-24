7a4bf856f7bef650d1d7ce60599315a93871371b
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2018-05-24 12:41:09 +1200

    #68741 Tool for undoing checked out tag files

---
+++ TagFileUtility/Models/GetFileParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TagFileHarvester.Models
+{
+  public class GetFileParams
+  {
+    [JsonProperty(PropertyName = "filespaceid", Required = Required.Always)]
+    public string filespaceid;
+    [JsonProperty(PropertyName = "path", Required = Required.Always)]
+    public string path;
+  }
+}
---
+++ TagFileUtility/Models/Organization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagFileHarvester.Models
+{
+  public class Organization : IEquatable<Organization>
+  {
+    public string filespaceId = String.Empty;
+    public string shortName = String.Empty;
+    public string orgId = String.Empty;
+    public string orgDisplayName = String.Empty;
+    public string orgTitle = String.Empty;
+
+    public bool Equals(Organization other)
+    {
+      return filespaceId == other.filespaceId;
+    }
+  }
+}
