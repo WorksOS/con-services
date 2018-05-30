ce449a221d02932070cc7df21fde4a6b6ce2a981
Jeannie May <Jeannie_May@trimble.com>
2018-05-30 13:18:16 +1200

    #67424 UpdateProject Boundary - add GeofenceService mocks and upgrade solutions

--- MasterDataProxies/src/Interfaces/IGeofenceProxy.cs
+++ MasterDataProxies/src/Interfaces/IGeofenceProxy.cs
@@ -5,18 +5,24 @@ using VSS.MasterData.Models.Models;
 
 namespace VSS.MasterData.Proxies.Interfaces
 {
-    public interface IGeofenceProxy :ICacheProxy
-    {
-      Task<string> GetGeofenceBoundary(string customerUid, string geofenceUid, IDictionary<string, string> customHeaders = null);
+  public interface IGeofenceProxy : ICacheProxy
+  {
+    Task<string> GetGeofenceBoundary(string customerUid, string geofenceUid,
+      IDictionary<string, string> customHeaders = null);
 
-      Task<Guid> CreateGeofence(Guid customerGuid, string geofenceName, string description, string geofenceType,
-          string geometryWKT, int fillColor, bool isTransparent, Guid userUid, double areaSqMeters,
-          IDictionary<string, string> customHeaders = null);
+    Task<Guid> CreateGeofence(Guid customerGuid, string geofenceName, string description, string geofenceType,
+      string geometryWKT, int fillColor, bool isTransparent, Guid userUid, double areaSqMeters,
+      IDictionary<string, string> customHeaders = null);
 
-      Task<List<GeofenceData>> GetGeofences(string customerUid, IDictionary<string, string> customHeaders = null);
+    Task<Guid> UpdateGeofence(Guid geofenceGuid, Guid customerGuid, string geofenceName, string description,
+      string geofenceType,
+      string geometryWKT, int fillColor, bool isTransparent, Guid userUid, double areaSqMeters,
+      IDictionary<string, string> customHeaders = null);
 
-      Task<GeofenceData> GetGeofenceForCustomer(string customerUid, string geofenceUid,
-        IDictionary<string, string> customHeaders = null);
+    Task<List<GeofenceData>> GetGeofences(string customerUid, IDictionary<string, string> customHeaders = null);
 
-    }
+    Task<GeofenceData> GetGeofenceForCustomer(string customerUid, string geofenceUid,
+      IDictionary<string, string> customHeaders = null);
+
+  }
 }
