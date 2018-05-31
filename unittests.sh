fd956ccaf1ef408fd53f561b3d92ece7edc026b7
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-05-31 14:21:37 +1200

    Upload test results

--- unittests.sh
+++ unittests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(cd ./test/UnitTests/MasterDataProjectTests && dotnet test VSS.Project.WebApi.Tests.csproj --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\"  )
+(cd ./test/UnitTests/MasterDataProjectTests && dotnet test VSS.Project.WebApi.Tests.csproj --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\" && cp -R TestResults /build  )
 
 if [ $? -ne 0 ]; then exit 1
 fi
