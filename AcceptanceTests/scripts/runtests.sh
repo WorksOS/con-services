fc3c7bfba12a124aac86b49c172fcbc4a11b452c
David Glassenbury <david_glassenbury@trimble.com>
2018-05-31 16:11:49 +1200

    Change runtest.sh for jenkins build

--- AcceptanceTests/scripts/runtests.sh
+++ AcceptanceTests/scripts/runtests.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
-echo "Accept tests are starting .... "
-echo "Wait for 60 seconds"
-sleep 60s
+echo "Project AcceptanceTests are starting .... "
+echo "Wait for 20 seconds"
+sleep 20s
 echo "Check the database and kafka to see if port is available"
 # Polling the database and kafka status before test
-/bin/bash wait-for-it.sh localhost:3306 -t 0
-/bin/bash wait-for-it.sh localhost:9092 -t 0
-echo "Wait for 120 seconds"
-sleep 120s
-# Run the component tests
-echo "Run the component tests"
+/bin/sh wait-for-it.sh localhost:3306 -t 0
+/bin/sh wait-for-it.sh localhost:9092 -t 0
+echo "Wait for 20 seconds"
+sleep 20s
 
-echo "WebApiTests starting...."
-dotnet TestRun/TestRun.dll results=/testresults/WebApiTestsResults project=WebApiTests
-echo "WebApiTests finished"
-
-echo "Run Integration tests starting...."
-dotnet TestRun/TestRun.dll results=/testresults/IntegrationTestResults project=IntegrationTests
-echo "Integration tests finished"
-
-echo "Run Executor tests starting...."
-dotnet TestRun/TestRun.dll results=/testresults/ExecutorTestResults project=ExecutorTests
-echo "Executor tests finished"
-echo " "
+echo "Run the AcceptanceTests solution"
+(cd ./AcceptanceTests && dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
 echo " "
 echo " All acceptance tests completed"
 echo " "
