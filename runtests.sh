6e2ab12095fe84f241986305df7cb8e24f4b059c
David Glassenbury <david_glassenbury@trimble.com>
2018-06-01 10:26:12 +1200

    List out files for ac tests

--- runtests.sh
+++ runtests.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 echo "Project AcceptanceTests are starting .... "
+echo "List out files in current directory"
+ls
+
 echo "Wait for 20 seconds"
 sleep 20s
 echo "Check the database and kafka to see if port is available"
 # Polling the database and kafka status before test
-/bin/bash wait-for-it.sh localhost:3306 -t 0
-/bin/bash wait-for-it.sh localhost:9092 -t 0
-echo "Wait for 20 seconds"
-sleep 20s
+#/bin/bash wait-for-it.sh localhost:3306 -t 0
+#/bin/bash wait-for-it.sh localhost:9092 -t 0
+#echo "Wait for 20 seconds"
+#sleep 20s
 
 echo "Run the AcceptanceTests solution"
-(cd /build/AcceptanceTests && dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
+(cd /AcceptanceTests && dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
 echo " "
 echo " All acceptance tests completed"
 echo " "
