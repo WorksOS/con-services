9aaf3cd949e6729a39dcdc3c18afc6c81b4a6800
David Glassenbury <david_glassenbury@trimble.com>
2018-05-31 16:50:00 +1200

    Change runtests to use vstest

--- AcceptanceTests/scripts/runtests.sh
+++ AcceptanceTests/scripts/runtests.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 echo "Project AcceptanceTests are starting .... "
-echo "Wait for 20 seconds"
-sleep 20s
+echo "Wait for 5 seconds"
+sleep 5s
 echo "Check the database and kafka to see if port is available"
 # Polling the database and kafka status before test
 /bin/bash wait-for-it.sh localhost:3306 -t 0
 /bin/bash wait-for-it.sh localhost:9092 -t 0
-echo "Wait for 20 seconds"
-sleep 20s
+sleep 5s
 
-echo "Run the AcceptanceTests solution"
-(cd /build/AcceptanceTests && dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
+
+
+cd /app
+dotnet vstest ExecutorTests\ExecutorTests.dll --logger:\"xunit;LogFilePath=/TestResults/ExecutorTestsTestResults.xml\"
+
+dotnet vstest IntegrationTests\IntegrationTests.dll --logger:\"xunit;LogFilePath=/TestResults/IntegrationTestsResults.xml\"
+
+dotnet vstest WebApiTests\WebApiTests.dll --logger:\"xunit;LogFilePath=/TestResults/WebApiTestsTestResults.xml\"
 echo " "
 echo " All acceptance tests completed"
 echo " "
