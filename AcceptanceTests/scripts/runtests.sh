c218aed14e33d0dd42baa27c16448e1957e36097
David Glassenbury <david_glassenbury@trimble.com>
2018-05-31 16:26:04 +1200

    Update folder for runtest.sh

--- AcceptanceTests/scripts/runtests.sh
+++ AcceptanceTests/scripts/runtests.sh
@@ -4,13 +4,13 @@ echo "Wait for 20 seconds"
 sleep 20s
 echo "Check the database and kafka to see if port is available"
 # Polling the database and kafka status before test
-/bin/sh wait-for-it.sh localhost:3306 -t 0
-/bin/sh wait-for-it.sh localhost:9092 -t 0
+/bin/bash wait-for-it.sh localhost:3306 -t 0
+/bin/bash wait-for-it.sh localhost:9092 -t 0
 echo "Wait for 20 seconds"
 sleep 20s
 
 echo "Run the AcceptanceTests solution"
-(cd ./AcceptanceTests && dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
+(cd /build/AcceptanceTests && dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
 echo " "
 echo " All acceptance tests completed"
 echo " "
