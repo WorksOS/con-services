6c49391748fa5734351a5fac650d8cb6dedc9b2b
Aaron Morgan <aaron_morgan@trimble.com>
2018-06-04 22:06:59 +0000

    Merged PR 9584: Separate AWS S3 file transfer proxy out of Proxies package and into it's own package
    * Separated AWS S3 file transfer proxy out of Proxies package and into the new AWS.TransferProxy package.
    * Added unit tests for the TransferProxy ctor.
    
    The goal of this sustaining activity is to reduce the dependencies Proxies package requires and move those seldom used AWS requirements into a separate package (used by 3DP and Scheduler).
    
    Related work items: #70930

---
+++ AWS.TransferProxy/src/TransferProxy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using Microsoft.AspNetCore.Mvc;
+using VSS.AWS.TransferProxy.Interfaces;
+using VSS.ConfigurationStore;
+
+namespace VSS.AWS.TransferProxy
+{
+  public class TransferProxy : ITransferProxy
+  {
+    private readonly string awsAccessKey;
+    private readonly string awsSecretKey;
+    private readonly string awsBucketName;
+    private readonly TimeSpan awsLinkExpiry;
+
+    private const int MAXIMUM_EXPIRY_DAYS_FOR_PRESIGNED_URL = 7;
+
+    public TransferProxy(IConfigurationStore configStore)
+    {
+      awsAccessKey = configStore.GetValueString("AWS_ACCESS_KEY");
+      awsSecretKey = configStore.GetValueString("AWS_SECRET_KEY");
+      awsBucketName = configStore.GetValueString("AWS_BUCKET_NAME");
+
+      awsLinkExpiry = configStore.GetValueTimeSpan("AWS_PRESIGNED_URL_EXPIRY") ?? TimeSpan.FromDays(MAXIMUM_EXPIRY_DAYS_FOR_PRESIGNED_URL);
+
+      if (string.IsNullOrEmpty(awsAccessKey) ||
+          string.IsNullOrEmpty(awsSecretKey) ||
+          string.IsNullOrEmpty(awsBucketName))
+      {
+        throw new Exception("Missing environment variable AWS_ACCESS_KEY, AWS_SECRET_KEY or AWS_BUCKET_NAME");
+      }
+    }
+
+    public async Task<FileStreamResult> Download(string s3Key)
+    {
+      using (var transferUtil = new TransferUtility(awsAccessKey, awsSecretKey, RegionEndpoint.USWest2))
+      {
+        var stream = await transferUtil.OpenStreamAsync(awsBucketName, s3Key);
+        return new FileStreamResult(stream, "application/octet-stream");
+      }
+    }
+
+    public void Upload(Stream stream, string s3Key)
+    {
+      using (var transferUtil = new TransferUtility(awsAccessKey, awsSecretKey, RegionEndpoint.USWest2))
+      {
+        transferUtil.Upload(stream, awsBucketName, s3Key);
+      }
+    }
+
+    public string GeneratePreSignedUrl(string s3Key)
+    {
+      var request = new GetPreSignedUrlRequest
+      {
+        BucketName = awsBucketName,
+        Key = s3Key,
+        Expires = DateTime.Now.Add(awsLinkExpiry)
+      };
+
+      using (IAmazonS3 s3Client = new AmazonS3Client(awsAccessKey, awsSecretKey, RegionEndpoint.USWest2))
+      {
+        return s3Client.GetPreSignedURL(request);
+      }
+    }
+  }
+}
---
+++ AWS.TransferProxy/test/UnitTests/TransferProxyTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using VSS.ConfigurationStore;
+
+namespace VSS.AWS.TransferProxy.UnitTests
+{
+  [TestClass]
+  public class TransferProxyTests
+  {
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    [DataRow(null)]
+    [DataRow("")]
+    public void Should_throw_When_AWS_ACCESS_KEY_isnt_present(string value)
+    {
+      var mockStore = new Mock<IConfigurationStore>();
+      mockStore.Setup(x => x.GetValueString("AWS_ACCESS_KEY")).Returns(value);
+
+      _ = new TransferProxy(mockStore.Object);
+    }
+    
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    [DataRow(null)]
+    [DataRow("")]
+    public void Should_throw_When_AWS_SECRET_KEY_isnt_present(string value)
+    {
+      var mockStore = new Mock<IConfigurationStore>();
+
+      mockStore.Setup(x => x.GetValueString("AWS_ACCESS_KEY")).Returns("AWS_ACCESS_KEY");
+      mockStore.Setup(x => x.GetValueString("AWS_SECRET_KEY")).Returns(value);
+
+      _ = new TransferProxy(mockStore.Object);
+    }    
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    [DataRow(null)]
+    [DataRow("")]
+    public void Should_throw_When_AWS_BUCKET_NAME_isnt_present(string value)
+    {
+      var mockStore = new Mock<IConfigurationStore>();
+
+      mockStore.Setup(x => x.GetValueString("AWS_ACCESS_KEY")).Returns("AWS_ACCESS_KEY");
+      mockStore.Setup(x => x.GetValueString("AWS_SECRET_KEY")).Returns("AWS_SECRET_KEY");
+      mockStore.Setup(x => x.GetValueString("AWS_BUCKET_NAME")).Returns(value);
+
+      _ = new TransferProxy(mockStore.Object);
+    }  
+
+    [TestMethod]
+    [DataRow(null, 7)]
+    [DataRow("", 7)]
+    [DataRow("0", 0)]
+    [DataRow("4", 4)]
+    public void Should_not_throw_When_reading_AWS_PRESIGNED_URL_EXPIRY(string value, int expectedResult)
+    {
+      var result = TimeSpan.FromDays(expectedResult);
+      var mockStore = new Mock<IConfigurationStore>();
+
+      mockStore.Setup(x => x.GetValueString("AWS_ACCESS_KEY")).Returns("AWS_ACCESS_KEY");
+      mockStore.Setup(x => x.GetValueString("AWS_SECRET_KEY")).Returns("AWS_SECRET_KEY");
+      mockStore.Setup(x => x.GetValueString("AWS_BUCKET_NAME")).Returns("AWS_BUCKET_NAME");
+      mockStore.Setup(x => x.GetValueTimeSpan("AWS_PRESIGNED_URL_EXPIRY")).Returns(result);
+
+      _ = new TransferProxy(mockStore.Object);
+    }
+  }
+}
