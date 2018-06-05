ae132cace6766119a935fc13835575ed493e1d1d
David Glassenbury <david_glassenbury@trimble.com>
2018-06-05 16:30:08 +1200

    Update acceptance tests scripts

--- AcceptanceTests/scripts/runtests.sh
+++ AcceptanceTests/scripts/runtests.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 echo "Project AcceptanceTests are starting .... "
-echo "Wait for 5 seconds"
-sleep 5s
+echo "Wait for 55 seconds"
+sleep 55s
 echo "Check the database and kafka to see if port is available"
 # Polling the database and kafka status before test
-/bin/bash wait-for-it.sh localhost:3306 -t 0
-/bin/bash wait-for-it.sh localhost:9092 -t 0
-sleep 5s
+/bin/bash wait-for-it.sh db:3306 -t 0
+/bin/bash wait-for-it.sh kafka:9092 -t 0
+sleep 55s
 
 
 
 cd /app
-dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/ExecutorTestsTestResults.xml\"
-#dotnet vstest ExecutorTests/ExecutorTests.dll --logger:\"xunit;LogFilePath=/TestResults/ExecutorTestsTestResults.xml\"
+#dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/ExecutorTestsTestResults.xml\"
+dotnet vstest ExecutorTests/ExecutorTests.dll --logger:\"xunit;LogFilePath=/TestResults/ExecutorTestsTestResults.xml\"
 cd ..
-#dotnet vstest IntegrationTests/IntegrationTests.dll --logger:\"xunit;LogFilePath=/TestResults/IntegrationTestsResults.xml\"
+dotnet vstest IntegrationTests/IntegrationTests.dll --logger:\"xunit;LogFilePath=/TestResults/IntegrationTestsResults.xml\"
 
-#dotnet vstest WebApiTests/WebApiTests.dll --logger:\"xunit;LogFilePath=/TestResults/WebApiTestsTestResults.xml\"
+dotnet vstest WebApiTests/WebApiTests.dll --logger:\"xunit;LogFilePath=/TestResults/WebApiTestsTestResults.xml\"
 echo " "
 echo " All acceptance tests completed"
 echo " "
