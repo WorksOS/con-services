798818d36ea852a7611774cfd1367c192ced71c5
David Glassenbury <david_glassenbury@trimble.com>
2018-06-05 12:49:07 +1200

    Remove login for vss-jenkinsslave:core21

--- BuildTools/jenkins-slave-dotnetcore/buildImage.ps1
+++ BuildTools/jenkins-slave-dotnetcore/buildImage.ps1
@@ -1,3 +1,2 @@
-Invoke-Expression -Command (aws ecr get-login --no-include-email --profile vss-grant --region us-west-2)
 docker build -t registry.k8s.vspengg.com:80/vss-jenkinsslave:core21 ./
 docker push registry.k8s.vspengg.com:80/vss-jenkinsslave:core21
