fefc8fc89e36f2f48c50eb5b88cc7c06d6465168
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2018-06-07 09:06:53 +1200

    Add some unit tests

---
+++ tests/netstandard/RaptorClassLibrary.Tests.netcore/MDPPaletteTests.cs
@@ -0,0 +1,25 @@
+﻿using VSS.TRex.Rendering.Palettes;
+using Xunit;
+
+namespace RaptorClassLibrary.Tests.netcore
+{
+  public class MDPPaletteTests
+  {
+    [Fact]
+    public void CanCreateMDPPalette()
+    {
+      var palette = new MDPPalette();
+      Assert.NotNull(palette);
+      Assert.NotNull(palette.PaletteTransitions);
+    }
+
+    [Fact]
+    public void CanChooseMDPColor()
+    {
+      var palette = new MDPPalette();
+      var transitions = palette.PaletteTransitions;
+      var color = palette.ChooseColour(transitions[0].Value+1);
+      Assert.Equal(transitions[0].Color, color);
+    }
+  }
+}
