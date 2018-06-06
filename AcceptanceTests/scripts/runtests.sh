6a61337ef0fa418fc1ff98efe149f6f2e210bac2
David Glassenbury <david_glassenbury@trimble.com>
2018-06-06 16:58:55 +1200

    Update scripts and dockerfiles

--- AcceptanceTests/scripts/runtests.sh
+++ AcceptanceTests/scripts/runtests.sh
@@ -11,11 +11,8 @@ sleep 55s
 
 
 cd /app
-#dotnet test VSS.Visionlink.Project.AcceptanceTests.sln --logger:\"xunit;LogFilePath=/TestResults/ExecutorTestsTestResults.xml\"
 dotnet vstest ExecutorTests/ExecutorTests.dll --logger:\"xunit;LogFilePath=/TestResults/ExecutorTestsTestResults.xml\"
-cd ..
 dotnet vstest IntegrationTests/IntegrationTests.dll --logger:\"xunit;LogFilePath=/TestResults/IntegrationTestsResults.xml\"
-
 dotnet vstest WebApiTests/WebApiTests.dll --logger:\"xunit;LogFilePath=/TestResults/WebApiTestsTestResults.xml\"
 echo " "
 echo " All acceptance tests completed"
