6d0b27de0d993dc4ad46844cf3d37d15ce7fa159
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2018-06-08 13:26:20 +1200

    #70759. Added some client sub-grid tests.

---
+++ tests/netstandard/RaptorClassLibrary.Tests.netcore/SubGridTrees/Client/CMVClientLeafSubGridTests.cs
@@ -0,0 +1,29 @@
+﻿using VSS.TRex.SubGridTrees.Client;
+using VSS.TRex.SubGridTrees.Types;
+using VSS.TRex.SubGridTrees.Utilities;
+using VSS.TRex.Tests.netcore.TestFixtures;
+using VSS.TRex.Types;
+using Xunit;
+
+namespace VSS.TRex.Tests.SubGridTrees.Client
+{
+  public class CMVClientLeafSubGridTests : IClassFixture<DILoggingFixture>
+  {
+    [Fact]
+    public void Test_CMVClientLeafSubgridGridTests_Creation()
+    {
+      var clientGrid = ClientLeafSubgridFactoryFactory.Factory().GetSubGrid(GridDataType.CCV) as ClientCMVLeafSubGrid;
+      Assert.NotNull(clientGrid);
+    }
+
+    [Fact]
+    public void Test_NullCells()
+    {
+      var cell = new SubGridCellPassDataCMVEntryRecord();
+      cell.Clear();
+
+      var clientGrid = ClientLeafSubgridFactoryFactory.Factory().GetSubGrid(GridDataType.CCV) as ClientCMVLeafSubGrid;
+      SubGridUtilities.SubGridDimensionalIterator((x, y) => Assert.True(clientGrid.Cells[x, y].Equals(cell)));
+    }
+  }
+}
---
+++ tests/netstandard/RaptorClassLibrary.Tests.netcore/SubGridTrees/Client/MachineTargetSpeedClientLeafSubGridTests.cs
@@ -0,0 +1,28 @@
+﻿using VSS.TRex.SubGridTrees.Client;
+using VSS.TRex.SubGridTrees.Utilities;
+using VSS.TRex.Tests.netcore.TestFixtures;
+using VSS.TRex.Types;
+using Xunit;
+
+namespace VSS.TRex.Tests.SubGridTrees.Client
+{
+  public class MachineTargetSpeedClientLeafSubGridTests : IClassFixture<DILoggingFixture>
+  {
+    [Fact]
+    public void Test_MachineTargetSpeedClientLeafSubgridGridTests_Creation()
+    {
+      var clientGrid = ClientLeafSubgridFactoryFactory.Factory().GetSubGrid(GridDataType.MachineSpeedTarget) as ClientMachineTargetSpeedLeafSubGrid;
+      Assert.NotNull(clientGrid);
+    }
+
+    [Fact]
+    public void Test_NullCells()
+    {
+      var cell = new MachineSpeedExtendedRecord();
+      cell.Clear();
+
+      var clientGrid = ClientLeafSubgridFactoryFactory.Factory().GetSubGrid(GridDataType.MachineSpeedTarget) as ClientMachineTargetSpeedLeafSubGrid;
+      SubGridUtilities.SubGridDimensionalIterator((x, y) => Assert.True(clientGrid.Cells[x, y].Equals(cell)));
+    }
+  }
+}
