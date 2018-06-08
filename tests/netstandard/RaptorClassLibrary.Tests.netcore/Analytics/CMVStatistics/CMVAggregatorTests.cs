11032f9f541113a64c7c5069beb2a1e485c66187
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2018-06-08 12:44:31 +1200

    #70759. Added unit tests.

---
+++ tests/netstandard/RaptorClassLibrary.Tests.netcore/Analytics/CMVStatistics/CMVAggregatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RaptorClassLibrary.Tests.netcore.Analytics.Common;
+using VSS.TRex.Analytics.CMVStatistics;
+using VSS.TRex.Cells;
+using Xunit;
+
+namespace VSS.TRex.Tests.Analytics.CMVStatistics
+{
+  public class CMVAggregatorTests : BaseTests
+  {
+    [Fact]
+    public void Test_TemperatureAggregator_Creation()
+    {
+      var aggregator = new CMVAggregator();
+
+      Assert.True(aggregator.SiteModelID == Guid.Empty, "Invalid initial value for SiteModelID.");
+      Assert.True(aggregator.CellSize < TOLERANCE, "Invalid initial value for CellSize.");
+      Assert.True(aggregator.SummaryCellsScanned == 0, "Invalid initial value for SummaryCellsScanned.");
+      Assert.True(aggregator.CellsScannedOverTarget == 0, "Invalid initial value for CellsScannedOverTarget.");
+      Assert.True(aggregator.CellsScannedAtTarget == 0, "Invalid initial value for CellsScannedAtTarget.");
+      Assert.True(aggregator.CellsScannedUnderTarget == 0, "Invalid initial value for CellsScannedUnderTarget.");
+      Assert.True(aggregator.IsTargetValueConstant, "Invalid initial value for IsTargetValueConstant.");
+      Assert.True(!aggregator.MissingTargetValue, "Invalid initial value for MissingTargetValue.");
+      Assert.True(!aggregator.OverrideMachineCMV, "Invalid initial value for OverrideTemperatureWarningLevels.");
+      Assert.True(aggregator.OverridingMachineCMV == CellPass.NullCCV, "Invalid initial value for OverridingMachineCMV.");
+      Assert.True(aggregator.LastTargetCMV == CellPass.NullCCV, "Invalid initial value for LastTargetCMV.");
+    }
+  }
+}
