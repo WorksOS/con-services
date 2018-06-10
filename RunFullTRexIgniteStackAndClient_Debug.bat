11c2487cda240c8b21a6a6a30cbed1fbd3f62d23
Raymond Wilson <raymond_wilson@trimble.com>
2018-06-11 11:05:14 +1200

    Change folder of tiling service to be consistent. Make root namespace VSS.TRex for more projects.

--- RunFullTRexIgniteStackAndClient_Debug.bat
+++ RunFullTRexIgniteStackAndClient_Debug.bat
@@ -2,7 +2,7 @@ start ..\VSS.TRex\src\framework\TRexImmutableDataServer\bin\debug\TRexImmutableD
 timeout 7
 start ..\VSS.TRex\src\framework\TRexMutableDataServer\bin\debug\TRexMutableDataServer.exe SpatialDivision=0
 timeout 7
-start ..\VSS.TRex\src\framework\TRexServerApplication\bin\debug\TRexTileServer.exe
+start ..\VSS.TRex\src\framework\TRexTileServer\bin\debug\TRexTileServer.exe
 timeout 7
 start ..\VSS.TRex\src\framework\TRexDesignElevationsServer\bin\debug\TRexDesignElevationsServer.exe
 timeout 7
@@ -11,4 +11,4 @@ timeout 7
 start ..\VSS.TRex\src\framework\TRexGridActivator\bin\debug\TRexGridActivator.exe
 rem dotnet C:\Dev\VSS.TRex\src\tools\VSS.TRex.Service.Deployer\bin\Debug\netcoreapp2.0\VSS.TRex.Service.Deployer.dll
 
- 
+	
--- src/framework/TRexMutableDataServer/Properties/Settings.Designer.cs
+++ src/framework/TRexMutableDataServer/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TRexMutableDataServer.Properties {
+namespace VSS.TRex.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
