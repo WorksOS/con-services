eeb7e79657b191ce0a4066aadf24d1805ae85e6b
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2018-06-11 09:56:05 +1200

    #71269 Remove unused validation constants so not duplicated in 3dpm

--- MasterDataModels/src/Utilities/ValidationConstants.cs
+++ MasterDataModels/src/Utilities/ValidationConstants.cs
@@ -6,36 +6,6 @@
   public class ValidationConstants
   {
     /// <summary>
-    /// Minimum station value.
-    /// </summary>
-    public const double MIN_STATION = -10000;
-
-    /// <summary>
-    /// Maximum station value.
-    /// </summary>
-    public const double MAX_STATION = 1000000;
-
-    /// <summary>
-    /// Minimum offset value.
-    /// </summary>
-    public const double MIN_OFFSET = -500;
-
-    /// <summary>
-    /// Maximum offset value.
-    /// </summary>
-    public const double MAX_OFFSET = 500;
-
-    /// <summary>
-    /// Minimum elevation value.
-    /// </summary>
-    public const double MIN_ELEVATION = -10000;
-
-    /// <summary>
-    /// Maximum elevation value.
-    /// </summary>
-    public const double MAX_ELEVATION = 10000;
-
-    /// <summary>
     /// Minimum production data layer number.
     /// </summary>
     public const int MIN_LAYER_NUMBER = -1000;
@@ -44,26 +14,6 @@
     /// Maximum production data layer number.
     /// </summary>
     public const int MAX_LAYER_NUMBER = 1000;
-
-    /// <summary>
-    /// Maximum production data thickness.
-    /// </summary>
-    public const double MAX_THICKNESS = 100;
-
-    /// <summary>
-    /// Minimum production data thickness.
-    /// </summary>
-    public const double MIN_THICKNESS = 0.005;
-
-    /// <summary>
-    /// Minimum production data no change tolerance for volumes.
-    /// </summary>
-    public const double MIN_NO_CHANGE_TOLERANCE = 0.0;
-
-    /// <summary>
-    /// Maximum production data no change tolerance for volumes.
-    /// </summary>
-    public const double MAX_NO_CHANGE_TOLERANCE = 0.1;
 
     /// <summary>
     /// Minimum temperature range in °C for filter
