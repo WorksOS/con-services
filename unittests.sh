a986bb1b8b1be5281683cd94eeccb42da2e02ca5
Josh Nimmo <josh_nimmo@trimble.com>
2018-06-13 11:58:53 +1200

    missing escape

--- unittests.sh
+++ unittests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 (cd ./test/UnitTests/MasterDataProjectTests && dotnet test VSS.Project.WebApi.Tests.csproj  --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\" \
- /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutputDirectory=/TestCoverage/
+ /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutputDirectory=/TestCoverage/ \
  && cp -R TestResults /build  )
 
 if [ $? -ne 0 ]; then exit 1
