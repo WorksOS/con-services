868ada6dac53cf3e402da4acc97e4c36d471096c
Josh Nimmo <josh_nimmo@trimble.com>
2018-06-13 12:05:39 +1200

    reorder args

--- unittests.sh
+++ unittests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-(cd ./test/UnitTests/MasterDataProjectTests && dotnet test VSS.Project.WebApi.Tests.csproj  --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\" \
- /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutputDirectory=/TestCoverage/ \
- && cp -R TestResults /build  )
+(cd ./test/UnitTests/MasterDataProjectTests && dotnet test VSS.Project.WebApi.Tests.csproj \
+  /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutputDirectory=/TestCoverage/ --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\" \
+  && cp -R TestResults /build  )
 
 if [ $? -ne 0 ]; then exit 1
 fi
