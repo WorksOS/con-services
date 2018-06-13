dc6cd98d3e82ee6ed411082583a8b4ab661a52f1
Josh Nimmo <josh_nimmo@trimble.com>
2018-06-13 12:24:40 +1200

    simplify

--- unittests.sh
+++ unittests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 (cd ./test/UnitTests/MasterDataProjectTests && dotnet test VSS.Project.WebApi.Tests.csproj \
-  /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutputDirectory=/TestResults/TestCoverage --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\" \
-  && cp -R TestResults /build  )
+  /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutputDirectory=/TestResults/TestCoverage --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
 
 if [ $? -ne 0 ]; then exit 1
 fi
