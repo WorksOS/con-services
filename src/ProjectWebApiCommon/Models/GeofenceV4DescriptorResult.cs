3e27877a6e457da2d9e51fad2e4e47427f08b2ef
Jeannie May <Jeannie_May@trimble.com>
2018-06-18 11:59:09 +1200

    #69585

--- src/ProjectWebApiCommon/Models/GeofenceV4DescriptorResult.cs
+++ src/ProjectWebApiCommon/Models/GeofenceV4DescriptorResult.cs
@@ -1,28 +1,9 @@
 using System;
-using System.Collections.Immutable;
-using VSS.MasterData.Models.ResultHandling.Abstractions;
 using VSS.MasterData.Repositories.DBModels;
-using VSS.VisionLink.Interfaces.Events.MasterData.Models;
 
 namespace VSS.MasterData.Project.WebAPI.Common.Models
 {
-
-  /// <summary>
-  /// List of geofences descriptors
-  /// </summary>
-  /// <seealso cref="ContractExecutionResult" />
-  public class GeofenceV4DescriptorsListResult : ContractExecutionResult
-  {
-    /// <summary>
-    /// Gets or sets the project descriptors.
-    /// </summary>
-    /// <value>
-    /// The project descriptors.
-    /// </value>
-    public ImmutableList<GeofenceV4Descriptor> GeofenceDescriptors { get; set; }
-  }
-
- 
+  
   /// <summary>
   ///   Describes VL geofence
   /// </summary>
