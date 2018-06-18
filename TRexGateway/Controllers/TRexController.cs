c06f982eede4fb06b044d488c2d484ae5efac594
Alan  Rose <alanjrose.nz@gmail.com>
2018-06-18 15:58:49 +1200

    #71663 Initial checkin

---
+++ TRexGateway/Controllers/TRexController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TRexGateway.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/TRex")]
+    public class TRexController : Controller
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from TRex";
+        }
+        
+    }
+}
