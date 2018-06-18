04595e0de4dc8947ba38faf1c820c24c26d9a01e
Alan  Rose <alanjrose.nz@gmail.com>
2018-06-18 16:10:44 +1200

    #71663 Add TagfileController

---
+++ TRexGateway/Controllers/TagfileController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TRexGateway.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Tagfile")]
+    public class TagfileController : Controller
+    {
+        // GET: api/Tagfile
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Tagfile/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+
+    }
+}
