8d8ebaaf20e9cc29ca6c2ab2784d0a6d06c7098a
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-07-06 11:22:49 +1200

    Added support for Generic request to Raptor v1 endpoints

--- MasterDataProxies/src/Interfaces/IRaptorProxy.cs
+++ MasterDataProxies/src/Interfaces/IRaptorProxy.cs
@@ -45,5 +45,8 @@ namespace VSS.MasterData.Proxies.Interfaces
 
     Task<BaseDataResult> UploadTagFile(string filename, byte[] data, string orgId = null,
       IDictionary<string, string> customHeaders = null);
+
+    Task<T> ExecuteGenericV1Request<T>(string route, object payload,
+      IDictionary<string, string> customHeaders = null);
   }
 }
