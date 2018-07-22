d6e9cac7a468841860e211a324b9ac0cd6f6c89a
Alan  Rose <alanjrose.nz@gmail.com>
2018-07-23 10:09:02 +1200

    #72731 Control startup of netcore services

--- DeleteNetCoreLogFiles.bat
+++ DeleteNetCoreLogFiles.bat
@@ -3,4 +3,5 @@ del "..\VSS.TRex\src\netstandard\services\VSS.TRex.Server.Application\bin\Debug\
 del "..\VSS.TRex\src\netstandard\services\VSS.TRex.Server.DesignElevation\bin\Debug\netcoreapp2.0\logs\VSS.TRex.Server.DesignElevation.log"
 del "..\VSS.TRex\src\netstandard\services\VSS.TRex.Server.MutableData\bin\Debug\netcoreapp2.0\logs\VSS.TRex.Server.MutableData.log"
 del "..\VSS.TRex\src\netstandard\services\VSS.TRex.Server.PSNode\bin\Debug\netcoreapp2.0\logs\VSS.TRex.Server.PSNode.log"
+del "..\VSS.TRex\src\netstandard\services\VSS.TRex.Server.Tile\bin\Debug\netcoreapp2.0\logs\VS.TRex.Server.Application.log"
 
