8efafc7038bd1d013236fdce7728b2b9b1c04f8c
AaronMorgan <aaron_morgan@trimble.com>
2018-07-26 10:37:32 +1200

    Revert "Added env vars for testing"
    This reverts commit 6131b32613f16352c59d7236f5279f236bb9b50c.

---
+++ AcceptanceTests/utilities/TestRun/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace TestRun
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var testController = new TestControl();
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Failed to run tests. Have you passed all the arguments");
+                Environment.Exit(1);
+                return;
+            }
+
+            var isAllPassed = testController.RunAllTests(args);
+
+            if (isAllPassed)
+            {
+                Console.WriteLine("Tests have all PASSED ");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Tests have FAILED ");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ AcceptanceTests/utilities/TestRun/Stats.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestRun
+{
+    public class Stats
+    {
+        // All test suites
+        private int globalFailCount;
+        private int globalErrorCount;
+        private int globalPassCount;
+        private int globalTestCount;
+
+        // One test suite / project / dll
+        private int suiteFailCount;
+        private int suiteErrorCount;
+        private int suitePassCount;
+        private int suiteTestCount;
+
+        private int classCount;
+        private readonly Stopwatch globalTime;
+        private readonly Stopwatch localTime;
+        public DateTime StartDateTimeOneTest { get; set; }
+        public DateTime EndDateTimeOneTest { get; set; }
+        public DateTime StartDateTimeAllTests { get; set; }
+        public DateTime EndDateTimeAllTests { get; set; }
+        public int ClassCount => classCount;
+        public int GlobalFailCount => globalFailCount;
+        public int GlobalErrorCount => globalErrorCount;
+        public int GlobalPassCount => globalPassCount;
+        public int GlobalTestCount => globalTestCount;
+        public int SuiteFailCount => suiteFailCount;
+        public int SuiteErrorCount => suiteErrorCount;
+        public int SuitePassCount => suitePassCount;
+        public int SuiteTestCount => suiteTestCount;
+        public TimeSpan GlobalTime => globalTime.Elapsed;
+        public TimeSpan LocalTime => localTime.Elapsed;
+
+        public Stats()
+        {
+            globalTime = new Stopwatch();
+            localTime = new Stopwatch();
+
+            globalTime.Start();
+        }
+
+        public void AddClassCount()
+        {
+            classCount++;
+        }
+
+        public void AddGlobalFailCount()
+        {
+            globalFailCount++;
+        }
+
+        public void AddGlobalErrorCount()
+        {
+            globalErrorCount++;
+        }
+
+        public void AddGlobalPassCount()
+        {
+            globalPassCount++;
+        }
+
+        public void AddGlobalCount()
+        {
+            globalTestCount++;
+        }
+
+         public void AddSuiteFailCount()
+        {
+            suiteFailCount++;
+            AddGlobalFailCount();
+        }
+
+        public void AddSuiteErrorCount()
+        {
+            suiteErrorCount++;
+            AddGlobalErrorCount();
+        }
+
+        public void AddSuitePassCount()
+        {
+            suitePassCount++;
+            AddGlobalPassCount();
+        }
+
+        public void AddSuiteCount()
+        {
+            suiteTestCount++;
+            AddClassCount();
+        }
+
+        public void ResetSuiteCount()
+        {
+            suiteTestCount = 0;
+            suitePassCount = 0;
+            suiteErrorCount = 0;
+            suiteFailCount = 0;
+        }
+
+        public void StartLocalTime()
+        {
+            localTime.Start();
+        }
+
+        public void ResetLocalTime()
+        {
+            localTime.Reset();
+        }
+
+        public void GetFinalResult()
+        {
+            localTime.Stop();
+            globalTime.Stop();
+        }
+    }
+}
