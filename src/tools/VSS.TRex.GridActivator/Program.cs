94a44c3f5d7abdeca16e360c2da615ab8654345b
Josh Nimmo <josh_nimmo@trimble.com>
2018-07-26 18:24:07 +1200

    swap activation order for testing purposes

--- src/tools/VSS.TRex.GridActivator/Program.cs
+++ src/tools/VSS.TRex.GridActivator/Program.cs
@@ -26,12 +26,14 @@ namespace VSS.TRex.GridActivator
 
       //TODO: Work out how we want to activate the grid in netcore. For now do it here directly.
       //Log.LogInformation("About to call ActivatePersistentGridServer.Instance().SetGridActive() for Immutable TRex grid");
-      bool result1 = ActivatePersistentGridServer.Instance().SetGridActive(TRexGrids.ImmutableGridName());
+
       //Log.LogInformation($"Activation process completed: Immutable = {result1}");
 
       //Log.LogInformation("About to call ActivatePersistentGridServer.Instance().SetGridActive() for Mutable TRex grid");
       bool result2 = ActivatePersistentGridServer.Instance().SetGridActive(TRexGrids.MutableGridName());
       //Log.LogInformation($"Activation process completed: Mutable = {result2}");
+
+      bool result1 = ActivatePersistentGridServer.Instance().SetGridActive(TRexGrids.ImmutableGridName());
 
       Log.LogInformation($"Immutable Grid Active: {result1}");
       if (!result1)
