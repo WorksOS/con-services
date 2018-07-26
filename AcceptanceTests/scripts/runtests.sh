fbcbaacdb9f79cf2dd889996e060dcc3d20bbbe2
David Glassenbury <david_glassenbury@trimble.com>
2018-07-27 11:32:28 +1200

    Update for running tests locally with vstest

--- AcceptanceTests/scripts/runtests.sh
+++ AcceptanceTests/scripts/runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Accept tests are starting .... "
-echo "Wait for 60 seconds"
-sleep 60s
+echo "Wait for 30 seconds"
+sleep 30s
 echo "Check the database and kafka to see if port is available"
 # Polling the database and kafka status before test
 /bin/bash wait-for-it.sh db:3306 -t 0
@@ -9,22 +9,25 @@ echo "Check the database and kafka to see if port is available"
 echo "Wait for 30 seconds"
 sleep 30s
 
-cd /app
 echo "WebApiTests starting...."
-dotnet vstest WebApiTests/WebApiTests.dll --logger:trx;LogFileName=WebApiTestsResults --ResultsDirectory:/testresults
+dotnet vstest WebApiTests/WebApiTests.dll --logger:trx
+cp TestResults/*.trx testresults/WebApiTests.trx
+rm TestResults/*.trx
 echo "WebApiTests finished"
 
 echo "Run Executor tests starting...."
-dotnet vstest ExecutorTests/ExecutorTests.dll --logger:trx;LogFileName=ExecutorTestResults --ResultsDirectory:/testresults
+dotnet vstest WebApiTests/WebApiTests.dll --logger:trx
+cp TestResults/*.trx testresults/WebApiTests.trx
+rm TestResults/*.trx
 echo "Executor tests finished"
 
 echo "Run Repository tests starting...."
-dotnet vstest RepositoryTests/RepositoryTests.dll --logger:trx;LogFileName=RepositoryTestResults --ResultsDirectory:/testresults
+dotnet vstest RepositoryTests/RepositoryTests.dll --logger:trx
+cp TestResults/*.trx testresults/RepositoryTests.trx
+rm TestResults/*.trx
 echo "Repository tests finished"
 
 echo " "
 echo " "
 echo " All acceptance tests completed"
 echo " "
-sleep 500s
-
