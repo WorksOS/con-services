073ffaea47b87368914e3efa5f480ede490ef1b4
Jeannie May <Jeannie_May@trimble.com>
2018-07-30 08:58:46 +1200

    disable gitignore

---
+++ build/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf artifacts
+rm -rf ./backup*
+
+dotnet publish ./src/VSS.Productivity3D.Filter.WebApi/VSS.Productivity3D.Filter.WebApi.csproj -o ../../artifacts/VSS.Productivity3D.Filter.WebApi -f netcoreapp2.0
+
+cp src/VSS.Productivity3D.Filter.WebApi/appsettings.json artifacts/VSS.Productivity3D.Filter.WebApi/
+cp src/VSS.Productivity3D.Filter.WebApi/bin/Docker/netcoreapp1.1/VSS.Productivity3D.Filter.WebAPI.xml artifacts/VSS.Productivity3D.Filter.WebApi/
+
+mkdir artifacts/logs
