e9a7b1553d5a7412d9a0aebec97ea4b6449477f4
Jeannie May <Jeannie_May@trimble.com>
2018-07-31 15:55:46 +1200

    k8s build too slow

--- database/runme.sh
+++ database/runme.sh
@@ -1,8 +1,12 @@
 echo ">>> Making sure MySQL is up"
-echo ">>> Sleep for 15 seconds."
-sleep 15s
 eval /scripts/wait-for-it.sh "$MYSQL_SERVER_NAME_VSPDB:$MYSQL_PORT" -t 0
 echo "<<< Done checking on MySQL"
 echo "<<< Migrate the database with flyway"
 eval flyway -url="jdbc:mysql://$MYSQL_SERVER_NAME_VSPDB" -schemas="$MYSQL_DATABASE_NAME" -user="$MYSQL_USERNAME" -password="$MYSQL_ROOT_PASSWORD" -locations=filesystem:/scripts/sql -validateOnMigrate=false -outOfOrder=true migrate
-echo "<<< Finished creating the database and tables"
+
+echo Infinite=$INF
+echo 
+if [ "$INF" ]; then
+    echo Doing infinite loop
+    exec /bin/bash -c "trap : TERM INT; sleep infinity & wait" ;
+fi  
