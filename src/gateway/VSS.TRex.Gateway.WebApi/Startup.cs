cfeb1328750ae3174ca370abfbbc9d9ddae1214e
Raymond Wilson <raymond_wilson@trimble.com>
2018-08-06 16:31:37 +1200

    #71307: Remove obsolete CORS comment

--- src/gateway/VSS.TRex.Gateway.WebApi/Startup.cs
+++ src/gateway/VSS.TRex.Gateway.WebApi/Startup.cs
@@ -96,7 +96,6 @@ namespace VSS.TRex.Gateway.WebApi
         app.UseDeveloperExceptionPage();
       }
 
-      //Enable CORS before TID so OPTIONS works without authentication
       app.UseCommon(SERVICE_TITLE);
       app.UseMvc();
     }
