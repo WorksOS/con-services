276124941197c2481f6954d7a2e95d7474179c97
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2018-08-06 16:33:39 +1200

    #73419. Updated NuGet packages..

--- src/WebApiModels/Compaction/Helpers/ICutFillRequestHelper.cs
+++ src/WebApiModels/Compaction/Helpers/ICutFillRequestHelper.cs
@@ -1,4 +1,5 @@
-﻿using VSS.Productivity3D.WebApi.Models.Compaction.Models;
+﻿using VSS.Productivity3D.Models.Models;
+using VSS.Productivity3D.WebApi.Models.Compaction.Models;
 
 namespace VSS.Productivity3D.WebApi.Models.Compaction.Helpers
 {
