85e7e148aeb92b6632abc64bbfd328a7b4eca560
Raymond Wilson <raymond_wilson@trimble.com>
2018-08-07 18:48:29 +1200

    Experimental IActionResult based endpoint for TTM export controller

--- src/gateway/VSS.TRex.Gateway.WebApi/Controllers/TINSurfaceExportController.cs
+++ src/gateway/VSS.TRex.Gateway.WebApi/Controllers/TINSurfaceExportController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VSS.ConfigurationStore;
@@ -11,6 +13,21 @@ using VSS.TRex.Gateway.Common.ResultHandling;
 
 namespace VSS.TRex.Gateway.WebApi.Controllers
 {
+  public class TTMAndMetaDatActioNResult// : IActionResult
+  {
+    public int a, b, c, d;
+
+    public FileStreamResult theFile;
+
+//    public byte[] data;
+
+    //   public Task ExecuteResultAsync(ActionContext context)
+//    {
+    //      throw new NotImplementedException();
+//      return null;
+//    }
+  }
+
   /// <summary>
   /// The controller for generating TIN surfaces decimatied from elevation data
   /// </summary>
@@ -57,7 +74,7 @@ namespace VSS.TRex.Gateway.WebApi.Controllers
         Filter = FilterResult.CreateFilter(null) // Todo: Get the actual filter from the filterUid
       };
 
-      // request.Validate();
+      request.Validate();
 
       var container = RequestExecutorContainer.Build<TINSurfaceExportExecutor>(ConfigStore, LoggerFactory, ServiceExceptionHandler, null, null);
 
@@ -67,5 +84,55 @@ namespace VSS.TRex.Gateway.WebApi.Controllers
 
       return tinResult;
     }
+
+    /// <summary>
+    /// Web service end point controller for TIN surface export
+    /// </summary>
+    /// <param name="projectUid"></param>
+    /// <param name="tolerance"></param>
+    /// <param name="filterUid"></param>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("api/v2/export/surface/ttm")]
+    public IActionResult GetTINSurface2([FromQuery] Guid projectUid,
+      [FromQuery] double? tolerance,
+      [FromQuery] Guid? filterUid)
+    {
+      Log.LogDebug("GetTINSurface: " + Request.QueryString);
+
+      Log.LogDebug($"Accept header is {Request.Headers["Accept"]}");
+
+      TINSurfaceExportRequest request = new TINSurfaceExportRequest
+      {
+        ProjectUid = projectUid,
+        Tolerance = tolerance,
+        Filter = FilterResult.CreateFilter(null) // Todo: Get the actual filter from the filterUid
+      };
+
+      request.Validate();
+
+      var container = RequestExecutorContainer.Build<TINSurfaceExportExecutor>(ConfigStore, LoggerFactory, ServiceExceptionHandler, null, null);
+
+      container.tINSurfaceExportRequestor = tINSurfaceExportRequestor;
+
+      var tinResult = WithServiceExceptionTryExecute(() => container.Process(request)) as TINSurfaceExportResult;
+
+      if (Request.Headers["Accept"].Equals("application/ttm"))
+        return new FileStreamResult(new MemoryStream(tinResult.TINData), "application/ttm");
+
+      if (Request.Headers["Accept"].Equals("application/ttm-and-metadata"))
+        return Ok(new TTMAndMetaDatActioNResult
+        {
+          a = tinResult.TINData.Length,
+
+         theFile = new FileStreamResult(new MemoryStream(tinResult.TINData), "application/ttm")
+        });
+
+      return new FileStreamResult(new MemoryStream(tinResult.TINData), "application/ttm")
+      {
+        FileDownloadName = "DecimatedTIN.ttm"
+      };
+    }
+
   }
 }
