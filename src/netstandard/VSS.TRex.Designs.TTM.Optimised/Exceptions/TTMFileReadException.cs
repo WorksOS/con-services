ae760dc45518216957a1da04e38dd8b3fe3f1031
Raymond Wilson <raymond_wilson@trimble.com>
2018-08-14 12:58:51 +1200

    #73707: Added specific TRex exception type for TTM exceptions

---
+++ src/netstandard/VSS.TRex.Designs.TTM.Optimised/Exceptions/TTMFileReadException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using VSS.TRex.Exceptions;
+
+namespace VSS.TRex.Designs.TTM.Optimised.Exceptions
+{
+  /// <summary>
+  /// Generic TTM read exception thrown while reading in a TTM fiile
+  /// </summary>
+    public class TTMFileReadException : TRexException
+    {
+      public TTMFileReadException(string message) : base(message)
+      {
+      }
+
+      public TTMFileReadException(string message, Exception E) : base(message, E)
+      {
+      }
+  }
+}
