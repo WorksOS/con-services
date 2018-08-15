cafa06a32ef803c7a84f8c89d5142067c4dd0531
Jeannie May <Jeannie_May@trimble.com>
2018-08-15 15:07:17 +1200

    k8s jenkins build different to local

--- AcceptanceTests/scripts/runtests.sh
+++ AcceptanceTests/scripts/runtests.sh
@@ -2,11 +2,10 @@
 echo "Scheduler Accept tests are starting .... "
 echo "Wait for 120 seconds"
 sleep 120s
-echo "Check the database and kafka to see if port is available"
+echo "Check the database ports are available"
 # Polling the database and kafka status before test
 /bin/bash wait-for-it.sh db:3306 -t 0
 /bin/bash wait-for-it.sh dbmssql:1433 -t 0
-#/bin/bash wait-for-it.sh kafka:9092 -t 0
 echo "Wait for 120 seconds"
 sleep 120s
 
