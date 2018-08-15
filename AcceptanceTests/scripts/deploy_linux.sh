8a791d9aa0ce3743e2d619dfa934ef64bae0e81d
Jeannie May <Jeannie_May@trimble.com>
2018-08-15 15:29:50 +1200

    k8s does jiggery-pokery with runtimejenkins.sh

--- AcceptanceTests/scripts/deploy_linux.sh
+++ AcceptanceTests/scripts/deploy_linux.sh
@@ -1,8 +1,11 @@
+#!/bin/bash 
+
 cd ..
 rm -rf deploy
 mkdir deploy
 cp Dockerfile deploy/
 cp scripts/runtests.sh deploy/
+cp scripts/runtestsjenkins.sh deploy/
 cp scripts/wait-for-it.sh deploy/
 cp scripts/rm_cr.sh deploy/
 mkdir deploy/testresults
