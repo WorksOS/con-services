0cd4315e73af7b16b3af06d74af355873b843c38
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-08-21 11:39:18 +1200

    Change contract for TIDPrincipal

--- src/Common/Filters/Authentication/RaptorAuthentication.cs
+++ src/Common/Filters/Authentication/RaptorAuthentication.cs
@@ -63,7 +63,7 @@ namespace VSS.Productivity3D.Common.Filters.Authentication
     /// Create 3dpm principal
     /// </summary>
     public override TIDCustomPrincipal CreatePrincipal(string userUid, string customerUid, string customerName, 
-      string userEmail, bool isApplicationContext, string tpaasApplicationName, IDictionary<string, string> contextHeaders)
+      string userEmail, bool isApplicationContext, IDictionary<string, string> contextHeaders, string tpaasApplicationName)
     {
       //Delegate customer->project association resolution to the principal object for now as it has execution context and can invalidate cache if required
       // note that userUid may actually be the ApplicationId if isApplicationContext
