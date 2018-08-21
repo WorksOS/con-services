13eb203a5392392a2ce92cee831cbcaf901b29b8
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2018-08-21 13:25:29 +1200

    #73963 Split Notifications from MockRaptorControlller. Add Mock end points for tile service calling 3dpm.

---
+++ MockProjectWebApi/src/Controllers/MockNotificationController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using VSS.MasterData.Models.Models;
+using VSS.VisionLink.Interfaces.Events.MasterData.Models;
+
+namespace MockProjectWebApi.Controllers
+{
+  public class MockNotificationController : Controller
+  {
+
+    /// <summary>
+    /// Dummies the add.
+    /// </summary>
+    [Route("api/v2/notification/addfile")]
+    [HttpGet]
+    public BaseDataResult DummyAddFileGet(
+      [FromQuery] Guid projectUid,
+      [FromQuery] ImportedFileType fileType,
+      [FromQuery] Guid fileUid,
+      [FromQuery] string fileDescriptor,
+      [FromQuery] long fileId,
+      [FromQuery] DxfUnitsType dXfUnitsType)
+    {
+      var hasDxfTiles = fileType == ImportedFileType.Linework || fileType == ImportedFileType.Alignment ||
+                        fileType == ImportedFileType.DesignSurface;
+      var res = new AddFileResult { MinZoomLevel = hasDxfTiles ? 15 : 0, MaxZoomLevel = hasDxfTiles ? 19 : 0 };
+      var message =
+        $"DummyAddFileGet: res {res}. projectUid {projectUid} fileType {fileType} fileUid {fileUid} fileDescriptor {fileDescriptor} fileId {fileId} dXfUnitsType {dXfUnitsType}";
+      Console.WriteLine(message);
+      return res;
+    }
+
+    /// <summary>
+    /// Dummies the delete.
+    /// </summary>
+    [Route("api/v2/notification/deletefile")]
+    [HttpGet]
+    public BaseDataResult DummyDeleteFileGet(
+      [FromQuery] Guid projectUid,
+      [FromQuery] ImportedFileType fileType,
+      [FromQuery] Guid fileUid,
+      [FromQuery] string fileDescriptor,
+      [FromQuery] long fileId)
+    {
+      var res = new BaseDataResult();
+      var message =
+        $"DummyDeleteFileGet: res {res}. projectUid {projectUid} fileType {fileType} fileUid {fileUid} fileDescriptor {fileDescriptor} fileId {fileId}";
+      Console.WriteLine(message);
+      return res;
+    }
+
+    /// <summary>
+    /// Dummies the update.
+    /// </summary>
+    [Route("api/v2/notification/updatefiles")]
+    [HttpGet]
+    public BaseDataResult DummyUpdateFilesGet(
+      [FromQuery] Guid projectUid,
+      [FromQuery] Guid[] fileUids)
+    {
+      var res = new BaseDataResult();
+      var message = $"DummyUpdateFilesGet: res {res}. projectUid {projectUid} fileUids {fileUids}";
+      Console.WriteLine(message);
+      return res;
+    }
+
+    /// <summary>
+    /// Dummies the nofication of change.
+    /// </summary>
+    [Route("api/v2/notification/importedfilechange")]
+    [HttpGet]
+    public BaseDataResult DummyNotifyImportedFileChange(
+      [FromQuery] Guid projectUid,
+      [FromQuery] Guid fileUid)
+    {
+      var res = new BaseDataResult();
+      var message = $"DummyNotifyImportedFileChange: res {res}. projectUid {projectUid} fileUid {fileUid}";
+      Console.WriteLine(message);
+      return res;
+    }
+
+
+    /// <summary>
+    /// Dummies the notification that a filterUid has been updated/deleted
+    /// </summary>
+    [Route("api/v2/notification/filterchange")]
+    [HttpGet]
+    public BaseDataResult DummyNotifyFilterChangeGet(
+      [FromQuery] Guid filterUid
+    )
+    {
+      var res = new BaseDataResult();
+      var message = $"DummyNotifyFilterChangeGet: res {res}. filterUid {filterUid}";
+      Console.WriteLine(message);
+      return res;
+    }
+
+    /// <summary>
+    /// Dummies the notification that a projectUid has been updated/deleted
+    /// </summary>
+    [Route("api/v2/notification/invalidatecache")]
+    [HttpGet]
+    public BaseDataResult DummyNotifyProjectChangeGet(
+      [FromQuery] Guid projectUid
+    )
+    {
+      var res = new BaseDataResult();
+      var message = $"DummyNotifyProjectChangeGet: res {res}. projectUid {projectUid}";
+      Console.WriteLine(message);
+      return res;
+    }
+
+
+  }
+}
---
+++ MockProjectWebApi/src/Utils/Colors.cs
@@ -0,0 +1,35 @@
+﻿namespace MockProjectWebApi.Utils
+{
+  /// <summary>
+  /// A set of predefined color constants that may be used when defining color palettes
+  /// </summary>
+  public static class Colors
+  {
+    // Turn off XML doc warnings
+    public const uint Aqua = 0x00FFFF;
+    public const uint Black = 0x000000;
+    public const uint Blue = 0x0000FF;
+    public const uint Brown = 0xAE5700;
+    public const uint Cyan = 0x00FFFF;
+    public const uint DkGray = 0x808080;
+    public const uint Fuchsia = 0xFF00FF;
+    public const uint Gray = 0x808080;
+    public const uint Green = 0x008000;
+    public const uint Lime = 0x00FF00;
+    public const uint LtGray = 0xC0C0C0;
+    public const uint Maroon = 0x800000;
+    public const uint Navy = 0x000080;
+    public const uint Olive = 0x808000;
+    public const uint Orange = 0xFFA500;
+    public const uint Purple = 0x800080;
+    public const uint Red = 0xFF0000;
+    public const uint Silver = 0xC0C0C0;
+    public const uint Teal = 0x008080;
+    public const uint White = 0xFFFFFF;
+    public const uint Yellow = 0xFFFF00;
+    public const uint None = 0x1FFFFFF;
+
+    public const uint RedGreenTransition = 0xFF8080;
+    public const uint BlueGreenTransition = 0x8080FF;
+  }
+}
