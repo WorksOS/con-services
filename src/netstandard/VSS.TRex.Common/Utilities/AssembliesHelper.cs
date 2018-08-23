d8578908ae3f399b09d9285a51d6442961124889
Raymond Wilson <raymond_wilson@trimble.com>
2018-08-23 15:06:40 +1200

    #74227: Don't attempt to load assemblies more than once

--- src/netstandard/VSS.TRex.Common/Utilities/AssembliesHelper.cs
+++ src/netstandard/VSS.TRex.Common/Utilities/AssembliesHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -10,13 +11,18 @@ namespace VSS.TRex.Common.Utilities
   {
     public static void LoadAllAssembliesForExecutingContext()
     {
+      // Find already loaded assemblies
+      Assembly[] asms = AppDomain.CurrentDomain.GetAssemblies();
+
       List<Assembly> allAssemblies = new List<Assembly>();
       string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
       foreach (string dll in Directory.GetFiles(path, "*.dll"))
         try
         {
-          allAssemblies.Add(Assembly.LoadFile(dll));
+          // Only load the assembly if not already present
+          if (!asms.Any(x => x.Location.Equals(dll)))
+            allAssemblies.Add(Assembly.LoadFile(dll));
         }
         catch
         {
