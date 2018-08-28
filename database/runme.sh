8cd477eb4051af26d25efa13c174ef928a2439ab
Jeannie May <Jeannie_May@trimble.com>
2018-08-29 07:12:04 +1200

    #74201 k8s flyways credentials

--- database/runme.sh
+++ database/runme.sh
@@ -1,5 +1,6 @@
 echo ">>> Making sure MySQL is up"
 eval /scripts/wait-for-it.sh "$MYSQL_SERVER_NAME_VSPDB:$MYSQL_PORT" -t 0
 echo "<<< Done checking on MySQL"
-echo flyway -url="jdbc:mysql://$MYSQL_SERVER_NAME_VSPDB" -schemas="$MYSQL_DATABASE_NAME" -user="$MYSQL_USERNAME" -password="$MYSQL_ROOT_PASSWORD" -locations=filesystem:/scripts/sql -validateOnMigrate=false -outOfOrder=true migrate
-eval flyway -url="jdbc:mysql://$MYSQL_SERVER_NAME_VSPDB" -schemas="$MYSQL_DATABASE_NAME" -user="$MYSQL_USERNAME" -password="$MYSQL_ROOT_PASSWORD" -locations=filesystem:/scripts/sql -validateOnMigrate=false -outOfOrder=true migrate
+echo flyway -url="jdbc:mysql://$MYSQL_SERVER_NAME_VSPDB" -schemas="$MYSQL_DATABASE_NAME" -user="$MYSQL_USERNAME_BUILD" '-password=$MYSQL_PASSWORD_BUILD' -locations=filesystem:/scripts/sql -validateOnMigrate=false -outOfOrder=true migrate
+eval flyway -url="jdbc:mysql://$MYSQL_SERVER_NAME_VSPDB" -schemas="$MYSQL_DATABASE_NAME" -user="$MYSQL_USERNAME_BUILD" '-password=$MYSQL_PASSWORD_BUILD' -locations=filesystem:/scripts/sql -validateOnMigrate=false -outOfOrder=true migrate
+ 
