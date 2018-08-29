00fd61b34ecafed36df2a64ef1d60883b8b56b8b
Raymond Wilson <raymond_wilson@trimble.com>
2018-08-29 16:16:01 +1200

    #74436: Upgraded .Net Core 2.0 projects to .Net Core 2.1

--- RunNetcoreTRexIgniteStack_Debug.bat
+++ RunNetcoreTRexIgniteStack_Debug.bat
@@ -1,14 +1,14 @@
 REM Build the TRex.netstandard.sln in VS2017 before running this batch file
-start "PSNode" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.PSNode/bin/Debug/netcoreapp2.0/VSS.TRex.Server.PSNode.dll"
+start "PSNode" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.PSNode/bin/Debug/netcoreapp2.1/VSS.TRex.Server.PSNode.dll"
 timeout 7
-start "MutableData" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.MutableData/bin/Debug/netcoreapp2.0/VSS.TRex.Server.MutableData.dll"
+start "MutableData" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.MutableData/bin/Debug/netcoreapp2.1/VSS.TRex.Server.MutableData.dll"
 timeout 7
-start "Application" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.Application/bin/Debug/netcoreapp2.0/VSS.TRex.Server.Application.dll"
+start "Application" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.Application/bin/Debug/netcoreapp2.1/VSS.TRex.Server.Application.dll"
 timeout 7
-start "DesignElevation" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.DesignElevation/bin/Debug/netcoreapp2.0/VSS.TRex.Server.DesignElevation.dll"
+start "DesignElevation" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.DesignElevation/bin/Debug/netcoreapp2.1/VSS.TRex.Server.DesignElevation.dll"
 timeout 7
-start "TINSurfaceExport" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.TINSurfaceExport/bin/Debug/netcoreapp2.0/VSS.TRex.Server.TINSurfaceExport.dll"
+start "TINSurfaceExport" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.TINSurfaceExport/bin/Debug/netcoreapp2.1/VSS.TRex.Server.TINSurfaceExport.dll"
 timeout 7
-start "TileRendering" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.TileRendering/bin/Debug/netcoreapp2.0/VSS.TRex.Server.TileRendering.dll"
+start "TileRendering" cmd.exe /k "dotnet src/netstandard/services/VSS.TRex.Server.TileRendering/bin/Debug/netcoreapp2.1/VSS.TRex.Server.TileRendering.dll"
 timeout 7
 
