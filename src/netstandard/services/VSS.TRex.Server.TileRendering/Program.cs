ad8b959a7ca32ba1d0c44f090138aef77f233b1f
Raymond Wilson <raymond_wilson@trimble.com>
2018-08-29 16:05:38 +1200

    #74436: Implement new rendering service. Restrict rendering server itself to accept only TILE_RENDERING_NODE role requests.

---
+++ src/netstandard/services/VSS.TRex.Server.TileRendering/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using VSS.TRex.CoordinateSystems;
+using VSS.TRex.CoordinateSystems.Interfaces;
+using VSS.TRex.DI;
+using VSS.TRex.ExistenceMaps.Interfaces;
+using VSS.TRex.GridFabric.Models.Arguments;
+using VSS.TRex.GridFabric.Models.Responses;
+using VSS.TRex.Pipelines;
+using VSS.TRex.Pipelines.Interfaces;
+using VSS.TRex.Pipelines.Interfaces.Tasks;
+using VSS.TRex.Rendering.Abstractions;
+using VSS.TRex.Rendering.Executors.Tasks;
+using VSS.TRex.Rendering.Implementations.Core2;
+using VSS.TRex.Rendering.Servers.Client;
+using VSS.TRex.SiteModels.Interfaces;
+using VSS.TRex.Storage;
+using VSS.TRex.Storage.Interfaces;
+using VSS.TRex.SurveyedSurfaces;
+using VSS.TRex.SurveyedSurfaces.Interfaces;
+
+namespace VSS.TRex.Server.TileRendering
+{
+  class Program
+  {
+    private static ISubGridPipelineBase SubGridPipelineFactoryMethod(PipelineProcessorPipelineStyle key)
+    {
+      switch (key)
+      {
+        case PipelineProcessorPipelineStyle.DefaultProgressive:
+          return new SubGridPipelineProgressive<SubGridsRequestArgument, SubGridRequestsResponse>();
+        default:
+          return null;
+      }
+    }
+
+    private static ITask SubGridTaskFactoryMethod(PipelineProcessorTaskStyle key)
+    {
+      switch (key)
+      {
+        case PipelineProcessorTaskStyle.PVMRendering:
+          return new PVMRenderingTask();
+        default:
+          return null;
+      }
+    }
+
+    private static void DependencyInjection()
+    {
+      DIBuilder.New()
+        .AddLogging()
+        .Add(x => x.AddSingleton<IStorageProxyFactory>(new StorageProxyFactory()))
+        .Add(x => x.AddTransient<ISurveyedSurfaces>(factory => new SurveyedSurfaces.SurveyedSurfaces()))
+        .Add(x => x.AddSingleton<ISurveyedSurfaceFactory>(new SurveyedSurfaceFactory()))
+        .Build()
+        .Add(x => x.AddSingleton<ISiteModels>(new SiteModels.SiteModels()))
+
+        // The renderer factory that allows tile rendering services access Bitmap etc platform dependent constructs
+        .Add(x => x.AddSingleton<IRenderingFactory>(new RenderingFactory()))
+
+        .Add(x => x.AddSingleton<ICoordinateConversion>(new CoordinateConversion()))
+        .Add(x => x.AddSingleton<IExistenceMaps>(new ExistenceMaps.ExistenceMaps()))
+        .Add(x => x.AddSingleton<IPipelineProcessorFactory>(new PipelineProcessorFactory()))
+        .Add(x => x.AddSingleton<Func<PipelineProcessorPipelineStyle, ISubGridPipelineBase>>(provider => SubGridPipelineFactoryMethod))
+        .Add(x => x.AddTransient<IRequestAnalyser>(factory => new RequestAnalyser()))
+        .Add(x => x.AddSingleton<Func<PipelineProcessorTaskStyle, ITask>>(provider => SubGridTaskFactoryMethod))
+
+        .Complete();
+    }
+
+    // This static array ensures that all required assemblies are included into the artifacts by the linker
+    private static void EnsureAssemblyDependenciesAreLoaded(ILogger Log)
+    {
+      // This static array ensures that all required assemblies are included into the artifacts by the linker
+      Type[] AssemblyDependencies =
+      {
+        typeof(VSS.TRex.Geometry.BoundingIntegerExtent2D),
+        typeof(VSS.TRex.GridFabric.BaseIgniteClass),
+        typeof(VSS.TRex.Common.SubGridsPipelinedReponseBase),
+        typeof(VSS.TRex.Logging.Logger),
+        typeof(VSS.TRex.DI.DIContext),
+        typeof(VSS.TRex.Storage.StorageProxy),
+        typeof(VSS.TRex.SiteModels.SiteModel),
+        typeof(VSS.TRex.Cells.CellEvents),
+        typeof(VSS.TRex.CoordinateSystems.LLH),
+        typeof(VSS.TRex.ExistenceMaps.ExistenceMaps),
+        typeof(VSS.TRex.Filters.CellPassAttributeFilter),
+        typeof(VSS.TRex.GridFabric.BaseIgniteClass),
+        typeof(VSS.TRex.Machines.Machine),
+        typeof(VSS.TRex.Pipelines.PipelineProcessor),
+        typeof(VSS.TRex.Profiling.CellLiftBuilder),
+        typeof(VSS.TRex.Rendering.PlanViewTileRenderer),
+        typeof(VSS.TRex.Services.Designs.DesignsService),
+        typeof(VSS.TRex.Services.SurveyedSurfaces.SurveyedSurfaceService),
+        typeof(VSS.TRex.SubGrids.CutFillUtilities),
+        typeof(VSS.TRex.SubGridTrees.Client.ClientCMVLeafSubGrid),
+        typeof(VSS.TRex.SubGridTrees.Core.Utilities.SubGridUtilities),
+        typeof(VSS.TRex.SurveyedSurfaces.SurveyedSurface),
+        typeof(VSS.TRex.Rendering.Implementations.Core2.RenderingFactory),
+        typeof(VSS.TRex.Rendering.Implementations.Core2.GridFabric.Responses.TileRenderResponse_Core2),
+      };
+
+      foreach (var asmType in AssemblyDependencies)
+        if (asmType.Assembly == null)
+          Log.LogError($"Assembly for type {asmType} has not been loaded.");
+    }
+
+
+    static void Main(string[] args)
+    {
+      DependencyInjection();
+
+      ILogger Log = Logging.Logger.CreateLogger<Program>();
+
+      EnsureAssemblyDependenciesAreLoaded(Log);
+
+      Log.LogInformation("Creating service");
+      Log.LogDebug("Creating service");
+
+      var server = new TileRenderingServer();
+      Console.WriteLine("Press anykey to exit");
+      Console.ReadLine();
+    }
+  }
+}
