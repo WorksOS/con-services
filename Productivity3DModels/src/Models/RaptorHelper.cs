8d27798b47a629046e666cc82a1b76b93b173682
Stephen Post <stephen_post@trimble.com>
2018-08-31 15:19:57 +1200

    #74226 Changes to Raptor logic when determining colors for Design 3d Update Productivity Models to 1.7.4

--- Productivity3DModels/src/Models/RaptorHelper.cs
+++ Productivity3DModels/src/Models/RaptorHelper.cs
@@ -26,6 +26,7 @@ namespace VSS.Productivity3D.Models.Models
         switch (mode)
         {
           case DisplayMode.Height:
+          case DisplayMode.Design3D:
             //VL has 30 hardwired but Raptor can handle anything
             //count = 30;
             break;
@@ -91,7 +92,7 @@ namespace VSS.Productivity3D.Models.Models
             break;
         }
 
-        if (mode != DisplayMode.Height && count != palettes.Count)
+        if ((mode != DisplayMode.Height && mode != DisplayMode.Design3D) && count != palettes.Count)
         {
           throw new ServiceException(HttpStatusCode.BadRequest,
             new ContractExecutionResult(ContractExecutionStatesEnum.ValidationError,
@@ -101,7 +102,7 @@ namespace VSS.Productivity3D.Models.Models
         for (int i = 1; i < palettes.Count; i++)
         {
           //Special case of below/above colors for elevation
-          var invalid = (mode == DisplayMode.Height && ((i == 1 && palettes[0].value == -1) || (i == palettes.Count - 1 && palettes[i].value == -1))) ? false :
+          var invalid = ((mode == DisplayMode.Height || mode == DisplayMode.Design3D) && ((i == 1 && palettes[0].value == -1) || (i == palettes.Count - 1 && palettes[i].value == -1))) ? false :
             mode == DisplayMode.CutFill
               ? palettes[i].value > palettes[i - 1].value
               : palettes[i].value < palettes[i - 1].value;
