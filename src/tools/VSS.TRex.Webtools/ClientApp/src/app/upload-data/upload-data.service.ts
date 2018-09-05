05152d888c725994a407448ab9f6889a6132bf1d
Josh Nimmo <josh_nimmo@trimble.com>
2018-09-05 13:28:48 +1200

    tidy up

--- src/tools/VSS.TRex.Webtools/ClientApp/src/app/upload-data/upload-data.service.ts
+++ src/tools/VSS.TRex.Webtools/ClientApp/src/app/upload-data/upload-data.service.ts
@@ -28,30 +28,6 @@ export class UploadDataService {
     this.handleError = httpErrorHandler.createHandleError('UploadDataService');
   }
 
-  ///** GET heroes from the server */
-  //getHeroes(): Observable<Hero[]> {
-  //  return this.http.get<Hero[]>(this.heroesUrl)
-  //    .pipe(
-  //      catchError(this.handleError('getHeroes', []))
-  //    );
-  //}
-
-  /* GET heroes whose name contains search term */
-  //searchHeroes(term: string): Observable<Hero[]> {
-  //  term = term.trim();
-
-  //  // Add safe, URL encoded search parameter if there is a search term
-  //  const options = term ?
-  //    { params: new HttpParams().set('name', term) } : {};
-
-  //  return this.http.get<Hero[]>(this.heroesUrl, options)
-  //    .pipe(
-  //      catchError(this.handleError<Hero[]>('searchHeroes', []))
-  //    );
-  //}
-
-  //////// Save methods //////////
-
   /** POST: submit a tag file to trex */
   addTagfile(tagFile: ITagFileRequest): Observable<ITagFileRequest> {
     return this.http.post<ITagFileRequest>(this.tagUploadUrl, tagFile, httpOptions)
@@ -59,25 +35,5 @@ export class UploadDataService {
         catchError(this.handleError('submitTagFile', tagFile))
       );
   }
-
-  ///** DELETE: delete the hero from the server */
-  //deleteHero(id: number): Observable<{}> {
-  //  const url = `${this.heroesUrl}/${id}`; // DELETE api/heroes/42
-  //  return this.http.delete(url, httpOptions)
-  //    .pipe(
-  //      catchError(this.handleError('deleteHero'))
-  //    );
-  //}
-
-  ///** PUT: update the hero on the server. Returns the updated hero upon success. */
-  //updateHero(hero: Hero): Observable<Hero> {
-  //  httpOptions.headers =
-  //    httpOptions.headers.set('Authorization', 'my-new-auth-token');
-
-  //  return this.http.put<Hero>(this.heroesUrl, hero, httpOptions)
-  //    .pipe(
-  //      catchError(this.handleError('updateHero', hero))
-  //    );
-  //}
 }
 
