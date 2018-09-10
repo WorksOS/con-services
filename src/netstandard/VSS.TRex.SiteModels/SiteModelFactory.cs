a77b71d0a43c09b72c04a3c197cbd3aa10b47ed0
Raymond Wilson <raymond_wilson@trimble.com>
2018-09-11 10:53:03 +1200

    #74790: Added ISiteModelFactory

---
+++ src/netstandard/VSS.TRex.SiteModels/SiteModelFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using VSS.TRex.SiteModels.Interfaces;
+
+namespace VSS.TRex.SiteModels
+{
+  public class SiteModelFactory : ISiteModelFactory
+  {
+    public ISiteModel NewSiteModel() => new SiteModel();
+    public ISiteModel NewSiteModel(Guid id) => new SiteModel(id);
+  }
+}
