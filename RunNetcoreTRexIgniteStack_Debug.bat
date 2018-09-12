c7b3f40146aa2ecbaf7ec84dea37f250d9999c27
Raymond Wilson <raymond_wilson@trimble.com>
2018-09-13 08:59:20 +1200

    #74790: Reduce time out in .Net core service running script from 7 seconds to 6 seconds.

--- RunNetcoreTRexIgniteStack_Debug.bat
+++ RunNetcoreTRexIgniteStack_Debug.bat
@@ -1,13 +1,13 @@
 REM Build the TRex.netstandard.sln in VS2017 before running this batch file
 start /D "src/netstandard/services/VSS.TRex.Server.PSNode/bin/Debug/netcoreapp2.1/" "PSNode" cmd.exe /k "dotnet VSS.TRex.Server.PSNode.dll"
-timeout 7
+timeout 6
 start /D "src/netstandard/services/VSS.TRex.Server.MutableData/bin/Debug/netcoreapp2.1/" "MutableData" cmd.exe /k "dotnet VSS.TRex.Server.MutableData.dll"
-timeout 7
+timeout 6
 start /D "src/netstandard/services/VSS.TRex.Server.Application/bin/Debug/netcoreapp2.1/" "Application" cmd.exe /k "dotnet VSS.TRex.Server.Application.dll"
-timeout 7
+timeout 6
 start /D "src/netstandard/services/VSS.TRex.Server.DesignElevation/bin/Debug/netcoreapp2.1/" "DesignElevation" cmd.exe /k "dotnet VSS.TRex.Server.DesignElevation.dll"
-timeout 7
+timeout 6
 start /D "src/netstandard/services/VSS.TRex.Server.TINSurfaceExport/bin/Debug/netcoreapp2.1/" "TINSurfaceExport" cmd.exe /k "dotnet VSS.TRex.Server.TINSurfaceExport.dll"
-timeout 7
+timeout 6
 start /D "src/netstandard/services/VSS.TRex.Server.TileRendering/bin/Debug/netcoreapp2.1/" "TileRendering" cmd.exe /k "dotnet VSS.TRex.Server.TileRendering.dll"
 
