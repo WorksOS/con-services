6142470622de6d0230989e4a62ae2336208ad175
Raymond Wilson <raymond_wilson@trimble.com>
2018-09-22 22:29:37 +1200

    #74790: Remove framework centric batch files

--- RunTRexImmutableServerCluster.bat
+++
@@ -1,5 +0,0 @@
-start C:\Dev\VSS.TRex\src\framework\TRexImmutableDataServer\bin\x64\release\TRexImmutableDataServer.exe SpatialDivision=0
-start C:\Dev\VSS.TRex\src\framework\TRexImmutableDataServer\bin\x64\release\TRexImmutableDataServer.exe SpatialDivision=1
-start C:\Dev\VSS.TRex\src\framework\TRexImmutableDataServer\bin\x64\release\TRexImmutableDataServer.exe SpatialDivision=2
-start C:\Dev\VSS.TRex\src\framework\TRexImmutableDataServer\bin\x64\release\TRexImmutableDataServer.exe SpatialDivision=3
-
