6315ba031a7f0f78eaa9ecb97dbfbeaa5dc937af
Josh Nimmo <josh_nimmo@trimble.com>
2018-09-25 16:20:25 +1200

    try in quotes

--- src/WebApi/SetupWebAPI.ps1
+++ src/WebApi/SetupWebAPI.ps1
@@ -38,7 +38,7 @@ else
    & sc.exe config lanmanworkstation depend= "MrxSmb20/NSI"
    & sc.exe qc lanmanworkstation
    & sc.exe start lanmanworkstation
-   $myCmd = "net use z: "+ $SHAREUNC +" v3L0c1R^pt0R! /user:svcRaptor /persistent:yes"
+   $myCmd = "net use z: "+ $SHAREUNC +" 'v3L0c1R^pt0R!' /user:svcRaptor /persistent:yes"
    Write-Host $myCmd
    Write-Host "Mapping Raptor ProductionData folder to Z: drive"
    & cmd /c $myCmd
