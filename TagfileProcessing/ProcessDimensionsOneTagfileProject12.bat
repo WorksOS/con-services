075f1bd5924ee36480a0774431303669e70a3180
Alan Rose <alan_rose@trimble.com>
2018-09-28 10:25:38 +1200

    #75478 Move tagfile batch files to new location

---
+++ TagfileProcessing/ProcessDimensionsOneTagfileProject12.bat
@@ -0,0 +1,2 @@
+start ..\..\VSS.TRex\src\framework\TRexTAGFileSubmittor\bin\debug\TRexTAGFileSubmittor.exe "{00000000-0000-0000-0000-000000000012}" "tests\TestData\Dimensions7Cells2Subgrids\1551J025SW--ACOM--121031164609.tag" "{00000000-0000-0000-0000-000000000001}"
+
