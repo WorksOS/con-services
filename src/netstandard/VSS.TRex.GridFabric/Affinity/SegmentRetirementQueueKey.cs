1a72193c211b9d7768984c8b6666dbe8e0cbebd8
Raymond Wilson <raymond_wilson@trimble.com>
2018-09-28 13:45:21 +1200

    #75414: Add segment retirement queue key

---
+++ src/netstandard/VSS.TRex.GridFabric/Affinity/SegmentRetirementQueueKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VSS.TRex.GridFabric.Interfaces;
+
+namespace VSS.TRex.GridFabric.Affinity
+{
+  public class SegmentRetirementQueueKey : ISegmentRetirementQueueKey
+  {
+    public Guid ProjectID { get; set; }
+  }
+}
