de024d51d9712569f2c70d08d8130ffaa7c2f3f7
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2018-10-04 10:34:02 +1300

    #75552 Update temperature details AT model

--- AcceptanceTests/tests/ProductionDataSvc.AcceptanceTests/Models/CompactionTemperatureDetailResult.cs
+++ AcceptanceTests/tests/ProductionDataSvc.AcceptanceTests/Models/CompactionTemperatureDetailResult.cs
@@ -7,12 +7,15 @@ namespace ProductionDataSvc.AcceptanceTests.Models
 {
   public class CompactionTemperatureDetailResult : RequestResult, IEquatable<CompactionTemperatureDetailResult>
   {
+    const int DECIMAL_PLACES = 2;
 
     #region Members
     /// <summary>
-    /// The Temperature summary data results
+    /// Collection of temperature percentages where each element represents the percentage of the matching index temperature target range provided in the 
+    /// temperatutre list member of the temperature details request representation.
     /// </summary>
-    public CompactionTemperatureDetailResult.TemperatureDetailsData temperatureDetailsData { get; set; }
+    [JsonProperty(PropertyName = "percents")]
+    public double[] Percents { get; set; }
     #endregion
 
     #region Constructors
@@ -30,13 +33,7 @@ namespace ProductionDataSvc.AcceptanceTests.Models
       if (other == null)
         return false;
 
-      if (this.temperatureDetailsData == null)
-      {
-        return this.Code == other.Code &&
-               this.Message == other.Message;
-      }
-
-      return this.temperatureDetailsData.Equals(other.temperatureDetailsData) &&
+      return Common.ArraysOfDoublesAreEqual(this.Percents, other.Percents, DECIMAL_PLACES) &&
              this.Code == other.Code &&
              this.Message == other.Message;
     }
@@ -75,41 +72,6 @@ namespace ProductionDataSvc.AcceptanceTests.Models
       return JsonConvert.SerializeObject(this, Formatting.Indented);
     }
     #endregion
-    /// <summary>
-    /// Temperature summary data returned
-    /// </summary>
-    public class TemperatureDetailsData : IEquatable<TemperatureDetailsData>
-    {
-      /// <summary>
-      /// Collection of temperature percentages where each element represents the percentage of the matching index temperature target range provided in the 
-      /// temperatutre list member of the temperature details request representation.
-      /// </summary>
-      [JsonProperty(PropertyName = "percents")]
-      public double[] Percents { get; set; }
-
-      /// <summary>
-      /// Gets the total coverage area for the production data - not the total area specified in filter
-      /// </summary>
-      /// <value>
-      /// The total coverage area in sq meters.
-      /// </value>
-      [JsonProperty(PropertyName = "totalAreaCoveredSqMeters")]
-      public double TotalAreaCoveredSqMeters { get; set; }
-
-
-      public bool Equals(TemperatureDetailsData other)
-      {
-        const int DECIMAL_PLACES = 2;
-
-        if (other == null)
-        {
-          return false;
-        }
-
-        return Math.Round(this.TotalAreaCoveredSqMeters, 2) == Math.Round(other.TotalAreaCoveredSqMeters, 2) &&
-               Common.ArraysOfDoublesAreEqual(this.Percents, other.Percents, DECIMAL_PLACES);
-      }
-    }
-
+ 
   }
 }
