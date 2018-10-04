39ce97987179a3cf3f16fdaf9e27ee2d6bec6b6c
Raymond Wilson <raymond_wilson@trimble.com>
2018-10-04 14:08:26 +1300

    #75636: Add new Binarizable based request, argument and response classes

---
+++ src/netstandard/VSS.TRex.TAGFiles/GridFabric/Arguments/SubmitTAGFileRequestArgument.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using Apache.Ignite.Core.Binary;
+using VSS.TRex.GridFabric.Arguments;
+
+namespace VSS.TRex.TAGFiles.GridFabric.Arguments
+{
+  public class SubmitTAGFileRequestArgument : BaseRequestBinarizableArgument
+  {
+        private const byte versionNumber = 1;
+
+        /// <summary>
+        /// Overridden ID of the project to process the TAG files into
+        /// </summary>
+        public Guid? ProjectID { get; set; }
+
+        /// <summary>
+        /// Overridden ID of the asset to process the TAG files into
+        /// </summary>
+        //public long AssetID { get; set; } = -1;
+        public Guid? AssetID { get; set; }
+
+        /// <summary>
+        /// Name of physical tagfile
+        /// </summary>
+        public string TAGFileName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The content of the TAG file being submitted
+        /// </summary>
+        public byte[] TagFileContent { get; set; }
+
+        /// <summary>
+        /// Helps TFA service determine correct project
+        /// </summary>
+        public string TCCOrgID { get; set; } = string.Empty;
+
+        /// <summary>
+        ///  Default no-arg constructor
+        /// </summary>
+        public SubmitTAGFileRequestArgument()
+        {
+        }
+
+    public override void ToBinary(IBinaryRawWriter writer)
+    {
+      writer.WriteByte(versionNumber);
+      writer.WriteGuid(ProjectID);
+      writer.WriteGuid(AssetID);
+      writer.WriteString(TAGFileName);
+      writer.WriteString(TCCOrgID);
+      writer.WriteByteArray(TagFileContent);
+    }
+
+    public override void FromBinary(IBinaryRawReader reader)
+    {
+      byte readVersionNumber = reader.ReadByte();
+
+      Debug.Assert(readVersionNumber == versionNumber, $"Invalid version number: {readVersionNumber}");
+
+      ProjectID = reader.ReadGuid();
+      AssetID = reader.ReadGuid();
+      TAGFileName = reader.ReadString();
+      TCCOrgID = reader.ReadString();
+      TagFileContent = reader.ReadByteArray();
+    }
+  }
+}
---
+++ src/netstandard/VSS.TRex.TAGFiles/GridFabric/Responses/ProcessTAGFileResponse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Apache.Ignite.Core.Binary;
+using VSS.TRex.GridFabric.Responses;
+
+namespace VSS.TRex.TAGFiles.GridFabric.Responses
+{
+  public class ProcessTAGFileResponse : BaseRequestBinarizableResponse
+  {
+    public const byte versionNumber = 1;
+
+    public List<ProcessTAGFileResponseItem> Results { get; set; } = new List<ProcessTAGFileResponseItem>();
+
+    /// <summary>
+    /// Default no-arg constructor
+    /// </summary>
+    public ProcessTAGFileResponse()
+    {
+    }
+
+    public override void ToBinary(IBinaryRawWriter writer)
+    {
+      writer.WriteByte(versionNumber);
+      writer.WriteInt(Results.Count);
+      Results.ForEach(result => result.ToBinary(writer));
+    }
+
+    public override void FromBinary(IBinaryRawReader reader)
+    {
+      byte readVersionNumber = reader.ReadByte();
+
+      Debug.Assert(readVersionNumber == versionNumber, $"Invalid version number: {readVersionNumber}");
+
+      for (int i = 0; i < reader.ReadInt(); i++)
+      {
+        Results.Add(new ProcessTAGFileResponseItem(reader));
+      }
+    }
+  }
+}
