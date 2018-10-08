740c4174f2dd7ee420afa4ac437a7cd648153759
Josh Nimmo <josh_nimmo@trimble.com>
2018-10-08 15:49:41 +1300

    update tfa proxy to return correct result for trex tagfile processing

--- MasterDataProxies/src/Interfaces/ITagFileAuthProjectProxy.cs
+++ MasterDataProxies/src/Interfaces/ITagFileAuthProjectProxy.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using VSS.MasterData.Models.ResultHandling.Abstractions;
 using VSS.MasterData.Models.Models;
+using VSS.MasterData.Models.ResultHandling;
 
 namespace VSS.MasterData.Proxies.Interfaces
 {
@@ -11,14 +12,14 @@ namespace VSS.MasterData.Proxies.Interfaces
      * [Route("api/v2/project/getUids")]     [HttpPost]
      * returns GetProjectAndAssetUidsResult
      */
-    Task<ContractExecutionResult> GetProjectAndAssetUids(GetProjectAndAssetUidsRequest getProjectAndAssetUidsRequest,
+    Task<GetProjectAndAssetUidsResult> GetProjectAndAssetUids(GetProjectAndAssetUidsRequest getProjectAndAssetUidsRequest,
       IDictionary<string, string> customHeaders = null);
 
     /*
      * [Route("api/v2/project/getUid")]     [HttpPost]
      * returns GetProjectUidResult
      */
-    Task<ContractExecutionResult> GetProjectUid(GetProjectUidRequest getProjectUidRequest,
+    Task<GetProjectAndAssetUidsResult> GetProjectUid(GetProjectUidRequest getProjectUidRequest,
       IDictionary<string, string> customHeaders = null);
 
   }
