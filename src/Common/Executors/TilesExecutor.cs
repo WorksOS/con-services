5da1ea02910f95a19cdd8209ab9c55548d1563f2
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2018-10-12 09:49:00 +1300

    #75637. Some refactoring...

--- src/Common/Executors/TilesExecutor.cs
+++ src/Common/Executors/TilesExecutor.cs
@@ -32,7 +32,7 @@ namespace VSS.Productivity3D.Common.Executors
     {
       try
       {
-        TileRequest request = item as TileRequest;
+        var request = item as TileRequest;
 
         if (request == null)
           ThrowRequestTypeCastException<TileRequest>();
@@ -42,64 +42,66 @@ namespace VSS.Productivity3D.Common.Executors
         if (useTrexGateway)
           return trexCompactionDataProxy.SendProductionDataTileRequest(request, customHeaders).Result;
 
-        RaptorConverters.convertGridOrLLBoundingBox(
-          request.BoundBoxGrid, request.BoundBoxLatLon, out TWGS84Point bl, out TWGS84Point tr, out bool coordsAreGrid);
-
-        var filter1 = RaptorConverters.ConvertFilter(request.FilterId1, request.Filter1, request.ProjectId);
-        var filter2 = RaptorConverters.ConvertFilter(request.FilterId2, request.Filter2, request.ProjectId);
-        var volType = RaptorConverters.ConvertVolumesType(request.ComputeVolumesType);
-
-        if (volType == TComputeICVolumesType.ic_cvtBetween2Filters)
-        {
-          RaptorConverters.AdjustFilterToFilter(ref filter1, filter2);
-        }
-
-        RaptorConverters.reconcileTopFilterAndVolumeComputationMode(ref filter1, ref filter2, request.Mode, request.ComputeVolumesType);
-
-        TASNodeErrorStatus raptorResult = raptorClient.GetRenderedMapTileWithRepresentColor(
-          request.ProjectId ?? -1,
-          ASNodeRPC.__Global.Construct_TASNodeRequestDescriptor(request.CallId ?? Guid.NewGuid(), 0,
-            TASNodeCancellationDescriptorType.cdtWMSTile),
-          RaptorConverters.convertDisplayMode(request.Mode),
-          RaptorConverters.convertColorPalettes(request.Palettes, request.Mode),
-          bl, tr,
-          coordsAreGrid,
-          request.Width,
-          request.Height,
-          filter1,
-          filter2,
-          RaptorConverters.convertOptions(null, request.LiftBuildSettings, request.ComputeVolNoChangeTolerance,
-            request.FilterLayerMethod, request.Mode, request.SetSummaryDataLayersVisibility),
-          RaptorConverters.DesignDescriptor(request.DesignDescriptor),
-          volType,
-          request.RepresentationalDisplayColor,
-          out MemoryStream tile);
-
-        log.LogTrace($"Received {raptorResult} as a result of execution and tile is {tile == null}");
-
-        if (raptorResult == TASNodeErrorStatus.asneOK ||
-            raptorResult == TASNodeErrorStatus.asneInvalidCoordinateRange)
-        {
-          if (tile != null)
-            return ConvertResult(tile, raptorResult);
-          else
-            return new ContractExecutionResult(ContractExecutionStatesEnum.InternalProcessingError, "Null tile returned");
-        }
-        else
-        {
-          log.LogTrace(
-            $"Failed to get requested tile with error: {ContractExecutionStates.FirstNameWithOffset((int)raptorResult)}.");
-
-          throw new ServiceException(HttpStatusCode.BadRequest, new ContractExecutionResult(
-            ContractExecutionStatesEnum.InternalProcessingError,
-            $"Failed to get requested tile with error: {ContractExecutionStates.FirstNameWithOffset((int)raptorResult)}."));
-        }
-
+        return ProcessWithRaptor(request);
       }
       finally
       {
         ContractExecutionStates.ClearDynamic();
       }
+    }
+
+    private ContractExecutionResult ProcessWithRaptor(TileRequest request)
+    {
+      RaptorConverters.convertGridOrLLBoundingBox(
+        request.BoundBoxGrid, request.BoundBoxLatLon, out TWGS84Point bl, out TWGS84Point tr, out bool coordsAreGrid);
+
+      var filter1 = RaptorConverters.ConvertFilter(request.FilterId1, request.Filter1, request.ProjectId);
+      var filter2 = RaptorConverters.ConvertFilter(request.FilterId2, request.Filter2, request.ProjectId);
+      var volType = RaptorConverters.ConvertVolumesType(request.ComputeVolumesType);
+
+      if (volType == TComputeICVolumesType.ic_cvtBetween2Filters)
+      {
+        RaptorConverters.AdjustFilterToFilter(ref filter1, filter2);
+      }
+
+      RaptorConverters.reconcileTopFilterAndVolumeComputationMode(ref filter1, ref filter2, request.Mode, request.ComputeVolumesType);
+
+      var raptorResult = raptorClient.GetRenderedMapTileWithRepresentColor(
+        request.ProjectId ?? -1,
+        ASNodeRPC.__Global.Construct_TASNodeRequestDescriptor(request.CallId ?? Guid.NewGuid(), 0,
+          TASNodeCancellationDescriptorType.cdtWMSTile),
+        RaptorConverters.convertDisplayMode(request.Mode),
+        RaptorConverters.convertColorPalettes(request.Palettes, request.Mode),
+        bl, tr,
+        coordsAreGrid,
+        request.Width,
+        request.Height,
+        filter1,
+        filter2,
+        RaptorConverters.convertOptions(null, request.LiftBuildSettings, request.ComputeVolNoChangeTolerance,
+          request.FilterLayerMethod, request.Mode, request.SetSummaryDataLayersVisibility),
+        RaptorConverters.DesignDescriptor(request.DesignDescriptor),
+        volType,
+        request.RepresentationalDisplayColor,
+        out var tile);
+
+      log.LogTrace($"Received {raptorResult} as a result of execution and tile is {tile == null}");
+
+      if (raptorResult == TASNodeErrorStatus.asneOK ||
+          raptorResult == TASNodeErrorStatus.asneInvalidCoordinateRange)
+      {
+        if (tile != null)
+          return ConvertResult(tile, raptorResult);
+
+        return new ContractExecutionResult(ContractExecutionStatesEnum.InternalProcessingError, "Null tile returned");
+      }
+
+      log.LogTrace(
+        $"Failed to get requested tile with error: {ContractExecutionStates.FirstNameWithOffset((int)raptorResult)}.");
+
+      throw new ServiceException(HttpStatusCode.BadRequest, new ContractExecutionResult(
+        ContractExecutionStatesEnum.InternalProcessingError,
+        $"Failed to get requested tile with error: {ContractExecutionStates.FirstNameWithOffset((int)raptorResult)}."));
     }
 
     private TileResult ConvertResult(MemoryStream tile, TASNodeErrorStatus raptorResult)
