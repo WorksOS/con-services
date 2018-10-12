ca09dca23db140a6127d6ad5e5f64edc92cec519
Elspeth Jeet <elspeth_jeet@trimble.com>
2018-10-12 00:56:12 +0000

    Merged PR 11104: Sandbox: Changes for CAP
    3dpm is the producer
    
    Related work items: #72395

--- MasterDataModels/src/Models/AddFileResult.cs
+++ MasterDataModels/src/Models/AddFileResult.cs
@@ -1,7 +1,14 @@
-﻿namespace VSS.MasterData.Models.Models
+﻿using System;
+using VSS.MasterData.Models.ResultHandling.Abstractions;
+
+namespace VSS.MasterData.Models.Models
 {
-  public class AddFileResult : BaseDataResult
+  public class AddFileResult : ContractExecutionResult
   {
+    public AddFileResult(int code, string message)
+      : base(code, message)
+    { }
+
     /// <summary>
     /// The minimum zoom level that DXF tiles have been generated for.
     /// </summary>
@@ -10,5 +17,20 @@
     /// The maximum zoom level that DXF tiles have been generated for.
     /// </summary>
     public int MaxZoomLevel;
+
+    /// <summary>
+    /// The unique ID of the file
+    /// </summary>
+    public Guid FileUid;
+
+    /// <summary>
+    /// The details of the file
+    /// </summary>
+    public FileDescriptor FileDescriptor;
+
+    /// <summary>
+    /// The email address of the user who added the file
+    /// </summary>
+    public string UserEmailAddress;
   }
 }
