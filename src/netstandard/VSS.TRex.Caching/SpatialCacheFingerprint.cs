4c5b80948aadd0459d1ca1360a6d8163b340f71d
Raymond Wilson <raymond_wilson@trimble.com>
2018-10-23 23:32:26 +1300

    #76404: Flesh out fingerprint construction logic

--- src/netstandard/VSS.TRex.Caching/SpatialCacheFingerprint.cs
+++ src/netstandard/VSS.TRex.Caching/SpatialCacheFingerprint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using VSS.TRex.Filters.Interfaces;
 using VSS.TRex.Types;
 
 namespace VSS.TRex.Caching
@@ -7,17 +8,26 @@ namespace VSS.TRex.Caching
   {
     /// <summary>
     /// Constructs a string representing the fingerprint of a request required spatial data. The fingerprint is designed to identify a context within
-    /// which cached general subgrid results 
+    /// which cached general subgrid results may be stored and accessed during query processing.
     /// </summary>
-    /// <param name="ProjectUID"></param>
-    /// <param name="gridDataType"></param>
-    /// <param name="filterFingerprint"></param>
-    /// <param name="surveyedSurfaceCount"></param>
-    /// <param name="excludedSurveyedSurfaces"></param>
+    /// <param name="projectUID">The Guid identifying the project within the fingerprint</param>
+    /// <param name="gridDataType">The type of data being requested in the query</param>
+    /// <param name="filters">The set of filters that govern the query</param>
+    /// <param name="includedSurveyedSurfaces">The list of surveyed surfaces included in the query, expressed as a an array of Guids</param>
     /// <returns></returns>
-    public static string ConstructFingerprint(Guid ProjectUID, GridDataType gridDataType, string filterFingerprint, int surveyedSurfaceCount, Guid[] excludedSurveyedSurfaces)
+    public static string ConstructFingerprint(Guid projectUID, GridDataType gridDataType, IFilterSet filters, Guid[] includedSurveyedSurfaces)
     {
-      return "";
+      string fingerprint = $"{projectUID}-{gridDataType}";
+
+      if ((filters?.Filters.Length ?? 0) > 0)
+        foreach (var filter in filters?.Filters)
+          fingerprint = fingerprint + filter.AttributeFilter.SpatialCacheFingerprint();
+
+      if (includedSurveyedSurfaces.Length > 0)
+        foreach (var guid in includedSurveyedSurfaces)
+          fingerprint = fingerprint + $"-{guid}";
+
+      return fingerprint;
     }
   }
 }
