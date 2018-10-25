cf5f77b7161dd6d2d1c846cf1399f02aafcf04ec
Raymond Wilson <raymond_wilson@trimble.com>
2018-10-25 14:09:39 +1300

    #76404: Code tidy.

--- tests/netstandard/VSS.TRex.Tests/Caching/TRexSpatialMemoryCacheTests.cs
+++ tests/netstandard/VSS.TRex.Tests/Caching/TRexSpatialMemoryCacheTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using VSS.TRex.Caching;
 using VSS.TRex.SubGridTrees.Interfaces;
 using Xunit;
