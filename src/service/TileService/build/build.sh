2188997238c25ff82cc49ef8880652d0af7f7950
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-11-09 09:14:02 +1300

    MIgration of Tiling service completed

--- src/service/TileService/build/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf artifacts
-
-dotnet publish ./src/VSS.Tile.Service.WebApi/VSS.Tile.Service.WebApi.csproj -o ../../artifacts/VSS.Tile.Service.WebApi --framework netcoreapp2.1 --configuration Docker --runtime linux-x64
-
-cp src/VSS.Tile.Service.WebApi/appsettings.json artifacts/VSS.Tile.Service.WebApi/
-
-mkdir artifacts/logs
