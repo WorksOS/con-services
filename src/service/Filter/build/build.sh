35e3da7c63f8a7aa6c12708dc600c804cce7a4c7
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-11-09 11:33:50 +1300

    Filter service upgrade

--- src/service/Filter/build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf artifacts
-
-dotnet publish ../src/VSS.Productivity3D.Filter.WebApi/VSS.Productivity3D.Filter.WebApi.csproj -o ../../artifacts -f netcoreapp2.0
-dotnet publish ../src/VSS.Productivity3D.Filter.Cleanup/VSS.Productivity3D.Filter.Cleanup.csproj -o ../../artifacts -f netcoreapp2.0
-
-mkdir artifacts/logs
--- src/service/Filter/build/unittests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-(cd ./test/UnitTests/VSS.Productivity3D.Filter.Tests && dotnet test VSS.Productivity3D.Filter.Tests.csproj \
-  /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutputDirectory=/TestResults/TestCoverage --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
-if [ $? -ne 0 ]; then exit 1
-fi
-
