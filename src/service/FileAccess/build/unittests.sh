94b1cda4bdcc5831b7ffdc2c94a0d350d9c0911b
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-11-09 09:44:06 +1300

    Added FileAccess service

--- src/service/FileAccess/build/unittests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-(cd ./test/UnitTests/WebApiTests && dotnet test WebApiTests.csproj \
-  /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=/TestResults/TestCoverage --logger:\"xunit;LogFilePath=/TestResults/TestResults.xml\")
-if [ $? -ne 0 ]; then exit 1
-fi
-
