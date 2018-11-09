556ca434f6404591095e716d8ecf19ec3b16f9bb
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-11-09 15:50:19 +1300

    Moved consumers to services folder

--- src/Common/MasterDataConsumers/RunLocal.bat
+++
@@ -1,6 +0,0 @@
-set DOCKER_TLS_VERIFY=1
-set DOCKER_HOST=tcp://10.97.96.103:2376
-set DOCKER_CERT_PATH=Merino-ProjectMDM
-set DOCKER_MACHINE_NAME="Merino-ProjectMDM"
-
-docker-compose -f docker-compose-dev.yml up --build 
