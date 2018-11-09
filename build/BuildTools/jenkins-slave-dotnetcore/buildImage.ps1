f94cc296d8f8ea8d68cc9b620a9ee24e94564f96
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-11-09 20:26:17 +1300

    Build tools added

---
+++ build/BuildTools/jenkins-slave-dotnetcore/buildImage.ps1
@@ -0,0 +1,2 @@
+docker build -t registry.k8s.vspengg.com:80/vss-jenkinsslave:core21 ./
+docker push registry.k8s.vspengg.com:80/vss-jenkinsslave:core21
--- src/Common/BuildTools/jenkins-slave-dotnetcore/buildImage.ps1
+++
@@ -1,2 +0,0 @@
-docker build -t registry-docker-registry.jenkins:5000/vss-jenkinsslave:core21 ./
-docker push registry-docker-registry.jenkins:5000/vss-jenkinsslave:core21
