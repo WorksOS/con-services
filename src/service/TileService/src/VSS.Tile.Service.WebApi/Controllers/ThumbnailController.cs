f4b5b3ac7ee45be9a244c6e780d254fc8813403b
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-11-12 22:02:44 +1300

    #77032 Move locking class to Common

--- src/service/TileService/src/VSS.Tile.Service.WebApi/Controllers/ThumbnailController.cs
+++ src/service/TileService/src/VSS.Tile.Service.WebApi/Controllers/ThumbnailController.cs
@@ -17,6 +17,7 @@ using VSS.Tile.Service.Common.Extensions;
 using VSS.Tile.Service.Common.Helpers;
 using VSS.Tile.Service.Common.ResultHandling;
 using VSS.Tile.Service.Common.Services;
+using VSS.WebApi.Common;
 
 namespace VSS.Tile.Service.WebApi.Controllers
 {
@@ -184,73 +185,6 @@ namespace VSS.Tile.Service.WebApi.Controllers
 
       var result = await GetGeofenceThumbnailPng(geofenceUid);
       return GetStreamContents(result);
-    }
-
-
-    public sealed class AsyncDuplicateLock
-    {
-      private sealed class RefCounted<T>
-      {
-        public RefCounted(T value)
-        {
-          RefCount = 1;
-          Value = value;
-        }
-
-        public int RefCount { get; set; }
-        public T Value { get; private set; }
-      }
-
-      private static readonly Dictionary<string, RefCounted<SemaphoreSlim>> SemaphoreSlims
-        = new Dictionary<string, RefCounted<SemaphoreSlim>>();
-
-      private SemaphoreSlim GetOrCreate(string key)
-      {
-        RefCounted<SemaphoreSlim> item;
-        lock (SemaphoreSlims)
-        {
-          if (SemaphoreSlims.TryGetValue(key, out item))
-          {
-            ++item.RefCount;
-          }
-          else
-          {
-            item = new RefCounted<SemaphoreSlim>(new SemaphoreSlim(1, 1));
-            SemaphoreSlims[key] = item;
-          }
-        }
-        return item.Value;
-      }
-
-      public IDisposable Lock(string key)
-      {
-        GetOrCreate(key).Wait();
-        return new Releaser { Key = key };
-      }
-
-      public async Task<IDisposable> LockAsync(string key)
-      {
-        await GetOrCreate(key).WaitAsync().ConfigureAwait(false);
-        return new Releaser { Key = key };
-      }
-
-      private sealed class Releaser : IDisposable
-      {
-        public string Key { get; set; }
-
-        public void Dispose()
-        {
-          RefCounted<SemaphoreSlim> item;
-          lock (SemaphoreSlims)
-          {
-            item = SemaphoreSlims[Key];
-            --item.RefCount;
-            if (item.RefCount == 0)
-              SemaphoreSlims.Remove(Key);
-          }
-          item.Value.Release();
-        }
-      }
     }
 
     private static AsyncDuplicateLock _lock = new AsyncDuplicateLock();
