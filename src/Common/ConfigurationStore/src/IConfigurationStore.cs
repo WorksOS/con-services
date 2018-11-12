23f163dd3318e47486555d951a8f996611e07fe1
Jeannie May <Jeannie_May@trimble.com>
2018-11-13 09:57:03 +1300

    #75228 merge fix part 3

--- src/Common/ConfigurationStore/src/IConfigurationStore.cs
+++ src/Common/ConfigurationStore/src/IConfigurationStore.cs
@@ -11,11 +11,19 @@ namespace VSS.ConfigurationStore
     bool GetValueBool(string v, bool defaultValue);
     int GetValueInt(string v);
     int GetValueInt(string v, int defaultValue);
+    long GetValueLong(string v);
+    long GetValueLong(string v, long defaultValue);
+    double GetValueDouble(string v);
+    double GetValueDouble(string v, double defaultValue);
     TimeSpan? GetValueTimeSpan(string v);
     TimeSpan GetValueTimeSpan(string v, TimeSpan defaultValue);
     string GetConnectionString(string connectionType, string databaseNameKey);
     string GetConnectionString(string connectionType);
     IConfigurationSection GetSection(string key);
     IConfigurationSection GetLoggingConfig();
+
+    bool UseKubernetes { get; }
+    string KubernetesConfigMapName { get; }
+    string KubernetesNamespace { get; }
   }
 }
