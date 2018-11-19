3db97e8e5bbdafc6eb156d3dfc9f5f94eafde718
Raymond Wilson <raymond_wilson@trimble.com>
2018-11-20 10:05:34 +1300

    #77069: Switch grid activator batch file from framework to .Net standard version

--- src/service/TRex/RunActivateGrid.bat
+++ src/service/TRex/RunActivateGrid.bat
@@ -1,4 +1,3 @@
-start ..\TRex\src\framework\TRexGridActivator\bin\debug\TRexGridActivator.exe
-
+start /D "src/tools/VSS.TRex.GridActivator/bin/Debug/netcoreapp2.1/" "GridActivator" cmd.exe /k "dotnet VSS.TRex.GridActivator.dll"
 
 	
