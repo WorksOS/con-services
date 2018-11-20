e464a291687e60812a0791bbf9985a9499a59531
Raymond Wilson <raymond_wilson@trimble.com>
2018-11-20 13:56:53 +1300

    #77069: Added IBinarizable support to TAGFileBufferQueueItem

---
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/BinarizableSerialization/ToFomBinary_TAGFileBufferQueueItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using VSS.TRex.TAGFiles.Models;
+using Xunit;
+
+namespace VSS.TRex.Tests.BinarizableSerialization
+{
+  public class ToFomBinary_TAGFileBufferQueueItem
+  {
+    [Fact]
+    public void Test_TAGFileBufferQueueItem_Simple()
+    {
+      SimpleBinarizableInstanceTester.TestClass<TAGFileBufferQueueItem>("Empty TAGFileBufferQueueItem not same after round trip serialisation");
+    }
+
+    [Fact]
+    public void Test_TAGFileBufferQueueItem()
+    {
+      var filter = new TAGFileBufferQueueItem
+      {
+        Content = new byte[] {1, 10, 25, 100},
+        InsertUTC = new DateTime(2000, 1, 1, 1, 1, 1, 1),
+        ProjectID = Guid.NewGuid(),
+        AssetID = Guid.NewGuid(),
+        IsJohnDoe = true,
+        FileName = "fileName"
+      };
+
+      var result = SimpleBinarizableInstanceTester.TestClass(filter, "Custom TAGFileBufferQueueItem not same after round trip serialisation");
+
+      Assert.True(result.member.FileName.Equals("fileName") &&
+                  result.member.InsertUTC.Equals(new DateTime(2000, 1, 1, 1, 1, 1, 1)), 
+        "Post IEquality<T> comparer based comparison failure");
+    }
+  }
+}
