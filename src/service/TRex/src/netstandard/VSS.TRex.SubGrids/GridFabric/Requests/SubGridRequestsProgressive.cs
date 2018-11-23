9a41e8aa9c2832e8ac6e5d2c4e7da7a95d7331c0
Raymond Wilson <raymond_wilson@trimble.com>
2018-11-23 18:10:47 +1300

    #77069: Factor out common request preparation code

--- src/service/TRex/src/netstandard/VSS.TRex.SubGrids/GridFabric/Requests/SubGridRequestsProgressive.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.SubGrids/GridFabric/Requests/SubGridRequestsProgressive.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using VSS.TRex.GridFabric.Arguments;
 using VSS.TRex.GridFabric.Interfaces;
-using VSS.TRex.GridFabric.Models;
 using VSS.TRex.GridFabric.Responses;
 using VSS.TRex.SubGrids.GridFabric.ComputeFuncs;
 using VSS.TRex.SubGrids.GridFabric.Listeners;
@@ -28,7 +27,7 @@ namespace VSS.TRex.SubGrids.GridFabric.Requests
         /// <summary>
         /// The listener to which the processing engine may send in-progress updates during processing of the overall subgrids request
         /// </summary>
-        public SubGridListener Listener { get; set; }
+        private SubGridListener Listener { get; set; }
 
         /// <summary>
         /// The MsgGroup into which the listener has been added
@@ -74,6 +73,21 @@ namespace VSS.TRex.SubGrids.GridFabric.Requests
             MsgGroup = null;
         }
 
+        private void PrepareForExecution()
+        {
+          CheckArguments();
+
+          // Construct the argument to be supplied to the compute cluster
+          PrepareArgument();
+
+          Log.LogInformation($"Prepared argument has TRexNodeId = {arg.TRexNodeID}");
+          Log.LogInformation($"Production Data mask in argument to renderer contains {ProdDataMask.CountBits()} subgrids");
+          Log.LogInformation($"Surveyed Surface mask in argument to renderer contains {SurveyedSurfaceOnlyMask.CountBits()} subgrids");
+
+          CreateSubGridListener();
+
+        }
+
         /// <summary>
         /// Overrides the base Execute() semantics to add a listener available for in-progress updates of information
         /// from the processing engine.
@@ -81,14 +95,7 @@ namespace VSS.TRex.SubGrids.GridFabric.Requests
         /// <returns></returns>
         public override TSubGridRequestsResponse Execute()
         {
-            CheckArguments();
-
-            // Construct the argument to be supplied to the compute cluster
-            PrepareArgument();
-
-            Log.LogInformation($"Prepared argument has TRexNodeId = {arg.TRexNodeID}");
-            Log.LogInformation($"Production Data mask in argument to renderer contains {ProdDataMask.CountBits()} subgrids");
-            Log.LogInformation($"Surveyed Surface mask in argument to renderer contains {SurveyedSurfaceOnlyMask.CountBits()} subgrids");
+            PrepareForExecution();
 
             Task<ICollection<TSubGridRequestsResponse>> taskResult = null;
 
@@ -96,8 +103,6 @@ namespace VSS.TRex.SubGrids.GridFabric.Requests
             sw.Start();
             try
             {
-                CreateSubGridListener();
-
                 // Construct the function to be used
                 IComputeFunc<TSubGridsRequestArgument, TSubGridRequestsResponse> func = new SubGridsRequestComputeFuncProgressive<TSubGridsRequestArgument, TSubGridRequestsResponse>();
 
@@ -123,23 +128,18 @@ namespace VSS.TRex.SubGrids.GridFabric.Requests
         /// <returns></returns>
         public override Task<TSubGridRequestsResponse> ExecuteAsync()
         {
-            CheckArguments();
-         
-            // Construct the argument to be supplied to the compute cluster
-            PrepareArgument();
-         
-            Log.LogInformation($"Prepared argument has TRexNodeId = {arg.TRexNodeID}");
-            Log.LogInformation($"Production Data mask in argument to renderer contains {ProdDataMask.CountBits()} subgrids");
-            Log.LogInformation($"Surveyed Surface mask in argument to renderer contains {SurveyedSurfaceOnlyMask.CountBits()} subgrids");
-                 
-            CreateSubGridListener();
-         
+            PrepareForExecution();
+
             // Construct the function to be used
             IComputeFunc<TSubGridsRequestArgument, TSubGridRequestsResponse> func = new SubGridsRequestComputeFuncProgressive<TSubGridsRequestArgument, TSubGridRequestsResponse>();
 
-            Log.LogInformation("Returning async Task() for request");
-
-            return _Compute.BroadcastAsync(func, arg).ContinueWith(result => result.Result.Aggregate((first, second) => (TSubGridRequestsResponse) first.AggregateWith(second)));
+            return _Compute.BroadcastAsync(func, arg)
+              .ContinueWith(result => result.Result.Aggregate((first, second) => (TSubGridRequestsResponse) first.AggregateWith(second)))
+              .ContinueWith(x =>
+              {
+                Log.LogInformation($"SubgridRequests.Execute() for DM:{TRexTask.PipeLine.DataModelID} from node {TRexTask.TRexNodeID} for data type {TRexTask.GridDataType}");
+                return x.Result;
+              });
         }
 
         #region IDisposable Support
