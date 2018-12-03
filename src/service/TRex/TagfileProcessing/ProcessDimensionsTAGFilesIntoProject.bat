86b10cc601dccfb7a29cbebe41657d8ff45cdd4c
Alan Rose <alan_rose@trimble.com>
2018-12-03 14:06:11 +1300

    #77668 Fix tagfile batch processing files

--- src/service/TRex/TagfileProcessing/ProcessDimensionsTAGFilesIntoProject.bat
+++ src/service/TRex/TagfileProcessing/ProcessDimensionsTAGFilesIntoProject.bat
@@ -1,4 +1,4 @@
-start /D "..\src\tools\VSS.TRex.Tools.TAGfileSubmitter\bin\debug\netcoreapp2.1" "TAGFileSubmittor" cmd.exe /K "dotnet VSS.TRex.Tools.TAGFileSubmittor.dll \"{F13F2458-0000-424F-A995-4426A00771AE}\" \"J:\PP\Construction\Office software\SiteVision Office\Test Files\VisionLink Data\Dimensions 2012\Dimensions2012-Model 381\""
+dotnet "..\src\tools\VSS.TRex.Tools.TAGfileSubmitter\bin\debug\netcoreapp2.1\VSS.TRex.Tools.TAGFileSubmittor.dll" "{F13F2458-0000-424F-A995-4426A00771AE}" "J:\PP\Construction\Office software\SiteVision Office\Test Files\VisionLink Data\Dimensions 2012\Dimensions2012-Model 381"
 
 
 
