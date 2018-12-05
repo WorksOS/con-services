7262d9fc923a1360280669263a76eeb9d0ece91f
Stephen Post <stephen_post@trimble.com>
2018-12-05 21:24:27 +0000

    Merged PR 11777: #77599 - Add single project response to MockWebAPI
    This is required for the Push Service changes, but I need to generate a MockWebAPI image from master to be used in the acceptance tests. (This code exists in my other PR also)
    
    Related work items: #77599

---
+++ src/Common/MasterDataModels/src/ResultHandling/ProjectDataSingleResult.cs
@@ -0,0 +1,12 @@
+﻿using VSS.MasterData.Models.Models;
+
+namespace VSS.MasterData.Models.ResultHandling
+{
+  /// <summary>
+  /// Single project Descriptor result class
+  /// </summary>
+  public class ProjectDataSingleResult : BaseDataResult
+  {
+    public ProjectData ProjectDescriptor { get;set; }
+  }
+}
