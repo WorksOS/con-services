ed4b287b023dabd0b2cd8d91f18399c5c1ca083b
Aaron Morgan <aaron_morgan@trimble.com>
2018-12-11 21:16:49 +0000

    Merged PR 11848: Add filter validation check to v2 Cell Datum endpoint
    As a result of disabling response caching @<Josh Nimmo> found one of the Cell Datum tests was then failing, receiving a response code 200 instead of the expected 400.
    
    This highlighted the issue that the filter UID passed to the endpoint, while a valid Guid, didn't match a filter in the 'mock' database. Because of the nature of the 'GetFilter()' logic it simply returns null in this instance and when the executor receives a null Filter it dutifully passes that to Raptor, resulting in a valid, if not expected, response.
    
    Because v2 endpoints in 3DP in a sense are defined by their filter requirement I've modified the `GetCompactionFilter()` method to take an optional `bool filterMustExist` parameter and for the Cell Datum controller set this to true. This brought the test back into alignment.
    
    It raises a larger issue; should all v2 endpoints that receive a filter UID do the same forced requirement? Generally speaking if the UI gives us a filter uid it is valid so we don't fall into this null filter edge case. But the hole exists all the same. (note, the language here, if the endpoint receives a filter uid but it doesn't resolve to a filter; this is different from receiving a null uid.)
    
    Related work items: #77948

--- src/service/3DP/test/UnitTests/WebApiTests/Coord/Models/CoordinateSystemFileTests.cs
+++ src/service/3DP/test/UnitTests/WebApiTests/Coord/Models/CoordinateSystemFileTests.cs
@@ -1,48 +1,47 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using VSS.Productivity3D.Common.ResultHandling;
 using VSS.Productivity3D.Models.Validation;
 using VSS.Productivity3D.WebApiModels.Coord.Models;
 
 namespace VSS.Productivity3D.WebApiTests.Coord.Models
 {
-    [TestClass]
-    public class CoordinateSystemFileTests
+  [TestClass]
+  public class CoordinateSystemFileTests
+  {
+    [TestMethod]
+    public void CanCreateCoordinateSystemFileTest()
     {
-        [TestMethod]
-        public void CanCreateCoordinateSystemFileTest()
-        {
-            var validator = new DataAnnotationsValidator();
-            string fileName = "test.dc";
-            byte[] fileContent = { 0, 1, 2, 3, 4, 5, 6, 7 };
+      var validator = new DataAnnotationsValidator();
+      string fileName = "test.dc";
+      byte[] fileContent = { 0, 1, 2, 3, 4, 5, 6, 7 };
 
-            // Test the CreateCoordinateSystemFile() method with valid parameters... 
-            CoordinateSystemFile coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(1, fileContent, "test.dc");
-            Assert.IsTrue(validator.TryValidate(coordSystemFile, out ICollection<ValidationResult> results));
+      // Test the CreateCoordinateSystemFile() method with valid parameters... 
+      CoordinateSystemFile coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(1, fileContent, "test.dc");
+      Assert.IsTrue(validator.TryValidate(coordSystemFile, out ICollection<ValidationResult> results));
 
-            // Test the CreateCoordinateSystemFile() method with an invalid projectID... 
-            coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(-1, fileContent, fileName);
-            Assert.IsFalse(validator.TryValidate(coordSystemFile, out results));
+      // Test the CreateCoordinateSystemFile() method with an invalid projectID... 
+      coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(-1, fileContent, fileName);
+      Assert.IsFalse(validator.TryValidate(coordSystemFile, out results));
 
-            // Test the CreateCoordinateSystemFile() method with a file name length exceeds 256 characters... 
-            string prefix = "overlimit";
-            //int maxCount = (int)(CoordinateSystemFile.MAX_FILE_NAME_LENGTH / prefix.Length);
+      // Test the CreateCoordinateSystemFile() method with a file name length exceeds 256 characters... 
+      string prefix = "overlimit";
+      //int maxCount = (int)(CoordinateSystemFile.MAX_FILE_NAME_LENGTH / prefix.Length);
 
-            for (int i = 1; prefix.Length <= CoordinateSystemFile.MAX_FILE_NAME_LENGTH; i++)
-                prefix += prefix;
+      for (int i = 1; prefix.Length <= CoordinateSystemFile.MAX_FILE_NAME_LENGTH; i++)
+        prefix += prefix;
 
-            coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(1, fileContent, prefix + fileName);
-            Assert.IsFalse(validator.TryValidate(coordSystemFile, out results));
+      coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(1, fileContent, prefix + fileName);
+      Assert.IsFalse(validator.TryValidate(coordSystemFile, out results));
 
-            // Test the CreateCoordinateSystemFile() method with no file name provided... 
-            coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(1, fileContent, string.Empty);
-            Assert.IsFalse(validator.TryValidate(coordSystemFile, out results));
+      // Test the CreateCoordinateSystemFile() method with no file name provided... 
+      coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(1, fileContent, string.Empty);
+      Assert.IsFalse(validator.TryValidate(coordSystemFile, out results));
 
-            // Test the CreateCoordinateSystemFile() method with no content provided... 
-            coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(1, null, fileName);
-            Assert.IsFalse(validator.TryValidate(coordSystemFile, out results));
-        }
+      // Test the CreateCoordinateSystemFile() method with no content provided... 
+      coordSystemFile = CoordinateSystemFile.CreateCoordinateSystemFile(1, null, fileName);
+      Assert.IsFalse(validator.TryValidate(coordSystemFile, out results));
+    }
 
     [TestMethod]
     public void CoordinateSystemFileValidationRequestTest()
