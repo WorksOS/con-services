170faabbec9ad2fa0b8fdc65cc3bf29d8114d88b
Raymond Wilson <raymond_wilson@trimble.com>
2018-12-20 07:58:07 +1300

    #78293: Implement new GUDI has functions and use it in affinity partition key calculations.

---
+++ src/service/TRex/src/netstandard/VSS.TRex.Common/Utilities/GUIDHashCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VSS.TRex.Common.Utilities
+{
+  /// <summary>
+  /// Implements a hashing function for GUIDs for TRex to use as a partitioning function for Ignite
+  /// which is not affected by underlying hashing implementation changes in the .Net platform
+  /// </summary>
+  public static class GuidHashCode
+  {
+    /// <summary>
+    /// Performs a byte-wise hash of the content of the GUID. Unfortunately this is not as fast as the .Net
+    /// implementation which uses an unsafe context XOR of the four internal integers contained in the GUID
+    /// (which is in itself may not not stable due to hardware big/little Endianness)
+    /// </summary>
+    /// <param name="g"></param>
+    /// <returns></returns>
+    public static int Hash(Guid g)
+    {
+      byte[] b = g.ToByteArray();
+
+      return ((b[0]  << 24) | (b[1]  << 16) | (b[2]  << 8) | b[3]) ^
+             ((b[4]  << 24) | (b[5]  << 16) | (b[6]  << 8) | b[7]) ^
+             ((b[8]  << 24) | (b[9]  << 16) | (b[10] << 8) | b[11]) ^
+             ((b[12] << 24) | (b[13] << 16) | (b[14] << 8) | b[15]);
+    }
+  }
+}
