415f67c2f93e6dd77b0ec78b82b00781722fe96b
AaronMorgan <aaron_morgan@trimble.com>
2018-12-21 01:33:57 +1300

    #77787 | FILTER | Fixed CRLF endings in RunAcceptanceTests.sh.

--- src/service/Filter/build/RunAcceptanceTests.sh
+++ src/service/Filter/build/RunAcceptanceTests.sh
@@ -7,12 +7,8 @@ ls
 
 echo "Wait for 20 seconds"
 sleep 20s
+
 echo "Check the database and kafka to see if port is available"
-# Polling the database and kafka status before test
-#/bin/bash wait-for-it.sh localhost:3306 -t 0
-#/bin/bash wait-for-it.sh localhost:9092 -t 0
-#echo "Wait for 20 seconds"
-#sleep 20s
 
 echo "Run the AcceptanceTests solution"
 cd AcceptanceTests
@@ -20,4 +16,3 @@ dotnet test VSS.Visionlink.Filter.AcceptanceTests.sln --logger \"xunit;LogFileNa
 echo " "
 echo " All acceptance tests completed"
 echo " "
-
