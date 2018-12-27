e918423727dfc580b71d39119c8ce879150aa8ca
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2018-12-27 13:00:57 +1300

    ***NO_CI*** futher EKS improvements

--- build/BuildTools/jenkins-slave-docker/buildImage.ps1
+++ build/BuildTools/jenkins-slave-docker/buildImage.ps1
@@ -1,3 +1,3 @@
 Invoke-Expression -Command (aws ecr get-login --no-include-email --profile vss-grant --region us-west-2)
-docker build -t 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-jenkinsslave:docker-img-k8s ./
-docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/vss-jenkinsslave:docker-img-k8s
+docker build -t 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:jenkins-docker-k8s ./
+docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:jenkins-docker-k8s
