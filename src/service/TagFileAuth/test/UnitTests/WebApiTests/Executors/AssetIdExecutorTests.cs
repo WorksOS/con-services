4f1f14239526a6f061811f9b9ccf864dc869c108
Jeannie May <Jeannie_May@trimble.com>
2019-01-08 10:54:28 +1300

    #78554 Allow EC520 device in TFA v1 endpoint. Fix mono solution.

--- src/service/TagFileAuth/test/UnitTests/WebApiTests/Executors/AssetIdExecutorTests.cs
+++ src/service/TagFileAuth/test/UnitTests/WebApiTests/Executors/AssetIdExecutorTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -7,6 +8,10 @@ using VSS.Productivity3D.TagFileAuth.WebAPI.Models.Executors;
 using VSS.Productivity3D.TagFileAuth.WebAPI.Models.Models;
 using VSS.Productivity3D.TagFileAuth.WebAPI.Models.ResultHandling;
 using VSS.VisionLink.Interfaces.Events.MasterData.Interfaces;
+using Moq;
+using VSS.MasterData.Models.Models;
+using VSS.MasterData.Models.ResultHandling;
+using VSS.MasterData.Repositories.ExtendedModels;
 
 namespace WebApiTests.Executors
 {
@@ -57,6 +62,52 @@ namespace WebApiTests.Executors
       Assert.AreEqual(-1, result.assetId, "executor returned incorrect AssetId");
       Assert.AreEqual(0, result.machineLevel, "executor returned incorrect serviceType, should be unknown(0)");      
     }
-      
+
+    [TestMethod]
+    [DataRow("SNM940", "snm940Serial", "SNM940", "snm940Serial", "SNM940", "snm940Serial")]
+    [DataRow("SNM941", "snm941Serial", "SNM941", "snm941Serial", "SNM941", "snm941Serial")]
+    [DataRow("SNM940", "snm940Serial", "SNM941", "snm941Serial", "SNM941", "snm941Serial")]
+    [DataRow("EC520", "ec520Serial", "EC520", "ec520Serial", "EC520", "ec520Serial")]
+    public async Task AssetUidExecutor_GetAssetDevice_HappyPath(
+      string deviceTypeRequested, string radioSerialRequested,
+      string deviceTypeExisting, string radioSerialExisting,
+      string deviceTypeExpected, string radioSerialExpected)
+    {
+      var assetDeviceIdsToBeReturned = new AssetDeviceIds()
+      {
+        AssetUID = Guid.NewGuid().ToString(),
+        DeviceType = deviceTypeExpected,
+        DeviceUID = Guid.NewGuid().ToString(),
+        RadioSerial = radioSerialExpected
+      };
+
+      var logger = serviceProvider.GetRequiredService<ILoggerFactory>().CreateLogger<AssetIdExecutorTests>();
+      Mock<IDeviceRepository> deviceRepo = new Mock<IDeviceRepository>();
+      deviceRepo.Setup(d => d.GetAssociatedAsset(radioSerialRequested, deviceTypeRequested)).ReturnsAsync(assetDeviceIdsToBeReturned);
+
+      DataRepository dataRepository = new DataRepository(logger, configStore, null, deviceRepo.Object,
+        null, null, null, null, null);
+     
+      var assetDevice = await dataRepository.LoadAssetDevice(radioSerialRequested, deviceTypeRequested);
+      Assert.AreEqual(assetDevice.DeviceType, deviceTypeExpected);
+      Assert.AreEqual(assetDevice.RadioSerial, radioSerialExpected);
+    }
+
+    [TestMethod]
+    [DataRow("SNM940", "snm940Serial")]
+    public async Task AssetUidExecutor_GetAssetDevice_UnHappyPath(
+      string deviceTypeRequested, string radioSerialRequested)
+    {
+      var logger = serviceProvider.GetRequiredService<ILoggerFactory>().CreateLogger<AssetIdExecutorTests>();
+      Mock<IDeviceRepository> deviceRepo = new Mock<IDeviceRepository>();
+      deviceRepo.Setup(d => d.GetAssociatedAsset(radioSerialRequested, deviceTypeRequested)).ReturnsAsync((AssetDeviceIds)null);
+
+      DataRepository dataRepository = new DataRepository(logger, configStore, null, deviceRepo.Object,
+        null, null, null, null, null);
+
+      var assetDevice = await dataRepository.LoadAssetDevice(radioSerialRequested, deviceTypeRequested);
+      Assert.IsNull(assetDevice);
+    }
+
   }
 }
--- src/service/TagFileAuth/test/UnitTests/WebApiTests/Models/ProjectAndAssetUidsRequestTests.cs
+++ src/service/TagFileAuth/test/UnitTests/WebApiTests/Models/ProjectAndAssetUidsRequestTests.cs
@@ -10,9 +10,9 @@ namespace WebApiTests.Models
     [TestMethod]
     [DataRow("", 999, "rs45", "", 89, 179, 30)] // invalid deviceType
     [DataRow("", 6, "rs45", "", 91, 179, 21)] // invalid lat
-    [DataRow("", 6, "rs45", "", 89, 181, 22)] // invalid long
+    [DataRow("", 46, "rs45", "", 89, 181, 22)] // invalid long
     [DataRow("scooby", 6, "rs45", "", 89, 179, 36)] // invalid projectUid
-    [DataRow("", 1, "", "", 89, 179, 37)] // missing radioSerial
+    [DataRow("", 56, "", "", 89, 179, 37)] // missing radioSerial
     public void ValidateGetProjectAndAssetUidsRequest_ValidationErrors
       (string projectUid, int deviceType, string radioSerial, string tccOrgUid,
         double latitude, double longitude, int errorCode)
@@ -27,9 +27,10 @@ namespace WebApiTests.Models
     }
 
     [TestMethod]
+    [DataRow("", 0, "rs45", "", 89, 179, 0)]
     [DataRow("", 6, "rs45", "", 89, 179, 0)]
-    [DataRow("87e6bd66-54d8-4651-8907-88b15d81b2d7", 1, "rs45", "", 89, 179, 0)]
-    [DataRow("87e6bd66-54d8-4651-8907-88b15d81b2d7", 1, "", "87e6bd65-54d8-4651-8907-88b15d81b2d7", 89, 179, 0)]
+    [DataRow("87e6bd66-54d8-4651-8907-88b15d81b2d7", 46, "rs45", "", 89, 179, 0)]
+    [DataRow("87e6bd66-54d8-4651-8907-88b15d81b2d7", 56, "", "87e6bd65-54d8-4651-8907-88b15d81b2d7", 89, 179, 0)]
     public void ValidateGetProjectAndAssetUidsRequest_ValidationHappyPath
     (string projectUid, int deviceType, string radioSerial, string tccOrgUid,
       double latitude, double longitude, int errorCode)
