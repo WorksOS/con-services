55b67ba1c9cd29c1d44d3ee528a90a2e267ef055
AaronMorgan <aaron_morgan@trimble.com>
2019-01-13 22:29:25 +1300

    #77787 | MONO | Attempt at resolving CI build; update build files from /master branch.

--- src/service/Filter/build/RunAcceptanceTests.sh
+++ src/service/Filter/build/RunAcceptanceTests.sh
@@ -7,8 +7,12 @@ ls
 
 echo "Wait for 20 seconds"
 sleep 20s
-
 echo "Check the database and kafka to see if port is available"
+# Polling the database and kafka status before test
+#/bin/bash wait-for-it.sh localhost:3306 -t 0
+#/bin/bash wait-for-it.sh localhost:9092 -t 0
+#echo "Wait for 20 seconds"
+#sleep 20s
 
 echo "Run the AcceptanceTests solution"
 cd AcceptanceTests
