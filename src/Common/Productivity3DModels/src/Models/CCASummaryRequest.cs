f5419fda036cb052ca92ff0d9d3af8de7548c95d
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2019-01-15 15:44:21 +1300

    #77946. Initial implementation. NO_CI, please!

---
+++ src/Common/Productivity3DModels/src/Models/CCASummaryRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace VSS.Productivity3D.Models.Models
+{
+  /// <summary>
+  /// The request representation used for a CCA Summary request.
+  /// </summary>
+  public class CCASummaryRequest : ProjectID
+  {
+    /// <summary>
+    /// The filter instance to use in the request.
+    /// Value may be null.
+    /// </summary>
+    [JsonProperty(PropertyName = "filter", Required = Required.Default)]
+    public FilterResult Filter { get; private set; }
+
+    /// <summary>
+    /// Default private constructor.
+    /// </summary>
+    private CCASummaryRequest()
+    {
+    }
+
+    /// <summary>
+    /// Overload constructor with parameters.
+    /// </summary>
+    public CCASummaryRequest(
+      Guid? projectUid,
+      FilterResult filter
+    )
+    {
+      ProjectUid = projectUid;
+      Filter = filter;
+    }
+
+    /// <summary>
+    /// Validates all properties
+    /// </summary>
+    public override void Validate()
+    {
+      base.Validate();
+
+      Filter?.Validate();
+    }
+  }
+}
