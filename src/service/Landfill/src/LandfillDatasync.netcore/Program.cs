752e4281c51ea917135d594e8a60f74c5357a130
David Glassenbury <david_glassenbury@trimble.com>
2019-01-16 22:37:04 +0000

    Merged PR 12180: Landfill changes to enable running data sync for a single customer.
    Landfill changes to enable running data sync for a single customer. Also change to use eks for alpha.
    
    Related work items: #78116

--- src/service/Landfill/src/LandfillDatasync.netcore/Program.cs
+++ src/service/Landfill/src/LandfillDatasync.netcore/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Security.Policy;
 using log4net;
 using log4net.Config;
+using Microsoft.Extensions.Logging.Abstractions;
+using VSS.ConfigurationStore;
 
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml",
+  Watch = true)]
 namespace LandfillDatasync.netcore
 {
   internal class Program
@@ -12,13 +17,24 @@ namespace LandfillDatasync.netcore
 
     private static void Main(string[] args)
     {
-      //TODO logging is not working!!
+      var configurationStore = new GenericConfiguration(new NullLoggerFactory());
+
+      Log.Debug("Landfill Data Sync starting");
    //   XmlConfigurator.Configure( new FileInfo("log4net.xml"));
-      var dataSync = new DataSynchronizer(Log);
+      var dataSync = new DataSynchronizer(Log, configurationStore);
+
+      // Optionally specify a specific customer to process (this will be null if not specified)
+      // If a specific customerUID is provided land fill data sync will only find projects for that customer
+      var customerUid = configurationStore.GetValueString("LANDFILL_CUSTOMER_UID", string.Empty);
+      if (Guid.TryParse(customerUid, out var guid))
+      {
+        dataSync.CustomerUid = guid;
+        Log.Debug($"Processing CustomerUID: {guid}");
+      }
 
       // *************  Process the volumes for the last nn days  *************** 
-      var noOfDaysVolsVar = Environment.GetEnvironmentVariable("NoOfDaysBackForVolumes");
-      var noOfDaysVols = 30;
+      var noOfDaysVolsVar = configurationStore.GetValueString("NoOfDaysBackForVolumes", string.Empty);
+      var noOfDaysVols = -30;
       if (!string.IsNullOrEmpty(noOfDaysVolsVar))
       {
         noOfDaysVols = -Math.Abs(Convert.ToInt32(noOfDaysVolsVar));
@@ -27,8 +43,8 @@ namespace LandfillDatasync.netcore
       Log.Debug("***** Finished Processing volumes ***** ");
 
       // *************  Process the CCA for the last nn days  *************** 
-      var noOfDaysCca = Environment.GetEnvironmentVariable("NoOfDaysBackForCCA");
-      var ccaDaysBackFill = 30;
+      var noOfDaysCca = configurationStore.GetValueString("NoOfDaysBackForCCA", string.Empty);
+      var ccaDaysBackFill = -30;
       if (!string.IsNullOrEmpty(noOfDaysCca))
       {
         ccaDaysBackFill = -Math.Abs(Convert.ToInt32(noOfDaysCca));
