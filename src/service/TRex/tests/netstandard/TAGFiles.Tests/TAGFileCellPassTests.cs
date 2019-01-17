d6ae10aa7deb2a9585557a20b98fe9a2c6bc0852
Raymond Wilson <raymond_wilson@trimble.com>
2019-01-17 22:12:56 +1300

    #78894: Wire in mutation hook into new TAG file test for cell passes

--- src/service/TRex/tests/netstandard/TAGFiles.Tests/TAGFileCellPassTests.cs
+++ src/service/TRex/tests/netstandard/TAGFiles.Tests/TAGFileCellPassTests.cs
@@ -9,26 +9,29 @@ using Xunit;
 
 namespace TAGFiles.Tests
 {
-  public class TAGFileCellPassTests
+  public class TAGFileCellPassTests : IClassFixture<DITagFileFixture>
   {
+    private static readonly ICell_NonStatic_MutationHook hook = DIContext.Obtain<ICell_NonStatic_MutationHook>();
+
     [Fact]
     public void Test_TAGFileCellPassGeneration_Default()
     {
       // Setup the mutation hook to capture cell pass generation
-      string cellPassFileName = $@"c:\temp\CellPasses-{DateTime.Now.Ticks}.txt";
+      string cellPassFileName = $@"c:\temp\TRex-UnitTests-TAGFiles-CellPasses-{DateTime.Now.Ticks}.txt";
       var writer = new CellPassWriter(new StreamWriter(new FileStream(cellPassFileName, FileMode.CreateNew, FileAccess.ReadWrite)));
-      DIContext.Obtain<ICell_NonStatic_MutationHook>().SetActions(writer); 
+      hook.SetActions(writer); 
 
       // Read in the TAG file
       DITagFileFixture.ReadTAGFile("TestTAGFile.tag");
 
       // Close the writer and clean up the DI context
+      hook.ClearActions();
       writer.Close();
-      DIBuilder.Continue().Remove<ICell_NonStatic_MutationHook>();
 
-      //  examine the result
+      // Examine the result
       var lines = File.ReadAllLines(cellPassFileName);
-      lines.Length.Should().Be(1);
+      lines.Length.Should().Be(16525);
     }
   }
 }
+
