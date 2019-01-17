9a5ddb454b07fa3eb12a2108016722234778fb45
Alan Rose <alan_rose@trimble.com>
2019-01-17 14:24:24 +1300

    #78898 Fix unit tests

--- src/service/TRex/tests/netstandard/TAGFiles.Tests/TAGFileConverterTests.cs
+++ src/service/TRex/tests/netstandard/TAGFiles.Tests/TAGFileConverterTests.cs
@@ -81,7 +81,7 @@ namespace TAGFiles.Tests
       converter.Processor.OnGroundFlagSet.Should().Be(true);
 
       DateTime theTime = new DateTime(2012, 11, 1, 20, 53, 23, 841, DateTimeKind.Unspecified);
-      converter.Processor.OnGrounds.GetOnGroundAtDateTime(theTime).Should().NotBe(OnGroundState.YesMachineSoftware);
+      converter.Processor.OnGrounds.GetOnGroundAtDateTime(theTime).Should().Be(OnGroundState.YesMachineSoftware);
     }
 
 
