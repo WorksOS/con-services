38d7acc02d2a98e367d5a42cde1fb973882ac003
Raymond Wilson <raymond_wilson@trimble.com>
2019-01-18 14:18:19 +1300

    #78894: Simplify CellPassWriter and tests that use it to not require StreamWriter support to a file system object.

--- src/service/TRex/tests/netstandard/TAGFiles.Tests/Utilities/CellPassWriter.cs
+++ src/service/TRex/tests/netstandard/TAGFiles.Tests/Utilities/CellPassWriter.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using System;
 using VSS.TRex.Cells;
 using VSS.TRex.SubGridTrees.Server.Interfaces;
 
@@ -10,38 +10,41 @@ namespace TAGFiles.Tests.Utilities
   public class CellPassWriter : ICellPassWriter
   {
     /// <summary>
-    /// The TextWriter used to emit cell pass information
+    /// The supplied delegate to handle each formatted line representing a cell pass mutation action (add, remove, replace)
     /// </summary>
-    public TextWriter Writer { get; set; }
+    public Action<string> WriteLine { get; set; }
 
     /// <summary>
     /// Controls whether to output short form pass information (just location and time), or full pass information
     /// </summary>
     public bool ShortFormOutput { get; set; } = true;
 
-    public CellPassWriter(TextWriter writer)
+    private void WriteToOutput(string line)
     {
-      Writer = writer;
+      WriteLine.Invoke(line);
+    }
+
+    public CellPassWriter(Action<string> writeLine)
+    {
+      WriteLine = writeLine;
     }
 
     public void AddPass(uint X, uint Y, Cell_NonStatic cell, CellPass pass, int position)
     {
       var passString = ShortFormOutput ? $"Time:{pass.Time:yyyy-MM-dd hh-mm-ss.fff}" : $"{pass}";
-      Writer.WriteLine($"AddPass {X}:{Y}->{passString}");
+      WriteToOutput($"AddPass {X}:{Y}->{passString}");
     }
 
     public void RemovePass(uint X, uint Y, int passIndex)
     {
-      Writer.WriteLine($"RemovePass {X}:{Y}, Position:{passIndex}");
+      WriteToOutput($"RemovePass {X}:{Y}, Position:{passIndex}");
     }
 
     public void ReplacePass(uint X, uint Y, Cell_NonStatic cell, int position, CellPass pass)
     {
       var passString = ShortFormOutput ? $"Time:{pass.Time:yyyy-MM-dd hh-mm-ss.fff}" : $"{pass}";
-      Writer.WriteLine($"ReplacePass {X}:{Y}:{position}->{passString}");
+      WriteToOutput($"ReplacePass {X}:{Y}:{position}->{passString}");
     }
-
-    public void Close() => Writer.Close();
 
     public void SetActions(ICell_NonStatic_MutationHook actions)
     {
