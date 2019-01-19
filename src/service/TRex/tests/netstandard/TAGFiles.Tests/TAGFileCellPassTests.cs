4b171e5765309d5ef74b4b8ffe9e41956c164358
Raymond Wilson <raymond_wilson@trimble.com>
2019-01-19 23:06:19 +1300

    #78894: Add initial test to verify specific cell pass creation between Raptor and TRex

--- src/service/TRex/tests/netstandard/TAGFiles.Tests/TAGFileCellPassTests.cs
+++ src/service/TRex/tests/netstandard/TAGFiles.Tests/TAGFileCellPassTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using TAGFiles.Tests.Utilities;
@@ -38,6 +40,33 @@ namespace TAGFiles.Tests
       // Temporarily save the file to capture it
       var fn = Path.Combine(Path.GetTempPath(), fileName + ".MutationLog.txt");
       File.WriteAllLines(fn, Lines);
+    }
+
+    [Theory]
+    [InlineData("2652J085SW--CASE CX160C--121107210953.tag", "CellMutationLog-2652J085SW--CASE CX160C--121107210953.tag.txt", 16525, 16525)]
+    public void Test_TAGFileCellPassGeneration_CompareKnownCellPassConstruction_Dimensions2018CaseMachine(string tagFileName, string mutationLogFileName, int cellPassCount, int lineCount)
+    {
+      var Lines = new List<string>();
+
+      //CellMutationLog-2652J085SW--CASE CX160C--121107210953.tag
+      // Setup the mutation hook to capture cell pass generation
+      ICell_NonStatic_MutationHook Hook = DIContext.Obtain<ICell_NonStatic_MutationHook>();
+
+      Hook.SetActions(new CellPassWriter(x => Lines.Add(x)));
+      try
+      {
+        DITagFileFixture.ReadTAGFile("Dimensions2018-CaseMachine", tagFileName);
+      }
+      finally
+      {
+        Hook.ClearActions();
+      }
+
+      // Load the 'truth' mutation log
+      var mutationLog = File.ReadAllLines(Path.Combine("TestData", "TagFiles", "Dimensions2018-CaseMachine", mutationLogFileName));
+
+      for (int i = 0; i < Lines.Count; i++)
+        Lines[i].Should().Be(mutationLog[i]);
     }
   }
 }
