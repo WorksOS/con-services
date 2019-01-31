e482d3ef0d4b0b0bc4decf3ffeae621c6914d8d0
Raymond Wilson <raymond_wilson@trimble.com>
2019-02-01 11:08:24 +1300

    #79098: Some initial code tidying in subgrid requests logic

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/Requests/LoggingMode/Utilities.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/Requests/LoggingMode/Utilities.cs
@@ -142,9 +142,13 @@ namespace VSS.TRex.Tests.Requests.LoggingMode
           PassType = PassType.Front
         });
 
+      // Ensure all cell passes register the correct elevation mapping mode
+      foreach (var cellPass in cellPasses)
+        siteModel.MachinesTargetValues[0].ElevationMappingModeStateEvents.GetValueAtDate(cellPass.Time, out _).Should().Be(ElevationMappingMode.MinimumElevation);
+
       AddSingleCellWithPasses(siteModel, 0, 0, cellPasses, 1, numPassesToCreate);
 
-      // Add a set of passes in the second minute to capture latest elevation mode filling
+      // Add a set of passes in the second minute to capture elevation mode filling
       cellPasses = Enumerable.Range(0, numPassesToCreate).Select(x =>
         new CellPass
         {
@@ -154,6 +158,10 @@ namespace VSS.TRex.Tests.Requests.LoggingMode
           PassType = PassType.Front
         });
 
+      // Ensure all cell passes register the correct elevation mapping mode
+      foreach (var cellPass in cellPasses)
+        siteModel.MachinesTargetValues[0].ElevationMappingModeStateEvents.GetValueAtDate(cellPass.Time, out _).Should().Be(ElevationMappingMode.LatestElevation);
+
       AddSingleCellWithPasses(siteModel, 0, 0, cellPasses, 1, 2 * numPassesToCreate);
 
       // Add a set of passes in the third minute to capture latest elevation mode cutting back through the original cut zone
@@ -162,11 +170,28 @@ namespace VSS.TRex.Tests.Requests.LoggingMode
         {
           InternalSiteModelMachineIndex = 0,
           Time = baseTime.AddMinutes(2).AddSeconds(x * timeIncrementSeconds),
-          Height = baseHeight + x * -heightDecrement,
+          Height = baseHeight + x * heightDecrement,
           PassType = PassType.Front
         });
 
+      // Ensure all cell passes register the correct elevation mapping mode
+      foreach (var cellPass in cellPasses)
+        siteModel.MachinesTargetValues[0].ElevationMappingModeStateEvents.GetValueAtDate(cellPass.Time, out _).Should().Be(ElevationMappingMode.MinimumElevation);
+
       AddSingleCellWithPasses(siteModel, 0, 0, cellPasses, 1, 3 * numPassesToCreate);
+
+      // Add a set of passes in the fourth minute to mimic side trimming of the hole while in minimum elevation mode.
+      // Use half the height increment of these passes to discriminate them from the cutting passes created above,.
+      cellPasses = Enumerable.Range(0, numPassesToCreate).Select(x =>
+        new CellPass
+        {
+          InternalSiteModelMachineIndex = 0,
+          Time = baseTime.AddMinutes(3).AddSeconds(x * timeIncrementSeconds),
+          Height = baseHeight + x * (heightDecrement / 2),
+          PassType = PassType.Front
+        });
+
+      AddSingleCellWithPasses(siteModel, 0, 0, cellPasses, 1, 4 * numPassesToCreate);
 
       // Ensure all cell passes register the correct elevation mapping mode
       foreach (var cellPass in cellPasses)
