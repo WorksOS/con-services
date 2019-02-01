8beae674725b748572f4be7ad041b6642c62b13a
Stephen Post <stephen_post@trimble.com>
2019-02-01 02:15:01 +0000

    Merged PR 12369: 3D Now empty service
    An empty service for the 3d now service,
    
    Related work items: #79404

---
+++ src/service/3dNow/Service/VSS.Productivity3D.3DNow/HeartbeatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VSS.Productivity3D.Push
+{
+  public class HeartbeatController : Controller
+  {
+    [HttpGet("/")]
+    public IActionResult Echo()
+    {
+      return Json(new
+      {
+        CurrentTime = DateTime.UtcNow,
+      });
+    }
+  }
+}
