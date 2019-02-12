22231f7b5a799c2e35fc5e6a026dbdee8b81d513
Raymond Wilson <raymond_wilson@trimble.com>
2019-02-13 09:49:49 +1300

    #79098: Remove dead code

--- src/service/TRex/src/netstandard/VSS.TRex.Designs/TTMDesign.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Designs/TTMDesign.cs
@@ -572,9 +572,6 @@ namespace VSS.TRex.Designs
               if (x < triangleCellExtents[i].MinX || x > triangleCellExtents[i].MaxX || y < triangleCellExtents[i].MinY || y > triangleCellExtents[i].MaxY)
                 continue; // No intersection, move to next triangle
 
-              //Triangle tri = TriangleItems[SpatialIndexOptimisedTriangles[arrayReference.TriangleArrayIndex + i]];
-              //double Z = XYZ.GetTriangleHeightEx(ref VertexItems[tri.Vertex0], ref VertexItems[tri.Vertex1], ref VertexItems[tri.Vertex2], X, Y);
-
               double Z = GetHeight2(ref TriangleItems[SpatialIndexOptimisedTriangles[arrayReference.TriangleArrayIndex + i]], X, Y);
 
               if (Z != Common.Consts.NullReal)
