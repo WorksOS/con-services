d7f8f71de337998cdfab01e25541c377ad9ddaa8
Raymond Wilson <raymond_wilson@trimble.com>
2019-02-18 10:12:13 +1300

    #80024: PR feedback

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/Volumes/SimpleVolumesRequestTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/Volumes/SimpleVolumesRequestTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentAssertions;
 using VSS.TRex.Filters;
 using VSS.TRex.Geometry;
 using VSS.TRex.Tests.TestFixtures;
@@ -11,103 +12,80 @@ using Xunit;
 
 namespace VSS.TRex.Tests.Volumes
 {
-    public class SimpleVolumesRequestTests : IClassFixture<DITAGFileAndSubGridRequestsWithIgniteFixture>
+  public class SimpleVolumesRequestTests : IClassFixture<DITAGFileAndSubGridRequestsWithIgniteFixture>
+  {
+    [Fact]
+    public void Test_SimpleVolumesRequest_Creation1()
     {
-        [Fact]
-        public void Test_SimpleVolumesRequest_Creation1()
-        {
-            SimpleVolumesRequest_ApplicationService request = new SimpleVolumesRequest_ApplicationService();
+      SimpleVolumesRequest_ApplicationService request = new SimpleVolumesRequest_ApplicationService();
 
-            Assert.NotNull(request);
-        }
-
-        [Fact]
-        public void Test_SimpleVolumesRequest_Creation2()
-        {
-            SimpleVolumesRequest_ClusterCompute request = new SimpleVolumesRequest_ClusterCompute();
-
-            Assert.NotNull(request);
-        }
-
-        [Fact]
-        public void Test_SimpleVolumesRequest_ApplicationService_DefaultFilterToFilter_Execute_NoData()
-        {
-            DITAGFileAndSubGridRequestsWithIgniteFixture.AddApplicationGridRouting<SimpleVolumesRequestComputeFunc_ApplicationService, SimpleVolumesRequestArgument, SimpleVolumesResponse>();
-            DITAGFileAndSubGridRequestsWithIgniteFixture.AddClusterComputeGridRouting<SimpleVolumesRequestComputeFunc_ClusterCompute, SimpleVolumesRequestArgument, SimpleVolumesResponse>();
-
-            SimpleVolumesRequest_ApplicationService request = new SimpleVolumesRequest_ApplicationService();
-            SimpleVolumesRequestArgument arg = new SimpleVolumesRequestArgument
-            {
-                ProjectID = Guid.NewGuid(),
-                VolumeType = VolumeComputationType.Between2Filters,
-                BaseFilter = new CombinedFilter
-                {
-                    AttributeFilter = 
-                    {
-                        ReturnEarliestFilteredCellPass = true,
-                    }
-                },
-                TopFilter = new CombinedFilter(),
-                BaseDesignID = Guid.Empty,
-                TopDesignID = Guid.Empty,
-                CutTolerance = 0.001,
-                FillTolerance = 0.001
-            };
-
-            SimpleVolumesResponse response = request.Execute(arg);
-
-            // This request will fail (Ignite not accessible from unit tests) and will return a 
-            // response with null values
-
-            Assert.NotNull(response);
-            Assert.True(!response.Cut.HasValue &&
-                        !response.Fill.HasValue &&
-                        !response.TotalCoverageArea.HasValue &&
-                        !response.CutArea.HasValue &&
-                        !response.FillArea.HasValue &&
-                        response.BoundingExtentGrid.Equals(BoundingWorldExtent3D.Null()),
-              // No LL conversion available yet && !response.BoundingExtentLLH.Equals(BoundingWorldExtent3D.Null()),
-              "Response values are not null, unexpected");
+      Assert.NotNull(request);
     }
 
-        [Fact]
-        public void Test_SimpleVolumesRequest_ClusterCompute_DefaultFilterToFilter_Execute_NoData()
-        {       
-            DITAGFileAndSubGridRequestsWithIgniteFixture.AddClusterComputeGridRouting<SimpleVolumesRequestComputeFunc_ClusterCompute, SimpleVolumesRequestArgument, SimpleVolumesResponse>();
+    [Fact]
+    public void Test_SimpleVolumesRequest_Creation2()
+    {
+      SimpleVolumesRequest_ClusterCompute request = new SimpleVolumesRequest_ClusterCompute();
 
-            SimpleVolumesRequest_ClusterCompute request = new SimpleVolumesRequest_ClusterCompute();
-            SimpleVolumesRequestArgument arg = new SimpleVolumesRequestArgument
-            {
-                ProjectID = Guid.NewGuid(), // = 6; This needs to change to refer to an actual project
-                VolumeType = VolumeComputationType.Between2Filters,
-                BaseFilter = new CombinedFilter
-                {
-                    AttributeFilter =
-                    {
-                        ReturnEarliestFilteredCellPass = true,
-                    }
-                },
-                TopFilter = new CombinedFilter(),
-                BaseDesignID = Guid.Empty,
-                TopDesignID = Guid.Empty,
-                CutTolerance = 0.001,
-                FillTolerance = 0.001
-            };
-
-            SimpleVolumesResponse response = request.Execute(arg);
-
-            // This request will fail (Ignite not accessible from unit tests) and will return a 
-            // response with null values
-
-            Assert.NotNull(response);
-            Assert.True(!response.Cut.HasValue &&
-                        !response.Fill.HasValue &&
-                        !response.TotalCoverageArea.HasValue &&
-                        !response.CutArea.HasValue &&
-                        !response.FillArea.HasValue &&
-                        response.BoundingExtentGrid.Equals(BoundingWorldExtent3D.Null()),
-                // No LL conversion available yet && !response.BoundingExtentLLH.Equals(BoundingWorldExtent3D.Null()),
-                "Response values are not null, unexpected");
-        }
+      Assert.NotNull(request);
     }
+
+    private SimpleVolumesRequestArgument SimpleDefaultRequestArg()
+    {
+      return new SimpleVolumesRequestArgument
+      {
+        ProjectID = Guid.NewGuid(),
+        VolumeType = VolumeComputationType.Between2Filters,
+        BaseFilter = new CombinedFilter
+        {
+          AttributeFilter =
+          {
+            ReturnEarliestFilteredCellPass = true,
+          }
+        },
+        TopFilter = new CombinedFilter(),
+        BaseDesignID = Guid.Empty,
+        TopDesignID = Guid.Empty,
+        CutTolerance = 0.001,
+        FillTolerance = 0.001
+      };
+    }
+
+    private void CheckResponseContainsNullValues(SimpleVolumesResponse response)
+    {
+      response.Should().NotBeNull();
+      response.Cut.Should().BeNull();
+      response.Fill.Should().BeNull();
+      response.CutArea.Should().BeNull();
+      response.FillArea.Should().BeNull();
+      response.TotalCoverageArea.Should().BeNull();
+      response.BoundingExtentGrid.Should().BeEquivalentTo(BoundingWorldExtent3D.Null());
+    }
+
+    [Fact]
+    public void Test_SimpleVolumesRequest_ApplicationService_DefaultFilterToFilter_Execute_NoData()
+    {
+      DITAGFileAndSubGridRequestsWithIgniteFixture.AddApplicationGridRouting<SimpleVolumesRequestComputeFunc_ApplicationService, SimpleVolumesRequestArgument, SimpleVolumesResponse>();
+      DITAGFileAndSubGridRequestsWithIgniteFixture.AddClusterComputeGridRouting<SimpleVolumesRequestComputeFunc_ClusterCompute, SimpleVolumesRequestArgument, SimpleVolumesResponse>();
+
+      SimpleVolumesRequest_ApplicationService request = new SimpleVolumesRequest_ApplicationService();
+      SimpleVolumesResponse response = request.Execute(SimpleDefaultRequestArg());
+
+      // This is a no data test, so the response will be null
+      CheckResponseContainsNullValues(response);
+    }
+
+    [Fact]
+    public void Test_SimpleVolumesRequest_ClusterCompute_DefaultFilterToFilter_Execute_NoData()
+    {
+      DITAGFileAndSubGridRequestsWithIgniteFixture.AddClusterComputeGridRouting<SimpleVolumesRequestComputeFunc_ClusterCompute, SimpleVolumesRequestArgument, SimpleVolumesResponse>();
+
+      SimpleVolumesRequest_ClusterCompute request = new SimpleVolumesRequest_ClusterCompute();
+
+      SimpleVolumesResponse response = request.Execute(SimpleDefaultRequestArg());
+
+      // This is a no data test, so the response will be null
+      CheckResponseContainsNullValues(response);
+    }
+  }
 }
