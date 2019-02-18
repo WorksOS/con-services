35f233e82f45b2c7fe8c0fb844e7dcbe3009e2d9
Raymond Wilson <raymond_wilson@trimble.com>
2019-02-19 08:31:27 +1300

    #88024: Add additional type, value and non-null count assertions

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/Exports/Patches/PatchRequestTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/Exports/Patches/PatchRequestTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using FluentAssertions;
 using VSS.Productivity3D.Models.Enums;
 using VSS.TRex.Cells;
@@ -13,6 +11,7 @@ using VSS.TRex.GridFabric.Arguments;
 using VSS.TRex.GridFabric.Responses;
 using VSS.TRex.SiteModels.Interfaces;
 using VSS.TRex.SubGrids.GridFabric.ComputeFuncs;
+using VSS.TRex.SubGridTrees.Client;
 using VSS.TRex.SubGridTrees.Interfaces;
 using VSS.TRex.Tests.TestFixtures;
 using VSS.TRex.Types;
@@ -109,6 +108,12 @@ namespace VSS.TRex.Tests.Exports.Patches
       response.Should().NotBeNull();
       response.SubGrids.Should().NotBeNull();
       response.SubGrids.Count.Should().Be(10);
+
+      response.SubGrids.ForEach(x => x.Should().BeOfType<ClientHeightAndTimeLeafSubGrid>());
+
+      int nonNullCellCount = 0;
+      response.SubGrids.ForEach(x => nonNullCellCount += ((ClientHeightAndTimeLeafSubGrid)x).CountNonNullCells());
+      nonNullCellCount.Should().Be(2743);
     }
 
     [Fact]
@@ -125,6 +130,9 @@ namespace VSS.TRex.Tests.Exports.Patches
       response.Should().NotBeNull();
       response.SubGrids.Should().NotBeNull();
       response.SubGrids.Count.Should().Be(1);
+      response.SubGrids[0].CountNonNullCells().Should().Be(1);
+      response.SubGrids[0].Should().BeOfType<ClientHeightAndTimeLeafSubGrid>();
+      ((ClientHeightAndTimeLeafSubGrid)response.SubGrids[0]).Cells[0, 0].Should().BeApproximately(5.5f, 0.000001f);
     }
   }
 }
