c6bfc20ae0c1278a82f6a0301f32f1e816699461
Raymond Wilson <raymond_wilson@trimble.com>
2019-03-12 23:06:51 +1300

    #80650: Complete coverage for TRexGrids ans TRexCaches

---
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/Storage/TRexGridsTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using VSS.TRex.GridFabric.Grids;
+using VSS.TRex.Storage.Models;
+using Xunit;
+
+namespace VSS.TRex.Tests.Storage
+{
+  public class TRexGridsTests
+  {
+    [Fact]
+    public void GridNames()
+    {
+      TRexGrids.GridNames().Should().Contain(TRexGrids.GridName(StorageMutability.Mutable), TRexGrids.GridName(StorageMutability.Immutable));
+    }
+
+    [Fact]
+    public void MutableGridName()
+    {
+      TRexGrids.GridName(StorageMutability.Mutable).Should().NotBeNullOrWhiteSpace();
+      TRexGrids.MutableGridName().Should().Be(TRexGrids.GridName(StorageMutability.Mutable));
+    }
+
+    [Fact]
+    public void ImmutableGridName()
+    {
+      TRexGrids.GridName(StorageMutability.Immutable).Should().NotBeNullOrWhiteSpace();
+      TRexGrids.MutableGridName().Should().Be(TRexGrids.GridName(StorageMutability.Mutable));
+    }
+  }
+}
