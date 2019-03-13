1c465714d748014cf5e938e33371c2238fb7740b
Raymond Wilson <raymond_wilson@trimble.com>
2019-03-14 08:10:52 +1300

    #80650: More tests plus added TestBinary_ReaderWriterBufferedHelper

---
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/BinaryReaderWriter/TestBinary_ReaderWriterBufferedHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using FluentAssertions;
+using VSS.TRex.Common.Utilities.Interfaces;
+
+namespace VSS.TRex.Tests.BinaryReaderWriter
+{
+  public static class TestBinary_ReaderWriterBufferedHelper
+  {
+    public static void RoundTripSerialise<T>(T instance) where T : IBinaryReaderWriterBuffered, new()
+    {
+      byte[]  buffer = new byte[10000];
+
+      // Test using standard Read()/Write()
+      var writer = new BinaryWriter(new MemoryStream());
+      instance.Write(writer, buffer);
+
+      (writer.BaseStream as MemoryStream).Position = 0;
+      var instance2 = new T();
+      instance2.Read(new BinaryReader(writer.BaseStream as MemoryStream), buffer);
+
+      instance.Should().BeEquivalentTo(instance2);
+    }
+  }
+}
