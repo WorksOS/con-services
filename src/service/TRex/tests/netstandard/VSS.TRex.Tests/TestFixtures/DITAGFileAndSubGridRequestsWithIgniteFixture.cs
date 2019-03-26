bc3f6f34ea1125c375945075811531a50a11b6d7
Raymond Wilson <raymond_wilson@trimble.com>
2019-03-27 10:50:48 +1300

    #81498: Added AddSurveyedSurfaceToSiteModel and ConstructSingleFlatTriangleSurveyedSurfaceOffsetFromOrigin methods to fixture

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/TestFixtures/DITAGFileAndSubGridRequestsWithIgniteFixture.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/TestFixtures/DITAGFileAndSubGridRequestsWithIgniteFixture.cs
@@ -30,6 +30,7 @@ using VSS.TRex.SiteModels.GridFabric.Events;
 using VSS.TRex.SiteModels.Interfaces;
 using VSS.TRex.SiteModels.Interfaces.Events;
 using VSS.TRex.SubGridTrees.Interfaces;
+using VSS.TRex.SurveyedSurfaces.Interfaces;
 using VSS.TRex.Types;
 using Consts = VSS.TRex.ExistenceMaps.Interfaces.Consts;
 
@@ -115,8 +116,8 @@ namespace VSS.TRex.Tests.TestFixtures
       var filePathAndName = Path.Combine(filePath, fileName);
 
       TTMDesign ttm = new TTMDesign(SubGridTreeConsts.DefaultCellSize);
-      var designLoadResult = ttm.LoadFromFile(filePathAndName, constructIndexFilesOnLoad); 
-      designLoadResult.Should().Be(DesignLoadResult.Success);
+      var loadResult = ttm.LoadFromFile(filePathAndName, constructIndexFilesOnLoad);
+      loadResult.Should().Be(DesignLoadResult.Success);
 
       BoundingWorldExtent3D extents = new BoundingWorldExtent3D();
       ttm.GetExtents(out extents.MinX, out extents.MinY, out extents.MaxX, out extents.MaxY);
@@ -130,7 +131,7 @@ namespace VSS.TRex.Tests.TestFixtures
         new DesignDescriptor(designUid, filePath, fileName, 0), extents);
       existenceMaps.SetExistenceMap(siteModel.ID, Consts.EXISTENCE_MAP_DESIGN_DESCRIPTOR, designSurface.ID, ttm.SubGridOverlayIndex());
 
-      // Tell the sitemodels collection the site model has changed 
+      // Tell the site models collection the site model has changed 
       DIContext.Obtain<ISiteModels>().SiteModelAttributesHaveChanged(new SiteModelAttributesChangedEvent
         {
           SiteModelID = siteModel.ID,
@@ -155,6 +156,52 @@ namespace VSS.TRex.Tests.TestFixtures
       return designUid;
     }
 
+    public static Guid AddSurveyedSurfaceToSiteModel(ref ISiteModel siteModel, string filePath, string fileName, DateTime asAtDate,
+     bool constructIndexFilesOnLoad)
+    {
+      var filePathAndName = Path.Combine(filePath, fileName);
+
+      TTMDesign ttm = new TTMDesign(SubGridTreeConsts.DefaultCellSize);
+      var loadResult = ttm.LoadFromFile(filePathAndName, constructIndexFilesOnLoad);
+      loadResult.Should().Be(DesignLoadResult.Success);
+
+      BoundingWorldExtent3D extents = new BoundingWorldExtent3D();
+      ttm.GetExtents(out extents.MinX, out extents.MinY, out extents.MaxX, out extents.MaxY);
+      ttm.GetHeightRange(out extents.MinZ, out extents.MaxZ);
+
+      Guid surveyedSurfaceUid = Guid.NewGuid();
+      var existenceMaps = DIContext.Obtain<IExistenceMaps>();
+
+      // Create the design surface in the site model
+      var surveyedSurface = DIContext.Obtain<ISurveyedSurfaceManager>().Add(siteModel.ID,
+        new DesignDescriptor(surveyedSurfaceUid, filePath, fileName, 0), asAtDate, extents);
+      existenceMaps.SetExistenceMap(siteModel.ID, Consts.EXISTENCE_SURVEYED_SURFACE_DESCRIPTOR, surveyedSurface.ID, ttm.SubGridOverlayIndex());
+
+      // Tell the site models collection the site model has changed 
+      DIContext.Obtain<ISiteModels>().SiteModelAttributesHaveChanged(new SiteModelAttributesChangedEvent
+      {
+        SiteModelID = siteModel.ID,
+        SurveyedSurfacesModified = true
+      });
+
+      // get the newly updated site model with the surveyed surface included
+      siteModel = DIContext.Obtain<ISiteModels>().GetSiteModel(siteModel.ID);
+
+      // Place the surveyed surface into the project temp folder prior to executing the render so the design profiler
+      // will not attempt to access the file from S3
+      var tempPath = FilePathHelper.GetTempFolderForProject(siteModel.ID);
+      var srcFileName = Path.Combine(filePath, fileName);
+      var destFileName = Path.Combine(tempPath, fileName);
+
+      File.Copy(srcFileName, destFileName);
+      File.Copy(srcFileName + TRex.Designs.TTM.Optimised.Consts.DESIGN_SUB_GRID_INDEX_FILE_EXTENSION,
+                destFileName + TRex.Designs.TTM.Optimised.Consts.DESIGN_SUB_GRID_INDEX_FILE_EXTENSION);
+      File.Copy(srcFileName + TRex.Designs.TTM.Optimised.Consts.DESIGN_SPATIAL_INDEX_FILE_EXTENSION,
+                destFileName + TRex.Designs.TTM.Optimised.Consts.DESIGN_SPATIAL_INDEX_FILE_EXTENSION);
+
+      return surveyedSurfaceUid;
+    }
+
     public static ISiteModel NewEmptyModel()
     {
       var siteModel = DIContext.Obtain<ISiteModels>().GetSiteModel(DITagFileFixture.NewSiteModelGuid, true);
@@ -176,6 +223,27 @@ namespace VSS.TRex.Tests.TestFixtures
 
       return DITAGFileAndSubGridRequestsWithIgniteFixture.AddDesignToSiteModel
         (ref siteModel, Path.GetDirectoryName(tempFileName), Path.GetFileName(tempFileName), true);
+    }
+
+    public static Guid ConstructSingleFlatTriangleSurveyedSurfaceOffsetFromOrigin(ref ISiteModel siteModel, float elevation, DateTime asAtDate, double offsetX, double offsetY)
+    {
+      // Make a mutable TIN containing a single triangle and as below and register it to the site model
+      VSS.TRex.Designs.TTM.TrimbleTINModel tin = new TrimbleTINModel();
+      tin.Vertices.InitPointSearch(-100 + offsetX, -100 + offsetY, 100 + offsetX, 100 + offsetY, 3);
+      tin.Triangles.AddTriangle(tin.Vertices.AddPoint(-25 + offsetX, -25, elevation),
+        tin.Vertices.AddPoint(25 + offsetX, -25 + offsetY, elevation),
+        tin.Vertices.AddPoint(0 + offsetX, 25 + offsetY, elevation));
+
+      var tempFileName = Path.GetTempFileName() + ".ttm";
+      tin.SaveToFile(tempFileName, true);
+
+      return DITAGFileAndSubGridRequestsWithIgniteFixture.AddSurveyedSurfaceToSiteModel
+        (ref siteModel, Path.GetDirectoryName(tempFileName), Path.GetFileName(tempFileName), asAtDate, true);
+    }
+
+    public static Guid ConstructSingleFlatTriangleSurveyedSurfaceAboutOrigin(ref ISiteModel siteModel, float elevation, DateTime asAtDate)
+    {
+      return ConstructSingleFlatTriangleSurveyedSurfaceOffsetFromOrigin(ref siteModel, elevation, asAtDate, 0, 0);
     }
 
     public new void Dispose()
