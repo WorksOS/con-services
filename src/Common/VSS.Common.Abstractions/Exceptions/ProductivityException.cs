b2b8ac8f7962e2debf340347eec27c782ffb76d6
Stephen Post <stephen_post@trimble.com>
2019-04-17 15:59:32 +1200

    Add custom exceptions for Service Discovery

---
+++ src/Common/VSS.Common.Abstractions/Exceptions/ProductivityException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VSS.Common.Abstractions.Exceptions
+{
+  public abstract class ProductivityException : Exception
+  {
+    protected ProductivityException(string message) : base(message)
+    {
+      
+    }
+
+    protected ProductivityException(string message, Exception innerException) : base(message, innerException)
+    {
+      
+    }
+  }
+}
---
+++ src/Common/VSS.Common.Abstractions/ServiceDiscovery/Exceptions/ServiceNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using VSS.Common.Abstractions.Exceptions;
+
+namespace VSS.Common.Abstractions.ServiceDiscovery.Exceptions
+{
+  /// <summary>
+  /// No service found in Service Discovery
+  /// </summary>
+  public class ServiceNotFoundException : ProductivityException
+  {
+    public ServiceNotFoundException(string serviceName) : base($"Could not find a service for '{serviceName}'")
+    {
+      
+    }
+  }
+}
