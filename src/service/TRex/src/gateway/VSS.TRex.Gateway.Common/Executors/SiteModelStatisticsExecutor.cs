88b805117f832815efff53da2aa5a696431dca50
Jeannie May <Jeannie_May@trimble.com>
2019-05-01 15:55:42 +1200

    #82845 ***NO_CI***

---
+++ src/service/TRex/src/gateway/VSS.TRex.Gateway.Common/Executors/SiteModelStatisticsExecutor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using VSS.ConfigurationStore;
+using VSS.MasterData.Models.Handlers;
+using VSS.MasterData.Models.Models;
+using VSS.MasterData.Models.ResultHandling.Abstractions;
+using VSS.Productivity3D.Models.Models;
+using VSS.Productivity3D.Models.ResultHandling;
+using VSS.TRex.Executors;
+
+namespace VSS.TRex.Gateway.Common.Executors
+{
+  public class SiteModelStatisticsExecutor : BaseExecutor
+  {
+    public SiteModelStatisticsExecutor(IConfigurationStore configStore, ILoggerFactory logger,
+      IServiceExceptionHandler exceptionHandler) : base(configStore, logger, exceptionHandler)
+    {
+    }
+
+    /// <summary>
+    /// Default constructor for RequestExecutorContainer.Build
+    /// </summary>
+    public SiteModelStatisticsExecutor()
+    {
+    }
+
+    protected override ContractExecutionResult ProcessEx<T>(T item)
+    {
+      var request = item as ProjectStatisticsTRexRequest;
+      if (request == null)
+      {
+        ThrowRequestTypeCastException<SiteModelStatisticsExecutor>();
+        return null; // to keep compiler happy
+      }
+
+      var siteModel = GetSiteModel(request.ProjectUid);
+      var extents = ProjectExtents.ProductionDataAndSurveyedSurfaces(request.ProjectUid,
+        request.ExcludedSurveyedSurfaceUids);
+
+      var result = new ProjectStatisticsResult();
+      if (extents != null)
+        result.extents = new BoundingBox3DGrid(
+          extents.MinX, extents.MinY, extents.MinZ,
+          extents.MaxX, extents.MaxY, extents.MaxZ
+        );
+
+      var startEndDates = siteModel.GetDateRange();
+      result.startTime = startEndDates.startUtc; // todoJeannie
+      result.endTime = startEndDates.endUtc;
+
+      result.cellSize = siteModel.Grid.CellSize;
+      result.indexOriginOffset = (int)siteModel.Grid.IndexOriginOffset;
+      return result;
+    }
+
+    /// <summary>
+    /// Processes the request asynchronously.
+    /// </summary>
+    protected override async Task<ContractExecutionResult> ProcessAsyncEx<T>(T item)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
