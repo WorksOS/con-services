732b29037e05231fad6dbad82b5abb5e93fbb388
Stephen Post <stephen_post@trimble.com>
2019-05-17 02:46:16 +0000

    Merged PR 13636: [SF 02862268] Cannot export data due to error converting  'Malay Peninsula Standard Time' to a Timezone
    See the bug details, but it's explain here: https://stackoverflow.com/questions/31589491/time-zone-issue-with-malay-peninsula-standard-time
    
    Related work items: #83396

--- src/service/3DP/test/UnitTests/WebApiTests/Compaction/Helpers/ExportRequestHelperTests.cs
+++ src/service/3DP/test/UnitTests/WebApiTests/Compaction/Helpers/ExportRequestHelperTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VSS.MasterData.Models.Models;
 using VSS.Productivity3D.WebApi.Models.Compaction.Helpers;
 
@@ -26,6 +27,24 @@ namespace VSS.Productivity3D.WebApiTests.Compaction.Helpers
         Timezone = "Aleutian Standard Time"
       },
       null);
+    }
+
+
+    [TestMethod]
+    public void ConvertMalayPeninsulaStandardTime()
+    {
+      // see https://stackoverflow.com/questions/31589491/time-zone-issue-with-malay-peninsula-standard-time
+      var expectedTimezone = TimeZoneInfo.FindSystemTimeZoneById("Singapore Standard Time");
+      double expectedOffset = expectedTimezone.GetUtcOffset(DateTime.Now).TotalHours;
+
+      // Bug: 83396
+      var prefs = ExportRequestHelper.ConvertUserPreferences(new UserPreferenceData
+        {
+          Timezone = "Malay Peninsula Standard Time"
+        },
+        null);
+
+      Assert.AreEqual(prefs.TimeZoneOffset, expectedOffset);
     }
   }
 }
