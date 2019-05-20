17ef62117379ce746a0279c4c68bde059c934168
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2019-05-20 13:34:35 +1200

    #81789 Handle activation/deactivation of ref surfaces

---
+++ src/service/Project/AcceptanceTests/tests/WebApiTests/FileImportTests-Dxf.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestUtility;
+
+namespace WebApiTests
+{
+  [TestClass]
+  public class FileImportTestsDxf
+  {
+    private readonly Msg msg = new Msg();
+
+    [TestMethod]
+    [DataRow("api/v4/importedfile")]
+    [DataRow("api/v4/importedfile/direct")]
+    public void TestImportdxfFileUsSurveyFeet(string uriRoot)
+    {
+      const string testName = "File Import 17";
+      msg.Title(testName, "Create standard project and customer then upload dxf file");
+      var ts = new TestSupport();
+      var importFile = new ImportFile(uriRoot);
+      var legacyProjectId = ts.SetLegacyProjectId();
+      var projectUid = Guid.NewGuid().ToString();
+      var customerUid = Guid.NewGuid();
+      var tccOrg = Guid.NewGuid();
+      var subscriptionUid = Guid.NewGuid();
+      var startDateTime = ts.FirstEventDate;
+      var endDateTime = new DateTime(9999, 12, 31);
+      var startDate = startDateTime.ToString("yyyy-MM-dd");
+      var endDate = endDateTime.ToString("yyyy-MM-dd");
+      const string geometryWkt = "POLYGON((-121.347189366818 38.8361907402694,-121.349260032177 38.8361656688414,-121.349217116833 38.8387897637231,-121.347275197506 38.8387145521594,-121.347189366818 38.8361907402694,-121.347189366818 38.8361907402694))";
+      var eventsArray = new[] {
+       "| TableName           | EventDate   | CustomerUID   | Name       | fk_CustomerTypeID | SubscriptionUID   | fk_CustomerUID | fk_ServiceTypeID | StartDate   | EndDate        | fk_ProjectUID | TCCOrgID | fk_SubscriptionUID |",
+      $"| Customer            | 0d+09:00:00 | {customerUid} | {testName} | 1                 |                   |                |                  |             |                |               |          |                    |",
+      $"| CustomerTccOrg      | 0d+09:00:00 | {customerUid} |            |                   |                   |                |                  |             |                |               | {tccOrg} |                    |",
+      $"| Subscription        | 0d+09:10:00 |               |            |                   | {subscriptionUid} | {customerUid}  | 19               | {startDate} | {endDate}      |               |          |                    |",
+      $"| ProjectSubscription | 0d+09:20:00 |               |            |                   |                   |                |                  | {startDate} |                | {projectUid}  |          | {subscriptionUid}  |"};
+      ts.PublishEventCollection(eventsArray);
+
+      ts.IsPublishToWebApi = true;
+      var projectEventArray = new[] {
+       "| EventType          | EventDate   | ProjectUID   | ProjectID         | ProjectName | ProjectType | ProjectTimezone           | ProjectStartDate                            | ProjectEndDate                             | ProjectBoundary | CustomerUID   | CustomerID        | IsArchived | CoordinateSystem      | Description |",
+      $"| CreateProjectEvent | 0d+09:00:00 | {projectUid} | {legacyProjectId} | {testName}  | Standard    | New Zealand Standard Time | {startDateTime:yyyy-MM-ddTHH:mm:ss.fffffff} | {endDateTime:yyyy-MM-ddTHH:mm:ss.fffffff}  | {geometryWkt}   | {customerUid} | {legacyProjectId} | false      | BootCampDimensions.dc | {testName}  |"};
+      ts.PublishEventCollection(projectEventArray);
+      var importFileArray = new[] {
+       "| EventType              | ProjectUid   | CustomerUid   | Name                              | ImportedFileType | FileCreatedUtc  | FileUpdatedUtc             | ImportedBy                 | IsActivated | DxfUnitsType | MinZoomLevel | MaxZoomLevel |",
+      $"| ImportedFileDescriptor | {projectUid} | {customerUid} | {TestFile.TestDxFfile.FullPath()} | 0                | {startDateTime} | {startDateTime.AddDays(5)} | testProjectMDM@trimble.com | true        | 2            | 15           | 19           |"};
+      var filesResult = importFile.SendRequestToFileImportV4(ts, importFileArray, 1, new ImportOptions(HttpMethod.Post, new[] { $"filename={TestFile.TestDxFfile}" }));
+      ts.CompareTheActualImportFileWithExpected(filesResult.ImportedFileDescriptor, importFile.ExpectedImportFileDescriptorSingleResult.ImportedFileDescriptor, true);
+    }
+
+    [TestMethod]
+    [DataRow("api/v4/importedfile")]
+    [DataRow("api/v4/importedfile/direct")]
+    public void TestImportdxfFileImperial(string uriRoot)
+    {
+      const string testName = "File Import 18";
+      msg.Title(testName, "Create standard project and customer then upload dxf file in imperial");
+      var ts = new TestSupport();
+      var importFile = new ImportFile(uriRoot);
+      var legacyProjectId = ts.SetLegacyProjectId();
+      var projectUid = Guid.NewGuid().ToString();
+      var customerUid = Guid.NewGuid();
+      var tccOrg = Guid.NewGuid();
+      var subscriptionUid = Guid.NewGuid();
+      var startDateTime = ts.FirstEventDate;
+      var endDateTime = new DateTime(9999, 12, 31);
+      var startDate = startDateTime.ToString("yyyy-MM-dd");
+      var endDate = endDateTime.ToString("yyyy-MM-dd");
+      const string geometryWkt = "POLYGON((-121.347189366818 38.8361907402694,-121.349260032177 38.8361656688414,-121.349217116833 38.8387897637231,-121.347275197506 38.8387145521594,-121.347189366818 38.8361907402694,-121.347189366818 38.8361907402694))";
+      var eventsArray = new[] {
+       "| TableName           | EventDate   | CustomerUID   | Name       | fk_CustomerTypeID | SubscriptionUID   | fk_CustomerUID | fk_ServiceTypeID | StartDate   | EndDate        | fk_ProjectUID | TCCOrgID | fk_SubscriptionUID |",
+      $"| Customer            | 0d+09:00:00 | {customerUid} | {testName} | 1                 |                   |                |                  |             |                |               |          |                    |",
+      $"| CustomerTccOrg      | 0d+09:00:00 | {customerUid} |            |                   |                   |                |                  |             |                |               | {tccOrg} |                    |",
+      $"| Subscription        | 0d+09:10:00 |               |            |                   | {subscriptionUid} | {customerUid}  | 19               | {startDate} | {endDate}      |               |          |                    |",
+      $"| ProjectSubscription | 0d+09:20:00 |               |            |                   |                   |                |                  | {startDate} |                | {projectUid}  |          | {subscriptionUid}  |"};
+      ts.PublishEventCollection(eventsArray);
+
+      ts.IsPublishToWebApi = true;
+      var projectEventArray = new[] {
+       "| EventType          | EventDate   | ProjectUID   | ProjectID         | ProjectName | ProjectType | ProjectTimezone           | ProjectStartDate                            | ProjectEndDate                             | ProjectBoundary | CustomerUID   | CustomerID        | IsArchived | CoordinateSystem      | Description |",
+      $"| CreateProjectEvent | 0d+09:00:00 | {projectUid} | {legacyProjectId} | {testName}  | Standard    | New Zealand Standard Time | {startDateTime:yyyy-MM-ddTHH:mm:ss.fffffff} | {endDateTime:yyyy-MM-ddTHH:mm:ss.fffffff}  | {geometryWkt}   | {customerUid} | {legacyProjectId} | false      | BootCampDimensions.dc | {testName}  |"};
+      ts.PublishEventCollection(projectEventArray);
+      var importFileArray = new[] {
+       "| EventType              | ProjectUid   | CustomerUid   | Name                              | ImportedFileType | FileCreatedUtc  | FileUpdatedUtc             | ImportedBy                 | IsActivated | DxfUnitsType | MinZoomLevel | MaxZoomLevel |",
+      $"| ImportedFileDescriptor | {projectUid} | {customerUid} | {TestFile.TestDxFfile.FullPath()} | 0                | {startDateTime} | {startDateTime.AddDays(5)} | testProjectMDM@trimble.com | true        | 1            | 15           | 19           |"};
+      var filesResult = importFile.SendRequestToFileImportV4(ts, importFileArray, 1, new ImportOptions(HttpMethod.Post, new[] { $"filename={TestFile.TestDxFfile}" }));
+      ts.CompareTheActualImportFileWithExpected(filesResult.ImportedFileDescriptor, importFile.ExpectedImportFileDescriptorSingleResult.ImportedFileDescriptor, true);
+    }
+
+    [TestMethod]
+    [DataRow("api/v4/importedfile")]
+    [DataRow("api/v4/importedfile/direct")]
+    public void TestImportdxfFileMetric(string uriRoot)
+    {
+      const string testName = "File Import 19";
+      msg.Title(testName, "Create standard project and customer then upload dxf file in metric");
+      var ts = new TestSupport();
+      var importFile = new ImportFile(uriRoot);
+      var legacyProjectId = ts.SetLegacyProjectId();
+      var projectUid = Guid.NewGuid().ToString();
+      var customerUid = Guid.NewGuid();
+      var tccOrg = Guid.NewGuid();
+      var subscriptionUid = Guid.NewGuid();
+      var startDateTime = ts.FirstEventDate;
+      var endDateTime = new DateTime(9999, 12, 31);
+      var startDate = startDateTime.ToString("yyyy-MM-dd");
+      var endDate = endDateTime.ToString("yyyy-MM-dd");
+      const string geometryWkt = "POLYGON((-121.347189366818 38.8361907402694,-121.349260032177 38.8361656688414,-121.349217116833 38.8387897637231,-121.347275197506 38.8387145521594,-121.347189366818 38.8361907402694,-121.347189366818 38.8361907402694))";
+      var eventsArray = new[] {
+       "| TableName           | EventDate   | CustomerUID   | Name       | fk_CustomerTypeID | SubscriptionUID   | fk_CustomerUID | fk_ServiceTypeID | StartDate   | EndDate        | fk_ProjectUID | TCCOrgID | fk_SubscriptionUID |",
+      $"| Customer            | 0d+09:00:00 | {customerUid} | {testName} | 1                 |                   |                |                  |             |                |               |          |                    |",
+      $"| CustomerTccOrg      | 0d+09:00:00 | {customerUid} |            |                   |                   |                |                  |             |                |               | {tccOrg} |                    |",
+      $"| Subscription        | 0d+09:10:00 |               |            |                   | {subscriptionUid} | {customerUid}  | 19               | {startDate} | {endDate}      |               |          |                    |",
+      $"| ProjectSubscription | 0d+09:20:00 |               |            |                   |                   |                |                  | {startDate} |                | {projectUid}  |          | {subscriptionUid}  |"};
+      ts.PublishEventCollection(eventsArray);
+
+      ts.IsPublishToWebApi = true;
+      var projectEventArray = new[] {
+       "| EventType          | EventDate   | ProjectUID   | ProjectID         | ProjectName | ProjectType | ProjectTimezone           | ProjectStartDate                            | ProjectEndDate                             | ProjectBoundary | CustomerUID   | CustomerID        | IsArchived | CoordinateSystem      | Description |",
+      $"| CreateProjectEvent | 0d+09:00:00 | {projectUid} | {legacyProjectId} | {testName}  | Standard    | New Zealand Standard Time | {startDateTime:yyyy-MM-ddTHH:mm:ss.fffffff} | {endDateTime:yyyy-MM-ddTHH:mm:ss.fffffff}  | {geometryWkt}   | {customerUid} | {legacyProjectId} | false      | BootCampDimensions.dc | {testName}  |"};
+      ts.PublishEventCollection(projectEventArray);
+      var importFileArray = new[] {
+       "| EventType              | ProjectUid   | CustomerUid   | Name                              | ImportedFileType | FileCreatedUtc  | FileUpdatedUtc             | ImportedBy                 | IsActivated | DxfUnitsType | MinZoomLevel | MaxZoomLevel |",
+      $"| ImportedFileDescriptor | {projectUid} | {customerUid} | {TestFile.TestDxFfile.FullPath()} | 0                | {startDateTime} | {startDateTime.AddDays(5)} | testProjectMDM@trimble.com | true        | 0            | 15           | 19           |"};
+      var filesResult = importFile.SendRequestToFileImportV4(ts, importFileArray, 1, new ImportOptions(HttpMethod.Post, new[] { $"filename={TestFile.TestDxFfile}" }));
+      ts.CompareTheActualImportFileWithExpected(filesResult.ImportedFileDescriptor, importFile.ExpectedImportFileDescriptorSingleResult.ImportedFileDescriptor, true);
+    }
+
+  }
+}
--- src/service/Project/src/ProjectWebApi/Controllers/FileImportV4Controller.cs
+++ src/service/Project/src/ProjectWebApi/Controllers/FileImportV4Controller.cs
@@ -8,11 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Org.BouncyCastle.Asn1;
 using VSS.AWS.TransferProxy.Interfaces;
-using VSS.Common.Abstractions;
 using VSS.Common.Abstractions.Configuration;
-using VSS.ConfigurationStore;
 using VSS.DataOcean.Client;
 using VSS.FlowJSHandler;
 using VSS.KafkaConsumer.Kafka;
@@ -636,6 +633,17 @@ namespace VSS.MasterData.Project.WebAPI.Controllers
       var importedFileResult = await UpsertFileInternal(filename, null, projectUid, ImportedFileType.ReferenceSurface, DxfUnitsType.Meters,
         fileCreatedUtc, fileUpdatedUtc, null, schedulerProxy, parentUid, offset);
 
+      //If parent design is deactivated then deactivate reference surface
+      if (!parent.IsActivated)
+      {
+        var filesToUpdate = new Dictionary<Guid, bool>();
+        filesToUpdate.Add(new Guid(importedFileResult.ImportedFileDescriptor.ImportedFileUid), false);
+        await DoActivation(projectUid.ToString(), filesToUpdate);
+        importedFiles = await ImportedFileRequestDatabaseHelper.GetImportedFileList(projectUid.ToString(), logger, userId, projectRepo);
+        importedFileResult.ImportedFileDescriptor = importedFiles.SingleOrDefault(i =>
+          i.ImportedFileUid == importedFileResult.ImportedFileDescriptor.ImportedFileUid);
+      }
+
       logger.LogInformation(
         $"CreateReferenceSurface. Completed successfully. Response: {JsonConvert.SerializeObject(importedFileResult)}");
 
@@ -725,6 +733,13 @@ namespace VSS.MasterData.Project.WebAPI.Controllers
           continue;
         }
 
+        if (existingFile.ImportedFileType == ImportedFileType.ReferenceSurface)
+        {
+          logger.LogError(
+            $"{functionId}. Attempt to set file activation on a reference surface. projectUid {projectUid}, fileUid: {activatedFileDescriptor.ImportedFileUid}");
+          continue;
+        }
+
         if (existingFile.IsActivated == activatedFileDescriptor.IsActivated)
         {
           logger.LogDebug(
@@ -735,6 +750,23 @@ namespace VSS.MasterData.Project.WebAPI.Controllers
         logger.LogInformation(
           $"{functionId}. File queued for updating: {JsonConvert.SerializeObject(existingFile)}");
         filesToUpdate.Add(new Guid(activatedFileDescriptor.ImportedFileUid), activatedFileDescriptor.IsActivated);
+
+        //If user is activating or deactivating a design which has reference surfaces, do as a group
+        if (existingFile.ImportedFileType == ImportedFileType.DesignSurface)
+        {
+          var children = importedFiles
+            .Where(f => f.ParentUid.HasValue && f.ParentUid.ToString() == existingFile.ImportedFileUid).ToList();
+          if (children.Count > 0)
+          {
+            logger.LogInformation(
+              $"{functionId}. Setting file activation state of reference surfaces for design {existingFile.ImportedFileUid}");
+            foreach (var child in children)
+            {
+              if (child.IsActivated != activatedFileDescriptor.IsActivated)
+                filesToUpdate.Add(new Guid(child.ImportedFileUid), activatedFileDescriptor.IsActivated);
+            }
+          }
+        }
       }
 
       if (!filesToUpdate.Any())
@@ -746,11 +778,7 @@ namespace VSS.MasterData.Project.WebAPI.Controllers
 
       try
       {
-        var dbUpdateResult = await SetFileActivatedState(projectUid, filesToUpdate);
-        var notificationTask = notificationHubClient.Notify(new ProjectChangedNotification(Guid.Parse(projectUid)));
-        var raptorTask = NotifyRaptorUpdateFile(new Guid(projectUid), dbUpdateResult);
-
-        await Task.WhenAll(notificationTask, raptorTask);
+        await DoActivation(projectUid, filesToUpdate);
 
         return Ok(new { Code = HttpStatusCode.OK, Message = "Success" });
       }
@@ -758,6 +786,15 @@ namespace VSS.MasterData.Project.WebAPI.Controllers
       {
         return new JsonResult(new { Code = HttpStatusCode.InternalServerError, exception.GetBaseException().Message });
       }
+    }
+
+    private async Task DoActivation(string projectUid, Dictionary<Guid, bool> filesToUpdate)
+    {
+      var dbUpdateResult = await SetFileActivatedState(projectUid, filesToUpdate);
+      var notificationTask = notificationHubClient.Notify(new ProjectChangedNotification(Guid.Parse(projectUid)));
+      var raptorTask = NotifyRaptorUpdateFile(new Guid(projectUid), dbUpdateResult);
+
+      await Task.WhenAll(notificationTask, raptorTask);
     }
 
     #endregion fileActivation
