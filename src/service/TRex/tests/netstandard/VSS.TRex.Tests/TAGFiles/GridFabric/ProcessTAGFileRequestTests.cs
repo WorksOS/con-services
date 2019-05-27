5f498f03658a313c0c445cc7a47f73c55647fff4
Raymond Wilson <raymond_wilson@trimble.com>
2019-05-27 13:37:54 +1200

    #81498: Implement simple ProcessTAGFileRequest unit tests

---
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/TAGFiles/GridFabric/ProcessTAGFileRequestTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Apache.Ignite.Core.Compute;
+using FluentAssertions;
+using VSS.TRex.DI;
+using VSS.TRex.TAGFiles.GridFabric.Arguments;
+using VSS.TRex.TAGFiles.GridFabric.Requests;
+using VSS.TRex.TAGFiles.GridFabric.Responses;
+using VSS.TRex.Tests.TestFixtures;
+using Xunit;
+
+namespace VSS.TRex.Tests.TAGFiles.GridFabric
+{
+  [UnitTestCoveredRequest(RequestType = typeof(ProcessTAGFileRequest))]
+  public class ProcessTAGFileRequestTests : IClassFixture<DITAGFileAndSubGridRequestsWithIgniteFixture>
+  {
+
+    private void InjectTAGFileProcessorMock()
+    {
+      DIBuilder
+        .Continue()
+        .Complete();
+
+      IgniteMock.AddApplicationGridRouting<IComputeFunc<ProcessTAGFileRequestArgument, ProcessTAGFileResponse>, ProcessTAGFileRequestArgument, ProcessTAGFileResponse>();
+    }
+
+    [Fact]
+    public void Creation()
+    {
+      var req = new ProcessTAGFileRequest();
+
+      req.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ProcessSingleTAGFile_FailWithInvalidTAGFile()
+    {
+      InjectTAGFileProcessorMock();
+
+      var req = new ProcessTAGFileRequest();
+      var arg = new ProcessTAGFileRequestArgument
+      {
+        TAGFiles = new List<ProcessTAGFileRequestFileItem>
+        {
+          new ProcessTAGFileRequestFileItem
+          {
+            IsJohnDoe = false, FileName = "ATAGFileName", TagFileContent = new byte[] {1, 2, 3, 4, 5}
+          }
+        }
+      };
+
+      var response = req.Execute(arg);
+      response.Results.Count.Should().Be(1);
+      response.Results[0].Success.Should().BeFalse();
+      response.Results[0].Exception.Should().BeNull();
+    }
+
+    [Fact]
+    public void ProcessSingleTAGFile_Success()
+    {
+      InjectTAGFileProcessorMock();
+
+      var fileName = Path.Combine("TestData", "TAGFiles", "TestTAGFile-TAGFile-Read-Stream.tag");
+
+      var req = new ProcessTAGFileRequest();
+      var arg = new ProcessTAGFileRequestArgument
+      {
+        TAGFiles = new List<ProcessTAGFileRequestFileItem>
+        {
+          new ProcessTAGFileRequestFileItem
+          {
+            IsJohnDoe = false,
+            FileName = fileName,
+            TagFileContent = File.ReadAllBytes(fileName)
+          }
+        }
+      };
+
+      var response = req.Execute(arg);
+      response.Results.Count.Should().Be(1);
+      response.Results[0].Success.Should().BeTrue();
+      response.Results[0].Exception.Should().BeNull();
+    }
+  }
+}
