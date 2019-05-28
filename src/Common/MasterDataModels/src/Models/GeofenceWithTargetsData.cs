fbe93fc17f18cd6d56c0ff239e261aeea0a1cb11
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2019-05-28 16:12:10 +1200

    #83607 MockWebApi and MD models for VUP associated geofences ***NO_CI***

---
+++ src/Common/MasterDataModels/src/Models/GeofenceWithTargetsData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using VSS.Common.Abstractions.MasterData.Interfaces;
+
+namespace VSS.MasterData.Models.Models
+{
+  public class GeofenceWithTargetsData : IMasterDataModel
+  {
+    public GeofenceData Geofence { get; set; }
+    //Unified Productivity also returns Target and Backfill data here which we don't care about
+
+    public List<string> GetIdentifiers() => Geofence?.GetIdentifiers();
+  }
+}
