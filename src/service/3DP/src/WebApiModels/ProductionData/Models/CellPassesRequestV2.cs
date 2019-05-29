e73eaf41dee8fa3c757729c2ce1d5aff7cbf4bbd
Stephen Post <stephen_post@trimble.com>
2019-05-29 22:40:54 +0000

    Merged PR 13764: DDV - 3dp endpoint
    Add v2 endpoint for cellpasses for use in DDV (it was already then in a v1 endpoint used by TBC).
    
    This is a first pass, for the UI to consume (but it seems to have all the data there, we may just need to tidy up the display models)
    
    Related work items: #83315

---
+++ src/service/3DP/src/WebApiModels/ProductionData/Models/CellPassesRequestV2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using VSS.Common.Exceptions;
+using VSS.MasterData.Models.ResultHandling.Abstractions;
+using VSS.Productivity3D.Common.Interfaces;
+using VSS.Productivity3D.Models.Models;
+
+namespace VSS.Productivity3D.WebApi.Models.ProductionData.Models
+{
+  public class CellPassesRequestV2 : CellPassesRequest, IValidatable
+  {
+    /// <summary>
+    /// Filter UID, if set will replace the Filter Model with the associated Filter
+    /// </summary>
+    [JsonProperty(PropertyName = "filterUid", Required = Required.Default)]
+    public Guid? FilterUid { get; set; }
+
+    public void SetFilter(FilterResult filter)
+    {
+      this.filter = filter;
+    }
+
+    public override void Validate()
+    {
+      base.Validate();
+
+      if (FilterUid.HasValue && filterId.HasValue )
+      {
+        throw new ServiceException(HttpStatusCode.BadRequest,
+          new ContractExecutionResult(ContractExecutionStatesEnum.ValidationError, "Cannot set FilterUID with FilterID"));
+      }
+    }
+  }
+}
