cd511bbc1b3fd910076588aac2a5c0c2ac9d9949
Raymond Wilson <raymond_wilson@trimble.com>
2019-06-04 08:58:42 +1200

    #81498: Add a set of batch files to process Southern Motorway TAG files from a local temp folder into a set of projects with good entropy spread in their project numbers

---
+++ src/service/TRex/TagfileProcessing/Local/ProcessSouthernMotorwayTAGFilesIntoProject2_Local.bat
@@ -0,0 +1,1 @@
+dotnet "..\..\src\tools\VSS.TRex.Tools.TAGfileSubmitter\bin\debug\netcoreapp2.1\VSS.TRex.Tools.TAGFileSubmittor.dll" "{9472176F-2222-41F0-8305-79DAD47EF385}" "C:\Temp\SouthernMotorwayProjectTAGFiles\All TagFiles"
