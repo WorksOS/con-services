144eb4bdcaf58ff081916c1b9bcb0becfaed1675
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2019-06-04 12:51:10 +1200

    #83914. Required changes. ***NO_CI***, please!

--- src/service/3DP/src/WebApi/Compaction/Controllers/GeoJsonController.cs
+++ src/service/3DP/src/WebApi/Compaction/Controllers/GeoJsonController.cs
@@ -2,13 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VSS.Common.Abstractions.Configuration;
-using VSS.Common.Exceptions;
-using VSS.ConfigurationStore;
-using VSS.MasterData.Models.ResultHandling.Abstractions;
 using VSS.MasterData.Proxies.Interfaces;
-#if RAPTOR
 using VSS.Productivity3D.Common.Algorithms;
-#endif
 using VSS.Productivity3D.Common.Interfaces;
 using VSS.Productivity3D.Models.Models;
 
@@ -33,7 +28,6 @@ namespace VSS.Productivity3D.WebApi.Compaction.Controllers
     {
       Log.LogDebug($"{nameof(PolylineReducePoints)}: {requestDto}");
 
-#if RAPTOR
       if (requestDto.MaxPoints > SmoothPolylineRequest.NOT_DEFINED && requestDto.MaxPoints < 3)
       {
         return StatusCode((int)HttpStatusCode.BadRequest, new { Message = "Cannot reduce polyline to fewer than 3 points." });
@@ -42,10 +36,6 @@ namespace VSS.Productivity3D.WebApi.Compaction.Controllers
       var fencePoints = DouglasPeucker.DouglasPeuckerByCount(requestDto.Coordinates, requestDto.MaxPoints);
 
       return StatusCode((int)HttpStatusCode.OK, new { coordinates = fencePoints } );
-#else
-      throw new ServiceException(HttpStatusCode.BadRequest,
-        new ContractExecutionResult(ContractExecutionStatesEnum.ValidationError, "TRex unsupported request"));
-#endif
     }
   }
 }
