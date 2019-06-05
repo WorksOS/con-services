c8c1dd767c7ada5705cf447586499e1e6dc14eb5
Raymond Wilson <raymond_wilson@trimble.com>
2019-06-05 13:50:33 +1200

    #81498: Modify segment pass adopter to radically reduce the number of memory allocations it performs by improving its algorithm

--- src/service/TRex/src/netstandard/VSS.TRex.TAGFiles/Classes/Swather/SwatherBase.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.TAGFiles/Classes/Swather/SwatherBase.cs
@@ -51,7 +51,7 @@ namespace VSS.TRex.TAGFiles.Classes.Swather
             // server interlock (ICServerModule.Server.AcquireLock) and we may interact
             // directly with the sub grid tree being populated
 
-            IServerLeafSubGrid SubGrid = Grid.ConstructPathToCell(cellX, cellY, SubGridPathConstructionType.CreateLeaf) as IServerLeafSubGrid;
+            var SubGrid = Grid.ConstructPathToCell(cellX, cellY, SubGridPathConstructionType.CreateLeaf) as IServerLeafSubGrid;
 
             SubGrid.AllocateLeafFullPassStacks();
 
