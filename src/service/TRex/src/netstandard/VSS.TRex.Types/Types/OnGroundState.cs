52cf477c8cd64de767ded0817b2c3662b2ec831f
Raymond Wilson <raymond_wilson@trimble.com>
2019-06-07 10:59:22 +1200

    #81498: Remove Enum.IsDefined from TAG Value Matchers for performance reasons.

--- src/service/TRex/src/netstandard/VSS.TRex.Types/Types/OnGroundState.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Types/Types/OnGroundState.cs
@@ -5,7 +5,7 @@
     /// mechanism used to report that state
     /// </summary>
     public enum OnGroundState : byte
-  {
+    {
         No,
         YesLegacy,
         YesMachineConfig,
@@ -13,5 +13,11 @@
         YesMachineSoftware,
         YesRemoteSwitch,
         Unknown
+    }
+
+    public static class OnGroundStateConsts
+  {
+      public const int ON_GROUND_STATE_MIN_VALUE = (int)OnGroundState.No;
+      public const int ON_GROUND_STATE_MAX_VALUE = (int)OnGroundState.Unknown;
     }
 }
