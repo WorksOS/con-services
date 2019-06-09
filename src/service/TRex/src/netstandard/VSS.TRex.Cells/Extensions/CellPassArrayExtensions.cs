70a9e0a64d309a07df920bdb908e94b79dde7af8
Raymond Wilson <raymond_wilson@trimble.com>
2019-06-10 10:02:03 +1200

    #84110: Removal of inefficient foreach and LINQ constructs from performance sensitive code.

---
+++ src/service/TRex/src/netstandard/VSS.TRex.Cells/Extensions/CellPassArrayExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace VSS.TRex.Cells.Extensions
+{
+  public static class CellPassArrayExtensions
+  {
+    /// <summary>
+    /// Locates the earliest time contained in the set of cell passes.
+    /// </summary>
+    /// <param name="passes"></param>
+    /// <returns></returns>
+    public static DateTime MinimumTime(this CellPass[] passes)
+    {
+      DateTime minValue = DateTime.MaxValue;
+
+      for (int i = 0; i < passes.Length; i++)
+        if (minValue < passes[i].Time)
+          minValue = passes[i].Time;
+
+      return minValue == DateTime.MaxValue ? DateTime.MinValue : minValue;
+    }
+
+    /// <summary>
+    /// Locates the earliest time contained in the set of cell passes.
+    /// </summary>
+    /// <param name="passes"></param>
+    /// <returns></returns>
+    public static short MaxInternalSiteModelMachineIndex(this CellPass[] passes)
+    {
+      short maxValue = short.MinValue;
+
+      for (int i = 0; i < passes.Length; i++)
+        if (passes[i].InternalSiteModelMachineIndex > maxValue)
+          maxValue = passes[i].InternalSiteModelMachineIndex;
+
+      return maxValue;
+    }
+  }
+}
