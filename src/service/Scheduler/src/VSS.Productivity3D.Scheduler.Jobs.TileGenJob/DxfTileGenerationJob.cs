c926928ef6e593fab39915f994a9d0dc545c71b9
AaronMorgan <aaron_morgan@trimble.com>
2019-06-10 21:20:45 +1200

    #84124 - Refactored Scheduler service to use a configuration setting to drive the DXF tile generation. ***NO_CI***

--- src/service/Scheduler/src/VSS.Productivity3D.Scheduler.Jobs.TileGenJob/DxfTileGenerationJob.cs
+++ src/service/Scheduler/src/VSS.Productivity3D.Scheduler.Jobs.TileGenJob/DxfTileGenerationJob.cs
@@ -2,23 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Threading;
 using System.Threading.Tasks;
-using App.Metrics.Health;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using VSS.Common.Abstractions.Configuration;
 using VSS.Common.Abstractions.Http;
 using VSS.Common.Exceptions;
 using VSS.MasterData.Models.ResultHandling.Abstractions;
 using VSS.Pegasus.Client;
-using VSS.Productivity3D.Push.Abstractions;
-using VSS.Productivity3D.Scheduler.Abstractions;
-using VSS.Productivity3D.Scheduler.Jobs.DxfTileJob.Models;
-using VSS.WebApi.Common;
 using VSS.Productivity.Push.Models.Notifications;
 using VSS.Productivity.Push.Models.Notifications.Models;
 using VSS.Productivity3D.Push.Abstractions.Notifications;
+using VSS.Productivity3D.Scheduler.Abstractions;
+using VSS.Productivity3D.Scheduler.Jobs.DxfTileJob.Models;
+using VSS.WebApi.Common;
 
 namespace VSS.Productivity3D.Scheduler.Jobs.DxfTileJob
 {
@@ -27,10 +25,6 @@ namespace VSS.Productivity3D.Scheduler.Jobs.DxfTileJob
   /// </summary>
   public class DxfTileGenerationJob : IJob
   {
-    // Bug 83657 - 3D pm and Scheduler failing to generate tiles in pegasus. 
-    // Disable this job until Pegasus is ready
-    private const bool isEnabled = false;
-
     public static Guid VSSJOB_UID = Guid.Parse("5f3eed28-58e8-451e-8459-5f5a39d5c3b6");
     public Guid VSSJobUid => VSSJOB_UID;
 
@@ -38,19 +32,18 @@ namespace VSS.Productivity3D.Scheduler.Jobs.DxfTileJob
     private readonly ITPaaSApplicationAuthentication authentication;
     private readonly INotificationHubClient notificationHubClient;
     private readonly ILogger log;
+    private readonly IConfigurationStore configStore;
 
-    public DxfTileGenerationJob(IPegasusClient pegasusClient, ITPaaSApplicationAuthentication authn, INotificationHubClient notificationHubClient, ILoggerFactory logger)
+    public DxfTileGenerationJob(IConfigurationStore configurationStore, IPegasusClient pegasusClient, ITPaaSApplicationAuthentication authn, INotificationHubClient notificationHubClient, ILoggerFactory logger)
     {
+      configStore = configurationStore;
       this.pegasusClient = pegasusClient;
       authentication = authn;
       this.notificationHubClient = notificationHubClient;
       log = logger.CreateLogger<DxfTileGenerationJob>();
     }
 
-    public Task Setup(object o)
-    {
-      return Task.FromResult(true);
-    }
+    public Task Setup(object o) => Task.FromResult(true);
 
     public async Task Run(object o)
     {
@@ -69,42 +62,41 @@ namespace VSS.Productivity3D.Scheduler.Jobs.DxfTileJob
       //Validate the parameters
       request.Validate();
 
-      // Bug 83657 - 3D pm and Scheduler failing to generate tiles in pegasus. 
-      // This is disabled until we are ready to move to Pegasus 
-      if (isEnabled)
+      if (configStore.GetValueBool("SCHEDULER_ENABLE_DXF_TILE_GENERATION").HasValue &&
+          configStore.GetValueBool("SCHEDULER_ENABLE_DXF_TILE_GENERATION").Value)
       {
+        log.LogInformation($"DXF tile deneration with Pegasus enabled: {JsonConvert.SerializeObject(request)}");
+
         var dataOceanPath = $"{Path.DirectorySeparatorChar}{request.DataOceanRootFolder}{Path.DirectorySeparatorChar}{request.CustomerUid}{Path.DirectorySeparatorChar}{request.ProjectUid}";
         var dxfFileName = $"{dataOceanPath}{Path.DirectorySeparatorChar}{request.DxfFileName}";
         var dcFileName = $"{dataOceanPath}{Path.DirectorySeparatorChar}{request.DcFileName}";
-
+        
         var result = await pegasusClient.GenerateDxfTiles(dcFileName, dxfFileName, request.DxfUnitsType, CustomHeaders());
+        
         log.LogInformation($"Received Pegasus response for tile generation, filename: {dxfFileName}, result: `{JsonConvert.SerializeObject(result)}`");
+        
         var notifyParams = new DxfTileNotificationParameters
         {
           FileUid = request.ImportedFileUid,
           MinZoomLevel = result.MinZoom,
           MaxZoomLevel = result.MaxZoom
         };
+
         await notificationHubClient.Notify(new ProjectFileDxfTilesGeneratedNotification(notifyParams));
       }
       else
       {
-        log.LogInformation($"Dxf Tile Generation with Pegasus disabled (Bug 83657) - ignoring request: {JsonConvert.SerializeObject(request)}");
+        log.LogInformation($"DXF tile generation with Pegasus disabled (Bug 83657) - ignoring request: {JsonConvert.SerializeObject(request)}");
       }
     }
 
-    public Task TearDown(object o)
-    {
-      return Task.FromResult(true);
-    }
+    public Task TearDown(object o) => Task.FromResult(true);
 
-    private Dictionary<string, string> CustomHeaders()
-    {
-      return new Dictionary<string, string>
+    private Dictionary<string, string> CustomHeaders() =>
+      new Dictionary<string, string>
       {
         {"Content-Type", ContentTypeConstants.ApplicationJson},
         {"Authorization", $"Bearer {authentication.GetApplicationBearerToken()}"}
       };
-    }
   }
 }
