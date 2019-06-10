bca1f967c291d1506de402cfc7150c526e50698e
Stephen Post <stephen_post@trimble.com>
2019-06-10 23:12:55 +0000

    Merged PR 13715: #81670 - Use IAM instead of hardcoded AWS keys ***NO_CI***
    #81670 - Use IAM instead of hardcoded AWS keys ***NO_CI***
    
    Related work items: #81670

--- src/Common/AWS.TransferProxy/test/UnitTests/TransferProxyTests.cs
+++ src/Common/AWS.TransferProxy/test/UnitTests/TransferProxyTests.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
-using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Text;
 using Microsoft.Extensions.Logging.Abstractions;
 using VSS.Common.Abstractions.Configuration;
-using VSS.ConfigurationStore;
 
 namespace VSS.AWS.TransferProxy.UnitTests
 {
@@ -12,48 +12,51 @@ namespace VSS.AWS.TransferProxy.UnitTests
   public class TransferProxyTests
   {
     [TestMethod]
-    [ExpectedException(typeof(Exception))]
-    [DataRow(null)]
-    [DataRow("")]
-    [Ignore] //This shouldn't happen anymore
-    public void Should_throw_When_AWS_ACCESS_KEY_isnt_present(string value)
+    public void TestLoginNoKey()
     {
+      // We cannot use Access keys with S3 now, so we have to use assumed roles
+      // This test must work without providing any credentials
+      const string s3BaseUrl = "https://s3.us-west-2.amazonaws.com/";
+      var s3Key = "Test-s3Key" + DateTime.Now.Ticks.ToString();
+      
       var mockStore = new Mock<IConfigurationStore>();
-      mockStore.Setup(x => x.GetValueString("AWS_ACCESS_KEY")).Returns(value);
 
-      _ = new TransferProxy(mockStore.Object,new NullLogger<TransferProxy>());
+      mockStore.Setup(x => x.GetValueString("AWS_BUCKET_NAME")).Returns("AWS_BUCKET_NAME");
+
+      var transferProxy = new TransferProxy(mockStore.Object, new NullLogger<TransferProxy>(), "UnitTests");
+      var key = transferProxy.GeneratePreSignedUrl(s3Key);
+      Assert.IsNotNull(key);
+      // The s3 Url will contain the key, and base url at the start.
+      Assert.IsTrue(key.StartsWith(s3BaseUrl + s3Key));
     }
-    
-    [TestMethod]
-    [ExpectedException(typeof(Exception))]
-    [DataRow(null)]
-    [DataRow("")]
-    [Ignore] //This shouldn't happen anymore
-    public void Should_throw_When_AWS_SECRET_KEY_isnt_present(string value)
-    {
-      var mockStore = new Mock<IConfigurationStore>();
-
-      mockStore.Setup(x => x.GetValueString("AWS_ACCESS_KEY")).Returns("AWS_ACCESS_KEY");
-      mockStore.Setup(x => x.GetValueString("AWS_SECRET_KEY")).Returns(value);
-
-      _ = new TransferProxy(mockStore.Object, new NullLogger<TransferProxy>());
-    }    
 
     [TestMethod]
-    [ExpectedException(typeof(Exception))]
-    [DataRow(null)]
-    [DataRow("")]
-    [Ignore] //This shouldn't happen anymore
-    public void Should_throw_When_AWS_BUCKET_NAME_isnt_present(string value)
+    public void TestUpload()
     {
+      const string originalFileContents = "test-data-please-ignore";
+      var s3Key = "/unittests/Upload-test-s3Key" + DateTime.Now.Ticks + Guid.NewGuid();
+      var result = TimeSpan.FromHours(1);
       var mockStore = new Mock<IConfigurationStore>();
 
-      mockStore.Setup(x => x.GetValueString("AWS_ACCESS_KEY")).Returns("AWS_ACCESS_KEY");
-      mockStore.Setup(x => x.GetValueString("AWS_SECRET_KEY")).Returns("AWS_SECRET_KEY");
-      mockStore.Setup(x => x.GetValueString("AWS_BUCKET_NAME")).Returns(value);
+      mockStore.Setup(x => x.GetValueString("AWS_BUCKET_NAME")).Returns("vss-exports-stg");
+      mockStore.Setup(x => x.GetValueTimeSpan("AWS_PRESIGNED_URL_EXPIRY")).Returns(result);
+      
+      var transferProxy = new TransferProxy(mockStore.Object, new NullLogger<TransferProxy>());
 
-      _ = new TransferProxy(mockStore.Object, new NullLogger<TransferProxy>());
-    }  
+      using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(originalFileContents)))
+      {
+        transferProxy.Upload(ms, s3Key);
+      }
+
+      var s3ResultStream = transferProxy.Download(s3Key).Result.FileStream;
+      var resultMemoryStream = new MemoryStream();
+      s3ResultStream.CopyTo(resultMemoryStream);
+
+      resultMemoryStream.Seek(0, SeekOrigin.Begin);
+
+      var text = Encoding.UTF8.GetString(resultMemoryStream.ToArray());
+      Assert.AreEqual(originalFileContents, text);
+    }
 
     [TestMethod]
     [DataRow(null, 7)]
@@ -65,11 +68,9 @@ namespace VSS.AWS.TransferProxy.UnitTests
       var result = TimeSpan.FromDays(expectedResult);
       var mockStore = new Mock<IConfigurationStore>();
 
-      mockStore.Setup(x => x.GetValueString("AWS_ACCESS_KEY")).Returns("AWS_ACCESS_KEY");
-      mockStore.Setup(x => x.GetValueString("AWS_SECRET_KEY")).Returns("AWS_SECRET_KEY");
       mockStore.Setup(x => x.GetValueString("AWS_BUCKET_NAME")).Returns("AWS_BUCKET_NAME");
       mockStore.Setup(x => x.GetValueTimeSpan("AWS_PRESIGNED_URL_EXPIRY")).Returns(result);
-
+      
       _ = new TransferProxy(mockStore.Object, new NullLogger<TransferProxy>());
     }
   }
