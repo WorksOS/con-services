2dd10f254611cfb4a06a819151ddfa92a6f84464
AaronMorgan <aaron_morgan@trimble.com>
2019-06-12 11:53:34 +1200

    #84116 - Fix 3DP use of Regex ***NO_CI***

---
+++ src/service/3DP/src/WebApi/Middleware/URLRewriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+
+namespace VSS.Productivity3D.WebApi.Middleware
+{
+  /// <summary>
+  /// URL rewriting is the act of modifying request URLs based on one or more predefined rules.
+  /// This class provides functionality allowing us custom rewrite behaviour.
+  /// </summary>
+  public class URLRewriter
+  {
+    private static readonly Regex _regex = new Regex(@"(?<!:)(\/\/+)");
+
+    /// <summary>
+    /// Custom URL rewriter: Replaces all occurrences of // with /.
+    /// </summary>
+    /// <remarks>
+    /// This catch all is needed to work around errant behaviour in TBC where invalid path strings are sent to 3DP.
+    /// If that issue is addressed this rewrite could be removed.
+    /// Note: Custom rewriter is required as framework AddRewrite() method doesn't cater for our needs.
+    /// </remarks>
+    public static void RewriteMalformedPath(RewriteContext context)
+    {
+      var request = context.HttpContext.Request;
+
+      if (!_regex.IsMatch(request.Path.Value))
+      {
+        return;
+      }
+
+      request.Path = new PathString(_regex.Replace(request.Path.Value, "/"));
+    }
+  }
+}
---
+++ src/service/3DP/test/UnitTests/WebApiTests/Middleware/URLRewriterTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSS.Productivity3D.WebApi.Middleware;
+
+namespace VSS.Productivity3D.WebApiTests.Middleware
+{
+  [TestClass]
+  public class URLRewriterTests
+  {
+    [TestMethod]
+    [DataRow("/abc/def/ghi")]
+    [DataRow("/abc//def/ghi")]
+    [DataRow("//abc")]
+    [DataRow("/abc/def//ghi//")]
+    public void RewriteMalformedPath_should_parse_out_invalid_characters(string url)
+    {
+      var request = new DefaultHttpContext().Request;
+      request.Method = "GET";
+      request.Path = new PathString(url);
+
+      var context = new RewriteContext { HttpContext = request.HttpContext };
+
+      URLRewriter.RewriteMalformedPath(context);
+
+      Assert.IsFalse(request.Path.Value.Contains(@"//"));
+    }
+  }
+}
