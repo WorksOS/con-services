7ee5770811eeda62d093a7676a05c781a2d4fea8
Raymond Wilson <raymond_wilson@trimble.com>
2019-06-13 14:11:50 +1200

    #84110: Add additional information into the recyclable stream manager heart beat (#disposed and #finalised) to aid finding cod locations that are not disposing streams (of any sort) in a timely fashion)

--- src/service/TRex/src/netstandard/VSS.TRex.Common/HeartbeatLoggers/RecycledMemoryStreamHeartBeatLogger.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Common/HeartbeatLoggers/RecycledMemoryStreamHeartBeatLogger.cs
@@ -10,15 +10,21 @@ namespace VSS.TRex.Common
     private static RecyclableMemoryStreamManager _manager = DIContext.Obtain<RecyclableMemoryStreamManager>();
 
     private long _blockCreationCount = 0;
+    private long _streamCreationCount = 0;
+    private long _streamDisposedCount = 0;
+    private long _streamFinalizedCount = 0;
 
     public override string ToString()
     {
-      return $"Num blocks current/created: {_manager.SmallPoolInUseSize / _manager.BlockSize}/{_blockCreationCount}, Small block memory in use/free: {(1.0 * _manager.SmallPoolInUseSize)/1e6:F3}Mb/{(1.0 * _manager.SmallPoolFreeSize)/1e6:F3}Mb";
+      return $"Streams created/disposed/finalized: {_streamCreationCount}/{_streamDisposedCount}/{_streamFinalizedCount} Num blocks current/created: {_manager.SmallPoolInUseSize / _manager.BlockSize}/{_blockCreationCount}, Small block memory in use/free: {(1.0 * _manager.SmallPoolInUseSize)/1e6:F3}Mb/{(1.0 * _manager.SmallPoolFreeSize)/1e6:F3}Mb";
     }
 
     public RecycledMemoryStreamHeartBeatLogger()
     {
       _manager.BlockCreated += () => { Interlocked.Increment(ref _blockCreationCount); };
+      _manager.StreamCreated += () => { Interlocked.Increment(ref _streamCreationCount); };
+      _manager.StreamDisposed += () => { Interlocked.Increment(ref _streamDisposedCount); };
+      _manager.StreamFinalized += () => { Interlocked.Increment(ref _streamFinalizedCount); };
     }
   }
 }
