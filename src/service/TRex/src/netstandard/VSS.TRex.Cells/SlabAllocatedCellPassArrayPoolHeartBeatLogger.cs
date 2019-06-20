f2c8ba4fbdd3ab5be16347091239dc79cfdee8a2
Raymond Wilson <raymond_wilson@trimble.com>
2019-06-20 19:00:06 +1200

    #84110: Refine heartbeat logger support

--- src/service/TRex/src/netstandard/VSS.TRex.Cells/SlabAllocatedCellPassArrayPoolHeartBeatLogger.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Cells/SlabAllocatedCellPassArrayPoolHeartBeatLogger.cs
@@ -5,19 +5,26 @@ namespace VSS.TRex.Cells
 {
   public class SlabAllocatedCellPassArrayPoolHeartBeatLogger
   {
-    private StringBuilder sb = new StringBuilder();
+    private readonly StringBuilder sb = new StringBuilder();
 
-    public string ToString()
+    public override string ToString()
     {
-      var stats = SlabAllocatedCellPassArrayPoolHelper.Caches.Statistics();
+      var stats = SlabAllocatedCellPassArrayPoolHelper.Caches?.Statistics();
 
-      sb.Clear();
-      foreach (var stat in stats)
+      if (stats != null)
       {
-        sb.AppendLine($"Index: {stat.poolIndex}, ArraySize: {stat.arraySize}, Capacity: {stat.capacity}, Available: {stat.availableItems}");
+        sb.Clear();
+        sb.AppendLine("SlabAllocatedCellPassArrayPool: Index/ArraySize/Capacity/Available/Rented: ");
+
+        foreach (var stat in stats)
+        {
+          sb.AppendLine($"{stat.poolIndex}/{stat.arraySize}/{stat.capacity}/{stat.availableItems}/{stat.capacity - stat.availableItems}");
+        }
+
+        return sb.ToString();
       }
 
-      return sb.ToString();
+      return "SlabAllocatedCellPassArrayPoolHelper caches not available yet.";
     }
   }
 }
