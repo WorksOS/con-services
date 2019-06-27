dee1c6216f0138ba3d062531f56b28431bd9e325
Raymond Wilson <raymond_wilson@trimble.com>
2019-06-27 13:35:04 +1200

    #84110: Make DI injection of generic array pool implementations optional

--- src/service/TRex/src/netstandard/VSS.TRex.IO/Helpers/GenericArrayPoolCacheHelper.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.IO/Helpers/GenericArrayPoolCacheHelper.cs
@@ -5,7 +5,7 @@ namespace VSS.TRex.IO.Helpers
   public static class GenericArrayPoolCacheHelper<T>
   {
     private static IGenericArrayPoolCaches<T> _caches;
-    public static IGenericArrayPoolCaches<T> Caches => _caches ?? (_caches = DIContext.Obtain<IGenericArrayPoolCaches<T>>());
+    public static IGenericArrayPoolCaches<T> Caches => _caches ?? (_caches = DIContext.Obtain<IGenericArrayPoolCaches<T>>() ?? new GenericArrayPoolCaches<T>());
 
     public static void Clear() => _caches = null;
   }
--- src/service/TRex/src/netstandard/VSS.TRex.IO/Helpers/GenericTwoDArrayCacheHelper.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.IO/Helpers/GenericTwoDArrayCacheHelper.cs
@@ -5,7 +5,7 @@ namespace VSS.TRex.IO.Helpers
     public static class GenericTwoDArrayCacheHelper<T>
     {
       private static ITwoDArrayCache<T> _caches;
-      public static ITwoDArrayCache<T> Caches => _caches ?? (_caches = DIContext.Obtain<ITwoDArrayCache<T>>());
+      public static ITwoDArrayCache<T> Caches => _caches ?? (_caches = DIContext.Obtain<ITwoDArrayCache<T>>() ?? new TwoDArrayCache<T>(32, 32, 10));
 
       public static void Clear() => _caches = null;
     }
