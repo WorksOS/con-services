3d4ac88253954d4359ddb34d20f49c9a6fc76471
Raymond Wilson <raymond_wilson@trimble.com>
2019-07-17 09:22:14 +1200

    #83129: PR feedback & code tidy

--- src/service/TRex/src/netstandard/VSS.TRex.Logging/TRexIgniteLogger.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Logging/TRexIgniteLogger.cs
@@ -59,7 +59,7 @@ namespace VSS.TRex.Logging
     /// </returns>
     public bool IsEnabled(Apache.Ignite.Core.Log.LogLevel level)
     {
-      return /*(level > LogLevel.Debug) && */(_logger?.IsEnabled(ConvertLogLevel2(level)) ?? false);
+      return (level > LogLevel.Debug) && (_logger?.IsEnabled(ConvertLogLevel2(level)) ?? false);
     }
 
     /// <summary>
