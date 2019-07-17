6d083cbe63af8b21b7bd9bc180bcb1cbc84518e8
AaronMorgan <aaron_morgan@trimble.com>
2019-07-17 16:22:54 +1200

    #85607 - Fixed Serilog so it no longer creates a log file per process. ***NO_CI***

--- src/Common/LoggingExtensions/SerilogExtensions/SerilogExtensions.cs
+++ src/Common/LoggingExtensions/SerilogExtensions/SerilogExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
@@ -19,9 +20,9 @@ namespace VSS.Serilog.Extensions
     /// <param name="config">Optional configuration overrides.</param>
     /// <param name="httpContextAccessor">Used for the <see cref="HttpContextEnricher"/> to log the interservice RequestID.</param>
     /// <returns>Returns the Serilog.Core.Logger instance.</returns>
-    public static Logger Configure(string logFilename = null, IConfigurationRoot config = null, IHttpContextAccessor httpContextAccessor = null)
+    public static Logger Configure(string logFilename = "app.log", IConfigurationRoot config = null, IHttpContextAccessor httpContextAccessor = null)
     {
-      const string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss,fff} [{ThreadId}] {Level:u3} [{SourceContext}]{RequestID} {Message} {EscapedException}{NewLine}";
+      const string outputTemplateConsole = "{Timestamp:yyyy-MM-dd HH:mm:ss,fff} [{ThreadId}] {Level:u3} [{SourceContext}]{RequestID} {Message} {EscapedException}{NewLine}";
 
       var logger = new LoggerConfiguration()
                    .MinimumLevel.Debug()
@@ -34,57 +35,75 @@ namespace VSS.Serilog.Extensions
                    .Enrich.With(new HttpContextEnricher(httpContextAccessor))
                    .WriteTo.Console(
                      LogEventLevel.Debug,
-                     outputTemplate: outputTemplate,
+                     outputTemplate: outputTemplateConsole,
                      theme: AnsiConsoleTheme.Code);
 
-      // If we start deploying Release configurations then the following options could be compiled out during development.
-      logger.WriteTo.File(
-        Path.Combine(Directory.GetCurrentDirectory(), $"logs/{logFilename}"),
-        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss,fff} [{ThreadId}] {Level:u3} [{SourceContext}]{RequestID} {Message}{NewLine}{Exception}",
-        shared: true);
+      // Setup WriteTo:File, merging global configuration settings with the following default WriteTo values.
+      // Microsoft ConfigurationBuilder applies a latest wins strategy.
+      const string outputTemplateFile = "{Timestamp:yyyy-MM-dd HH:mm:ss,fff} [{ThreadId}] {Level:u3} [{SourceContext}]{RequestID} {Message}{NewLine}{Exception}";
+      var defaultWriteToFileConfig = new Dictionary<string, string>
+      {
+        { "Serilog:WriteTo:0:Name", "File" },
+        { "Serilog:WriteTo:0:Args:path", $@".\logs\{logFilename}" },
+        { "Serilog:WriteTo:0:Args:outputTemplate", outputTemplateFile },
+        { "Serilog:WriteTo:0:Args:shared", "true" }
+      };
+
+      var defaultConfig = new ConfigurationBuilder()
+                          .SetBasePath(Directory.GetCurrentDirectory())
+                          .AddInMemoryCollection(initialData: defaultWriteToFileConfig);
 
       if (config == null)
       {
-        const string configFilename = "appsettings.json";
-
-        try
-        {
-          string basePath;
-
-          if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), configFilename)))
-          {
-            basePath = Directory.GetCurrentDirectory();
-          }
-          else if (File.Exists(Path.Combine(AppContext.BaseDirectory, configFilename)))
-          {
-            // Likely if the application assembly is executed via a script from a different location.
-            basePath = Path.Combine(AppContext.BaseDirectory);
-          }
-          else
-          {
-            throw new FileNotFoundException();
-          }
-
-          config = new ConfigurationBuilder()
-                   .SetBasePath(basePath)
-                   .AddJsonFile(path: configFilename)
-                   .Build();
-        }
-        catch (FileNotFoundException)
-        {
-          if (string.IsNullOrEmpty(logFilename))
-          {
-            throw new Exception($"Unable to resolve {configFilename} location; must provide either a valid logFilename or appsettings.json configuration file.");
-          }
-        }
+        TryLoadAppSettingsJson(defaultConfig);
       }
-
-      if (config != null)
+      else
       {
-        logger.ReadFrom.Configuration(config);
+        defaultConfig.AddConfiguration(config);
       }
+
+      logger.ReadFrom.Configuration(defaultConfig.Build());
 
       return logger.CreateLogger();
+    }
+
+    /// <summary>
+    /// Locates appsettings.json and loads its Serilog settings into the provided IConfigurationBuilder
+    /// </summary>
+    /// <remarks>
+    /// This is a backup mechanism in-case the application hasn't provided it's own ConfigurationBuilder but
+    /// there is an appsettings.json present.
+    /// </remarks>
+    /// <param name="defaultConfig">The log configuration to add appsettings.json settings too.</param>
+    private static void TryLoadAppSettingsJson(IConfigurationBuilder defaultConfig)
+    {
+      const string configFilename = "appsettings.json";
+
+      try
+      {
+        string basePath;
+
+        if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), configFilename)))
+        {
+          basePath = Directory.GetCurrentDirectory();
+        }
+        // e.g. if the application assembly is executed via a script from a different location to itself.
+        else if (File.Exists(Path.Combine(AppContext.BaseDirectory, configFilename)))
+        {
+          basePath = Path.Combine(AppContext.BaseDirectory);
+        }
+        else
+        {
+          throw new FileNotFoundException();
+        }
+
+        defaultConfig.SetBasePath(basePath)
+                     .AddJsonFile(path: configFilename);
+      }
+      catch (FileNotFoundException)
+      {
+        throw new Exception($"Unable to resolve {configFilename} location; must provide either a valid logFilename or appsettings.json configuration file.");
+      }
     }
   }
 }
