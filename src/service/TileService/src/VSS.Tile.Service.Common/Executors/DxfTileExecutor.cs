2ebfbb821f91a08aa2a06220c47fe1277cced074
Josh Nimmo <josh_nimmo@trimble.com>
2019-07-22 12:26:07 +1200

    make path work for tcc

--- src/service/TileService/src/VSS.Tile.Service.Common/Executors/DxfTileExecutor.cs
+++ src/service/TileService/src/VSS.Tile.Service.Common/Executors/DxfTileExecutor.cs
@@ -273,6 +273,7 @@ namespace VSS.Tile.Service.Common.Executors
     /// <returns>The downloaded tile if it exists</returns>
     private async Task<byte[]> DownloadTile(string fullTileName, string what)
     {
+      fullTileName = useDataOcean ? fullTileName : fullTileName.Replace("/tiles/xyz/", "/Z"); //Hack for POC make path work for tcc
       byte[] tileData = null;
       Stream stream;
       if (useDataOcean)
