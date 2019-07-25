512bd384137b17cc25c283fbfac899faaee5f874
Raymond Wilson <raymond_wilson@trimble.com>
2019-07-25 22:50:34 +1200

    #85838: Implement ComputeFilterPatch()

--- src/service/TRex/src/netstandard/VSS.TRex.Designs/SVLAlignmentDesign.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Designs/SVLAlignmentDesign.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using VSS.TRex.Common;
+using VSS.TRex.Common.Utilities;
 using VSS.TRex.Designs.Models;
 using VSS.TRex.Designs.SVL;
 using VSS.TRex.Geometry;
 using VSS.TRex.SubGridTrees;
+using VSS.TRex.SubGridTrees.Interfaces;
 
 namespace VSS.TRex.Designs
 {
@@ -14,28 +16,133 @@ namespace VSS.TRex.Designs
   {
     private static readonly ILogger Log = Logging.Logger.CreateLogger<SVLAlignmentDesign>();
 
-    /// <summary>
-    /// Reference to the NFF file representing the SVL description of a set of guidance alignments
-    /// </summary>
-    //private TNFFFile NFFFile;
+    private double _cellSize;
 
     /// <summary>
     /// Represents the master guidance alignment selected from the NFFFile.
     /// </summary>
     private TNFFGuidableAlignmentEntity Data;
 
+    private BoundingWorldExtent3D BoundingBox = BoundingWorldExtent3D.Inverted();
+
     /// <summary>
     /// Constructs a guidance alignment design with a cell size used for computing filter patches
     /// </summary>
-    /// <param name="ACellSize"></param>
-    public SVLAlignmentDesign(double ACellSize)
+    /// <param name="cellSize"></param>
+    public SVLAlignmentDesign(double cellSize)
     {
+      _cellSize = cellSize;
 
+      Data = new TNFFGuidableAlignmentEntity();
     }
+
+    private struct Corner
+    {
+      public int X, Y;
+
+      public Corner(int x, int y)
+      {
+        X = x;
+        Y = y;
+      }
+    }
+
+    private static Corner[] Corners =
+    {
+      new Corner(0, 0),
+      new Corner(SubGridTreeConsts.SubGridTreeDimension - 1, 0),
+      new Corner(0, SubGridTreeConsts.SubGridTreeDimension - 1),
+      new Corner(SubGridTreeConsts.SubGridTreeDimension - 1, SubGridTreeConsts.SubGridTreeDimension - 1)
+    };
 
     public override bool ComputeFilterPatch(double StartStn, double EndStn, double LeftOffset, double RightOffset, SubGridTreeBitmapSubGridBits Mask, SubGridTreeBitmapSubGridBits Patch, double OriginX, double OriginY, double CellSize, double Offset)
     {
-      throw new NotImplementedException();
+      double LeftOffsetValue = -LeftOffset;
+      double RightOffsetValue = RightOffset;
+
+      if (LeftOffsetValue > RightOffsetValue)
+        MinMax.Swap(ref LeftOffsetValue, ref RightOffsetValue);
+
+      //  {$IFDEF DEBUG}
+      //   SIGLogMessage.PublishNoODS(Self, Format('Constructing filter patch for Stn:%.3f-%.3f, Ofs:%.3f-%.3f, OriginX:%.3f, OriginY:%.3f',
+      //   [StartStn, EndStn, LeftOffsetValue, RightOffsetValue, OriginX, OriginY]), slmcDebug);
+      //   {$ENDIF}
+
+      if (Data == null)
+      {
+        Log.LogError("No Data element provided to SVL filter patch calculation");
+        return false;
+      }
+
+      TNFFStationedLineworkEntity Element;
+      Patch.Clear();
+
+      // Check the corners of the sub grid. If all are out of the offset range then assume
+      // none of the cells are applicable. All four corners need to be on the same side of the
+      // alignment in terms of offset to fail the sub grid.
+      int CornersOutOfOffsetRange = 0;
+      int CornersOutOfOffsetRangeSign = 0;
+
+      double OriginXPlusHalfCellSize = OriginX + CellSize / 2;
+      double OriginYPlusHalfCellSize = OriginY + CellSize / 2;
+
+      for (int I = 0; I < Corners.Length; I++)
+      {
+        //    with kCorners[I] do
+        Data.ComputeStnOfs(OriginXPlusHalfCellSize + Corners[I].X * CellSize, OriginYPlusHalfCellSize + Corners[I].Y * CellSize, out double Stn, out double Ofs);
+
+        if (!((Stn != Consts.NullDouble && Ofs != Consts.NullDouble) && Range.InRange(Stn, StartStn, EndStn)))
+          if (!Range.InRange(Ofs, LeftOffsetValue, RightOffsetValue))
+          {
+            if (I == 0)
+              CornersOutOfOffsetRangeSign = Math.Sign(Ofs);
+
+            if (CornersOutOfOffsetRangeSign == Math.Sign(Ofs))
+              CornersOutOfOffsetRange++;
+            else
+              break;
+          }
+      }
+
+      if (CornersOutOfOffsetRange == Corners.Length)
+      {
+        // Return success with the empty patch
+        //{$IFDEF DEBUG}
+        //SIGLogMessage.PublishNoODS(Self, 'All corners of patch exceed stn:ofs boundary', slmcDebug);
+        //{$ENDIF}
+        return true;
+      }
+
+      // Iterate across the cells in the mask computing and checking the stn:ofs of
+      // each point using the previously successful element as a hint for the next
+      // computation
+      for (int I = 0; I < SubGridTreeConsts.SubGridTreeDimension; I++)
+      {
+        for (int J = 0; J < SubGridTreeConsts.SubGridTreeDimension; J++)
+        {
+          if (Mask.BitSet(I, J))
+          {
+            // Force element to be nil for all calculation until we resolve the issue
+            // of an in appropriate element 'capturing' the focus and then being used to
+            // calculate inappropriate offsets due to it's station range covering the
+            // points being computed.
+
+            Element = null;
+
+            Data.ComputeStnOfs(OriginXPlusHalfCellSize + I * CellSize, OriginYPlusHalfCellSize + J * CellSize,
+              out double Stn, out double Ofs, ref Element);
+
+            if (Stn != Consts.NullDouble && Ofs != Consts.NullDouble)
+              Patch.SetBitValue(I, J, Range.InRange(Stn, StartStn, EndStn) && Range.InRange(Ofs, LeftOffsetValue, RightOffsetValue));
+          }
+        }
+      }
+
+      //  {$IFDEF DEBUG}
+      //  SIGLogMessage.PublishNoODS(Self, Format('Filter patch construction successful with %d bits', [Patch.CountBits]), slmcDebug);
+      //  {$ENDIF}
+
+      return true;
     }
 
     public override List<XYZS> ComputeProfile(XYZ[] profilePath, double cellSize)
@@ -50,7 +157,10 @@ namespace VSS.TRex.Designs
 
     public override void GetExtents(out double x1, out double y1, out double x2, out double y2)
     {
-      throw new NotImplementedException();
+      x1 = BoundingBox.MinX;
+      y1 = BoundingBox.MinY;
+      x2 = BoundingBox.MaxX;
+      y2 = BoundingBox.MaxY;
     }
 
     public override void GetHeightRange(out double z1, out double z2)
@@ -92,7 +202,7 @@ namespace VSS.TRex.Designs
 
     public override DesignLoadResult LoadFromFile(string fileName, bool saveIndexFiles = true)
     {
-      var Result = DesignLoadResult.UnknownFailure;
+      DesignLoadResult Result;
       var NFFFile = TNFFFile.CreateFromFile(fileName);
 
       try
@@ -103,12 +213,15 @@ namespace VSS.TRex.Designs
         {
           if (NFFFile.GuidanceAlignments[I].IsMasterAlignment())
           {
-            Data = NFFFile.GuidanceAlignments[I] as TNFFGuidableAlignmentEntity;
+            Data = NFFFile.GuidanceAlignments[I];
 
             NFFFile.GuidanceAlignments.RemoveAt(I);
 
             if (Data != null)
+            {
+              BoundingBox = Data.BoundingBox();
               Result = DesignLoadResult.Success;
+            }
 
             break;
           }
