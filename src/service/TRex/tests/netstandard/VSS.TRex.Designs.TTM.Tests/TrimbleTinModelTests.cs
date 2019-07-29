9a73bb54a96ed5f695c65cb29cf44679fdcc5268
Raymond Wilson <raymond_wilson@trimble.com>
2019-07-29 22:29:16 +1200

    #85914: Re-enable some more

--- src/service/TRex/tests/netstandard/VSS.TRex.Designs.TTM.Tests/TrimbleTinModelTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Designs.TTM.Tests/TrimbleTinModelTests.cs
@@ -149,7 +149,7 @@ namespace VSS.TRex.Designs.TTM.Tests
 
             var TTM2 = new TrimbleTINModel();
             TTM2.LoadFromFile(fileName);
-            TTM2.ModelName.Should().Be(Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName)));
+            TTM2.ModelName.Should().Be(Path.GetFileNameWithoutExtension(fileName));
 
             File.Delete(fileName);
         }
