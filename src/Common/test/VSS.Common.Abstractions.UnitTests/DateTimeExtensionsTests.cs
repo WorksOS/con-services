9c209b347c3327d48669a2880bcbc8fa04b8dbe7
Josh Nimmo <josh_nimmo@trimble.com>
2019-08-07 00:19:41 +0000

    Merged PR 14428: Common Date fixes
    #Summary
    - This fixes the case where we have a local date but want the Iso8601DateTimeString, this change allows that to be obtained.
    - Fixes to helm chart to better handle long or uppercase chars in branch names
    
    ##More info
    
    When string dates such as "2019-07-30T23:35:12Z" are constructed using [FromQuery] in controllers they become "Local Dates" they have the correct time,  and we can get utc value of these (i.e. the original time passed in) by using the ToUniversalTime().
    
    In the case when we have a string with no timezone e.g. "2019-07-30T23:35:12" (No Z suffix) then this becomes an unknown type and we assume utc was intended.
    
    Related work items: #86173

--- src/Common/test/VSS.Common.Abstractions.UnitTests/DateTimeExtensionsTests.cs
+++ src/Common/test/VSS.Common.Abstractions.UnitTests/DateTimeExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using VSS.Common.Abstractions.Extensions;
+using FluentAssertions;
 using Xunit;
 
 namespace VSS.Common.Abstractions.UnitTests
@@ -12,16 +13,20 @@ namespace VSS.Common.Abstractions.UnitTests
     public void UtcDateShouldConvertToIso8601(DateTimeKind kind)
     {
       var date = new DateTime(2019, 03, 27, 11, 28, 33, kind);
-      var expected = "2019-03-27T11:28:33Z";
-      var actual = date.ToIso8601DateTimeString();
-      Assert.Equal(expected, actual);
+      date.ToIso8601DateTimeString().Should().Be("2019-03-27T11:28:33Z");
     }
 
-    [Fact]
-    public void LocalDateShouldNotConvertToIso8601()
+    [Theory]
+    [InlineData("2019-07-30T23:35:12Z", DateTimeKind.Local, "2019-07-30T23:35:12Z")]
+    [InlineData("2019-01-01T00:00:00", DateTimeKind.Unspecified, "2019-01-01T00:00:00Z")]
+    public void ParsedUtcDateStringShouldConvertToIso8601(string dateToParse, DateTimeKind expectedParseKind, string expectedUtcString)
     {
-      var date = new DateTime(2019, 03, 27, 11, 28, 33, DateTimeKind.Local);
-      Assert.Throws<ArgumentException>(() => date.ToIso8601DateTimeString());
+      var localDate = DateTime.Parse(dateToParse);
+      localDate.Kind.Should().Be(expectedParseKind);
+      localDate.ToIso8601DateTimeString().Should().Be(expectedUtcString);
     }
+
+
+
   }
 }
