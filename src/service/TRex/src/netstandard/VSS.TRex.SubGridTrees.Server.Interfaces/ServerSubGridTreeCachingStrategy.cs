4581f7a618b9c4ab9b63e5b56c3afceb694071b5
Raymond Wilson <raymond_wilson@trimble.com>
2019-08-10 19:11:12 +1200

    #86637: Define cache strategy enum

---
+++ src/service/TRex/src/netstandard/VSS.TRex.SubGridTrees.Server.Interfaces/ServerSubGridTreeCachingStrategy.cs
@@ -0,0 +1,18 @@
+﻿namespace VSS.TRex.SubGridTrees.Server.Interfaces
+{
+  public enum ServerSubGridTreeCachingStrategy
+  {
+    /// <summary>
+    /// Sub grids loaded from the grid layer are independently cached within a server sub grid tree as
+    /// fully hydrated sub grids read from the grid cache/persistence layer
+    /// </summary>
+    CacheSubGridsInTree,
+
+    /// <summary>
+    /// Sub grids are only cached within the grid cache/persistence layer in their un-hydrated/compressed form.
+    /// Sub grids retrieved using this strategy have a life cycle constrained by the involvement of the sub grid
+    /// in the processing of the request that stimulated the request for the sub grid in question
+    /// </summary>
+    CacheSubGridsInGridCache
+  }
+}
