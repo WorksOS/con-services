b166b9c19614f84a7ff246ea6f82dc5feafb1e36
Aaron Morgan <aaron_morgan@trimble.com>
2019-08-16 13:54:40 +1200

    #86747 - Add TCC to DataOcean migration tool.

---
+++ src/Tools/TCCToDataOcean/Types/MediaType.cs
@@ -0,0 +1,10 @@
+﻿namespace TCCToDataOcean.Types
+{
+  public static class MediaType
+  {
+    public const string APPLICATION_JSON = "application/json";
+    public const string TEXT_PLAIN = "text/plain";
+    public const string APPLICATION_OCTET_STREAM = "application/octet-stream";
+    public const string MULTIPART_FORM_DATA = "multipart/form-data";
+  }
+}
