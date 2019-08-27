395d5f4ce049b46e0aff7cbcdff56f221502aef7
Josh Nimmo <josh_nimmo@trimble.com>
2019-08-27 02:23:32 +0000

    Merged PR 14545: Add devices to connected site
    This contains:
    - Some helm chart tidying
    - Adds device (used for radio) to status message if radio serial number is present.
    
    Related work items: #86511

---
+++ src/service/TRex/src/interfaces/VSS.TRex.ConnectedSite.Gateway.Models/Models/StatusMessageDevice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using VSS.Productivity3D.Models.ResultHandling;
+using VSS.TRex.ConnectedSite.Gateway.Abstractions;
+using VSS.VisionLink.Interfaces.Events.Commands.MTS;
+
+namespace VSS.TRex.ConnectedSite.Gateway.Models
+{
+  public class StatusMessageDevice : IStatusMessageDevice
+  {
+    private string _model;
+    public string Model
+    {
+      get => _model == "torch" ? "SNM94x" : _model;
+      set => _model = value;
+    }
+
+    public string SerialNumber { get; set; }
+    public string Nickname { get; set; }
+    public string Firmware { get; set; }
+    public int? BatteryPercent { get; set; }
+    public string LicenseCodes { get; set; }
+    public DateTime? WarrantyExpirationUtc { get; set; }
+  }
+}
