681ad5648028a28be97e7b4cc553b6ae0fca32aa
Raymond Wilson <raymond_wilson@trimble.com>
2019-09-17 11:22:52 +1200

    #87972: Fix broken unit test

--- src/service/Filter/src/VSS.Productivity3D.Filter.Common/Models/FilterRequestFull.cs
+++ src/service/Filter/src/VSS.Productivity3D.Filter.Common/Models/FilterRequestFull.cs
@@ -32,7 +32,7 @@ namespace VSS.Productivity3D.Filter.Common.Models
       return new FilterRequestFull
       {
         FilterUid = request?.FilterUid ?? string.Empty,
-        FilterUids = request.FilterUids,
+        FilterUids = request?.FilterUids,
         Name = request?.Name ?? string.Empty,
         FilterJson = request?.FilterJson ?? string.Empty,
         FilterType = request?.FilterType ?? FilterType.Transient,
