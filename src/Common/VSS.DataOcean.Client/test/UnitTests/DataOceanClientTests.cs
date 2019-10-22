7d8eab3e1cbf1a878cb26fa635b216d52591226e
Aaron Morgan <aaron_morgan@trimble.com>
2019-10-22 14:04:33 +1300

    #88020 - Fixed broken unit tests.

--- src/Common/VSS.DataOcean.Client/test/UnitTests/DataOceanClientTests.cs
+++ src/Common/VSS.DataOcean.Client/test/UnitTests/DataOceanClientTests.cs
@@ -347,7 +347,6 @@ namespace VSS.DataOcean.Client.UnitTests
       Assert.False(success);
     }
 
-
     [Fact]
     public async Task CanGetExistingFolderId()
     {
@@ -391,7 +390,6 @@ namespace VSS.DataOcean.Client.UnitTests
       var Id = await client.GetFolderId($"{Path.DirectorySeparatorChar}{folderName}", null);
       Assert.Null(Id);
     }
-
 
     [Fact]
     public async Task CanGetExistingFileId()
@@ -463,7 +461,6 @@ namespace VSS.DataOcean.Client.UnitTests
     [Fact]
     public async Task CanGetExistingSingleFile()
     {
-      var downloadUrl = "https://fs-ro-us1.staging-tdata-cdn.com/r/2a0de221-1ab6-42e1-9c1e-08f4aa0cde59?Signature=Fw~5911qUEoVm5UPWPeNv79RonR8OFkaQ6XfXEmo86i9Fu7-wtiXevP9WYkrFbpDW3A7YAZva2qMLbrstuSGojpitZrArPpyAcowP2jurHQk~fvuHk601gkZMJNmmxJpaakSRoUH4rUiaan8ATC0k1OZKne5qX-vPgebvO3mZswQbrcEbwvln98pyXVHRX530epu58r05pOKPCSi-DuK8RUjtQleH-DZ3c~A3q7q73dhkMbumMFQMjDDOEzifQF1gZ7pOtpiUfcjxxU0Xzh3lX7WLGJn0TcFq6Cctylgpg0Xguw3dL4qpL2LAfjn7nNsQtuAukPUzmymCHcnP1qWJg__&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9mcy1yby11czEuc3RhZ2luZy10ZGF0YS1jZG4uY29tL3IvMmEwZGUyMjEtMWFiNi00MmUxLTljMWUtMDhmNGFhMGNkZTU5LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE1NDQ1Njg0NDd9fX1dfQ__&Key-Pair-Id=APKAJZFK5OCWBA5LQHUQ";
       var expectedResult = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3 };
       var stream = new MemoryStream(expectedResult);
       var expectedDownloadResult = new StreamContent(stream);
@@ -482,7 +479,7 @@ namespace VSS.DataOcean.Client.UnitTests
         Multifile = false,
         RegionPreferences = new List<string> { "us1" },
         Status = "AVAILABLE",
-        DataOceanDownload = new DataOceanTransfer { Url = downloadUrl }
+        DataOceanDownload = new DataOceanTransfer { Url = TestConstants.DownloadUrl }
       };
       var expectedFileBrowseResult = new BrowseFilesResult { Files = new List<DataOceanFile> { expectedFileResult } };
 
@@ -499,7 +496,7 @@ namespace VSS.DataOcean.Client.UnitTests
         .Setup(g => g.ExecuteRequest<BrowseFilesResult>(browseFileUrl, null, null, HttpMethod.Get, null, 3, false))
         .Returns(Task.FromResult(expectedFileBrowseResult));
       gracefulMock
-        .Setup(g => g.ExecuteRequestAsStreamContent(downloadUrl, HttpMethod.Get, null, null, null, 3, false))
+        .Setup(g => g.ExecuteRequestAsStreamContent(TestConstants.DownloadUrl, HttpMethod.Get, null, null, null, 3, false))
         .ReturnsAsync(expectedDownloadResult);
 
       serviceCollection.AddTransient(g => gracefulMock.Object);
@@ -553,7 +550,7 @@ namespace VSS.DataOcean.Client.UnitTests
     public async Task CanGetExistingMultiFile()
     {
       var fileName = $"{Path.DirectorySeparatorChar}tiles{Path.DirectorySeparatorChar}tiles.json";
-      var downloadUrl = "https://fs-ro-us1.staging-tdata-cdn.com/r/2a0de221-1ab6-42e1-9c1e-08f4aa0cde59/{path}?Signature=Fw~5911qUEoVm5UPWPeNv79RonR8OFkaQ6XfXEmo86i9Fu7-wtiXevP9WYkrFbpDW3A7YAZva2qMLbrstuSGojpitZrArPpyAcowP2jurHQk~fvuHk601gkZMJNmmxJpaakSRoUH4rUiaan8ATC0k1OZKne5qX-vPgebvO3mZswQbrcEbwvln98pyXVHRX530epu58r05pOKPCSi-DuK8RUjtQleH-DZ3c~A3q7q73dhkMbumMFQMjDDOEzifQF1gZ7pOtpiUfcjxxU0Xzh3lX7WLGJn0TcFq6Cctylgpg0Xguw3dL4qpL2LAfjn7nNsQtuAukPUzmymCHcnP1qWJg__&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9mcy1yby11czEuc3RhZ2luZy10ZGF0YS1jZG4uY29tL3IvMmEwZGUyMjEtMWFiNi00MmUxLTljMWUtMDhmNGFhMGNkZTU5LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE1NDQ1Njg0NDd9fX1dfQ__&Key-Pair-Id=APKAJZFK5OCWBA5LQHUQ";
+      var downloadUrl = TestConstants.DownloadUrl;
       var substitutedDownloadUrl = downloadUrl.Replace("{path}", fileName.Substring(1));
       var expectedResult = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3 };
       var stream = new MemoryStream(expectedResult);
@@ -625,7 +622,7 @@ namespace VSS.DataOcean.Client.UnitTests
     public async Task CanGetNonExistingMultiFile()
     {
       var fileName = $"{Path.DirectorySeparatorChar}tiles{Path.DirectorySeparatorChar}15{Path.DirectorySeparatorChar}18756{Path.DirectorySeparatorChar}2834.png";
-      var downloadUrl = "https://fs-ro-us1.staging-tdata-cdn.com/r/2a0de221-1ab6-42e1-9c1e-08f4aa0cde59/{path}?Signature=Fw~5911qUEoVm5UPWPeNv79RonR8OFkaQ6XfXEmo86i9Fu7-wtiXevP9WYkrFbpDW3A7YAZva2qMLbrstuSGojpitZrArPpyAcowP2jurHQk~fvuHk601gkZMJNmmxJpaakSRoUH4rUiaan8ATC0k1OZKne5qX-vPgebvO3mZswQbrcEbwvln98pyXVHRX530epu58r05pOKPCSi-DuK8RUjtQleH-DZ3c~A3q7q73dhkMbumMFQMjDDOEzifQF1gZ7pOtpiUfcjxxU0Xzh3lX7WLGJn0TcFq6Cctylgpg0Xguw3dL4qpL2LAfjn7nNsQtuAukPUzmymCHcnP1qWJg__&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9mcy1yby11czEuc3RhZ2luZy10ZGF0YS1jZG4uY29tL3IvMmEwZGUyMjEtMWFiNi00MmUxLTljMWUtMDhmNGFhMGNkZTU5LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE1NDQ1Njg0NDd9fX1dfQ__&Key-Pair-Id=APKAJZFK5OCWBA5LQHUQ";
+      var downloadUrl = TestConstants.DownloadUrl;
       var substitutedDownloadUrl = downloadUrl.Replace("{path}", fileName.Substring(1));
 
       const string folderName = "unittest";
@@ -673,8 +670,6 @@ namespace VSS.DataOcean.Client.UnitTests
 
     private Task<bool> CanPutFile(string status)
     {
-      var uploadUrl = "https://fs-us1.staging-tdata-cdn.com/r/11591287-648f-4c60-ae5c-80b61b12d78b?Signature=lD3yNv-YesLoQWwCYPVo-dzh9Xw0Q5kiCkPkckv67tOP1e~AfFiJv9jYAqmES0vQgkQvSqzvK4RJ2l2gXybdq3pvEDxeFbQtvAW-6hHMBd7q~KUMi4gW4GSD-mWtiH~4~576SEUn-uZl6reyaM6yRqPXjS2VhBJGnBWzdhU~HVEiMJERR5MSfZp~oXfi~Gq-0NbeiXF-zIp1EIuH-cEP69MZg4zXRQn~wbHdrkBeVQeaziPVo1Keg~xDoi5TkyBnfV5Lpc3ZlRvlgHdPPrLuzOKbHXnsH2rPSD3naZPNfxtCq-V8YapD2NnZGyTPX-2FE77y3~X8k-rWPboU210WdA__&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9mcy11czEuc3RhZ2luZy10ZGF0YS1jZG4uY29tL3IvMTE1OTEyODctNjQ4Zi00YzYwLWFlNWMtODBiNjFiMTJkNzhiIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNTQxNjI1NzA1fX19XX0_&Key-Pair-Id=APKAJZFK5OCWBA5LQHUQ";
-
       const string fileName = "dummy.dxf";
       var expectedFile = new DataOceanFile
       {
@@ -683,7 +678,7 @@ namespace VSS.DataOcean.Client.UnitTests
         Multifile = false,
         RegionPreferences = new List<string> { "us1" },
         Status = status,
-        DataOceanUpload = new DataOceanTransfer { Url = uploadUrl }
+        DataOceanUpload = new DataOceanTransfer { Url = TestConstants.UploadUrl }
       };
       var expectedFileResult = new DataOceanFileResult { File = expectedFile };
       var folderName = $"{Path.DirectorySeparatorChar}";
@@ -704,7 +699,7 @@ namespace VSS.DataOcean.Client.UnitTests
         .Setup(g => g.ExecuteRequest<DataOceanFileResult>(createUrl, It.IsAny<MemoryStream>(), null, HttpMethod.Post, null, 3, false))
         .ReturnsAsync(expectedFileResult);
       gracefulMock
-        .Setup(g => g.ExecuteRequestAsStreamContent(uploadUrl, HttpMethod.Put, null, It.IsAny<Stream>(), null, 3, false))
+        .Setup(g => g.ExecuteRequestAsStreamContent(TestConstants.UploadUrl, HttpMethod.Put, null, It.IsAny<Stream>(), null, 3, false))
         .ReturnsAsync(expectedUploadResult);
       gracefulMock.Setup(g => g.ExecuteRequest<DataOceanFileResult>(getUrl, null, null, HttpMethod.Get, null, 3, false))
         .Returns(Task.FromResult(expectedFileResult));
