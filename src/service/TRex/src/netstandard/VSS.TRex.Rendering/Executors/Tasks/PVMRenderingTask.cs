f33034888de8eaddd6b0a046d648de50a6b2be8d
Raymond Wilson <raymond_wilson@trimble.com>
2020-02-10 13:26:07 +1300

    Add accumulator to the PVM rendering task

--- src/service/TRex/src/netstandard/VSS.TRex.Rendering/Executors/Tasks/PVMRenderingTask.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Rendering/Executors/Tasks/PVMRenderingTask.cs
@@ -9,6 +9,14 @@ using VSS.TRex.SubGridTrees.Factories;
 using VSS.TRex.SubGridTrees.Interfaces;
 using VSS.TRex.SubGridTrees.Types;
 
+// Notes for consistent value accumulation and smoothing
+// - Construct a rectangular array of values to contain the overall bounds of cells in the default north orientation of the cell selection
+//    [as opposed to the project orienttion which is taken into account when the values are rendered]
+// - Copy the values from the sub grids as they arrive into the task
+// - Apply any required smoothing, gap filling etc, activities to the resulting grid of values once all subgrids have arrived
+// - render the arrya of values into the tile as if it were a single large subgrid
+
+
 namespace VSS.TRex.Rendering.Executors.Tasks
 {
   /// <summary>
@@ -19,6 +27,7 @@ namespace VSS.TRex.Rendering.Executors.Tasks
     private static readonly ILogger Log = Logging.Logger.CreateLogger(MethodBase.GetCurrentMethod().DeclaringType?.Name);
 
     public ISubGridTree SubGridTree { get; private set; }
+    public PVMTaskAccumulator Accumulator { get; set; }
 
     /// <summary>
     /// The tile renderer responsible for processing sub grid information into tile based thematic rendering
@@ -28,6 +37,7 @@ namespace VSS.TRex.Rendering.Executors.Tasks
     public PVMRenderingTask()
     {
     }
+
 
     private bool AddSubGridToTree(IClientLeafSubGrid leaf)
     {
@@ -46,16 +56,18 @@ namespace VSS.TRex.Rendering.Executors.Tasks
     public override bool TransferResponse(object response)
     {
       // Log.InfoFormat("Received a SubGrid to be processed: {0}", (response as IClientLeafSubGrid).Moniker());
-      bool result = false;
+      var result = false;
 
       if (base.TransferResponse(response))
       {
         if (!(response is IClientLeafSubGrid[] subGridResponses) || subGridResponses.Length == 0)
+        {
           Log.LogWarning("No sub grid responses returned");
+        }
         else
-          // May need to do this to intervene in case too may sub grids are being accumulated
-          //result = subGridResponses.Where(x => x != null).All(TileRenderer.Displayer.RenderSubGrid
-          result = subGridResponses.Where(x => x != null).All(AddSubGridToTree);
+        {
+          result = Accumulator?.Transcribe(subGridResponses) ?? subGridResponses.Where(x => x != null).All(TileRenderer.Displayer.RenderSubGrid);
+        }
       }
 
       return result;
