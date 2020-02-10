93edb4bf6470a34ead79d295806d3542572055b3
Raymond Wilson <raymond_wilson@trimble.com>
2020-02-10 15:31:07 +1300

    Refactor consistent displayer into a separate derived class for clarity

---
+++ src/service/TRex/src/netstandard/VSS.TRex.Rendering/Displayers/ProductionPVMConsistentDisplayer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace VSS.TRex.Rendering.Displayers
+{
+  public abstract class ProductionPVMConsistentDisplayer : ProductionPVMDisplayerBase
+  {
+    /// <summary>
+    /// Performs a 'consistent' render across a 2D array of collated values from queried subgrids.
+    /// Effectively this treats the passed array as if it were a subgrid of that size and renders it as
+    /// such against the MapView.
+    /// Essentially, this function should be called just once to render the entire set of data for a tile
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="valueStore"></param>
+    /// <param name="worldOriginX"></param>
+    /// <param name="worldOriginY"></param>
+    /// <param name="valueCellSizeX"></param>
+    /// <param name="valueCellSizeY"></param>
+    /// <returns></returns>
+    public bool PerformConsistentRender<T>(T[,] valueStore,
+      double worldOriginX, double worldOriginY, double valueCellSizeX, double valueCellSizeY)
+    {
+      var xDimension = valueStore.GetLength(0);
+      var yDimension = valueStore.GetLength(1);
+
+      var stepsPerPixelX = MapView.XPixelSize / valueCellSizeX;
+      var stepsPerPixelY = MapView.YPixelSize / valueCellSizeY;
+
+      stepX = Math.Min(MAX_STEP_SIZE, Math.Max(1, (int)Math.Truncate(stepsPerPixelX)));
+      stepY = Math.Min(MAX_STEP_SIZE, Math.Max(1, (int)Math.Truncate(stepsPerPixelY)));
+
+      stepXIncrement = stepX * valueCellSizeX;
+      stepYIncrement = stepY * valueCellSizeY;
+
+      stepXIncrementOverTwo = stepXIncrement / 2;
+      stepYIncrementOverTwo = stepYIncrement / 2;
+
+      // Draw the cells in the grid in stripes, starting from the southern most
+      // row in the grid and progressing from the western end to the eastern end
+      // (ie: bottom to top, left to right)
+
+      // See if this display supports cell strip rendering
+
+      var drawCellStrips = SupportsCellStripRendering();
+
+      // Skip-Iterate through the cells drawing them in strips
+
+      var temp = worldOriginY / stepYIncrement;
+      currentNorth = (Math.Truncate(temp) * stepYIncrement) - stepYIncrementOverTwo;
+      north_row = (int)Math.Floor((currentNorth - worldOriginY) / valueCellSizeY);
+
+      while (north_row < 0)
+      {
+        north_row += stepY;
+        currentNorth += stepYIncrement;
+      }
+
+      while (north_row < yDimension)
+      {
+        temp = worldOriginX / stepXIncrement;
+        currentEast = (Math.Truncate(temp) * stepXIncrement) + stepXIncrementOverTwo;
+        east_col = (int)Math.Floor((currentEast - worldOriginX) / valueCellSizeX);
+
+        while (east_col < 0)
+        {
+          east_col += stepX;
+          currentEast += stepXIncrement;
+        }
+
+        if (drawCellStrips)
+          DoStartRowScan();
+
+        while (east_col < xDimension)
+        {
+          if (drawCellStrips)
+            DoAccumulateStrip();
+          else
+            DoRenderCell();
+
+          currentEast += stepXIncrement;
+          east_col += stepX;
+        }
+
+        if (drawCellStrips)
+          DoEndRowScan();
+
+        currentNorth += stepYIncrement;
+        north_row += stepY;
+      }
+
+      return true;
+    }
+  }
+}
--- src/service/TRex/src/netstandard/VSS.TRex.Rendering/Displayers/ProductionPVMDisplayerBase.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Rendering/Displayers/ProductionPVMDisplayerBase.cs
@@ -9,7 +9,7 @@ namespace VSS.TRex.Rendering.Displayers
   {
     //private static readonly ILogger Log = Logging.Logger.CreateLogger<ProductionPVMDisplayerBase>();
 
-    private const int MAX_STEP_SIZE = 10000;
+    protected const int MAX_STEP_SIZE = 10000;
 
     private ISubGrid _subGrid;
 
@@ -24,19 +24,19 @@ namespace VSS.TRex.Rendering.Displayers
     protected ISubGrid SubGrid { get => _subGrid; set => SetSubGrid(value); }
 
     // Various quantities useful when displaying a sub grid full of grid data
-    private int stepX;
-    private int stepY;
+    protected int stepX;
+    protected int stepY;
 
-    private double stepXIncrement;
-    private double stepYIncrement;
-    private double stepXIncrementOverTwo;
-    private double stepYIncrementOverTwo;
+    protected double stepXIncrement;
+    protected double stepYIncrement;
+    protected double stepXIncrementOverTwo;
+    protected double stepYIncrementOverTwo;
 
     // Various quantities useful when iterating across cells in a sub grid and drawing them
 
     protected int north_row, east_col;
-    private double currentNorth;
-    private double currentEast;
+    protected double currentNorth;
+    protected double currentEast;
 
     private double cellSize;
 
@@ -151,94 +151,7 @@ namespace VSS.TRex.Rendering.Displayers
       return true;
     }
 
-    /// <summary>
-    /// Performs a 'consistent' render across a 2D array of collated values from queried subgrids.
-    /// Effectively this treats the passed array as if it were a subgrid of that size and renders it as
-    /// such against the MapView.
-    /// Essentially, this function should be called just once to render the entire set of data for a tile
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="valueStore"></param>
-    /// <param name="worldOriginX"></param>
-    /// <param name="worldOriginY"></param>
-    /// <param name="valueCellSizeX"></param>
-    /// <param name="valueCellSizeY"></param>
-    /// <returns></returns>
-    public bool PerformConsistentRender<T>(T[,] valueStore,
-      double worldOriginX, double worldOriginY, double valueCellSizeX, double valueCellSizeY)
-    {
-      var xDimension = valueStore.GetLength(0);
-      var yDimension = valueStore.GetLength(1);
-
-      var stepsPerPixelX = MapView.XPixelSize / valueCellSizeX;
-      var stepsPerPixelY = MapView.YPixelSize / valueCellSizeY;
-
-      stepX = Math.Min(MAX_STEP_SIZE, Math.Max(1, (int)Math.Truncate(stepsPerPixelX)));
-      stepY = Math.Min(MAX_STEP_SIZE, Math.Max(1, (int)Math.Truncate(stepsPerPixelY)));
-
-      stepXIncrement = stepX * valueCellSizeX;
-      stepYIncrement = stepY * valueCellSizeY;
-
-      stepXIncrementOverTwo = stepXIncrement / 2;
-      stepYIncrementOverTwo = stepYIncrement / 2;
-
-      // Draw the cells in the grid in stripes, starting from the southern most
-      // row in the grid and progressing from the western end to the eastern end
-      // (ie: bottom to top, left to right)
-
-      // See if this display supports cell strip rendering
-
-      var drawCellStrips = SupportsCellStripRendering();
-
-      // Skip-Iterate through the cells drawing them in strips
-
-      var temp = worldOriginY / stepYIncrement;
-      currentNorth = (Math.Truncate(temp) * stepYIncrement) - stepYIncrementOverTwo;
-      north_row = (int)Math.Floor((currentNorth - worldOriginY) / valueCellSizeY);
-
-      while (north_row < 0)
-      {
-        north_row += stepY;
-        currentNorth += stepYIncrement;
-      }
-
-      while (north_row < yDimension)
-      {
-        temp = worldOriginX / stepXIncrement;
-        currentEast = (Math.Truncate(temp) * stepXIncrement) + stepXIncrementOverTwo;
-        east_col = (int)Math.Floor((currentEast - worldOriginX) / valueCellSizeX);
-
-        while (east_col < 0)
-        {
-          east_col += stepX;
-          currentEast += stepXIncrement;
-        }
-
-        if (drawCellStrips)
-          DoStartRowScan();
-
-        while (east_col < xDimension)
-        {
-          if (drawCellStrips)
-            DoAccumulateStrip();
-          else
-            DoRenderCell();
-
-          currentEast += stepXIncrement;
-          east_col += stepX;
-        }
-
-        if (drawCellStrips)
-          DoEndRowScan();
-
-        currentNorth += stepYIncrement;
-        north_row += stepY;
-      }
-
-      return true;
-    }
-
-    private void DoRenderCell()
+    protected void DoRenderCell()
     {
       var colour = DoGetDisplayColour();
 
@@ -259,15 +172,15 @@ namespace VSS.TRex.Rendering.Displayers
     // clNone as the colour).
     protected abstract Color DoGetDisplayColour();
 
-    private void DoStartRowScan() => accumulatingScanLine = false;
+    protected void DoStartRowScan() => accumulatingScanLine = false;
 
-    private void DoEndRowScan()
+    protected void DoEndRowScan()
     {
       if (accumulatingScanLine)
         DoRenderStrip();
     }
 
-    private void DoAccumulateStrip()
+    protected void DoAccumulateStrip()
     {
       var displayColour = DoGetDisplayColour();
 
@@ -319,10 +232,6 @@ namespace VSS.TRex.Rendering.Displayers
     public MapSurface MapView { get; set; }
 
     public bool HasRenderedSubGrid { get; set; }
-
-//    public ProductionPVMDisplayerBase()
-//    {
-//    }
 
     public bool RenderSubGrid(IClientLeafSubGrid clientSubGrid)
     {
