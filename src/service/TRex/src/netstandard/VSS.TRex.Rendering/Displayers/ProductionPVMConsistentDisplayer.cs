894656f0073a25e95d69483da0ef9571f783a063
Raymond Wilson <raymond_wilson@trimble.com>
2020-02-11 15:55:19 +1300

    Code tidy

--- src/service/TRex/src/netstandard/VSS.TRex.Rendering/Displayers/ProductionPVMConsistentDisplayer.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Rendering/Displayers/ProductionPVMConsistentDisplayer.cs
@@ -13,7 +13,7 @@ namespace VSS.TRex.Rendering.Displayers
     public TP Palette;
 
     /// <summary>
-    /// A palette set accessor for use when a palette is only known bbyb its IPlanViewPalette interface
+    /// A palette set accessor for use when a palette is only known by its IPlanViewPalette interface
     /// </summary>
     /// <param name="palette"></param>
     public override void SetPalette(IPlanViewPalette palette)
@@ -32,12 +32,10 @@ namespace VSS.TRex.Rendering.Displayers
     /// <returns></returns>
     public override IPlanViewPalette GetPalette() => Palette;
 
-    // public MapSurface GetMapView() => MapView;
-
     /// <summary>
     /// Constructs a PVM task accumulator tailored to accumulate cell information to be rendered by this displayer
     /// Note: This intentionally does not pin the bounds of the accumulator to the bounds of the rendered map view
-    /// as mapview rotation and smoothign operations may require large areas of data to be requested to supply thye final
+    /// as mapview rotation and smoothing operations may require large areas of data to be requested to supply the final
     /// rendered outcome.
     /// </summary>
     /// <param name="cellsWidth"></param>
@@ -79,10 +77,10 @@ namespace VSS.TRex.Rendering.Displayers
 
 
     /// <summary>
-    /// Performs a 'consistent' render across a 2D array of collated values from queried subgrids.
+    /// Performs a 'consistent' render across a 2D array of collated values from queried sub grids.
     /// Effectively this treats the passed array as if it were a subgrid of that size and renders it as
     /// such against the MapView.
-    /// Essentially, this function should be called just once to render the entire set of data for a tile
+    /// This function should be called just once to render the entire set of data for a tile
     /// </summary>
     /// <returns></returns>
     public bool PerformConsistentRender()
@@ -105,7 +103,6 @@ namespace VSS.TRex.Rendering.Displayers
       // row in the grid and progressing from the western end to the eastern end
       // (ie: bottom to top, left to right)
 
-      // Skip-Iterate through the cells drawing them in strips
       DoSkipIterate(_taskAccumulator.OriginX, _taskAccumulator.OriginY, ValueStore.GetLength(0), ValueStore.GetLength(1));
 
       return true;
