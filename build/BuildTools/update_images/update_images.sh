3712e69488081a35761721fe4fa3f9d5750ea105
Josh Nimmo <josh_nimmo@trimble.com>
2020-02-20 22:09:45 +1300

    I can't see how this ever worked, it now does.

--- build/BuildTools/update_images/update_images.sh
+++ build/BuildTools/update_images/update_images.sh
@@ -1,16 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
+#sleep while dockerd starts usually only takes a second
+sleep 10
+eval $(aws ecr get-login --no-include-email --region us-west-2)
 docker pull microsoft/dotnet:2.1-sdk 
 docker pull microsoft/dotnet:2.0-sdk 
 docker pull microsoft/dotnet:2.1-runtime
 docker pull microsoft/dotnet:2.0-runtime  
 docker pull node:8
-docker tag microsoft/dotnet:2.1-sdk 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.1-sdk
-docker tag microsoft/dotnet:2.0-sdk 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.0-sdk
-docker tag microsoft/dotnet:2.1-runtime 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.1-runtime
-docker tag microsoft/dotnet:2.0-runtime 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.0-runtime
-docker tag node:8 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:node-8
-docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.1-sdk
-docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.0-sdk
-docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.1-runtime
-docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.0-runtime
-docker push 276986344560.dkr.ecr.us-west-2.amazonaws.com/base-images:node-8
+docker tag microsoft/dotnet:2.1-sdk 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.1-sdk
+docker tag microsoft/dotnet:2.0-sdk 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.0-sdk
+docker tag microsoft/dotnet:2.1-runtime 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.1-runtime
+docker tag microsoft/dotnet:2.0-runtime 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.0-runtime
+docker tag node:8 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:node-8
+docker push 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.1-sdk
+docker push 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.0-sdk
+docker push 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.1-runtime
+docker push 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:dotnet-2.0-runtime
+docker push 300213723870.dkr.ecr.us-west-2.amazonaws.com/base-images:node-8
+echo "finished update killing supervisor and stopping"
+pkill -f supervisord
