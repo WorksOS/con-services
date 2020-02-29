bb73760547113b4b09d3b8ea01fb3ffc3aa0380e
Dmitry Golovachev <dmitry_golovachev@trimble.com>
2020-02-29 21:36:52 +1300

    Dockerfile for mock api

--- build/BuildTools/update_images/update_images.sh
+++ build/BuildTools/update_images/update_images.sh
@@ -3,6 +3,7 @@ docker pull microsoft/dotnet:2.1-sdk
 docker pull microsoft/dotnet:2.0-sdk 
 docker pull mcr.microsoft.com/dotnet/core/sdk:3.1
 docker pull microsoft/dotnet:2.1-runtime
+docker pull microsoft/dotnet:2.1-aspnetcore-runtime
 docker pull microsoft/dotnet:2.0-runtime  
 docker pull mcr.microsoft.com/dotnet/core/runtime:3.1  
 docker pull node:8
@@ -10,6 +11,7 @@ docker tag microsoft/dotnet:2.1-sdk 940327799086.dkr.ecr.us-west-2.amazonaws.com
 docker tag microsoft/dotnet:2.0-sdk 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-2.0-sdk
 docker tag mcr.microsoft.com/dotnet/core/sdk:3.1 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-3.1-sdk
 docker tag microsoft/dotnet:2.1-runtime 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-2.1-runtime
+docker tag microsoft/dotnet:2.1-aspnetcore-runtime 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-2.1-aspnetcore-runtime
 docker tag microsoft/dotnet:2.0-runtime 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-2.0-runtime
 docker tag mcr.microsoft.com/dotnet/core/runtime:3.1 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-3.1-runtime
 docker tag node:8 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:node-8
@@ -17,6 +19,7 @@ docker push 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:do
 docker push 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-2.0-sdk
 docker push 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-3.1-sdk
 docker push 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-2.1-runtime
+docker push 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-2.1-aspnetcore-runtime
 docker push 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-2.0-runtime
 docker push 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:dotnet-3.1-runtime
 docker push 940327799086.dkr.ecr.us-west-2.amazonaws.com/rpd-ccss-base-images:node-8
