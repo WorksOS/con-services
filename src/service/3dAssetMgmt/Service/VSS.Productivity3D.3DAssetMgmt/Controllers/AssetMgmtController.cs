83bce7cc217c10ce12e3682b5d2e34a360ea8921
Aaron Morgan <aaron_morgan@trimble.com>
2020-03-24 16:43:39 +1300

    CCSSSCON-137: Get branch on origin; minor code tidying of the Asset Mgt service controllers.

--- src/service/3dAssetMgmt/Service/VSS.Productivity3D.3DAssetMgmt/Controllers/AssetMgmtController.cs
+++ src/service/3dAssetMgmt/Service/VSS.Productivity3D.3DAssetMgmt/Controllers/AssetMgmtController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using VSS.MasterData.Models.Handlers;
 using VSS.MasterData.Repositories;
 using VSS.MasterData.Repositories.DBModels;
 using VSS.MasterData.Repositories.ExtendedModels;
@@ -14,14 +13,14 @@ using VSS.Productivity3D.AssetMgmt3D.Models;
 
 namespace VSS.Productivity3D.AssetMgmt3D.Controllers
 {
-
   public class AssetMgmtController : BaseController
   {
     private readonly IAssetRepository assetRepository;
 
-    public AssetMgmtController(ILoggerFactory loggerFactory, IServiceExceptionHandler serviceExceptionHandler,
-      IAssetRepository assetRepository)
-      : base(loggerFactory, serviceExceptionHandler)
+    /// <summary>
+    /// Public constructor.
+    /// </summary>
+    public AssetMgmtController(IAssetRepository assetRepository)
     {
       this.assetRepository = assetRepository;
     }
@@ -29,8 +28,6 @@ namespace VSS.Productivity3D.AssetMgmt3D.Controllers
     /// <summary>
     /// Get a list of asset Uid/Id matches for Uids supplied
     /// </summary>
-    /// <response code="200">A list of matched assets.</response>
-    /// <response code="403">Invalid access token provided</response>
     [HttpPost("api/v1/assets/assetuids")]
     [ProducesResponseType(typeof(List<AssetDisplayModel>), 200)]
     public async Task<IActionResult> GetMatchingAssets([FromBody] List<Guid> assetUids)
@@ -46,8 +43,6 @@ namespace VSS.Productivity3D.AssetMgmt3D.Controllers
     /// <summary>
     /// Get a list of asset Uid/Id matches for Ids supplied
     /// </summary>
-    /// <response code="200">A list of matched assets.</response>
-    /// <response code="403">Invalid access token provided</response>
     [HttpPost("api/v1/assets/assetids")]
     [ProducesResponseType(typeof(List<AssetDisplayModel>), 200)]
     public async Task<IActionResult> GetMatchingAssets([FromBody] List<long> assetIds)
@@ -63,18 +58,16 @@ namespace VSS.Productivity3D.AssetMgmt3D.Controllers
     /// <summary>
     /// Get a potentially matching 2D asset  for a 3D asset
     /// </summary>
-    /// <response code="200">A list of matched assets.</response>
-    /// <response code="403">Invalid access token provided</response>
     [HttpGet("api/v1/assets/match3dasset/{assetUid}")]
     [ProducesResponseType(typeof(AssetDisplayModel), 200)]
     public async Task<IActionResult> GetMatching2DAssets([FromRoute] Guid assetUid)
     {
       Log.LogInformation($"Getting matching Assets for AssetUID3D: {assetUid}");
-      var matchingAsset = new MatchingAssets() {AssetUID3D = assetUid.ToString()};
+      var matchingAsset = new MatchingAssets { AssetUID3D = assetUid.ToString() };
       var result = await assetRepository.GetMatching3D2DAssets(matchingAsset);
 
       var model = result == null
-        ? new MatchingAssetsDisplayModel((int) AssetMgmt3DExecutionStates.ErrorCodes.NoMatchingAssets, "No matching assets found")
+        ? new MatchingAssetsDisplayModel((int)AssetMgmt3DExecutionStates.ErrorCodes.NoMatchingAssets, "No matching assets found")
         : new MatchingAssetsDisplayModel
         {
           AssetUID3D = result.AssetUID3D,
@@ -95,14 +88,12 @@ namespace VSS.Productivity3D.AssetMgmt3D.Controllers
     /// <summary>
     /// Get a potentially matching 3D asset  for a 2D asset
     /// </summary>
-    /// <response code="200">A list of matched assets.</response>
-    /// <response code="403">Invalid access token provided</response>
     [HttpGet("api/v1/assets/match2dasset/{assetUid}")]
     [ProducesResponseType(typeof(AssetDisplayModel), 200)]
     public async Task<IActionResult> GetMatching3DAssets([FromRoute] Guid assetUid)
     {
       Log.LogInformation($"Getting matching Assets for AssetUID2D: {assetUid}");
-      var matchingAsset = new MatchingAssets() { AssetUID2D = assetUid.ToString() };
+      var matchingAsset = new MatchingAssets { AssetUID2D = assetUid.ToString() };
       var result = await assetRepository.GetMatching3D2DAssets(matchingAsset);
 
       var model = result == null
--- src/service/3dAssetMgmt/Service/VSS.Productivity3D.3DAssetMgmt/Controllers/BaseController.cs
+++ src/service/3dAssetMgmt/Service/VSS.Productivity3D.3DAssetMgmt/Controllers/BaseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Security.Principal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using VSS.Common.Abstractions.Http;
 using VSS.MasterData.Models.Handlers;
@@ -12,31 +13,18 @@ namespace VSS.Productivity3D.AssetMgmt3D.Controllers
 {
   public abstract class BaseController : Controller
   {
-    protected BaseController(ILoggerFactory loggerFactory, IServiceExceptionHandler serviceExceptionHandler)
-    {
-      ServiceExceptionHandler = serviceExceptionHandler;
-      Log = loggerFactory.CreateLogger(GetType().Name);
-    }
+    private ILogger _log;
+    private IServiceExceptionHandler _serviceExceptionHandler;
 
-    protected ILogger Log { get; private set; }
-    
-    protected IServiceExceptionHandler ServiceExceptionHandler { get; private set; }
-
-    /// <summary>
-    /// Gets the custom customHeaders for the request.
-    /// </summary>
-    /// <value>
-    /// The custom customHeaders.
-    /// </value>
+    protected ILogger Log => _log ??= HttpContext?.RequestServices.GetRequiredService<ILoggerFactory>().CreateLogger(GetType().Name);
+    protected IServiceExceptionHandler ServiceExceptionHandler => _serviceExceptionHandler ??= HttpContext.RequestServices.GetRequiredService<IServiceExceptionHandler>();
     protected IDictionary<string, string> CustomHeaders => Request.Headers.GetCustomHeaders(true);
-
     protected string CustomerUid => Request.Headers[HeaderConstants.X_VISION_LINK_CUSTOMER_UID].ToString();
-
     protected string UserId => GetUserId();
 
     private string GetUserId()
     {
-      if (User is TIDCustomPrincipal principal && (principal.Identity is GenericIdentity identity))
+      if (User is TIDCustomPrincipal principal && principal.Identity is GenericIdentity identity)
       {
         return identity.Name;
       }
