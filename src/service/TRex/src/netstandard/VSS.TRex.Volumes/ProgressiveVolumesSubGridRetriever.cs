afb1fbc729e4ab9b899149ea7b66208270c7cf23
rwilson <raymond_wilson@trimble.com>
2020-03-28 13:45:38 +1300

    CCSSSCON-130: Define new grid date type for progressive volumes, add progressive sub grdi retriever to factory and move factory to PSNode out of VSS.TRex.SubGrids namespace

--- src/service/TRex/src/netstandard/VSS.TRex.Volumes/ProgressiveVolumesSubGridRetriever.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Volumes/ProgressiveVolumesSubGridRetriever.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using VSS.TRex.Common.Models;
+using VSS.TRex.Filters.Interfaces;
+using VSS.TRex.Geometry;
+using VSS.TRex.SiteModels.Interfaces;
+using VSS.TRex.Storage.Interfaces;
+using VSS.TRex.SubGrids;
 using VSS.TRex.SubGrids.Interfaces;
 using VSS.TRex.SubGridTrees;
 using VSS.TRex.SubGridTrees.Client.Interfaces;
+using VSS.TRex.SubGridTrees.Interfaces;
 using VSS.TRex.Types;
 
 namespace VSS.TRex.Volumes
@@ -13,8 +20,50 @@ namespace VSS.TRex.Volumes
    * Should always have a time range?
    *
    */
-  public class ProgressiveVolumesSubGridRetriever : ISubGridRetriever
+  public class ProgressiveVolumesSubGridRetriever : SubGridRetrieverBase, ISubGridRetriever
   {
+    /// <summary>
+    /// Constructor for the sub grid retriever helper
+    /// </summary>
+    /// <param name="siteModel">The project this sub gris is being retrieved from</param>
+    /// <param name="gridDataType">The type of client grid data sub grids to be returned by this retriever</param>
+    /// <param name="storageProxy">The Ignite storage proxy to be used when requesting data from the persistent store</param>
+    /// <param name="filter">The TRex spatial and attribute filtering description for the request</param>
+    /// <param name="filterAnnex">An annex of data related to cell by cell filtering where the attributes related to that cell may change from cell to cell</param>
+    /// <param name="hasOverrideSpatialCellRestriction">The spatially selected cells are masked by a rectangular restriction boundary</param>
+    /// <param name="overrideSpatialCellRestriction"></param>
+    /// <param name="prepareGridForCacheStorageIfNoSieving">The cell coordinate bounding box restricting cells involved in the request</param>
+    /// <param name="maxNumberOfPassesToReturn">The maximum number of passes in a cell in a sub grid that will be considered when processing the request</param>
+    /// <param name="areaControlSet">The skip/step area control set for selection of cells with sub grids for processing. Cells not identified by the control set will return null values.</param>
+    /// <param name="populationControl">The delegate responsible for populating events depended on for processing the request.</param>
+    /// <param name="pDExistenceMap">The production data existence map for the project the request relates to</param>
+    /// <param name="overrides">The set of overriding machine event values to use</param>
+    /// <param name="liftParams">The set of layer/lift analysis parameters to use</param>
+    public ProgressiveVolumesSubGridRetriever(ISiteModel siteModel,
+      GridDataType gridDataType,
+      IStorageProxy storageProxy,
+      ICombinedFilter filter,
+      ICellPassAttributeFilterProcessingAnnex filterAnnex,
+      bool hasOverrideSpatialCellRestriction,
+      BoundingIntegerExtent2D overrideSpatialCellRestriction,
+      bool prepareGridForCacheStorageIfNoSieving,
+      int maxNumberOfPassesToReturn,
+      AreaControlSet areaControlSet,
+      IFilteredValuePopulationControl populationControl,
+      ISubGridTreeBitMask pDExistenceMap,
+      IOverrideParameters overrides,
+      ILiftParameters liftParams)
+      : base(siteModel, gridDataType, filter, filterAnnex,
+        hasOverrideSpatialCellRestriction, overrideSpatialCellRestriction, prepareGridForCacheStorageIfNoSieving, maxNumberOfPassesToReturn,
+        storageProxy, areaControlSet, populationControl, pDExistenceMap, overrides, liftParams)
+    {
+    }
+
+    public override void RetrieveSubGridStripe(byte stripeIndex)
+    {
+      throw new NotImplementedException();
+    }
+
     public ServerRequestResult RetrieveSubGrid(IClientLeafSubGrid clientGrid, SubGridTreeBitmapSubGridBits cellOverrideMask)
     {
       throw new NotImplementedException();
---
+++ src/service/TRex/src/netstandard/services/VSS.TRex.Server.PSNode/SubGridRetrieverFactory.cs
@@ -0,0 +1,71 @@
+﻿using VSS.TRex.Caching.Interfaces;
+using VSS.TRex.Common.Models;
+using VSS.TRex.Filters.Interfaces;
+using VSS.TRex.Geometry;
+using VSS.TRex.SiteModels.Interfaces;
+using VSS.TRex.Storage.Interfaces;
+using VSS.TRex.SubGrids;
+using VSS.TRex.SubGrids.Interfaces;
+using VSS.TRex.SubGridTrees.Interfaces;
+using VSS.TRex.Types;
+using VSS.TRex.Volumes;
+
+namespace VSS.TRex.Server.PSNode
+{
+  /// <summary>
+  /// Factory class that constructs specialist sub grid retrievers according to the grid data type being requested
+  /// </summary>
+  public class SubGridRetrieverFactory : ISubGridRetrieverFactory
+  {
+    public ISubGridRetriever Instance(ISiteModel siteModel,
+      GridDataType gridDataType,
+      IStorageProxy storageProxy,
+      ICombinedFilter filter,
+      ICellPassAttributeFilterProcessingAnnex filterAnnex,
+      bool hasOverrideSpatialCellRestriction,
+      BoundingIntegerExtent2D overrideSpatialCellRestriction,
+      int maxNumberOfPassesToReturn,
+      AreaControlSet areaControlSet,
+      IFilteredValuePopulationControl populationControl,
+      ISubGridTreeBitMask pdExistenceMap,
+      ITRexSpatialMemoryCacheContext subGridCacheContext,
+      IOverrideParameters overrides,
+      ILiftParameters liftParams)
+    {
+      if (gridDataType == GridDataType.ProgressiveVolumes)
+      {
+        return new ProgressiveVolumesSubGridRetriever(siteModel,
+          gridDataType,
+          storageProxy,
+          filter,
+          filterAnnex,
+          hasOverrideSpatialCellRestriction,
+          overrideSpatialCellRestriction,
+          subGridCacheContext != null,
+          maxNumberOfPassesToReturn,
+          areaControlSet,
+          populationControl,
+          pdExistenceMap,
+          overrides,
+          liftParams);
+      }
+      else
+      {
+        return new SubGridRetriever(siteModel,
+          gridDataType,
+          storageProxy,
+          filter,
+          filterAnnex,
+          hasOverrideSpatialCellRestriction,
+          overrideSpatialCellRestriction,
+          subGridCacheContext != null,
+          maxNumberOfPassesToReturn,
+          areaControlSet,
+          populationControl,
+          pdExistenceMap,
+          overrides,
+          liftParams);
+      }
+    }
+  }
+}
