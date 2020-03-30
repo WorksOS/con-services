b27d94ba58691d1e6564d0b130e1a42073c9c943
rwilson <raymond_wilson@trimble.com>
2020-03-31 11:00:45 +1300

    CCSSSCON-150: Update Confluent.Kafka from 1.0.0-beta to 1.3.0

---
+++ src/service/Project/AcceptanceTests/tests/IntegrationTests/TestTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntegrationTests
+{
+  public class TestTest
+  {
+    [Fact]
+    public void Test()
+    {
+      var x = TestUtility.RdKafkaDriver.SendKafkaMessage("bob", "bob");
+    }
+  }
+}
