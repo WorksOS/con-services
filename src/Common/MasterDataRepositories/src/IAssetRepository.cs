44cc91144884ffd7958146c826db5b74b8fd0527
Aaron Morgan <aaron_morgan@trimble.com>
2020-03-31 14:38:25 +1300

    CCSSSCON-137: Return better mocks from the Asset location endpoint.

--- src/Common/MasterDataRepositories/src/IAssetRepository.cs
+++ src/Common/MasterDataRepositories/src/IAssetRepository.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using VSS.MasterData.Repositories.DBModels;
@@ -14,8 +14,8 @@ namespace VSS.MasterData.Repositories
     Task<IEnumerable<Asset>> GetAssets();
     Task<IEnumerable<Asset>> GetAllAssetsInternal();
     Task<IEnumerable<Asset>> GetAssets(string[] productFamily);
-    Task<IEnumerable<Asset>> GetAssets(IEnumerable<Guid> assetUids);
-    Task<IEnumerable<Asset>> GetAssets(IEnumerable<long> assetIds);
+    Task<List<Asset>> GetAssets(IEnumerable<Guid> assetUids);
+    Task<List<Asset>> GetAssets(IEnumerable<long> assetIds);
     Task<int> StoreEvent(IAssetEvent filterEvent);
     Task<MatchingAssets> GetMatching3D2DAssets(MatchingAssets asset);
   }
