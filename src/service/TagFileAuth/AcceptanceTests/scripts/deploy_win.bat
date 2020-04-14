8166c355e698ffc6d6991be32d3ca10df9ba23ba
Jeannie May <Jeannie_May@trimble.com>
2020-04-15 11:21:11 +1200

    #CCSSSCON-209 cleanup TFA AT run

--- src/service/TagFileAuth/AcceptanceTests/scripts/deploy_win.bat
+++ src/service/TagFileAuth/AcceptanceTests/scripts/deploy_win.bat
@@ -10,11 +10,9 @@ mkdir deploy\testresults
 dotnet restore --no-cache VSS.TagFileAuth.Service.AcceptanceTests.sln
 
 cd tests
+Write-Host "Publishing acceptance test projects" -ForegroundColor DarkGray
 dotnet publish WebApiTests\WebApiTests.csproj -o ..\..\deploy\WebApiTests -f netcoreapp3.1
 
-copy WebApiTests\appsettings.json ..\deploy\WebApiTests\
+copy WebApiTests\appsettings.json ..\..\deploy\WebApiTests\
 
-cd ..
-cd utilities
-dotnet publish TestRun\TestRun.csproj -o ..\..\deploy\TestRun -f netcoreapp3.1
 cd ..
