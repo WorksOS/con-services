6f5229b006644672049590417999722e30ad1750
rwilson <raymond_wilson@trimble.com>
2020-04-16 12:45:34 +1200

    CCSSSCON-217: Add initial test for JSON map reading

---
+++ src/service/TagFileAuth/test/UnitTests/WebApiTests/RadioSerialMap/CustomRadioSerialMapperTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSS.Productivity3D.TagFileAuth.WebAPI.Models.Executors;
+using VSS.Productivity3D.TagFileAuth.WebAPI.Models.RadioSerialMap;
+using WebApiTests.Executors;
+
+namespace WebApiTests.RadioSerialMap
+{
+  [TestClass]
+  public class CustomRadioSerialMapTests : ExecutorBaseTests
+  {
+    [TestMethod]
+    public void Creation()
+    {
+      var loggerFactory = ServiceProvider.GetRequiredService<ILoggerFactory>();
+
+      var mapper = new CustomRadioSerialProjectMap(loggerFactory);
+      Assert.IsTrue(mapper != null);
+    }
+  }
+}
