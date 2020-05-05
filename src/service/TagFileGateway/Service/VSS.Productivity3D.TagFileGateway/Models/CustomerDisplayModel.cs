5cf89a234aa0c0bd4046be9f6b3344f4b949cb46
Stephen Post <stephen_post@trimble.com>
2020-05-06 11:03:08 +1200

    Add Tag file gateway

---
+++ src/service/TagFileGateway/Service/VSS.Productivity3D.TagFileGateway/Models/CustomerDisplayModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using VSS.MasterData.Models.Models;
+
+namespace VSS.Productivity3D.TagFileForwarder.Models
+{
+  /// <summary>
+  /// Represents a Customer
+  /// </summary>
+  public class CustomerDisplayModel
+  {
+    public CustomerDisplayModel()
+    {
+      Projects = new List<ProjectDisplayModel>();
+    }
+    
+    /// <summary>
+    /// Unique Identifier for the customer
+    /// </summary>
+    public string CustomerUid { get; set; }
+
+    /// <summary>
+    /// Customer Name
+    /// </summary>
+    public string CustomerName { get; set; }
+
+    /// <summary>
+    /// Projects attached to this Customer, both active or archived projects are included
+    /// </summary>
+    public List<ProjectDisplayModel> Projects { get; set; }
+  }
+}
---
+++ src/service/TagFileGateway/VSS.Productivity3D.TagFileGateway.Common/Models/Executors/TagFileProcessExecutor.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using VSS.MasterData.Models.ResultHandling.Abstractions;
+
+namespace VSS.Productivity3D.TagFileGateway.Common.Models.Executors
+{
+  public class TagFileProcessExecutor : RequestExecutorContainer
+  {
+    protected override Task<ContractExecutionResult> ProcessAsyncEx<T>(T item)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
