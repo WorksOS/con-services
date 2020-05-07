17a13056ca84325ddb8a03b86fcc928fdd41b2fc
Stephen Post <stephen_post@trimble.com>
2020-05-07 17:42:07 +1200

    Remove doubled up class

--- src/service/TagFileGateway/VSS.Productivity3D.TagFileGateway.Common/Models/Sns/SnsPayload.cs
+++ src/service/TagFileGateway/VSS.Productivity3D.TagFileGateway.Common/Models/Sns/SnsPayload.cs
@@ -31,17 +31,4 @@ namespace VSS.Productivity3D.TagFileGateway.Common.Models.Sns
 
     public string SingingCertURL { get; set; }
   }
-
-  public class SnsTagFile
-  {
-    public string OrgId { get; set; }
-
-    public string FileName { get; set; }
-
-    public byte[] Data { get; set; }
-
-    public int FileSize { get; set; }
-
-    public string DownloadUrl { get; set; }
-  }
 }
