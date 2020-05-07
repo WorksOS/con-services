871b73ade011d115adcaf03f586be9ed7825cefd
rwilson <raymond_wilson@trimble.com>
2020-05-07 13:03:22 +1200

    CCSSCON-310: Added treatAsJohnDoe to tagfile submission pathway in web tools

--- src/service/TRex/src/tools/VSS.TRex.Webtools/Controllers/SandboxController.cs
+++ src/service/TRex/src/tools/VSS.TRex.Webtools/Controllers/SandboxController.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using VSS.TRex.Filters;
 
