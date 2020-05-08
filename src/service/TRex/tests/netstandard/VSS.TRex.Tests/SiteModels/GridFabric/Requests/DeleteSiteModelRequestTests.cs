19ff37b7b6c46e4fea13334b16ee2b5184035c53
rwilson <raymond_wilson@trimble.com>
2020-05-08 16:27:31 +1200

    CCSSSCON-311: Add a set of initial failing tests

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/GridFabric/Requests/DeleteSiteModelRequestTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/GridFabric/Requests/DeleteSiteModelRequestTests.cs
@@ -1,8 +1,18 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using VSS.TRex.Alignments;
+using VSS.TRex.CoordinateSystems;
+using VSS.TRex.Designs.Models;
+using VSS.TRex.Designs.Storage;
+using VSS.TRex.Geometry;
+using VSS.TRex.SiteModels;
 using VSS.TRex.SiteModels.GridFabric.ComputeFuncs;
 using VSS.TRex.SiteModels.GridFabric.Requests;
 using VSS.TRex.SiteModels.Interfaces;
+using VSS.TRex.SurveyedSurfaces;
 using VSS.TRex.Tests.TestFixtures;
+using VSS.TRex.Types;
 using Xunit;
 
 namespace VSS.TRex.Tests.SiteModels.GridFabric.Requests
@@ -12,30 +22,25 @@ namespace VSS.TRex.Tests.SiteModels.GridFabric.Requests
   {
     private void AddApplicationGridRouting() => IgniteMock.AddApplicationGridRouting<DeleteSiteModelRequestComputeFunc, DeleteSiteModelRequestArgument, DeleteSiteModelRequestResponse>();
 
-    private void VerifyModelIsEmpty(ISiteModel model)
+    private bool IsModelEmpty(ISiteModel model)
     {
       // Check that there are no elements in the storage proxy for the site model
       foreach (var cache in IgniteMock.MockedCacheDictionaries)
       {
-        cache.Keys.Count.Should().Be(0, $"Cache {cache} should have had all elements removed");
+        if (cache.Keys.Count > 0)
+          return false;
       }
+
+      return true;
     }
 
-    [Fact]
-    public void Creation()
+    private void VerifyModelIsEmpty(ISiteModel model)
     {
-      var req = new DeleteSiteModelRequest();
-      req.Should().NotBeNull();
+      IsModelEmpty(model).Should().BeTrue();
     }
 
-    [Fact]
-    public void DeleteEmptyModel()
+    private void DeleteTheModel(ISiteModel model)
     {
-      AddApplicationGridRouting();
-
-      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
-      model.Should().NotBeNull();
-
       var modelId = model.ID;
 
       var request = new DeleteSiteModelRequest();
@@ -45,5 +50,196 @@ namespace VSS.TRex.Tests.SiteModels.GridFabric.Requests
 
       VerifyModelIsEmpty(model);
     }
+
+
+    [Fact]
+    public void Creation()
+    {
+      var req = new DeleteSiteModelRequest();
+      req.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void DeleteEmptyModel_StandardMetaDataPersistence()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.SaveMetadataToPersistentStore(model.PrimaryStorageProxy);
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteEmptyModel_TAGFileIngestPersistence()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModel_WithProofingRuns()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.SiteProofingRuns.Add(new SiteProofingRun("Test Proofing Run", 0, DateTime.UtcNow.AddHours(-1), DateTime.UtcNow, new BoundingWorldExtent3D(0, 0, 1, 1)));
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModel_WithSiteModelMachineDesigns()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.SiteModelMachineDesigns.Add(new SiteModelMachineDesign(-1, "Test Name"));
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModel_WithSiteModelDesigns()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.SiteModelDesigns.Add(new SiteModelDesign("Test name", new BoundingWorldExtent3D(0, 0, 1, 1))); 
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModel_WithSiteDesigns()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.Designs.Add(new Design(Guid.NewGuid(), new DesignDescriptor(Guid.NewGuid(), "", ""), new BoundingWorldExtent3D(0, 0, 1, 1)));
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModel_WithSurveyedSurfaces()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.SurveyedSurfaces.Add(new SurveyedSurface(Guid.NewGuid(), new DesignDescriptor(Guid.NewGuid(), "", ""), DateTime.UtcNow, new BoundingWorldExtent3D(0, 0, 1, 1)));
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModel_WithAlignments()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.Alignments.Add(new Alignment(Guid.NewGuid(), new DesignDescriptor(Guid.NewGuid(), "", ""), new BoundingWorldExtent3D(0, 0, 1, 1)));
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModel_WithCSIB()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      var csibStream = new MemoryStream();
+      csibStream.Write(new byte[] { 70, 71, 72, 73 }, 0, 4);
+      csibStream.Position = 0;
+
+      model.PrimaryStorageProxy.WriteStreamToPersistentStore(model.ID,
+        CoordinateSystemConsts.kCoordinateSystemCSIBStorageKeyName,
+        FileSystemStreamType.CoordinateSystemCSIB,
+        csibStream, null);
+
+      model.CSIB().Should().NotBeEmpty();
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModel_WithExistenceMap()
+    {
+      AddApplicationGridRouting();
+
+      var model = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
+      model.Should().NotBeNull();
+
+      model.ExistenceMap[0, 0] = true;
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
+
+    [Fact]
+    public void DeleteModelWithTagFile()
+    {
+      var tagFiles = new[]
+      {
+        Path.Combine(TestHelper.CommonTestDataPath, "TestTAGFile.tag"),
+      };
+
+      var model = DITAGFileAndSubGridRequestsFixture.BuildModel(tagFiles, out _);
+      model.Should().NotBeNull();
+
+      model.SaveToPersistentStoreForTAGFileIngest(model.PrimaryStorageProxy);
+
+      IsModelEmpty(model).Should().BeFalse();
+
+      DeleteTheModel(model);
+    }
   }
 }
+
