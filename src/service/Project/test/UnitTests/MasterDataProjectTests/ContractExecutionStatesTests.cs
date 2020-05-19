e179d8a8fba11ec45a4989fe910e12fcf4e36b14
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2020-05-19 18:08:16 +1200

    CCSSSCON-276: To fix failing test...

--- src/service/Project/test/UnitTests/MasterDataProjectTests/ContractExecutionStatesTests.cs
+++ src/service/Project/test/UnitTests/MasterDataProjectTests/ContractExecutionStatesTests.cs
@@ -9,7 +9,7 @@ namespace VSS.MasterData.ProjectTests
     public void DynamicAddwithOffsetTest()
     {
       var projectErrorCodesProvider = new ProjectErrorCodesProvider();
-      Assert.Equal(123, projectErrorCodesProvider.DynamicCount);
+      Assert.Equal(124, projectErrorCodesProvider.DynamicCount);
       Assert.Equal("Supplied CoordinateSystem filename is not valid. Exceeds the length limit of 256, is empty, or contains illegal characters.", projectErrorCodesProvider.FirstNameWithOffset(2));
       Assert.Equal("LegacyImportedFileId has not been generated.", projectErrorCodesProvider.FirstNameWithOffset(50));
     }
