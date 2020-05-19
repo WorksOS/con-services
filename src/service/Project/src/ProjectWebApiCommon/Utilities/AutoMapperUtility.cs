2110856cd66fa59c0565671fe42caf0f631b43bc
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2020-05-19 14:55:43 +1200

    CCSSSCON-376 Fix bad merge

--- src/service/Project/src/ProjectWebApiCommon/Utilities/AutoMapperUtility.cs
+++ src/service/Project/src/ProjectWebApiCommon/Utilities/AutoMapperUtility.cs
@@ -135,11 +135,11 @@ namespace VSS.MasterData.Project.WebAPI.Common.Utilities
             .ForMember(dest => dest.Children, opt => opt.Ignore());
 
           cfg.CreateMap<ProjectResponseModel, ProjectData>()
-            .ForMember(dest => dest.ProjectUID, opt => opt.MapFrom(src => src.projectId))
-            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.projectName))
-            .ForMember(dest => dest.CustomerUID, opt => opt.MapFrom(src => src.accountId))
-            .ForMember(dest => dest.ProjectGeofenceWKT, opt => opt.MapFrom(src => RepositoryHelper.ProjectBoundaryToWKT(src.boundary)))
-            .ForMember(dest => dest.IanaTimeZone, opt => opt.MapFrom(src => src.timezone))
+            .ForMember(dest => dest.ProjectUID, opt => opt.MapFrom(src => src.ProjectId))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.ProjectName))
+            .ForMember(dest => dest.CustomerUID, opt => opt.MapFrom(src => src.AccountId))
+            .ForMember(dest => dest.ProjectGeofenceWKT, opt => opt.MapFrom(src => RepositoryHelper.ProjectBoundaryToWKT(src.Boundary)))
+            .ForMember(dest => dest.IanaTimeZone, opt => opt.MapFrom(src => src.Timezone))
             .ForMember(dest => dest.CoordinateSystemFileName, opt => opt.Ignore())
             .ForMember(dest => dest.CoordinateSystemLastActionedUTC, opt => opt.Ignore())
             .ForMember(dest => dest.ProjectTimeZone, opt => opt.Ignore())
