4715061617594ba4c497dd7240ebec417a267e80
Stephen Post <stephen_post@trimble.com>
2020-05-21 09:22:00 +1200

    Add Executor Tests

--- src/service/TagFileGateway/test/UnitTests/VSS.Productivity3D.TagFileGateway.UnitTests/TagFileProcessExecutorTests.cs
+++ src/service/TagFileGateway/test/UnitTests/VSS.Productivity3D.TagFileGateway.UnitTests/TagFileProcessExecutorTests.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using VSS.MasterData.Models.ResultHandling.Abstractions;
+using VSS.Productivity3D.Models.Models;
 using VSS.Productivity3D.TagFileGateway.Common.Executors;
 using Xunit;
 
@@ -17,6 +24,16 @@ namespace VSS.Productivity3D.TagFileGateway.UnitTests
       WebRequest.Reset();
       return RequestExecutorContainer.Build<TagFileProcessExecutor>(LoggerFactory, ConfigStore.Object, DataCache.Object, TagFileForwarder.Object, TransferProxy.Object, WebRequest.Object);
     }
+
+    private static CompactionTagFileRequest MockRequest =
+      new CompactionTagFileRequest
+      {
+        ProjectId = 554,
+        ProjectUid = Guid.NewGuid(),
+        FileName = "Machine Name--whatever--161230235959.tag",
+        Data = new byte[] { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9 },
+        OrgId = string.Empty
+      };
 
 
     [Fact]
@@ -37,7 +54,123 @@ namespace VSS.Productivity3D.TagFileGateway.UnitTests
 
       result.Should().NotBeNull();
       result.Code.Should().NotBe(0);
-
     }
+
+    [Fact]
+    public void ShouldUploadWhenTagFileForwarderThrowsException()
+    {
+      // This simulates a situation when TagFileForwarder cant connect to TRex
+      // We want to upload the tag file to S3, but return an error to the caller
+      var executor = CreateExecutor();
+
+      var key = TagFileProcessExecutor.GetS3Key(MockRequest.FileName);
+      var expectedS3Path = $"{TagFileProcessExecutor.CONNECTION_ERROR_FOLDER}/{key}";
+      var uploadedData = new List<byte>();
+
+      // Setup a failed connection
+      TagFileForwarder
+        .Setup(m => m.SendTagFileDirect(It.IsAny<CompactionTagFileRequest>(),
+          It.IsAny<IDictionary<string, string>>()))
+        .Throws<HttpRequestException>();
+
+      // Handle the upload, and save the data for validation
+      TransferProxy.Setup(m => m.Upload(It.IsAny<Stream>(), It.IsAny<string>()))
+        .Callback<Stream, string>((stream, path) => { uploadedData.AddRange(((MemoryStream)stream).ToArray()); });
+
+      // Run the test
+      var result = executor.ProcessAsync(MockRequest).Result;
+
+      // Validate we tried to upload
+      TagFileForwarder
+        .Verify(m => m.SendTagFileDirect(It.IsAny<CompactionTagFileRequest>(),
+            It.IsAny<IDictionary<string, string>>()),
+          Times.Exactly(1));
+
+      // Validate that the path was correct (we check the data separately)
+      TransferProxy.Verify(m => m.Upload(It.IsAny<MemoryStream>(), It.Is<string>(s => s == expectedS3Path)), Times.Once);
+
+      // Validate the data
+      uploadedData.Should().BeEquivalentTo(MockRequest.Data);
+
+      // Validate we got a non-zero result
+      result.Code.Should().NotBe(0);
+    }
+
+    [Fact]
+    public void ShouldUploadWhenTagFileForwarderFails()
+    {
+      var executor = CreateExecutor();
+
+      var key = TagFileProcessExecutor.GetS3Key(MockRequest.FileName);
+      var expectedS3Path = $"{key}";
+      var uploadedData = new List<byte>();
+      var expectedErrorCode = 55; // Executor should forward on the error code when tag file forwarder returns an error
+      // Setup a failed connection
+      TagFileForwarder
+        .Setup(m => m.SendTagFileDirect(It.IsAny<CompactionTagFileRequest>(),
+          It.IsAny<IDictionary<string, string>>()))
+        .Returns(Task.FromResult(new ContractExecutionResult(expectedErrorCode)));
+
+      // Handle the upload, and save the data for validation
+      TransferProxy.Setup(m => m.Upload(It.IsAny<Stream>(), It.IsAny<string>()))
+        .Callback<Stream, string>((stream, path) => { uploadedData.AddRange(((MemoryStream)stream).ToArray()); });
+
+      // Run the test
+      var result = executor.ProcessAsync(MockRequest).Result;
+
+      // Validate we tried to upload
+      TagFileForwarder
+        .Verify(m => m.SendTagFileDirect(It.IsAny<CompactionTagFileRequest>(),
+            It.IsAny<IDictionary<string, string>>()),
+          Times.Exactly(1));
+
+      // Validate that the path was correct (we check the data separately)
+      TransferProxy.Verify(m => m.Upload(It.IsAny<MemoryStream>(), It.Is<string>(s => s == expectedS3Path)), Times.Once);
+
+      // Validate the data
+      uploadedData.Should().BeEquivalentTo(MockRequest.Data);
+
+      // Validate we got a non-zero result
+      result.Code.Should().Be(expectedErrorCode);
+    }
+
+    [Fact]
+    public void ShouldUploadWhenTagFileForwarderPasses()
+    {
+      var executor = CreateExecutor();
+
+      var key = TagFileProcessExecutor.GetS3Key(MockRequest.FileName);
+      var expectedS3Path = $"{key}";
+      var uploadedData = new List<byte>();
+
+      // Setup a failed connection
+      TagFileForwarder
+        .Setup(m => m.SendTagFileDirect(It.IsAny<CompactionTagFileRequest>(),
+          It.IsAny<IDictionary<string, string>>()))
+        .Returns(Task.FromResult(new ContractExecutionResult(0)));
+
+      // Handle the upload, and save the data for validation
+      TransferProxy.Setup(m => m.Upload(It.IsAny<Stream>(), It.IsAny<string>()))
+        .Callback<Stream, string>((stream, path) => { uploadedData.AddRange(((MemoryStream)stream).ToArray()); });
+
+      // Run the test
+      var result = executor.ProcessAsync(MockRequest).Result;
+
+      // Validate we tried to upload
+      TagFileForwarder
+        .Verify(m => m.SendTagFileDirect(It.IsAny<CompactionTagFileRequest>(),
+            It.IsAny<IDictionary<string, string>>()),
+          Times.Exactly(1));
+
+      // Validate that the path was correct (we check the data separately)
+      TransferProxy.Verify(m => m.Upload(It.IsAny<MemoryStream>(), It.Is<string>(s => s == expectedS3Path)), Times.Once);
+
+      // Validate the data
+      uploadedData.Should().BeEquivalentTo(MockRequest.Data);
+
+      // Validate we got a non-zero result
+      result.Code.Should().Be(0);
+    }
+
   }
 }
