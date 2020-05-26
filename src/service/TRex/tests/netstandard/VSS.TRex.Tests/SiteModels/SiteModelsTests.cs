77d9b549b26493fb3cb71a58d33790be76ab2f9a
rwilson <raymond_wilson@trimble.com>
2020-05-26 20:07:50 +1200

    CCSSSCON-405: Added tests for GetSiteModelRaw()

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/SiteModelsTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/SiteModelsTests.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using FluentAssertions;
-using VSS.TRex.DI;
-using VSS.TRex.SiteModels.Interfaces;
 using VSS.TRex.Tests.TestFixtures;
 using Xunit;
 
@@ -31,6 +27,48 @@ namespace VSS.TRex.Tests.SiteModels
       sm.GetSiteModels().Should().NotBeNull();
       sm.GetSiteModels().Count.Should().Be(1);
       sm.GetSiteModels()[0].Should().BeEquivalentTo(siteModel);
+    }
+
+    [Fact]
+    public void GetSiteModel_WithMarkedForDeletion_Suppression()
+    {
+      var sm = new TRex.SiteModels.SiteModels();
+
+      var siteModelId = Guid.NewGuid();
+      var siteModel = sm.GetSiteModel(siteModelId, true);
+
+      siteModel.MarkForDeletion();
+
+      sm.GetSiteModel(siteModelId, false).Should().BeNull("Site models marked for deletion cannot be returned for standard query operations");
+    }
+
+    [Fact]
+    public void GetSiteModelRaw_WithoutMarkedForDeletion()
+    {
+      var sm = new TRex.SiteModels.SiteModels();
+
+      var siteModelId = Guid.NewGuid();
+      var siteModel = sm.GetSiteModel(siteModelId, true);
+      siteModel.SaveMetadataToPersistentStore(siteModel.PrimaryStorageProxy);
+
+      var queriedSiteModel = sm.GetSiteModelRaw(siteModelId);
+      queriedSiteModel.Should().NotBeNull();
+      queriedSiteModel.ID.Should().Be(siteModelId);
+    }
+
+    [Fact]
+    public void GetSiteModelRaw_WithMarkedForDeletion()
+    {
+      var sm = new TRex.SiteModels.SiteModels();
+
+      var siteModelId = Guid.NewGuid();
+      var siteModel = sm.GetSiteModel(siteModelId, true);
+
+      siteModel.MarkForDeletion();
+
+      var queriedSiteModel = sm.GetSiteModelRaw(siteModelId);
+      queriedSiteModel.Should().NotBeNull();
+      queriedSiteModel.ID.Should().Be(siteModelId);
     }
   }
 }
