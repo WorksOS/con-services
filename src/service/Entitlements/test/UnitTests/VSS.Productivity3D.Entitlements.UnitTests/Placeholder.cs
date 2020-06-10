a315b9e2a3a36ab6d534db97db3a729ad1a3a130
Stephen Post <stephen_post@trimble.com>
2020-06-10 16:21:04 +1200

    Add basic service - no logic for entitlements

---
+++ src/service/Entitlements/test/UnitTests/VSS.Productivity3D.Entitlements.UnitTests/Placeholder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VSS.Productivity3D.Entitlements.UnitTests
+{
+    class Placeholder
+    {
+      [Fact]
+      public void EmptyTest()
+      {
+        // TODO add tests as logic is added
+        Assert.True(true);
+      }
+    }
+}
