e22aea3965f7f7a167325041e349005f02c9f954
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2020-06-18 12:51:49 +1200

    CCSSSCON-497: To fix MockProjectWebApi service exception on start-up...

--- src/service/MockProjectWebApi/Startup.cs
+++ src/service/MockProjectWebApi/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +8,10 @@ using MockProjectWebApi.Services;
 using MockProjectWebApi.Utils;
 using VSS.Common.Abstractions.Configuration;
 using VSS.ConfigurationStore;
+using VSS.MasterData.Proxies;
+using VSS.MasterData.Proxies.Interfaces;
+using VSS.Productivity3D.Entitlements.Abstractions.Interfaces;
+using VSS.Productivity3D.Entitlements.Proxy;
 using VSS.WebApi.Common;
 
 namespace MockProjectWebApi
@@ -57,6 +62,8 @@ namespace MockProjectWebApi
       services.AddSingleton<IFiltersService, FiltersService>();
       services.AddSingleton<IImportedFilesService, ImportedFilesService>();
       services.AddSingleton<IProjectService, ProjectService>();
+      services.AddTransient<IWebRequest, GracefulWebRequest>();
+      services.AddTransient<IEntitlementProxy, EntitlementProxy>(); // required for license validation
     }
 
     protected override void ConfigureAdditionalAppSettings(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory factory)
