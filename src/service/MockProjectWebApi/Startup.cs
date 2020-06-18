73acf6a754210dd6d097250d53b4b027065c46b9
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2020-06-18 14:47:54 +1200

    CCSSSCON-497: Reverted previous changes.

--- src/service/MockProjectWebApi/Startup.cs
+++ src/service/MockProjectWebApi/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,10 +7,6 @@ using MockProjectWebApi.Services;
 using MockProjectWebApi.Utils;
 using VSS.Common.Abstractions.Configuration;
 using VSS.ConfigurationStore;
-using VSS.MasterData.Proxies;
-using VSS.MasterData.Proxies.Interfaces;
-using VSS.Productivity3D.Entitlements.Abstractions.Interfaces;
-using VSS.Productivity3D.Entitlements.Proxy;
 using VSS.WebApi.Common;
 
 namespace MockProjectWebApi
@@ -62,8 +57,8 @@ namespace MockProjectWebApi
       services.AddSingleton<IFiltersService, FiltersService>();
       services.AddSingleton<IImportedFilesService, ImportedFilesService>();
       services.AddSingleton<IProjectService, ProjectService>();
-      services.AddTransient<IWebRequest, GracefulWebRequest>();
-      services.AddTransient<IEntitlementProxy, EntitlementProxy>(); // required for license validation
+      //services.AddTransient<IWebRequest, GracefulWebRequest>();
+      //services.AddTransient<IEntitlementProxy, EntitlementProxy>(); // required for license validation
     }
 
     protected override void ConfigureAdditionalAppSettings(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory factory)
