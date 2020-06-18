1cab05518197f8e8c50d8a118ee6307af68c43ae
rwilson <raymond_wilson@trimble.com>
2020-06-18 16:57:20 +1200

    CCSSSCON-533: Define the shell of the rebuild project request tests

---
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/GridFabric/Requests/RebuildSiteModelRequestTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using VSS.TRex.SiteModels.GridFabric.ComputeFuncs;
+using VSS.TRex.SiteModels.GridFabric.Requests;
+using VSS.TRex.Tests.TestFixtures;
+using Xunit;
+
+namespace VSS.TRex.Tests.SiteModels.GridFabric.Requests
+{
+  [UnitTestCoveredRequest(RequestType = typeof(RebuildSiteModelRequest))]
+  public class RebuildSiteModelRequestTests : IClassFixture<DITAGFileAndSubGridRequestsWithIgniteFixture>
+  {
+    private void AddApplicationGridRouting() => IgniteMock.Mutable.AddApplicationGridRouting<RebuildSiteModelRequestComputeFunc, RebuildSiteModelRequestArgument, RebuildSiteModelRequestResponse>();
+
+    public RebuildSiteModelRequestTests()
+    {
+      // This resets all modified content in the Ignite mocks between tests
+      DITAGFileAndSubGridRequestsWithIgniteFixture.ResetDynamicMockedIgniteContent();
+    }
+
+    [Fact]
+    public void Creation()
+    {
+      var req = new RebuildSiteModelRequest();
+      req.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void NextTest()
+    {
+      AddApplicationGridRouting();
+
+      /// yada yada
+    }
+  }
+}
+
