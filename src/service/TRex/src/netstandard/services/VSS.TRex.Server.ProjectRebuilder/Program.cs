17fee05c26b2774c57665f82ca92854124c270e6
rwilson <raymond_wilson@trimble.com>
2020-06-19 13:04:27 +1200

    CCSSSCON-533: Add shell of VSS.TRex.Server.ProjectRebuilder node

---
+++ src/service/TRex/src/netstandard/services/VSS.TRex.Server.ProjectRebuilder/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using VSS.ConfigurationStore;
+using VSS.TRex.Common;
+using VSS.TRex.DI;
+using Microsoft.Extensions.Logging;
+using VSS.Common.Abstractions.Configuration;
+using VSS.TRex.Common.Interfaces;
+using System.Threading;
+using VSS.TRex.Storage.Models;
+using VSS.TRex.GridFabric.Grids;
+using VSS.TRex.Common.HeartbeatLoggers;
+using VSS.TRex.GridFabric.Servers.Client;
+using VSS.TRex.GridFabric.Models.Servers;
+
+namespace VSS.TRex.Server.ProjectRebuilder
+{
+  class Program
+  {
+    private static void DependencyInjection()
+    {
+      DIBuilder.New()
+        .AddLogging()
+        .Add(x => x.AddSingleton<IConfigurationStore, GenericConfiguration>())
+
+//        .Build()
+//        .Add(x => x.AddSingleton<IConvertCoordinates>(new ConvertCoordinates()))
+//        .Add(VSS.TRex.IO.DIUtilities.AddPoolCachesToDI)
+//        .Add(VSS.TRex.Cells.DIUtilities.AddPoolCachesToDI)
+        .Add(TRexGridFactory.AddGridFactoriesToDI)
+//        .Add(VSS.TRex.Storage.Utilities.DIUtilities.AddProxyCacheFactoriesToDI)
+//        .Build()
+//        .Add(x => x.AddTransient<ISurveyedSurfaces>(factory => new SurveyedSurfaces.SurveyedSurfaces()))
+//        .Add(x => x.AddSingleton<ISurveyedSurfaceFactory>(new SurveyedSurfaceFactory()))
+//        .Build()
+//        .Add(x => x.AddTransient<IFilterSet>(factory => new FilterSet()))
+//        .Add(x => x.AddSingleton<ISiteModels>(new SiteModels.SiteModels()))
+//        .Add(x => x.AddSingleton<ISiteModelFactory>(new SiteModelFactory()))
+//        .Add(ExistenceMaps.ExistenceMaps.AddExistenceMapFactoriesToDI)
+//        .Add(x => x.AddSingleton<IPipelineProcessorFactory>(new PipelineProcessorFactory()))
+//        .Add(x => x.AddSingleton<Func<PipelineProcessorPipelineStyle, ISubGridPipelineBase>>(provider => SubGridPipelineFactoryMethod))
+//        .Add(x => x.AddTransient<IRequestAnalyser>(factory => new RequestAnalyser()))
+//        .Add(x => x.AddSingleton<Func<PipelineProcessorTaskStyle, ITRexTask>>(provider => SubGridTaskFactoryMethod))
+//        .Add(x => x.AddSingleton<IClientLeafSubGridFactory>(ClientLeafSubGridFactoryFactory.CreateClientSubGridFactory()))
+        .Add(x => x.AddSingleton<ITRexHeartBeatLogger>(new TRexHeartBeatLogger()))
+//
+//        // Register the listener for site model attribute change notifications
+//        .Add(x => x.AddSingleton<ISiteModelAttributesChangedEventListener>(new SiteModelAttributesChangedEventListener(TRexGrids.ImmutableGridName())))
+//
+//        // Register the factory for the CellProfileAnalyzer for detailed call pass/lift cell profiles
+//        .Add(x => x.AddTransient<Func<ISiteModel, ISubGridTreeBitMask, IFilterSet, ICellLiftBuilder, IOverrideParameters, ILiftParameters, ICellProfileAnalyzer<ProfileCell>>>(
+//          factory => (siteModel, pDExistenceMap, filterSet, cellLiftBuilder, overrides, liftParams)
+//            => new CellProfileAnalyzer(siteModel, pDExistenceMap, filterSet, cellLiftBuilder, overrides, liftParams)))
+//
+//        // Register the factory for the CellProfileAnalyzer for summary volume cell profiles
+//        .Add(x => x.AddTransient<Func<ISiteModel, ISubGridTreeBitMask, IFilterSet, ICellLiftBuilder, ICellProfileAnalyzer<SummaryVolumeProfileCell>>>(
+//          factory => (siteModel, pDExistenceMap, filterSet, cellLiftBuilder) => null))
+//
+//        .Add(x => x.AddSingleton<IPipelineListenerMapper>(new PipelineListenerMapper()))
+
+        .Complete();
+    }
+
+    // This static array ensures that all required assemblies are included into the artifacts by the linker
+    private static void EnsureAssemblyDependenciesAreLoaded()
+    {
+      // This static array ensures that all required assemblies are included into the artifacts by the linker
+      Type[] AssemblyDependencies =
+      {
+        /* Copied from application service
+         * 
+        typeof(VSS.TRex.Analytics.MDPStatistics.MDPStatisticsAggregator),
+        typeof(VSS.TRex.Geometry.BoundingIntegerExtent2D),
+        typeof(VSS.TRex.Exports.Patches.PatchResult),
+        typeof(VSS.TRex.GridFabric.BaseIgniteClass),
+        typeof(VSS.TRex.Common.SubGridsPipelinedResponseBase),
+        typeof(VSS.TRex.Logging.Logger),
+        typeof(VSS.TRex.DI.DIContext),
+        typeof(VSS.TRex.Storage.StorageProxy),
+        typeof(VSS.TRex.SiteModels.SiteModel),
+        typeof(VSS.TRex.Cells.CellEvents),
+        typeof(VSS.TRex.Compression.AttributeValueModifiers),
+        typeof(VSS.TRex.CoordinateSystems.Models.LLH),
+        typeof(VSS.TRex.Designs.DesignBase),
+        typeof(VSS.TRex.Designs.TTM.HashOrdinate),
+        typeof(VSS.TRex.Designs.TTM.Optimised.HeaderConsts),
+        typeof(VSS.TRex.Events.CellPassFastEventLookerUpper),
+        typeof(VSS.TRex.ExistenceMaps.ExistenceMaps),
+        typeof(VSS.TRex.Filters.CellPassAttributeFilter),
+        typeof(VSS.TRex.GridFabric.BaseIgniteClass),
+        typeof(VSS.TRex.Machines.Machine),
+        typeof(VSS.TRex.Pipelines.PipelineProcessor<SubGridsRequestArgument>),
+        typeof(VSS.TRex.Profiling.CellLiftBuilder),
+        typeof(VSS.TRex.SubGrids.CutFillUtilities),
+        typeof(VSS.TRex.SubGridTrees.Client.ClientCMVLeafSubGrid),
+        typeof(VSS.TRex.SubGridTrees.Core.Utilities.SubGridUtilities),
+        typeof(VSS.TRex.SubGridTrees.Server.MutabilityConverter),
+        typeof(VSS.TRex.SurveyedSurfaces.SurveyedSurface),
+        typeof(VSS.TRex.Volumes.CutFillVolume),
+        typeof(VSS.TRex.CellDatum.GridFabric.Responses.CellDatumResponse_ApplicationService),
+        typeof(VSS.TRex.SiteModelChangeMaps.GridFabric.Services.SiteModelChangeProcessorService)
+        */
+      };
+
+      foreach (var asmType in AssemblyDependencies)
+      {
+        if (asmType.FullName == "DummyTypeName")
+          Console.WriteLine($"Assembly for type {asmType} has not been loaded.");
+      }
+    }
+
+    private static void DoServiceInitialisation()
+    {
+      // Register the heartbeat loggers
+      DIContext.Obtain<ITRexHeartBeatLogger>().AddContext(new MemoryHeartBeatLogger());
+      DIContext.Obtain<ITRexHeartBeatLogger>().AddContext(new IgniteNodeMetricsHeartBeatLogger(DIContext.Obtain<ITRexGridFactory>().Grid(StorageMutability.Mutable)));
+    }
+
+    static async Task<int> Main()
+    {
+      try
+      {
+        EnsureAssemblyDependenciesAreLoaded();
+        DependencyInjection();
+
+        var Log = Logging.Logger.CreateLogger<Program>();
+
+        Log.LogInformation("Creating service");
+        Log.LogDebug("Creating service");
+
+        var server = new ApplicationServiceServer(new[] {ServerRoles.PROJECT_REBUILDER_ROLE });
+
+        var cancelTokenSource = new CancellationTokenSource();
+        AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+        {
+          Console.WriteLine("Exiting");
+          DIContext.Obtain<ITRexGridFactory>().StopGrids();
+          cancelTokenSource.Cancel();
+        };
+
+        DoServiceInitialisation();
+
+        await Task.Delay(-1, cancelTokenSource.Token);
+        return 0;
+      }
+      catch (TaskCanceledException)
+      {
+        // Don't care as this is thrown by Task.Delay()
+        Console.WriteLine("Process exit via TaskCanceledException (SIGTERM)");
+        return 0;
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"Unhandled exception: {e}");
+        Console.WriteLine($"Stack trace: {e.StackTrace}");
+        return -1;
+      }
+    }
+  }
+}
