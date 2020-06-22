d980816efddc81ec3df9501c6eab6ac819b32bb1
rwilson <raymond_wilson@trimble.com>
2020-06-22 23:40:15 +1200

    CCSSSCON-533: Initia implementation of TagFileBufferQueue in rebuilder unit tests

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/Executors/ProjectRebuilderTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/Executors/ProjectRebuilderTests.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using FluentAssertions;
 using VSS.AWS.TransferProxy;
 using VSS.TRex.Common;
+using VSS.TRex.Common.Extensions;
 using VSS.TRex.DI;
 using VSS.TRex.GridFabric;
 using VSS.TRex.GridFabric.Interfaces;
@@ -11,10 +14,14 @@ using VSS.TRex.SiteModels.GridFabric.ComputeFuncs;
 using VSS.TRex.SiteModels.GridFabric.Requests;
 using VSS.TRex.SiteModels.Interfaces;
 using VSS.TRex.SiteModels.Interfaces.Executors;
+using VSS.TRex.Storage.Caches;
 using VSS.TRex.Storage.Interfaces;
+using VSS.TRex.Storage.Models;
+using VSS.TRex.TAGFiles.Classes.Queues;
 using VSS.TRex.TAGFiles.GridFabric.Arguments;
 using VSS.TRex.TAGFiles.GridFabric.ComputeFuncs;
 using VSS.TRex.TAGFiles.GridFabric.Responses;
+using VSS.TRex.TAGFiles.Models;
 using VSS.TRex.Tests.TestFixtures;
 using Xunit;
 
@@ -37,6 +44,7 @@ namespace VSS.TRex.Tests.SiteModels.GridFabric.Executors
     {
       IgniteMock.Mutable.AddApplicationGridRouting<DeleteSiteModelRequestComputeFunc, DeleteSiteModelRequestArgument, DeleteSiteModelRequestResponse>();
       IgniteMock.Mutable.AddApplicationGridRouting<SubmitTAGFileComputeFunc, SubmitTAGFileRequestArgument, SubmitTAGFileResponse>();
+      IgniteMock.Mutable.AddApplicationGridRouting<ProcessTAGFileComputeFunc, ProcessTAGFileRequestArgument, ProcessTAGFileResponse>();
     }
 
     [Fact]
@@ -68,7 +76,8 @@ namespace VSS.TRex.Tests.SiteModels.GridFabric.Executors
       result.DeletionResult.Should().Be(DeleteSiteModelResult.OK);
     }
 
-    [Fact] async void ExecuteAsync_SingleTAGFile()
+    [Fact]
+    public async void ExecuteAsync_SingleTAGFile()
     {
       AddApplicationGridRouting();
 
@@ -82,7 +91,27 @@ namespace VSS.TRex.Tests.SiteModels.GridFabric.Executors
 
       var rebuilder = CreateBuilder(siteModel.ID, false, TransferProxyType.TAGFiles);
 
-      var result = await rebuilder.ExecuteAsync();
+      var rebuilderTask = rebuilder.ExecuteAsync();
+      //var result = await rebuilder.ExecuteAsync();
+
+      // Wait until the rebuilder is in the monitoring state and then inject the contents of the tag file buffer queue cache into the handler
+      while (rebuilder.Metadata.Phase != RebuildSiteModelPhase.Monitoring)
+      {
+        await Task.Delay(1000);
+      }
+
+      // While the rebuilder is waiting in the monitoring state, inject the contents of the TAGFileBuffer queue into the TAG file processor
+      var mutableIgniteMock = DIContext.Obtain<Func<StorageMutability, IgniteMock>>()(StorageMutability.Mutable);
+      var mockQueueCacheDictionary = mutableIgniteMock.MockedCacheDictionaries[TRexCaches.TAGFileBufferQueueCacheName()] as Dictionary<ITAGFileBufferQueueKey, TAGFileBufferQueueItem>;
+
+      var handler = new TAGFileBufferQueueItemHandler();
+      handler.Should().NotBeNull();
+
+      // Inject the keys for the handler to use to extract the TAG file content to be processed.
+      mockQueueCacheDictionary.ForEach(kv => handler.Add(kv.Key));
+
+      // Now wait for the rebuilder task to complete
+      var result = await rebuilderTask;
 
       result.Should().NotBeNull();
       result.DeletionResult.Should().Be(DeleteSiteModelResult.OK);
