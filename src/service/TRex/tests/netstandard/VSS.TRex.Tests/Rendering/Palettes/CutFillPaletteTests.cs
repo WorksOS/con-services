228cc56b354f7357faf0e06b1219dfcae99de72b
Alan Rose <alan_rose@trimble.com>
2020-06-24 01:59:14 +1200

    536 More fixes to unit tests

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/Rendering/Palettes/CutFillPaletteTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/Rendering/Palettes/CutFillPaletteTests.cs
@@ -17,24 +17,36 @@ namespace VSS.TRex.Tests.Rendering.Palettes
 
       palette.Should().NotBeNull();
 
+      palette.PaletteTransitions[0].Value.Should().Be(10000.0);
+      palette.PaletteTransitions[0].Color.Should().Be(Color.DarkBlue);
+
+      palette.PaletteTransitions[1].Value.Should().Be(2.0);
+      palette.PaletteTransitions[1].Color.Should().Be(Color.DarkBlue);
+
+      palette.PaletteTransitions[2].Value.Should().Be(1.0);
+      palette.PaletteTransitions[2].Color.Should().Be(Color.SkyBlue);
+
+      palette.PaletteTransitions[3].Value.Should().Be(0.1);
+      palette.PaletteTransitions[3].Color.Should().Be(Color.Blue);
+
+      palette.PaletteTransitions[4].Value.Should().Be(0.0);
+      palette.PaletteTransitions[4].Color.Should().Be(Color.Green);
+
+      palette.PaletteTransitions[5].Value.Should().Be(-0.1);
+      palette.PaletteTransitions[5].Color.Should().Be(Color.Green);
+
+      palette.PaletteTransitions[6].Value.Should().Be(-1.0);
+      palette.PaletteTransitions[6].Color.Should().Be(Color.Yellow);
+
+      palette.PaletteTransitions[7].Value.Should().Be(-2.0);
+      palette.PaletteTransitions[7].Color.Should().Be(Color.Red);
+
+      palette.PaletteTransitions[8].Value.Should().Be(-10000.0);
+      palette.PaletteTransitions[8].Color.Should().Be(Color.Red);
+
       palette.PaletteTransitions.Should().NotBeNull();
-      palette.PaletteTransitions.Length.Should().Be(8);
-      palette.PaletteTransitions[0].Value.Should().Be(-10000.0);
-      palette.PaletteTransitions[0].Color.Should().Be(Color.Red);
-      palette.PaletteTransitions[1].Value.Should().Be(-2.0);
-      palette.PaletteTransitions[1].Color.Should().Be(Color.Red);
-      palette.PaletteTransitions[2].Value.Should().Be(-1.0);
-      palette.PaletteTransitions[2].Color.Should().Be(Color.Yellow);
-      palette.PaletteTransitions[3].Value.Should().Be(-0.1);
-      palette.PaletteTransitions[3].Color.Should().Be(Color.Green);
-      palette.PaletteTransitions[4].Value.Should().Be(0.1);
-      palette.PaletteTransitions[4].Color.Should().Be(Color.Blue);
-      palette.PaletteTransitions[5].Value.Should().Be(1.0);
-      palette.PaletteTransitions[5].Color.Should().Be(Color.SkyBlue);
-      palette.PaletteTransitions[6].Value.Should().Be(2.0);
-      palette.PaletteTransitions[6].Color.Should().Be(Color.DarkBlue);
-      palette.PaletteTransitions[7].Value.Should().Be(10000.0);
-      palette.PaletteTransitions[7].Color.Should().Be(Color.DarkBlue);
+      palette.PaletteTransitions.Length.Should().Be(9);
+      
     }
 
     [Fact]
@@ -44,12 +56,12 @@ namespace VSS.TRex.Tests.Rendering.Palettes
 
       palette.Should().NotBeNull();
       
-      var colors = new[] { Color.Red, Color.Red, Color.Yellow, Color.Blue, Color.SkyBlue, Color.DarkBlue, Color.DarkBlue };
+      var colors = new[] { Color.Red, Color.Yellow, Color.Green, Color.SkyBlue, Color.DarkBlue, Color.DarkBlue };
 
       for (var i = 0; i < colors.Length; i++)
-        palette.ChooseColour(CUT_FILL_VALUE + i * VALUE_INCREMENT).Should().Be(colors[i]);
+        palette.ChooseCutFillColour((float)(CUT_FILL_VALUE + i * VALUE_INCREMENT)).Should().Be(colors[i]);
 
-      palette.ChooseColour(0).Should().Be(Color.Green);
+      palette.ChooseCutFillColour(0).Should().Be(Color.Green);
     }
 
   }
