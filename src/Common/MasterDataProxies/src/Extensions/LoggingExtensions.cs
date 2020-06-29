242d393ab61577e2b0b02f247606ed809e9879ca
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2020-06-30 11:42:35 +1200

    CCSSSCON-569 Log headers in size order so JWT last and only one to get cut off.

--- src/Common/MasterDataProxies/src/Extensions/LoggingExtensions.cs
+++ src/Common/MasterDataProxies/src/Extensions/LoggingExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 
@@ -12,6 +13,22 @@ namespace VSS.MasterData.Proxies
 
     public static string LogHeaders(this IHeaderDictionary headers) => headers == null ? null : LogHeaders(headers, int.MaxValue);
 
-    public static string LogHeaders(this IHeaderDictionary headers, int logMaxChar, Formatting formatting = Formatting.None) => headers == null ? null : JsonConvert.SerializeObject(headers, formatting).Truncate(logMaxChar);
+    public static string LogHeaders(this IHeaderDictionary headers, int logMaxChar, Formatting formatting = Formatting.None) => LogHeadersInSizeOrder(headers, logMaxChar, formatting);
+
+    private static string LogHeadersInSizeOrder(IDictionary<string, string> headers, int logMaxChar, Formatting formatting)
+    {
+      if (headers == null)
+        return null;
+      var orderedHeaders = headers.OrderBy(h => h.Value.Length);
+      return JsonConvert.SerializeObject(orderedHeaders, formatting).Truncate(logMaxChar);
+    }
+
+    private static string LogHeadersInSizeOrder(IHeaderDictionary headers, int logMaxChar, Formatting formatting)
+    {
+      if (headers == null)
+        return null;
+      var orderedHeaders = headers.OrderBy(h => h.Value[0].Length);
+      return JsonConvert.SerializeObject(orderedHeaders, formatting).Truncate(logMaxChar);
+    }
   }
 }
