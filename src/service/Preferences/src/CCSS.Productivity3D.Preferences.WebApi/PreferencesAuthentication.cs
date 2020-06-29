126641cde9444ad7f119b06fc903bd641b29021a
Aaron Morgan <aaron_morgan@trimble.com>
2020-06-30 11:23:28 +1200

    CCSSSCON-598: Fix Preferences service build error.

--- src/service/Preferences/src/CCSS.Productivity3D.Preferences.WebApi/PreferencesAuthentication.cs
+++ src/service/Preferences/src/CCSS.Productivity3D.Preferences.WebApi/PreferencesAuthentication.cs
@@ -3,15 +3,15 @@ using Microsoft.Extensions.Logging;
 using VSS.Common.Abstractions.Clients.CWS.Interfaces;
 using VSS.Common.Abstractions.Configuration;
 using VSS.MasterData.Models.Handlers;
+using VSS.Productivity3D.Entitlements.Abstractions.Interfaces;
 using VSS.WebApi.Common;
 
 namespace VSS.Productivity3D.Filter.WebApi
 {
   public class PreferencesAuthentication : TIDAuthentication
   {
-    public PreferencesAuthentication(RequestDelegate next, ICwsAccountClient accountClient, IConfigurationStore store, ILoggerFactory logger, IServiceExceptionHandler serviceExceptionHandler) : base(next, accountClient, store, logger, serviceExceptionHandler)
-    {
-    }
+    public PreferencesAuthentication(RequestDelegate next, ICwsAccountClient accountClient, IConfigurationStore store, ILoggerFactory logger, IEntitlementProxy entitlementProxy, IServiceExceptionHandler serviceExceptionHandler) : base(next, accountClient, store, logger, entitlementProxy, serviceExceptionHandler)
+    { }
 
     public override bool RequireCustomerUid(HttpContext context)
     {
