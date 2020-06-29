5dc48d6725bb27328ddc73fbe56b7f9b07868f01
rwilson <raymond_wilson@trimble.com>
2020-06-29 14:12:03 +1200

    CCSSSCON-473: Code tidying, plus fix 3 instances of && vs & confusion in if statements

--- src/service/TRex/src/netstandard/VSS.TRex.Caching/SpatialCacheFingerprint.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.Caching/SpatialCacheFingerprint.cs
@@ -8,23 +8,25 @@ namespace VSS.TRex.Caching
   {
     /// <summary>
     /// Constructs a string representing the fingerprint of a request required spatial data. The fingerprint is designed to identify a context within
-    /// which cached general subgrid results may be stored and accessed during query processing.
+    /// which cached general sub grid results may be stored and accessed during query processing.
     /// </summary>
-    /// <param name="projectUID">The Guid identifying the project within the fingerprint</param>
+    /// <param name="projectUid">The Guid identifying the project within the fingerprint</param>
     /// <param name="gridDataType">The type of data being requested in the query</param>
     /// <param name="filter">The set of filters that govern the query</param>
     /// <param name="includedSurveyedSurfaces">The list of surveyed surfaces included in the query, expressed as a an array of GUIDs</param>
-    /// <returns></returns>
-    public static string ConstructFingerprint(Guid projectUID, GridDataType gridDataType, ICombinedFilter filter, Guid[] includedSurveyedSurfaces)
+    /// <returns>A string representing the uniqueness of this spatial cache element</returns>
+    public static string ConstructFingerprint(Guid projectUid, GridDataType gridDataType, ICombinedFilter filter, Guid[] includedSurveyedSurfaces)
     {
-      string fingerprint = $"{projectUID}-{gridDataType}";
+      var fingerprint = $"{projectUid}-{gridDataType}";
 
       if (filter != null)
         fingerprint = $"{fingerprint}-{filter.AttributeFilter.SpatialCacheFingerprint()}";
 
-      if (includedSurveyedSurfaces.Length > 0)
-        foreach (var guid in includedSurveyedSurfaces)
-          fingerprint = $"{fingerprint}-{guid}";
+      if (includedSurveyedSurfaces.Length == 0)
+        return fingerprint;
+
+      foreach (var guid in includedSurveyedSurfaces)
+        fingerprint = $"{fingerprint}-{guid}";
 
       return fingerprint;
     }
