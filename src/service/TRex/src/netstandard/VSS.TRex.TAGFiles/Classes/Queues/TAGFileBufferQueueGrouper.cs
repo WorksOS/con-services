298248e41a17fe8cd89509146ad9df95d71f99cc
rwilson <raymond_wilson@trimble.com>
2020-07-01 07:49:31 +1200

    CCSSSCON-473: Fix unit test again

--- src/service/TRex/src/netstandard/VSS.TRex.TAGFiles/Classes/Queues/TAGFileBufferQueueGrouper.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.TAGFiles/Classes/Queues/TAGFileBufferQueueGrouper.cs
@@ -23,7 +23,7 @@ namespace VSS.TRex.TAGFiles.Classes.Queues
         /// The maximum number of TAG files the grouper will permit in a bucket of TAG file before being committed to the 
         /// full buckets list.
         /// </summary>
-        private static readonly int _maxNumberOfTagFilesPerBucket = 10; //DIContext.Obtain<IConfigurationStore>().GetValueInt("MAX_GROUPED_TAG_FILES_TO_PROCESS_PER_PROCESSING_EPOCH", Consts.MAX_GROUPED_TAG_FILES_TO_PROCESS_PER_PROCESSING_EPOCH);
+        private static readonly int _maxNumberOfTagFilesPerBucket = DIContext.Obtain<IConfigurationStore>().GetValueInt("MAX_GROUPED_TAG_FILES_TO_PROCESS_PER_PROCESSING_EPOCH", Consts.MAX_GROUPED_TAG_FILES_TO_PROCESS_PER_PROCESSING_EPOCH);
 
         /// <summary>
         /// GroupMap is a dictionary (keyed on project UID) of
