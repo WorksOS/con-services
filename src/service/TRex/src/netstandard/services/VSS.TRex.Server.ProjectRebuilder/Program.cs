def3a2fc65fd27270c1ced19c4d4540d8d3568a5
rwilson <raymond_wilson@trimble.com>
2020-07-02 09:09:45 +1200

    CCSSSCON-533: Tidy and extend inclusion of types into project rebuilder

--- src/service/TRex/src/netstandard/services/VSS.TRex.Server.ProjectRebuilder/Program.cs
+++ src/service/TRex/src/netstandard/services/VSS.TRex.Server.ProjectRebuilder/Program.cs
@@ -54,46 +54,18 @@ namespace VSS.TRex.Server.ProjectRebuilder
       DIBuilder.New()
         .AddLogging()
         .Add(x => x.AddSingleton<IConfigurationStore, GenericConfiguration>())
-
         .Add(x => x.AddSingleton<Func<RebuildSiteModelCacheType, IStorageProxyCacheCommit>>((cacheType) => CacheFactory(cacheType)))
 
-        //        .Build()
-        //        .Add(x => x.AddSingleton<IConvertCoordinates>(new ConvertCoordinates()))
-        //        .Add(VSS.TRex.IO.DIUtilities.AddPoolCachesToDI)
-        //        .Add(VSS.TRex.Cells.DIUtilities.AddPoolCachesToDI)
+        .Build()
+        .Add(VSS.TRex.IO.DIUtilities.AddPoolCachesToDI)
         .Add(TRexGridFactory.AddGridFactoriesToDI)
-        //        .Add(VSS.TRex.Storage.Utilities.DIUtilities.AddProxyCacheFactoriesToDI)
-        //        .Build()
-        //        .Add(x => x.AddTransient<ISurveyedSurfaces>(factory => new SurveyedSurfaces.SurveyedSurfaces()))
-        //        .Add(x => x.AddSingleton<ISurveyedSurfaceFactory>(new SurveyedSurfaceFactory()))
-        //        .Build()
-        //        .Add(x => x.AddTransient<IFilterSet>(factory => new FilterSet()))
-        //        .Add(x => x.AddSingleton<ISiteModels>(new SiteModels.SiteModels()))
-        //        .Add(x => x.AddSingleton<ISiteModelFactory>(new SiteModelFactory()))
-        //        .Add(ExistenceMaps.ExistenceMaps.AddExistenceMapFactoriesToDI)
-        //        .Add(x => x.AddSingleton<IPipelineProcessorFactory>(new PipelineProcessorFactory()))
-        //        .Add(x => x.AddSingleton<Func<PipelineProcessorPipelineStyle, ISubGridPipelineBase>>(provider => SubGridPipelineFactoryMethod))
-        //        .Add(x => x.AddTransient<IRequestAnalyser>(factory => new RequestAnalyser()))
-        //        .Add(x => x.AddSingleton<Func<PipelineProcessorTaskStyle, ITRexTask>>(provider => SubGridTaskFactoryMethod))
-        //        .Add(x => x.AddSingleton<IClientLeafSubGridFactory>(ClientLeafSubGridFactoryFactory.CreateClientSubGridFactory()))
+        .Add(VSS.TRex.Storage.Utilities.DIUtilities.AddProxyCacheFactoriesToDI)
+        .Build()
+        .Add(x => x.AddSingleton<ISiteModels>(new SiteModels.SiteModels()))
+        .Add(x => x.AddSingleton<ISiteModelFactory>(new SiteModelFactory()))
+
         .Add(x => x.AddSingleton<ITRexHeartBeatLogger, TRexHeartBeatLogger>())
-        //
-        //        // Register the listener for site model attribute change notifications
-        //        .Add(x => x.AddSingleton<ISiteModelAttributesChangedEventListener>(new SiteModelAttributesChangedEventListener(TRexGrids.ImmutableGridName())))
-        //
-        //        // Register the factory for the CellProfileAnalyzer for detailed call pass/lift cell profiles
-        //        .Add(x => x.AddTransient<Func<ISiteModel, ISubGridTreeBitMask, IFilterSet, ICellLiftBuilder, IOverrideParameters, ILiftParameters, ICellProfileAnalyzer<ProfileCell>>>(
-        //          factory => (siteModel, pDExistenceMap, filterSet, cellLiftBuilder, overrides, liftParams)
-        //            => new CellProfileAnalyzer(siteModel, pDExistenceMap, filterSet, cellLiftBuilder, overrides, liftParams)))
-        //
-        //        // Register the factory for the CellProfileAnalyzer for summary volume cell profiles
-        //        .Add(x => x.AddTransient<Func<ISiteModel, ISubGridTreeBitMask, IFilterSet, ICellLiftBuilder, ICellProfileAnalyzer<SummaryVolumeProfileCell>>>(
-        //          factory => (siteModel, pDExistenceMap, filterSet, cellLiftBuilder) => null))
-        //
-        //        .Add(x => x.AddSingleton<IPipelineListenerMapper>(new PipelineListenerMapper()))
-
         .Add(x => x.AddSingleton<ITransferProxyFactory, TransferProxyFactory>())
-
         .Add(x => x.AddSingleton<Func<Guid, bool, TransferProxyType, ISiteModelRebuilder>>(factory => (projectUid, archiveTAGFiles, transferProxyType) => new SiteModelRebuilder(projectUid, archiveTAGFiles, transferProxyType)))
         .Add(x => x.AddSingleton<ISiteModelRebuilderManager, SiteModelRebuilderManager>())
         .Add(x => x.AddSingleton<Func<TransferProxyType, IS3FileTransfer>>(factory => proxyType => new S3FileTransfer(proxyType)))
@@ -108,40 +80,12 @@ namespace VSS.TRex.Server.ProjectRebuilder
       // This static array ensures that all required assemblies are included into the artifacts by the linker
       Type[] AssemblyDependencies =
       {
-        /* Copied from application service
-         * 
-        typeof(VSS.TRex.Analytics.MDPStatistics.MDPStatisticsAggregator),
-        typeof(VSS.TRex.Geometry.BoundingIntegerExtent2D),
-        typeof(VSS.TRex.Exports.Patches.PatchResult),
-        typeof(VSS.TRex.GridFabric.BaseIgniteClass),
-        typeof(VSS.TRex.Common.SubGridsPipelinedResponseBase),
-        */
-        typeof(VSS.TRex.Logging.Logger),
-        /*
         typeof(VSS.TRex.DI.DIContext),
-        typeof(VSS.TRex.Storage.StorageProxy),
-        typeof(VSS.TRex.SiteModels.SiteModel),
-        typeof(VSS.TRex.Cells.CellEvents),
-        typeof(VSS.TRex.Compression.AttributeValueModifiers),
-        typeof(VSS.TRex.CoordinateSystems.Models.LLH),
-        typeof(VSS.TRex.Designs.DesignBase),
-        typeof(VSS.TRex.Designs.TTM.HashOrdinate),
-        typeof(VSS.TRex.Designs.TTM.Optimised.HeaderConsts),
-        typeof(VSS.TRex.Events.CellPassFastEventLookerUpper),
-        typeof(VSS.TRex.ExistenceMaps.ExistenceMaps),
-        typeof(VSS.TRex.Filters.CellPassAttributeFilter),
+        typeof(VSS.TRex.Logging.Logger),
         typeof(VSS.TRex.GridFabric.BaseIgniteClass),
-        typeof(VSS.TRex.Machines.Machine),
-        typeof(VSS.TRex.Pipelines.PipelineProcessor<SubGridsRequestArgument>),
-        typeof(VSS.TRex.Profiling.CellLiftBuilder),
-        typeof(VSS.TRex.SubGrids.CutFillUtilities),
-        typeof(VSS.TRex.SubGridTrees.Client.ClientCMVLeafSubGrid),
-        typeof(VSS.TRex.SubGridTrees.Core.Utilities.SubGridUtilities),
-        typeof(VSS.TRex.SubGridTrees.Server.MutabilityConverter),
-        typeof(VSS.TRex.SurveyedSurfaces.SurveyedSurface),
-        typeof(VSS.TRex.Volumes.CutFillVolume),
-        typeof(VSS.TRex.CellDatum.GridFabric.Responses.CellDatumResponse_ApplicationService),
-        */
+        typeof(VSS.TRex.SiteModels.SiteModel),
+        typeof(VSS.TRex.Storage.StorageProxy),
+        typeof(VSS.TRex.TAGFiles.GridFabric.NodeFilters.TAGProcessorRoleBasedNodeFilter),
         typeof(VSS.TRex.SiteModelChangeMaps.GridFabric.NodeFilters.SiteModelChangeProcessorRoleBasedNodeFilter)
       };
 
