f4c61d6fb8c56f2921b26052fea3e074e4f9cbbb
rwilson <raymond_wilson@trimble.com>
2020-07-04 22:53:30 +1200

    CCSSSCON-666: Added Add TTM design test

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/Designs/GridFabric/AddTTMDesignRequestTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/Designs/GridFabric/AddTTMDesignRequestTests.cs
@@ -1,8 +1,12 @@
 ﻿using FluentAssertions;
+using VSS.Productivity3D.Models.ResultHandling.Profiling;
 using VSS.TRex.Designs.GridFabric.Arguments;
 using VSS.TRex.Designs.GridFabric.ComputeFuncs;
 using VSS.TRex.Designs.GridFabric.Requests;
 using VSS.TRex.Designs.GridFabric.Responses;
+using VSS.TRex.Designs.Models;
+using VSS.TRex.DI;
+using VSS.TRex.SiteModels.Interfaces;
 using VSS.TRex.Tests.TestFixtures;
 using Xunit;
 
@@ -11,7 +15,7 @@ namespace VSS.TRex.Tests.Designs.GridFabric
   [UnitTestCoveredRequest(RequestType = typeof(AddTTMDesignRequest))]
   public class AddTTMDesignRequestTests : IClassFixture<DITAGFileAndSubGridRequestsWithIgniteFixture>
   {
-    private void AddApplicationRouting() => IgniteMock.Immutable.AddApplicationGridRouting<AddTTMDesignComputeFunc, AddTTMDesignArgument, AddTTMDesignResponse>();
+    private void AddApplicationRouting() => IgniteMock.Mutable.AddApplicationGridRouting<AddTTMDesignComputeFunc, AddTTMDesignArgument, AddTTMDesignResponse>();
 
     [Fact]
     public void Creation()
@@ -36,13 +40,20 @@ namespace VSS.TRex.Tests.Designs.GridFabric
       var designID = System.Guid.NewGuid();
 
       var request = new AddTTMDesignRequest();
-      await request.ExecuteAsync(new AddTTMDesignArgument
+      var response = await request.ExecuteAsync(new AddTTMDesignArgument
       {
         ProjectID = siteModel.ID,
         DesignDescriptor = new TRex.Designs.Models.DesignDescriptor(designID, "folder", "filename"),
         Extents = new TRex.Geometry.BoundingWorldExtent3D(0, 0, 1, 1),
         ExistenceMap = new VSS.TRex.SubGridTrees.SubGridTreeSubGridExistenceBitMask()
       });
+
+      response.Should().NotBeNull();
+      response.DesignUid.Should().Be(designID);
+      response.RequestResult.Should().Be(DesignProfilerRequestResult.OK);
+
+      // Re-request the sitemodel to reflect the change
+      siteModel = DIContext.Obtain<ISiteModels>().GetSiteModel(siteModel.ID, false);
 
       siteModel.SetStorageRepresentationToSupply(TRex.Storage.Models.StorageMutability.Mutable);
       siteModel.Designs.Count.Should().Be(1);
