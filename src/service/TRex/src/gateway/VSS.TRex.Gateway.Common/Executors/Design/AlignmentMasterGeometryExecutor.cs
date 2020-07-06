32e25494f00d90d620de1b02ddc87635b99fa564
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2020-07-06 13:26:50 +1200

    CCSSSCON-677: Required changes to fix the bug...

--- src/service/TRex/src/gateway/VSS.TRex.Gateway.Common/Executors/Design/AlignmentMasterGeometryExecutor.cs
+++ src/service/TRex/src/gateway/VSS.TRex.Gateway.Common/Executors/Design/AlignmentMasterGeometryExecutor.cs
@@ -118,7 +118,7 @@ namespace VSS.TRex.Gateway.Common.Executors.Design
         AlignmentDesignID = request.DesignUid
       });
 
-      if (geometryResponse != null && geometryResponse.RequestResult != DesignProfilerRequestResult.OK)
+      if (geometryResponse != null && geometryResponse.RequestResult == DesignProfilerRequestResult.OK)
       {
         // Convert all coordinates from grid to lat/lon
         ConvertNEEToLLHCoords(siteModel.CSIB(), geometryResponse);
