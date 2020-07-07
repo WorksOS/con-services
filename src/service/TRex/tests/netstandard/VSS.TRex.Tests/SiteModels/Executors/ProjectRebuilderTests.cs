71c11d28ec13dec816e60422eaeffa26da5f7b1a
rwilson <raymond_wilson@trimble.com>
2020-07-07 14:06:25 +1200

    CCSSSCON-702: Triage test failure

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/Executors/ProjectRebuilderTests.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/SiteModels/Executors/ProjectRebuilderTests.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Policy;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using Nito.AsyncEx.Synchronous;
 using VSS.AWS.TransferProxy;
 using VSS.TRex.Common.Extensions;
@@ -31,6 +31,8 @@ namespace VSS.TRex.Tests.SiteModels.Executors
 {
   public class SiteModelRebuilderTests : IClassFixture<DITAGFileAndSubGridRequestsWithIgniteFixture>, IDisposable
   {
+    private static readonly ILogger Log = VSS.TRex.Logging.Logger.CreateLogger<SiteModelRebuilderTests>();
+
     private static ISiteModelRebuilder CreateBuilder(Guid projectUid, bool archiveTagFiles, TransferProxyType transferProxyType)
     {
       return new SiteModelRebuilder(projectUid, archiveTagFiles, transferProxyType)
@@ -107,7 +109,11 @@ namespace VSS.TRex.Tests.SiteModels.Executors
     [InlineData(false)]
     public async void ExecuteAsync_SingleTAGFile(bool treatMachineAsJohnDoe)
     {
+      var testGuid = Guid.NewGuid();
+
       AddApplicationGridRouting();
+
+      Log.LogInformation($"Test Guid {testGuid} Stage 0");
 
       // Construct a site model from a single TAG file
       var tagFiles = new[] { Path.Combine(TestHelper.CommonTestDataPath, "TestTAGFile.tag") };
@@ -117,17 +123,27 @@ namespace VSS.TRex.Tests.SiteModels.Executors
 
       var uidForArchiveRepresentation = treatMachineAsJohnDoe ? Guid.Empty : siteModel.Machines[0].ID;
 
+      Log.LogInformation($"Test Guid {testGuid} Stage 1");
+
       var s3Proxy = DIContext.Obtain<Func<TransferProxyType, IS3FileTransfer>>()(TransferProxyType.TAGFiles);
       s3Proxy.WriteFile(tagFiles[0], $"{siteModel.ID}/{uidForArchiveRepresentation}/{Path.GetFileName(tagFiles[0])}");
 
+      Log.LogInformation($"Test Guid {testGuid} Stage 2");
+
       var rebuilder = CreateBuilder(siteModel.ID, false, TransferProxyType.TAGFiles);
+
+      Log.LogInformation($"Test Guid {testGuid} Stage 3");
 
       // Add the rebuilder to the manager in a 'hands-off' mode to allow notification routing to it.
       var manager = DIContext.Obtain<ISiteModelRebuilderManager>();
       manager.AddRebuilder(rebuilder).Should().BeTrue();
 
+      Log.LogInformation($"Test Guid {testGuid} Stage 4");
+
       // Start the rebuild executing
       var rebuilderTask = rebuilder.ExecuteAsync();
+
+      Log.LogInformation($"Test Guid {testGuid} Stage 5");
 
       // Wait until the rebuilder is in the monitoring state and then inject the contents of the tag file buffer queue cache into the handler
       while (rebuilder.Metadata.Phase != RebuildSiteModelPhase.Monitoring)
@@ -135,15 +151,23 @@ namespace VSS.TRex.Tests.SiteModels.Executors
         await Task.Delay(1000);
       }
 
+      Log.LogInformation($"Test Guid {testGuid} Stage 6");
+
       // While the rebuilder is waiting in the monitoring state, inject the contents of the TAGFileBuffer queue into the TAG file processor
       var mutableIgniteMock = DIContext.Obtain<Func<StorageMutability, IgniteMock>>()(StorageMutability.Mutable);
       var mockQueueCacheDictionary = mutableIgniteMock.MockedCacheDictionaries[TRexCaches.TAGFileBufferQueueCacheName()] as Dictionary<ITAGFileBufferQueueKey, TAGFileBufferQueueItem>;
 
+      Log.LogInformation($"Test Guid {testGuid} Stage 7");
+
       var handler = new TAGFileBufferQueueItemHandler();
       handler.Should().NotBeNull();
 
+      Log.LogInformation($"Test Guid {testGuid} Stage 8");
+        
       // Inject the keys for the handler to use to extract the TAG file content to be processed.
       mockQueueCacheDictionary.ForEach(kv => handler.Add(kv.Key));
+
+      Log.LogInformation($"Test Guid {testGuid} Stage 0");
 
       // Now wait for the rebuilder task to complete
       var result = await rebuilderTask;
@@ -163,8 +187,13 @@ namespace VSS.TRex.Tests.SiteModels.Executors
       siteModel.Machines.Count.Should().Be(1);
       siteModel.Machines[0].IsJohnDoeMachine.Should().Be(treatMachineAsJohnDoe);
 
+      Log.LogInformation($"Test Guid {testGuid} Stage 9");
+
       // Belt and braces - clean the mocked TAG file buffer queue
       mockQueueCacheDictionary.Clear();
+
+      Log.LogInformation($"Test Guid {testGuid} Stage 10");
+
     }
 
     public void Dispose()
