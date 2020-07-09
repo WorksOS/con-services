66a9d02f0fb22b50e3654ad0627b871b261ca450
Aaron Morgan <aaron_morgan@trimble.com>
2020-07-09 12:23:18 +1200

    CCSSSCON-756: Removed diagnostic tests from CoreX.Wrapper.

--- src/Common/CoreX.Wrapper/tests/UnitTests/Tests/CoreXWrapperTests.cs
+++ src/Common/CoreX.Wrapper/tests/UnitTests/Tests/CoreXWrapperTests.cs
@@ -24,15 +24,6 @@ namespace CoreX.Wrapper.UnitTests.Tests
       var csibStr = CoreX.GetCSIBFromDCFile(DCFile.GetFilePath(DCFile.UTM_32_NN1954_08));
 
       csibStr.Should().NotBeNullOrEmpty();
-    }    
-    
-    [Fact]
-    public void GetCSIBFromDCFile_should_return_expected_CSIB_string2()
-    {
-      var content = "MDBUTVNDIFYxMC03MCAgICAgICAwICAgMTEvMDEvMjAxMiAxMToyNTEzMzExMQ0KMTBUTVVudGl0bGVkIEpvYiAgICAxMjIyMTINCjc4VE0xMQ0KRDVUTSAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIA0KRDhUTSAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgDQo2NFRNMzM2LjIwNjU1NTM3MTAwMDAtMTE1LjAyNjI2NzgxODAwMC4wMDAwMDAwMDAwMDAwMDM2NzMuNzA4MDAwMDAwMDA3MTk4LjA4MTAwMDAwMDAwMC4wMDAwMDAwMDAwMDAwMDEuMDAwMDg2NzIzMDAwMDAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIA0KNjVUTTIwOTI1NjA0LjQ3NDE3MDAyOTguMjU3MjIyOTMyODkwDQo0OVRNMzIwOTI1NjA0LjQ3NDE2NzAyOTguMjU3MjIzNTYzMDAwMC4wMDAwMDAwMDAwMDAwMDAuMDAwMDAwMDAwMDAwMDAwLjAwMDAwMDAwMDAwMDAwMC4wMDAwMDAwMDAwMDAwMDAuMDAwMDAwMDAwMDAwMDAwLjAwMDAwMDAwMDAwMDAwMC4wMDAwMDAwMDAwMDAwMA0KNTBUTTM5MzMuMDU0MjcwMDAwMDA4MTc0LjE0MDEyMDAwMDAwMC4wMDg3MzAwMDAwMDAwMDAuMDAwNDUwMDAwMDAwMDAwLjAwMTkwNzk5NzAwMDAwMS4wMDAwMTMwMTMwMDAwMA0KQzhUTTRTQ1M5MDAgTG9jYWxpemF0aW9uICAgICAgICAgICAgIFNDUzkwMCBSZWNvcmQgICAgICAgICAgICAgICAgICAgV0dTODQgRXF1aXZhbGVudCBEYXR1bSAgICAgICAgICANCg==";
-      var csibStr = CoreX.GetCSIBFromDCFileContent(content);
-
-      csibStr.Should().NotBeNullOrEmpty();
     }
 
     [Fact]
@@ -42,17 +33,6 @@ namespace CoreX.Wrapper.UnitTests.Tests
         TestConsts.DIMENSIONS_2012_DC_COORDINATE_SYSTEM_ID, new LLH(), CoordinateTypes.LocalLLE, CoordinateTypes.NormalizedNEE));
 
       ex.Message.Should().Be("Failed to create GeodeticX transformer, error 'gecCSIB_INVALID_CSIB'");
-    }
-
-    [Theory]
-    [InlineData(TestConsts.ddd, true)]
-    [InlineData(TestConsts.DIMENSIONS_2012_DC_CSIB, true)]
-    [InlineData(TestConsts.DIMENSIONS_2012_DC_COORDINATE_SYSTEM_ID, false)]
-    public void Validate_CSIB_strings(string csibCandiate, bool expectedResult)
-    {
-      CoreX.ValidateCsibString(csibCandiate)
-           .Should()
-           .Be(expectedResult);
     }
   }
 }
