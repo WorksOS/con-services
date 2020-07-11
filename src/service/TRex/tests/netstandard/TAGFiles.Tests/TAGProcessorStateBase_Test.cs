f4ce098306706e65a4c97dc7dd176c0b6def7704
rwilson <raymond_wilson@trimble.com>
2020-07-11 18:02:23 +1200

    Add DILoggingFixture to TAGValueMatcherTests

---
+++ src/service/TRex/tests/netstandard/TAGFiles.Tests/TAGProcessorStateBase_Test.cs
@@ -0,0 +1,17 @@
+﻿using VSS.TRex.TAGFiles.Classes.States;
+using VSS.TRex.TAGFiles.Types;
+
+namespace TAGFiles.Tests
+{
+  public class TAGProcessorStateBase_Test : TAGProcessorStateBase
+  {
+    public EpochStateEvent TriggeredEpochStateEvent = EpochStateEvent.Unknown;
+
+    public override bool DoEpochStateEvent(EpochStateEvent eventType)
+    {
+      TriggeredEpochStateEvent = eventType;
+
+      return base.DoEpochStateEvent(eventType);
+    }
+  }
+}
