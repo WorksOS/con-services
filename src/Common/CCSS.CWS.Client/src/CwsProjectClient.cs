95f7a120e2dbd66921d392378c689a380dad1665
Jeannie May <Jeannie_May@trimble.com>
2020-07-15 09:18:25 +1200

    #CCSSSCON-460 remove change from PR

--- src/Common/CCSS.CWS.Client/src/CwsProjectClient.cs
+++ src/Common/CCSS.CWS.Client/src/CwsProjectClient.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using System.Runtime.InteropServices.ComTypes;
 using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using VSS.Common.Abstractions.Cache.Interfaces;
@@ -37,13 +37,13 @@ namespace CCSS.CWS.Client
     ///   Cache to include userUid as different users have access to a different project set
     ///   cws team to generate a detailed list in 1 shot CCSSSCON-409
     /// </summary>
-    public async Task<ProjectDetailListResponseModel> GetProjectsForCustomer(Guid customerUid, Guid? userUid = null, bool includeSettings = true, 
+    public async Task<ProjectDetailListResponseModel> GetProjectsForCustomer(Guid customerUid, Guid? userUid = null, bool includeSettings = true,
       CwsProjectType? type = null, ProjectStatus? status = null, bool onlyAdmin = false, IHeaderDictionary customHeaders = null)
     {
       log.LogDebug($"{nameof(GetProjectsForCustomer)}: customerUid {customerUid} userUid {userUid}");
 
       var projectSummaryListResponseModel = await GetProjectsForMyCustomer(customerUid, userUid, includeSettings, type, status, customHeaders);
-      var projects = onlyAdmin ? 
+      var projects = onlyAdmin ?
         projectSummaryListResponseModel.Projects.Where(p => p.UserProjectRole == UserProjectRoleEnum.Admin) :
         projectSummaryListResponseModel.Projects;
       var projectDetailListResponseModel = new ProjectDetailListResponseModel
@@ -79,7 +79,7 @@ namespace CCSS.CWS.Client
         // Do we need to filter for a specific type?
         if (type != null)
         {
-          queryParams.Add(new KeyValuePair<string, string>("projectType", ((int) type.Value).ToString()));
+          queryParams.Add(new KeyValuePair<string, string>("projectType", ((int)type.Value).ToString()));
         }
 
         if (status != null)
@@ -150,7 +150,7 @@ namespace CCSS.CWS.Client
 
         // If we don't add this query param, then get project will never return archived projects
         // However, there is no harm to having it always on for active projects
-        var queryParams = new List<KeyValuePair<string, string>>() {new KeyValuePair<string, string>("includeArchived", "true")};
+        var queryParams = new List<KeyValuePair<string, string>>() { new KeyValuePair<string, string>("includeArchived", "true") };
 
         projectDetailResponseModel = await GetData<ProjectDetailResponseModel>(route, projectUid, userUid, queryParams, customHeaders);
 
@@ -182,6 +182,7 @@ namespace CCSS.CWS.Client
 
       /* todo
        BadRequest {"status":400,"code":9040,"message":"Project name is already in account","moreInfo":"Please provide this id to support, while contacting, TraceId 5ece31b18aacd06f7888ceced1a82b2e","timestamp":1590571441686,"fieldErrors":[{"field":"projectName","attemptedValue":"wotevaPMan1"}]}
+
       Request returned non-ok code InternalServerError with response {"status":500,"code":9047,"message":"Add project failed as project settings save failed","moreInfo":"Please provide this id to support, while contacting, TraceId 5ece3287f81ddbc95b77a00eb8e1a940","timestamp":1590571656489,"fieldErrors":[{"field":"exception","attemptedValue":"org.springframework.web.client.HttpServerErrorException: 500 null"},{"field":"Authorization","attemptedValue":"Bearer 07e73c3eb55f52fd4d0cf92f1d8d0785"},{"field":"Endpoint","attemptedValue":"https://api-stg.trimble.com/t/trimble.com/cws-profilesettings-stg/1.0/projects/{projectId}"},{"field":"Cause","attemptedValue":"500 null"},{"field":"projectId","attemptedValue":"trn::profilex:us-west-2:project:9e598d24-2e82-4641-8976-7bb8639b47fc"}]}
        */
       log.LogDebug($"{nameof(CreateProject)}: createProjectResponseModel {JsonConvert.SerializeObject(createProjectResponseModel)}");
@@ -210,7 +211,7 @@ namespace CCSS.CWS.Client
       await UpdateData($"/projects/{projectTrn}/boundary", projectBoundary, null, customHeaders);
     }
 
-    private  ProjectDetailResponseModel GetProjectDetailsFromSummary(ProjectSummaryResponseModel project, Guid customerUid)
+    private ProjectDetailResponseModel GetProjectDetailsFromSummary(ProjectSummaryResponseModel project, Guid customerUid)
     {
       // No need to query the metadata endpoint anymore, as the summary result with includeSettings has all the details
       return new ProjectDetailResponseModel
@@ -221,7 +222,7 @@ namespace CCSS.CWS.Client
         ProjectType = project.ProjectType,
         Status = project.Status,
         UserProjectRole = project.UserProjectRole,
-        ProjectSettings = new ProjectSettingsModel {Boundary = project.Boundary, TimeZone = project.TimeZone}
+        ProjectSettings = new ProjectSettingsModel { Boundary = project.Boundary, TimeZone = project.TimeZone }
       };
     }
   }
