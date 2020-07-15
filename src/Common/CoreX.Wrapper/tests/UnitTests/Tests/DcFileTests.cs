8659e98746fe9d66db1f03ab48f967488fbe3bb6
Aaron Morgan <aaron_morgan@trimble.com>
2020-07-15 21:22:41 +1200

    CCSSSCON-857: Minor fix to create fewer CoreXCSIB blob containers when processing arrays of coordinates.

--- src/Common/CoreX.Wrapper/tests/UnitTests/Tests/DcFileTests.cs
+++ src/Common/CoreX.Wrapper/tests/UnitTests/Tests/DcFileTests.cs
@@ -13,7 +13,6 @@ namespace CoreX.Wrapper.UnitTests.Tests
     private readonly IConvertCoordinates _convertCoordinates;
 
     private const double LL_CM_TOLERANCE = 0.00000001;
-    private const double GRID_CM_TOLERANCE = 0.01;
 
     public DcFileTests(UnitTestBaseFixture testFixture)
     {
@@ -23,6 +22,7 @@ namespace CoreX.Wrapper.UnitTests.Tests
     public string GetCSIBFromDC(string dcFilename) =>
       _convertCoordinates.DCFileToCSIB(DCFile.GetFilePath(dcFilename));
 
+    [Category("Slow")]
     [Theory]
     [Description("Sanity tests validating only height varies when VERT_ADJUST is present.")]
     [InlineData(36.21730699569774, -115.0372771786517, 608.9999852774359, ReturnAs.Degrees, DCFile.DIMENSIONS_2012_DC_FILE_WITHOUT_VERT_ADJUST)]
@@ -40,15 +40,5 @@ namespace CoreX.Wrapper.UnitTests.Tests
       xyz.Y.Should().BeApproximately(lat, LL_CM_TOLERANCE);
       xyz.Z.Should().BeApproximately(height, LL_CM_TOLERANCE);
     }
-
-    //[Theory]
-    //[InlineData(DCFile.BOOTCAMP_2012_WITH_VERT_ADJUST)]
-    //[InlineData(DCFile.BOOTCAMP_2012_WITHOUT_VERT_ADJUST)]
-    //public void CoordinateService_ImportCSIBFromDC(string dcFilename)
-    //{
-    //  var csib = GetCSIBFromDC(DCFile.GetFilePath(dcFilename));
-
-    //  csib.Should().Be(_csib);
-    //}
   }
 }
