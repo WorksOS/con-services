286011e294ee10c2911f80828ee1636119c140e9
Jeannie May <Jeannie_May@trimble.com>
2020-07-21 22:33:53 +1200

    #CCSSSCON-460 more connectedSite references

--- src/service/3dNow/Service/VSS.Productivity3D.3DNow/Now3DAuthentication.cs
+++ src/service/3dNow/Service/VSS.Productivity3D.3DNow/Now3DAuthentication.cs
@@ -4,13 +4,15 @@ using VSS.Common.Abstractions.Clients.CWS.Interfaces;
 using VSS.Common.Abstractions.Configuration;
 using VSS.MasterData.Models.Handlers;
 using VSS.MasterData.Proxies.Interfaces;
+using VSS.Productivity3D.Entitlements.Abstractions.Interfaces;
 using VSS.WebApi.Common;
 
 namespace VSS.Productivity3D.Now3D
 {
   public class Now3DAuthentication : TIDAuthentication
   {
-    public Now3DAuthentication(RequestDelegate next, ICwsAccountClient accountProxy, IConfigurationStore store, ILoggerFactory logger, IServiceExceptionHandler serviceExceptionHandler) : base(next, accountProxy, store, logger, serviceExceptionHandler)
+    public Now3DAuthentication(RequestDelegate next, ICwsAccountClient accountProxy, IConfigurationStore store, ILoggerFactory logger, IEntitlementProxy entitlementProxy, IServiceExceptionHandler serviceExceptionHandler) 
+      : base(next, accountProxy, store, logger, entitlementProxy, serviceExceptionHandler)
     { }
 
     public override bool RequireCustomerUid(HttpContext context)
