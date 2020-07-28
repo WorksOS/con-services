0eaf1280bd247f22f284aa2b25fa474fedd10966
Aaron Morgan <aaron_morgan@trimble.com>
2020-07-29 09:51:32 +1200

    CCSSSCON-1086: Revert changes to Azure build pipeline script.

--- build/azure-build-pipeline.ps1
+++ build/azure-build-pipeline.ps1
@@ -18,9 +18,6 @@ PARAM (
     #[Parameter(Mandatory = $false)][string]$systemAccessToken, 
 )
 
-# Load component scripts.
-. ../scripts/aws-docker-login.ps1
-
 enum ReturnCode {
     SUCCESS
     INVALID_ACTION
@@ -223,6 +220,29 @@ function Push-Container-Image {
     Write-Host "`nImage push complete" -ForegroundColor Green
 
     Exit-With-Code ([ReturnCode]::SUCCESS)
+}
+
+function Login-Aws {
+    Write-Host "`nAuthenticating with AWS ECR..." -ForegroundColor Green
+    Write-Host "Determining AWS CLI version..."
+
+    aws --version
+
+    $awsVersion = (aws --version).Split(' ')[0].Split('/')[1].Split(' ')
+    $versionMajorMinor = [decimal]($awsVersion[0].SubString(0, $awsVersion.LastIndexOf('.')))
+    $canUseGetLoginPassword = $versionMajorMinor -ge 1.18
+
+    if ($canUseGetLoginPassword) {
+        # Azure pipelines use a recent version of AWS CLI that has replaced get-login with get-login-password.
+        aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 940327799086.dkr.ecr.us-west-2.amazonaws.com
+        if (-not $?) { Exit-With-Code ([ReturnCode]::AWS_ECR_LOGIN_FAILED) }
+    }
+    else {
+        # Retain backward compatibility for running locally on team development PCs with older AWS CLI installed.
+        Write-Host "Found older version of AWS CLI, failing back to 'get-login'`n"
+        Invoke-Expression -Command (aws ecr get-login --no-include-email --region us-west-2 --profile fsm-okta)
+        if (-not $?) { Exit-With-Code ([ReturnCode]::AWS_ECR_LOGIN_FAILED) }
+    }
 }
 
 # May be required when interacting with TGL or trmb-ccss Nuget servers.
