306704507aadabd2be1b4e9ddc753937435be7dc
Aaron Morgan <aaron_morgan@trimble.com>
2020-07-28 20:56:16 +1200

    CCSSSCON-1086: Initial pre-work; minor code tidying; improved error logging in CoreX.Wrapper when geodata file isn't found.

--- src/service/3DP/src/WebApi/Compaction/Controllers/LineworkController.cs
+++ src/service/3DP/src/WebApi/Compaction/Controllers/LineworkController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VSS.Common.Abstractions.Configuration;
-using VSS.Common.Exceptions;
-using VSS.MasterData.Models.ResultHandling.Abstractions;
-using VSS.MasterData.Proxies.Interfaces;
 using VSS.Productivity3D.Common.Interfaces;
 using VSS.Productivity3D.Project.Abstractions.Interfaces;
-using VSS.Productivity3D.WebApi.Compaction.ActionServices;
 using VSS.Productivity3D.WebApi.Models.Compaction.Executors;
 using VSS.Productivity3D.WebApi.Models.Compaction.Models;
 using VSS.Productivity3D.WebApi.Models.Compaction.ResultHandling;
@@ -37,72 +32,11 @@ namespace VSS.Productivity3D.WebApi.Compaction.Controllers
 
       var result = await RequestExecutorContainerFactory
                          .Build<LineworkFileExecutor>(LoggerFactory, configStore: ConfigStore, trexCompactionDataProxy: TRexCompactionDataProxy)
-                         .ProcessAsync(requestDto);
+                         .ProcessAsync(requestDto) as DxfLineworkFileResult;
 
       return result.Code == 0
-        ? StatusCode((int)HttpStatusCode.OK, ((DxfLineworkFileResult)result).ConvertToGeoJson(requestDto.ConvertLineStringCoordsToPolygon, requestDto.MaxVerticesPerBoundary))
+        ? StatusCode((int)HttpStatusCode.OK, result.ConvertToGeoJson(requestDto.ConvertLineStringCoordsToPolygon, requestDto.MaxVerticesPerBoundary))
         : StatusCode((int)HttpStatusCode.BadRequest, result);
-    }
-
-    /// <summary>
-    /// Gets a DXF line work representation of an alignment.
-    /// </summary>
-    /// <returns>A zipped file containing the line work and a result code and message</returns>
-    [HttpGet("api/v2/linework/alignment")]
-    [Obsolete("Use the 'api/v2/designs/alignment/master/geometry' endpoint to get geometry data for an alignment.")]
-    public async Task<FileResult> GetLineworkFromAlignment([FromQuery] Guid projectUid, [FromQuery] Guid alignmentUid, [FromServices] IPreferenceProxy prefProxy)
-    {
-      Log.LogDebug($"{nameof(GetLineworkFromAlignment)}: projectUid={projectUid} alignmentUid={alignmentUid}");
-
-#if RAPTOR
-      var projectId = GetLegacyProjectId(projectUid);
-      var designDescriptor = GetAndValidateDesignDescriptor(projectUid, alignmentUid, OperationType.GeneratingDxf);
-      var userPreferences = prefProxy.GetUserPreferences(GetUserId(), CustomHeaders);
-
-      await Task.WhenAll(projectId, designDescriptor, userPreferences);
-
-      var dxfUnitsType = DxfUnitsType.Meters;
-
-      switch (userPreferences.Result.Units.UnitsType())
-      {
-        case UnitsTypeEnum.Metric:
-          dxfUnitsType = DxfUnitsType.Meters;
-          break;
-        case UnitsTypeEnum.Imperial:
-          dxfUnitsType = DxfUnitsType.ImperialFeet;
-          break;
-        case UnitsTypeEnum.US:
-          dxfUnitsType = DxfUnitsType.UsSurveyFeet;
-          break;
-      }
-
-      var request = AlignmentLineworkRequest.Create(projectUid, projectId.Result, designDescriptor.Result.File, dxfUnitsType);
-      var result = await RequestExecutorContainerFactory
-        .Build<AlignmentLineworkExecutor>(LoggerFactory, RaptorClient, null, ConfigStore)
-        .ProcessAsync(request) as AlignmentLineworkResult;
-
-      var outputStream = new MemoryStream();
-      using (var zipArchive = new ZipArchive(outputStream, ZipArchiveMode.Create, true))
-      {
-        //Note: When all DXF tile generation stuff removed this can be changed as the project
-        //service creates its own generated name. It doesn't use the one from the zip file.
-        var suffix = FileUtils.GeneratedFileSuffix(ImportedFileType.Alignment);
-        string generatedName =
-          FileUtils.GeneratedFileName(designDescriptor.Result.File.FileName, suffix, FileUtils.DXF_FILE_EXTENSION);
-        var dxfZipEntry = zipArchive.CreateEntry(generatedName);
-        using (var stream = dxfZipEntry.Open())
-        {
-          result.DxfData?.CopyTo(stream);
-        }
-      }
-
-      // Don't forget to seek back, or else the content length will be 0
-      outputStream.Seek(0, SeekOrigin.Begin);
-      return new FileStreamResult(outputStream, ContentTypeConstants.ApplicationZip);
-#else
-      throw new ServiceException(HttpStatusCode.BadRequest,
-        new ContractExecutionResult(ContractExecutionStatesEnum.ValidationError, "TRex unsupported request"));
-#endif
     }
   }
 }
