b68bb71ba7897242d0092ad7d97121474d2d95da
rwilson <raymond_wilson@trimble.com>
2020-07-31 17:15:38 +1200

    CCSSSCON-1067: Working unit test for issue triage

---
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/Jira/Jira1067Tests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using VSS.TRex.Common;
+using VSS.TRex.Designs.GridFabric.Arguments;
+using VSS.TRex.Designs.GridFabric.ComputeFuncs;
+using VSS.TRex.Designs.GridFabric.Responses;
+using VSS.TRex.Designs.Models;
+using VSS.TRex.Filters;
+using VSS.TRex.Tests.TestFixtures;
+using VSS.TRex.Volumes.GridFabric.Arguments;
+using VSS.TRex.Volumes.GridFabric.ComputeFuncs;
+using VSS.TRex.Volumes.GridFabric.Requests;
+using VSS.TRex.Volumes.GridFabric.Responses;
+using Xunit;
+
+namespace VSS.TRex.Tests.Jira
+{
+  public class Jira1067Tests : IClassFixture<DITAGFileAndSubGridRequestsWithIgniteFixture>
+  {
+    private void AddApplicationGridRouting() => IgniteMock.Immutable.AddApplicationGridRouting<SimpleVolumesRequestComputeFunc_ApplicationService, SimpleVolumesRequestArgument, SimpleVolumesResponse>();
+
+    private void AddClusterComputeGridRouting()
+    {
+      IgniteMock.Immutable.AddClusterComputeGridRouting<SimpleVolumesRequestComputeFunc_ClusterCompute, SimpleVolumesRequestArgument, SimpleVolumesResponse>();
+    }
+
+    private void AddDesignProfilerGridRouting()
+    {
+      IgniteMock.Immutable.AddApplicationGridRouting
+        <CalculateDesignElevationPatchComputeFunc, CalculateDesignElevationPatchArgument, CalculateDesignElevationPatchResponse>();
+    }
+
+    private SimpleVolumesRequestArgument SimpleDefaultRequestArg(Guid ProjectUid)
+    {
+      return new SimpleVolumesRequestArgument
+      {
+        ProjectID = ProjectUid,
+        VolumeType = VolumeComputationType.Between2Filters,
+        BaseFilter = new CombinedFilter
+        {
+          AttributeFilter =
+          {
+            ReturnEarliestFilteredCellPass = true,
+          }
+        },
+        TopFilter = new CombinedFilter(),
+        BaseDesign = new DesignOffset(),
+        TopDesign = new DesignOffset(),
+        CutTolerance = 0.001,
+        FillTolerance = 0.001
+      };
+    }
+
+    private void CheckDefaultFilterToFilterSingleTAGFileResponse(SimpleVolumesResponse response)
+    {
+      //Was, response = {Cut:1.00113831634521, Fill:2.48526947021484, Cut Area:117.5652, FillArea: 202.9936, Total Area:353.0424, BoundingGrid:MinX: 537669.2, MaxX:537676.34, MinY:5427391.44, MaxY:5427514.52, MinZ: 1E+308, MaxZ:1E+308, BoundingLLH:MinX: 1E+308, MaxX:1E+308, MinY:1...
+      const double EPSILON = 0.000001;
+      response.Should().NotBeNull();
+      response.Cut.Should().BeApproximately(0.99982155303955178, EPSILON);
+      response.Fill.Should().BeApproximately(2.4776475891113323, EPSILON);
+      response.CutArea.Should().BeApproximately(113.86600000000001, EPSILON);
+      response.FillArea.Should().BeApproximately(200.56600000000006, EPSILON);
+      response.TotalCoverageArea.Should().BeApproximately(353.0424, EPSILON);
+
+      response.BoundingExtentGrid.MinX.Should().BeApproximately(537669.2, EPSILON);
+      response.BoundingExtentGrid.MinY.Should().BeApproximately(5427391.44, EPSILON);
+      response.BoundingExtentGrid.MaxX.Should().BeApproximately(537676.34, EPSILON);
+      response.BoundingExtentGrid.MaxY.Should().BeApproximately(5427514.52, EPSILON);
+      response.BoundingExtentGrid.MinZ.Should().Be(Consts.NullDouble);
+      response.BoundingExtentGrid.MaxZ.Should().Be(Consts.NullDouble);
+    }
+
+    [Trait("Dev Only", "")]
+    //[Fact(Skip ="Dev Only")]
+    [Fact]
+    public async Task FilterToFilter()
+    {
+      AddApplicationGridRouting();
+      AddClusterComputeGridRouting();
+
+      var tagFiles = Directory.GetDirectories(@"C:\Temp\Tonsasenfiles").Where(x => x.Contains("2005")).SelectMany(Directory.GetFiles).Take(500).ToArray();
+      var siteModel = DITAGFileAndSubGridRequestsFixture.BuildModel(tagFiles, out _);
+
+      var request = new SimpleVolumesRequest_ApplicationService();
+      var response = await request.ExecuteAsync(SimpleDefaultRequestArg(siteModel.ID));
+
+      CheckDefaultFilterToFilterSingleTAGFileResponse(response);
+    }
+  }
+}
