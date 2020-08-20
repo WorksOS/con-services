6107f7d0e71946cf14149097443e5adcf3cba749
rwilson <raymond_wilson@trimble.com>
2020-08-21 11:21:08 +1200

    Add exception trappers to two compute func Invoke() methods that use the 'argument-serialised-in-compute-func' model.

--- src/service/TRex/src/netstandard/VSS.TRex.CellDatum/GridFabric/ComputeFuncs/CellDatumRequestComputeFunc_ClusterCompute.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.CellDatum/GridFabric/ComputeFuncs/CellDatumRequestComputeFunc_ClusterCompute.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Reflection;
 using Apache.Ignite.Core.Binary;
 using Apache.Ignite.Core.Compute;
 using Microsoft.Extensions.Logging;
 using Nito.AsyncEx.Synchronous;
+using VSS.Productivity3D.Models.ResultHandling;
 using VSS.TRex.CellDatum.Executors;
 using VSS.TRex.CellDatum.GridFabric.Arguments;
 using VSS.TRex.CellDatum.GridFabric.Responses;
@@ -22,32 +22,37 @@ namespace VSS.TRex.CellDatum.GridFabric.ComputeFuncs
   {
     private const byte VERSION_NUMBER = 1;
 
-    private static readonly ILogger Log = Logging.Logger.CreateLogger<CellDatumRequestComputeFunc_ClusterCompute>();
+    private static readonly ILogger _log = Logging.Logger.CreateLogger<CellDatumRequestComputeFunc_ClusterCompute>();
 
     public CellDatumRequestArgument_ClusterCompute Argument { get; set; }
 
     public CellDatumResponse_ClusterCompute Invoke()
     {
-      Log.LogInformation("In CellDatumRequestComputeFunc_ClusterCompute.Invoke()");
+      _log.LogInformation("In CellDatumRequestComputeFunc_ClusterCompute.Invoke()");
 
       try
       {
         var request = new CellDatumComputeFuncExecutor_ClusterCompute();
 
-        Log.LogInformation("Executing CellDatumRequestComputeFunc_ClusterCompute.ExecuteAsync()");
+        _log.LogInformation("Executing CellDatumRequestComputeFunc_ClusterCompute.ExecuteAsync()");
 
         if (Argument == null)
           throw new ArgumentException("Argument for ComputeFunc must be provided");
 
         return request.ExecuteAsync
         (
-          Argument, 
+          Argument,
           new SubGridSpatialAffinityKey(SubGridSpatialAffinityKey.DEFAULT_SPATIAL_AFFINITY_VERSION_NUMBER_TICKS, Argument.ProjectID, Argument.OTGCellX, Argument.OTGCellY)
         ).WaitAndUnwrapException();
       }
+      catch (Exception e)
+      {
+        _log.LogError(e, "Exception computing cell datum response on cluster");
+        return new CellDatumResponse_ClusterCompute { ReturnCode = CellDatumReturnCode.UnexpectedError, TimeStampUTC = DateTime.MinValue, Value = 0.0};
+      }
       finally
       {
-        Log.LogInformation("Exiting CellDatumRequestComputeFunc_ClusterCompute.Invoke()");
+        _log.LogInformation("Exiting CellDatumRequestComputeFunc_ClusterCompute.Invoke()");
       }
     }
 
