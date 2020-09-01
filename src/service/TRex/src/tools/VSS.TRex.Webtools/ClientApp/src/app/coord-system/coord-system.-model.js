a08f6eda129dc5627809fdaccbb28832535ac8c5
Anatoli Kozoub <Anatoli_Kozoub@trimble.com>
2020-09-02 11:51:36 +1200

    CCSSSCON1454: Required implementation. Phase I.

---
+++ src/service/TRex/src/tools/VSS.TRex.Webtools/ClientApp/src/app/coord-system/coord-system.-model.js
@@ -0,0 +1,8 @@
+export class CoordinateSystemFile {
+    constructor(projectUid, csFileContent, csFileName) {
+        this.ProjectUid = projectUid;
+        this.CSFileContent = csFileContent;
+        this.CSFileName = csFileName;
+    }
+}
+//# sourceMappingURL=coord-system.-model.js.map
