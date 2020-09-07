150ecb3b44505f0c26e11581074c5eb168fd5d95
Alan Rose <alan_rose@trimble.com>
2020-09-07 16:09:08 +1200

    1192 Tidy up

--- src/service/TRex/src/netstandard/VSS.TRex.TAGFiles/Executors/ACSTranslator.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.TAGFiles/Executors/ACSTranslator.cs
@@ -26,10 +26,7 @@ namespace VSS.TRex.TAGFiles.Executors
     {
       if (projectUid == null) return string.Empty;
       var siteModel = DIContext.Obtain<ISiteModels>().GetSiteModel((Guid)projectUid, false);
-      if (siteModel == null)
-        return string.Empty;
-      else
-        return siteModel.CSIB() ?? string.Empty;
+      return siteModel?.CSIB() ?? string.Empty;
     }
 
     public List<UTMCoordPointPair> TranslatePositions(Guid? targetProjectUid, List<UTMCoordPointPair> coordPositions)
