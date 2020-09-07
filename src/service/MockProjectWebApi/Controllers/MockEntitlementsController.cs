6fe2472d882151fca6048863e5ff3bc6990a3669
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2020-09-08 10:00:59 +1200

    CCSSSCON-502 Add mock end point for entitlement service

---
+++ src/service/MockProjectWebApi/Controllers/MockEntitlementsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VSS.Productivity3D.Entitlements.Abstractions.Models.Request;
+using VSS.Productivity3D.Entitlements.Abstractions.Models.Response;
+
+namespace MockProjectWebApi.Controllers
+{
+  public class MockEntitlementsController : Controller
+  {
+    protected readonly ILogger Logger;
+    protected readonly ILoggerFactory LoggerFactory;
+
+
+    public MockEntitlementsController(ILoggerFactory loggerFactory)
+    {
+      LoggerFactory = loggerFactory;
+      Logger = loggerFactory.CreateLogger(GetType());
+    }
+
+    [HttpPost("api/v1/entitlement")]
+    public IActionResult GetMockEntitlement([FromBody] EntitlementRequestModel request)
+    {
+      Logger.LogInformation($"{nameof(GetMockEntitlement)}: UserUid={request.UserUid}");
+
+      var response = new EntitlementResponseModel
+      {
+        Feature = request.Feature,
+        Sku = request.Sku,
+        IsEntitled = true,
+        OrganizationIdentifier = request.OrganizationIdentifier,
+        UserUid = request.UserUid,
+        UserEmail = request.UserEmail
+      };
+      return Json(response);
+    }
+  }
+}
