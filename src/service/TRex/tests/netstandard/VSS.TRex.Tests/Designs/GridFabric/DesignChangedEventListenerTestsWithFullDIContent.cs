649687c8c95898ae148d22de7709ef039b30ce6e
Alan Rose <alan_rose@trimble.com>
2020-09-09 11:04:22 +1200

    1464 Check local file removed in unit test

--- src/service/TRex/tests/netstandard/VSS.TRex.Tests/Designs/GridFabric/DesignChangedEventListenerTestsWithFullDIContent.cs
+++ src/service/TRex/tests/netstandard/VSS.TRex.Tests/Designs/GridFabric/DesignChangedEventListenerTestsWithFullDIContent.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using FluentAssertions;
+using VSS.TRex.Common.Utilities;
 using VSS.TRex.Designs.GridFabric.Arguments;
 using VSS.TRex.Designs.GridFabric.ComputeFuncs;
 using VSS.TRex.Designs.GridFabric.Events;
@@ -48,15 +50,18 @@ namespace VSS.TRex.Tests.Designs.GridFabric
     [Fact]
     public void TestRemoveDesignFromDesignCache()
     {
-
       AddApplicationRouting();
-
+ 
       var siteModel = DITAGFileAndSubGridRequestsWithIgniteFixture.NewEmptyModel();
       var designUid = DITAGFileAndSubGridRequestsWithIgniteFixture.AddDesignToSiteModel(ref siteModel, TestHelper.CommonTestDataPath, TESTFILENAME, true);
+      var localStorage = Path.Combine(FilePathHelper.GetTempFolderForProject(siteModel.ID), TESTFILENAME);
 
       var designFiles = DIContext.ObtainOptional<IDesignFiles>();
       designFiles.NumDesignsInCache().Should().Be(0);
       designFiles.Lock(designUid, siteModel, SubGridTreeConsts.DefaultCellSize, out var loadResult);
+
+      File.Exists(localStorage).Should().BeTrue();
+
       designFiles.NumDesignsInCache().Should().Be(1);
       var design1 = loadResult.Should().Be(DesignLoadResult.Success);
       design1.Should().NotBeNull();
@@ -71,6 +76,7 @@ namespace VSS.TRex.Tests.Designs.GridFabric
 
       var listener = new DesignChangedEventListener(TRexGrids.ImmutableGridName());
       listener.Invoke(Guid.Empty, message).Should().BeTrue();
+      File.Exists(localStorage).Should().BeFalse();
 
       designFiles.NumDesignsInCache().Should().Be(0); 
 
