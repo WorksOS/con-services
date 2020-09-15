7bb67807e9d0a2e2f7fa89bba2660ebad8251cbe
Elspeth Jeet <elspeth.jeet@trimble.co.nz>
2020-09-15 12:34:55 +1200

    CCSSSCON-1620 Rework entitlements service for UI

---
+++ src/service/Entitlements/Clients/VSS.Productivity3D.Entitlements.Abstractions/Models/Request/ExternalEntitlementRequestModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace VSS.Productivity3D.Entitlements.Abstractions.Models.Request
+{
+  /// <summary>
+  /// Model for external entitlement requests i.e. from WorksOS UI
+  /// </summary>
+  public class ExternalEntitlementRequestModel
+  {
+    /// <summary>
+    /// An identifier to represent the Organization
+    /// Example is a CustomerUID in WorksOS
+    /// </summary>
+    [JsonProperty(Required = Required.Always)]
+    [Required]
+    public string OrganizationIdentifier { get; set; }
+
+    /// <summary>
+    /// The application name used to map the feature requested
+    /// </summary>
+    [JsonProperty(PropertyName="feature", Required = Required.Always)]
+    [Required]
+    public string ApplicationName { get; set; }
+
+    /// <summary>
+    /// The email address of the user. 
+    /// </summary>
+    [JsonProperty(Required = Required.Default)]
+    public string UserEmail { get; set; }
+
+  }
+}
