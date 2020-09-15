19ad717e373aea510477cf4b76e6085424d8a366
rwilson <raymond_wilson@trimble.com>
2020-09-15 16:39:46 +1200

    CCSSSCON-1603: Tidy SegmentElevationRangeCalculator

--- src/service/TRex/src/netstandard/VSS.TRex.SubGridTrees.Server/Utilities/SegmentElevationRangeCalculator.cs
+++ src/service/TRex/src/netstandard/VSS.TRex.SubGridTrees.Server/Utilities/SegmentElevationRangeCalculator.cs
@@ -8,35 +8,32 @@ namespace VSS.TRex.SubGridTrees.Server.Utilities
         /// <summary>
         /// Calculates the elevation range that all the passes within the given segment span.
         /// </summary>
-        /// <param name="segment"></param>
-        /// <param name="minElevation"></param>
-        /// <param name="maxElevation"></param>
         /// <returns>True if the result of the calculation yields a valid elevation range</returns>
-        public static bool CalculateElevationRangeOfPasses(ISubGridCellSegmentPassesDataWrapper segment, 
+        public static bool CalculateElevationRangeOfPasses(ISubGridCellSegmentPassesDataWrapper segment,
             out double minElevation, out double maxElevation)
         {
             minElevation = 1E10;
             maxElevation = -1E10;
 
-            double min = minElevation;
-            double max = maxElevation;
+            var min = minElevation;
+            var max = maxElevation;
 
-            for (int i = 0; i < SubGridTreeConsts.SubGridTreeDimension; i++)
+            for (var i = 0; i < SubGridTreeConsts.SubGridTreeDimension; i++)
             {
-              for (int j = 0; j < SubGridTreeConsts.SubGridTreeDimension; j++)
+              for (var j = 0; j < SubGridTreeConsts.SubGridTreeDimension; j++)
               {
-                int _PassCount = segment.PassCount(i, j);
+                var passCount = segment.PassCount(i, j);
 
-                for (int PassIndex = 0; PassIndex < _PassCount; PassIndex++)
+                for (var passIndex = 0; passIndex < passCount; passIndex++)
                 {
                   // Todo: Delegate this down to the segment to avoid the PassHeight() abstraction
-                  float _height = segment.PassHeight(i, j, PassIndex);
+                  var height = segment.PassHeight(i, j, passIndex);
 
-                  if (_height > max)
-                    max = _height;
+                  if (height > max)
+                    max = height;
 
-                  if (_height < min)
-                    min = _height;
+                  if (height < min)
+                    min = height;
                 }
               }
             }
