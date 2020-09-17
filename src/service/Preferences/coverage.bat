14794b24ad1314d8454e6bea9aa45f248f4266e8
Aaron Morgan <aaron_morgan@trimble.com>
2020-09-17 21:16:30 +1200

    CCSSSCON-1679: Migrate Preferences service from Jenkins to Azure.

--- src/service/Preferences/coverage.bat
+++
@@ -1,6 +0,0 @@
-echo "Calculating coverage with OpenCover"
-dotnet restore test\UnitTests\CCSS.Productivity3D.Preferences.Tests.csproj
-.\tools\opencover.console.exe -target:"dotnet.exe"  -targetargs:"test test\UnitTests\CCSS.Productivity3D.Preferences.Tests.csproj" -mergeoutput -hideskipped:File -output:coverage.xml -register:user -oldstyle -filter:"+[*]CCSS.Productivity3D.Preferences.*"
-echo "Generating HTML report"
-.\tools\OpenCoverToCoberturaConverter.exe -input:coverage.xml -output:outputCobertura.xml -sources:%WORKSPACE%
-.\tools\Report\ReportGenerator.exe -reports:coverage.xml -targetdir:.\CoverageReport
