2fb238294951ac02eb7362de04a788778b42c470
Aaron Morgan <aaron_morgan@trimble.com>
2020-09-22 15:56:15 +1200

    CCSSSCON-1506: Resolve service identifiers using reflection.

---
+++ src/service/Healthz/test/UnitTests/ServiceResolverTests.cs
@@ -0,0 +1,25 @@
+﻿using CCSS.WorksOS.Healthz.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace CCSS.WorksOS.Healthz.UnitTests
+{
+  public class ServiceResolverTests
+  {
+    [Fact]
+    public void Known_identifiers_Should_be_populated() =>
+      ServiceResolver.GetKnownServiceIdentifiers().Should().NotBeNullOrEmpty();
+
+    [Fact]
+    public void Known_identifiers_Should_not_contain_duplicates() =>
+      ServiceResolver.GetKnownServiceIdentifiers().Should().OnlyHaveUniqueItems();
+
+    [Fact]
+    public void Known_identifiers_Should_not_contain_ignored_services()
+    {
+      var identifiers = ServiceResolver.GetKnownServiceIdentifiers();
+
+      identifiers.Should().NotContain(new[] { "assetmgmt3d-service" });
+    }
+  }
+}
