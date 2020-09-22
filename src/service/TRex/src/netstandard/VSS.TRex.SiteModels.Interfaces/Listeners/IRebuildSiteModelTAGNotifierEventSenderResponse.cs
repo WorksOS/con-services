409d03acd9ba1f6357d149f3ab09a80f92996111
rwilson <raymond_wilson@trimble.com>
2020-09-22 16:44:13 +1200

    CCSSSCON-1663: Use compute invoke pattern for site model rebuilder TAG file processed notifications

---
+++ src/service/TRex/src/netstandard/VSS.TRex.SiteModels.Interfaces/Listeners/IRebuildSiteModelTAGNotifierEventSenderResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VSS.TRex.SiteModels.Interfaces.Listeners
+{
+  public interface IRebuildSiteModelTAGNotifierEventSenderResponse
+  {
+    public bool Success { get; set; }
+    public Guid NodeUid { get; set; }
+  }
+}
